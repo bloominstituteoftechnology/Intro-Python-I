384f24c8058273625a7f933d005765d9db170e3a
maxwashington <mnwashington18@gmail.com>
2018-07-08 17:19:31 -0500

    Installed pipenv Finished bignum, comp, datatypes, func, hello, lists, and printf

--- src/comp.py
+++ src/comp.py
@@ -4,7 +4,13 @@
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [x % 2]
+
+# for i in y:
+#     if y % 2 == 0
+#         print(y)
+#     else:
+#         print("You fail at life!")
 
 print(y)
 
