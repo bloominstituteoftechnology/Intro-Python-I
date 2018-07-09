c7ad67672626205739ebde4b96b529a7699d78e9
AmyShackles <amyshackles@gmail.com>
2018-07-09 12:07:09 -0700

    Started working on the project
    - Finished hello.py
    - Finished bignum.py
    - Finished datatypes.py
    - Working on printf.py

--- src/printf.py
+++ src/printf.py
@@ -2,9 +2,10 @@ x = 10
 y = 2.24552
 z = "I like turtles!"
 
-# Using the printf operator (%), print the following feeding in the values of x,
-# y, and z:
+# Using the printf operator (%), print the following feeding in the values of
+# x, y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print('x is %(x)i, y is %(y)i, z is %(z)s' %
+      {"x": 10, "y": 2.24552, "z": "I like turtles!"})
 
 # Use the 'format' string method to print the same thing
