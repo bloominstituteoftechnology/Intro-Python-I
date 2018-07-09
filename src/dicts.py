0c9dcb40d874564b25ee1984005d766eb8c22848
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-07-09 16:15:36 -0500

    completed obj, dicts, func

--- src/dicts.py
+++ src/dicts.py
@@ -9,4 +9,10 @@
 waypoints = [
 ]
 
+waypoints.append({"lat": 234, "lon": 123, "name": "point 1" })
+waypoints.append({"lat": 657, "lon": 975, "name": "point 2" })
+waypoints.append({"lat": 111, "lon": 165, "name": "point 3" })
+
 # Write a loop that prints out all the field values for all the waypoints
+for entry in waypoints:
+    print(entry)
