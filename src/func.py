271b297f7f6bc8679f26c1193f63303a9dcdd720
Danny Vail <danny_vail@outlook.com>
2018-07-09 17:23:42 -0400

    Completed func.py - Practice with functions and conditionals

--- src/func.py
+++ src/func.py
@@ -1,6 +1,12 @@
 # Write a function is_even that will return true if the passed in number is even.
+def is_even(num):
+    return int(num) % 2 == 0
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
+num = input("Enter an integer: ")
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
+if is_even(num):
+    print('Even!')
+else:
+    print('Odd')
