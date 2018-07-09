3794040378d9ec784834eaf7c55cfa6bb6dacdfe
samarv <samarvir1996@gmail.com>
2018-07-10 03:47:04 +0530

    obj left

---
+++ src/.ipynb_checkpoints/obj-checkpoint.py
@@ -0,0 +1,21 @@
+# Make a class LatLon that can be passed parameters `lat` and `lon` to the
+# constructor
+        
+# Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
+# constructor. It should inherit from LatLon.
+
+# Make a class Geocache that can be passed parameters `name`, `difficulty`,
+# `size`, `lat`, and `lon` to the constructor. What should it inherit from?
+
+# Make a new waypoint "Catacombs", 41.70505, -121.51521
+
+# Print it
+#
+# Without changing the following line, how can you make it print into something
+# more human-readable?
+print(w)
+
+# Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
+
+# Print it--also make this print more nicely
+print(g)
