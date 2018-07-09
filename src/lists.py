67dc1631b8b595a5b8add9e0cd2799cc5941fd9f
Obo Agboghidi <oagboghi2@gmail.com>
2018-07-09 16:20:33 -0700

    finished lists

--- src/lists.py
+++ src/lists.py
@@ -8,18 +8,24 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # [command here]
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # [command here]
+x.append(y[0])
+x.append(y[1])
+x.append(y[2])
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # [command here]
+del x[4]
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # [command here]
+x.insert(5,99)
 print(x)
 
 # Print the length of list x
