db0831e1de97401f0c3c9d078412a8c2ef6f88ff
Matthias Bakken <matthias.bakken@gmail.com>
2018-07-09 13:36:31 -0700

    completed comp and now working on Obj then adv

--- src/func.py
+++ src/func.py
@@ -5,7 +5,7 @@ num = input("Enter a number: ")
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 def is_even(num):
-  if num % 2 == 0:
+  if int(num) % 2 == 0:
     print('Even!')
   else:
     print('Odd')
