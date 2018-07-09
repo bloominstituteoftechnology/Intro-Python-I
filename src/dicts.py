5b3d3d3a3e80bf5f5d65ea43b187a16c569a075e
Matthias Bakken <matthias.bakken@gmail.com>
2018-07-09 10:32:34 -0700

    day one project almost complete

--- src/dicts.py
+++ src/dicts.py
@@ -7,6 +7,23 @@
 # Make up three entries of various values.
 
 waypoints = [
+  {
+    "lat": 36.395912,
+    "lon": -91.888281,
+    "name": "Somewhere in USA"
+  },
+  {
+    "lat": 36.385912,
+    "lon": -91.889281,
+    "name": "Somewhere else in USA"
+  },
+  {
+    "lat": 36.395812,
+    "lon": -91.898281,
+    "name": "Somewhere else again in USA"
+  }
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for x in waypoints:
+  print(x)
