015a9454370c4cd6f517ea2a680e5ed8bc78b288
Zach Campbell <beazlebubbles@gmail.com>
2018-07-09 15:55:38 -0600

    Fixed func.py

--- src/func.py
+++ src/func.py
@@ -1,11 +1,16 @@
 # Write a function is_even that will return true if the passed in number is
 # even.
 
+
+def is_even(x):
+    return x % 2 == 0
+
+
 # Read a number from the keyboard
 num = int(input("Enter a number: "))
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-if num % 2 == 0:
+if is_even(num):
     print("Even!")
 else:
     print("Odd")
