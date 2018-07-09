45c457e52f27f42912ddaf77a315d189dd3d63d9
maxwashington <mnwashington18@gmail.com>
2018-07-09 15:07:56 -0500

    Finished Cal and Dists

--- src/dicts.py
+++ src/dicts.py
@@ -7,6 +7,23 @@
 # Make up three entries of various values.
 
 waypoints = [
+    {
+        "lat": 18,
+        "lon": -234,
+        "name": "Somewhere"
+    },
+    {
+        "lat": 32,
+        "lon": 324,
+        "name": "End of the World"
+    },
+    {
+        "lat": 435,
+        "lon": -45,
+        "name": "Happy Place"
+    }
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for way in waypoints:
+    print(way['name'], way['lat'], way['lon'])
