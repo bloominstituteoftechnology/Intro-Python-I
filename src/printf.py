6251655ed8fdb4fd4a931c8628a874ca8642f439
kammagik <kammagik@yahoo.com>
2018-07-09 12:49:35 -0700

    printf

--- src/printf.py
+++ src/printf.py
@@ -5,6 +5,7 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print("%s %s %s" % (10, 2.24552, "I like turtles!"))
 
 # Use the 'format' string method to print the same thing
+print("{} {} {}".format(10, 2.24552, "I like turtles!"))
