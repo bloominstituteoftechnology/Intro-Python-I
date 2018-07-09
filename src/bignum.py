6e8d95b1249dd918c40810979cbb80ea53530d59
bbenefield89 <bsquared18@gmail.com>
2018-07-09 18:23:42 -0400

    Day 1: completed

--- src/bignum.py
+++ src/bignum.py
@@ -1,1 +1,6 @@
 # Print out 2 to the 65536 power
+from math import pow
+
+big_num = pow(2, 65536)
+
+print(big_num)
--- src/lists.py
+++ src/lists.py
@@ -8,18 +8,24 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # [command here]
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # [command here]
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # [command here]
+x = [ i for i in x if i != 8 ]
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # [command here]
+x_len = len(x)
+insert_index = x_len - 1
+x.insert(insert_index, 99)
 print(x)
 
 # Print the length of list x
