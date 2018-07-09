092dc2d72bd74670888cc05e0b72a231aed826ad
Moises Dobarganes <designerexpert@gmail.com>
2018-07-09 18:46:40 -0400

    Started playing around with Py

--- src/dicts.py
+++ src/dicts.py
@@ -7,6 +7,20 @@
 # Make up three entries of various values.
 
 waypoints = [
+    {
+        'lat': 100,
+        'lon': 200,
+        'name': 'Teh Frawg!'
+    },
+    {
+        'lat': 200,
+        'lon': 100,
+        'name': 'Teh Bawse!'
+    }
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for i in waypoints:
+    print('-------------------')
+    for j in i:
+        print(j, ':', i[j])
--- src/printf.py
+++ src/printf.py
@@ -5,6 +5,7 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print('x is %(x)i, y is %(y).2f, z is %(z)s' % {'x':x, 'y':y, 'z':z})
 
 # Use the 'format' string method to print the same thing
+print('x is {x}, y is {y:.2f}, z is {z}'.format(x=x, y=y,z=z))
