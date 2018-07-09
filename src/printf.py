07275eec0285397a0b2692d308f0aec7b20ae93e
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-07-09 18:41:20 -0400

    printf with .format method

--- src/printf.py
+++ src/printf.py
@@ -6,10 +6,10 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-print('x is %2d, y is %2.2f, z is "%s"' % (x, y, z))
+print('x is %2d, y is %2.5f, z is "%s"' % (x, y, z))
 
 # Use the 'format' string method to print the same thing
 
-formatString = 'x is %2d, y is %2.2f, z is "%s"' % (x, y, z)
+formatString = 'x is {0}, y is {1}, z is "{2}" '.format(x,y,z)
 
 print(formatString)
