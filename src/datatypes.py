7a5e1b14467b3f0756d1f3668a154449f124db4d
kammagik <kammagik@yahoo.com>
2018-07-09 16:26:43 -0700

    fixed datatypes

--- src/datatypes.py
+++ src/datatypes.py
@@ -2,7 +2,8 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
-print(5 + 7)
+print('x is %d, y is %.2f, z is "%s"' % (x, y, z))
 
 # Write a print statement that combines x + y into the string value 57
-print("5" + "7")
+
+print('x is {}, y is {:.2f}, z is "{}"'.format(x, y, z))
