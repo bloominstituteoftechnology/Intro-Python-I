10761c9a4e4dbea934c1aff3271727ce8a5d5b06
Jacob Henry <jacobiswinning@gmail.com>
2018-07-09 16:41:36 -0600

    working on cal.py

--- src/dicts.py
+++ src/dicts.py
@@ -7,6 +7,23 @@
 # Make up three entries of various values.
 
 waypoints = [
+  {
+    "lat": 2108.21,
+    "lon": 321.00,
+    "name": "WP 1"
+  },
+  {
+    "lat": 2103.01,
+    "lon": 321.00,
+    "name": "WP 2"
+  },
+  {
+    "lat": 2000.21,
+    "lon": 300.00,
+    "name": "WP 3"
+  }
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for i in waypoints:
+  print(i['lat'], i['lon'], i['name'])
