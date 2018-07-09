a249beb2ec5f605713726742564017525d6480b7
Zach Campbell <beazlebubbles@gmail.com>
2018-07-09 11:24:32 -0600

    Completed dicts.py

--- src/dicts.py
+++ src/dicts.py
@@ -7,6 +7,17 @@
 # Make up three entries of various values.
 
 waypoints = [
+    {"lat": 10,
+     "lon": 20,
+     "name": "Mr. Waypoint"},
+    {"lat": 100,
+     "lon": 200,
+     "name": "Mrs. Waypoint"},
+    {"lat": 50,
+     "lon": 100,
+     "name": "Waypoint Jr."}
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for i in waypoints:
+    print(f'lat: {i["lat"]}, lon: {i["lon"]}, name: {i["name"]}')
