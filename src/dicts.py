8163649ea5728c367c71dfc0649e325638e13f8e
derrick-mei <derrick.mei.168@gmail.com>
2018-07-09 16:17:55 -0500

    dicts.py

--- src/dicts.py
+++ src/dicts.py
@@ -6,7 +6,22 @@
 #
 # Make up three entries of various values.
 
-waypoints = [
+waypoints = [{
+    'lat': 1,
+    'lon': 1.5,
+    'name': 'closest'
+},
+{
+    'lat': 2,
+    'lon': 2.5,
+    'name': 'close'
+},{
+    'lat':3,
+    'lon':3.5,
+    'name': 'not so close'
+}
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for i in waypoints:
+    print(i['lat'], i['lon'], i['name'])
