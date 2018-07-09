21f98c7fb292b5d5d88be028e189826ef8f9878c
mister-corn <code@ronaldlibago.com>
2018-07-09 14:14:49 -0700

    Forgot `dicts.py`. Whoops. Here it is!

--- src/dicts.py
+++ src/dicts.py
@@ -7,6 +7,24 @@
 # Make up three entries of various values.
 
 waypoints = [
+  {
+    "lat": 34.049968,
+    "lon": -118.2393978,
+    "name": 'Far Bar'
+  },
+  {
+    "lat": 34.094600,
+    "lon": -117.721755,
+    "name": 'Eureka!'
+  },
+    {
+    "lat": 34.0507809,
+    "lon": -118.2519762,
+    "name": 'La Cita'
+  }
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for waypoint in waypoints:
+  for key in waypoint:
+    print(waypoint[key])
