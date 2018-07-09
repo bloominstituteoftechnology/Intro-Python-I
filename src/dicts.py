2e626c24671347b1a42a1b2b8737f56dab422bd5
Danny Vail <danny_vail@outlook.com>
2018-07-09 17:06:19 -0400

    Completed dicts.py - Practice with dictionaries, loops and string formatting

--- src/dicts.py
+++ src/dicts.py
@@ -7,6 +7,26 @@
 # Make up three entries of various values.
 
 waypoints = [
+    {
+        'lat': '27.979168',
+        'lon': '-82.539337',
+        'name': 'TPA'
+    },
+    {
+        'lat': '36.131687',
+        'lon': '-86.668823',
+        'name': 'BNA'
+    },
+    {
+        'lat': '39.849312',
+        'lon': '-104.673828',
+        'name': 'DEN'
+    }
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for entry in waypoints:
+    print(entry)
+
+for entry in waypoints:
+    print('{e[name]} is located at {e[lat]} latitude, {e[lon]} longitude.'.format(e=entry))
