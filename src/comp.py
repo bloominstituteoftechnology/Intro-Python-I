468fc9db8ca7b637d7a2fc02925757eb613beff9
AmyShackles <amyshackles@gmail.com>
2018-07-09 16:46:16 -0700

    Edited cal and comp.py

--- src/comp.py
+++ src/comp.py
@@ -2,9 +2,5 @@
 # the user entered into list x.
 
 x = input("Enter comma-separated numbers: ").split(',')
-
-# What do you need between the square brackets to make it work?
-
 y = [int(i) for i in x if int(i) % 2 == 0]
-
 print(y)
