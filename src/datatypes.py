d0c3990ffbc471086841f6524b3749ae75c50d42
Marco A Guzman <marcoguzman16@gmail.com>
2018-07-09 14:33:17 -0700

    hello,bignum,datatypes,printf

--- src/datatypes.py
+++ src/datatypes.py
@@ -2,7 +2,11 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
-print(x + y)
+print(x + y) 
+
+5 + int("7")
 
 # Write a print statement that combines x + y into the string value 57
 print(x + y)
+
+str(5) + "7"
