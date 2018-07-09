63a2c02054c01f8fa0993c53746b041407154341
NandoTheessen <nando.theessen@gmail.com>
2018-07-09 22:52:39 +0200

    removed unecessary comments and prints

--- src/obj.py
+++ src/obj.py
@@ -14,9 +14,7 @@ class Waypoint(LatLon):
         self.name = name
 
     def __str__(self):
-            # return("blabla  pi%s%s%s".format(self.name, self.lat, self.lon))
             return f'name: {self.name}, lat: {self.lat}, lon: {self.lon}'
-            # return("%s%s%s".format(self.name, self.lat, self.lon))
 
 
 # # Make a class Geocache that can be passed parameters `name`, `difficulty`,
@@ -29,9 +27,7 @@ class Geocache(Waypoint):
         self.difficulty = difficulty
 
     def __str__(self):
-            # return("blabla  pi%s%s%s".format(self.name, self.lat, self.lon))
             return f'name: {self.name}, difficulty: {self.difficulty}, size: {self.size},  lat: {self.lat}, lon: {self.lon}'
-            # return("%s%s%s".format(self.name, self.lat, self.lon))
 
 # # Make a new waypoint "Catacombs", 41.70505, -121.51521
 w = Waypoint('Catacombs', 41.70505, - 121.51521)
