bbddae17245ce5be3d1f3e1bc17a2c9bb35dcffd
maxwashington <mnwashington18@gmail.com>
2018-07-09 17:45:20 -0500

    Working on Obj. A little confused about inheirtens and the use of super

--- src/obj.py
+++ src/obj.py
@@ -1,9 +1,18 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
+class LatLon:
+    def __init__(self, lat=0, lon=0):
+        self.lat = lat
+        self.lon = lon
         
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon.
 
+class Waypoint(LatLon):
+    def __init__(self, name, lat=0, lon=0):
+        super(). __init__(lat, lon)
+        self.name = name
+        
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
