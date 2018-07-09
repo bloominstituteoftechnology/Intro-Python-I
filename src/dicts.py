8733d3dd87192460055662b0ce336ec4e02331d7
jesuarva <jesuarva.developer@gmail.com>
2018-07-09 23:07:42 +0200

    END dicts.py

--- src/dicts.py
+++ src/dicts.py
@@ -7,6 +7,30 @@
 # Make up three entries of various values.
 
 waypoints = [
+  {
+    'lat': 'x1',
+    'lon': 'y1',
+    "name": 'z1',
+  },
+  {
+    'lat': 'x2',
+    'lon': 'y2',
+    "name": 'z2',
+  },
+  {
+    'lat': 'x3',
+    'lon': 'y3',
+    "name": 'z3',
+  },
 ]
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
+for index, dic in enumerate(waypoints):
+  print('Dic: %s' % (index))
+  for i in dic:
+    print("""
+    key: {0} - value: {1}
+    """.format(i, dic[i]))
+
+
