a6b204682653b4bee0697c39394557ed75372838
jesuarva <jesuarva.developer@gmail.com>
2018-07-10 01:28:39 +0200

    END obj.py

--- src/obj.py
+++ src/obj.py
@@ -1,21 +1,50 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
+class LatLon:
+  def __init__(self, lat, lon):
+    self.lat = lat
+    self.lon = lon
         
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon.
+class Waypoint(LatLon):
+  def __init__(self, lat, lon, name):
+    super().__init__(lat, lon)
+    self.name = name
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
+class Geocache(Waypoint):
+  def __init__(self, lat, lon, name, difficulty, size):
+    super().__init__(lat, lon, name)
+    self.difficulty = difficulty
+    self.size = size
 
 # Make a new waypoint "Catacombs", 41.70505, -121.51521
+w = Waypoint(41.70505, -121.51521,"Catacombs")
 
 # Print it
 #
 # Without changing the following line, how can you make it print into something
 # more human-readable?
+w = """
+Waypoint:
+lat {0}
+lon {1}
+name {2}
+""".format(w.lat, w.lon, w.name)
 print(w)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
+g = Geocache(44.052137, -121.41556, "Newberry Views", 1.5, 2)
 
 # Print it--also make this print more nicely
+g = """
+Geocache:
+lat {0}
+lon {1}
+name {2}
+difficulty {3}
+size {4}
+""".format(g.lat, g.lon, g.name, g.difficulty, g.size)
 print(g)
