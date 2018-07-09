b00eb7d510206912a7f39e4e5a5e568c9de6404b
Christina Kopecky <kopecky12112@gmail.com>
2018-07-09 14:17:43 -0700

    dictionary done

--- src/dicts.py
+++ src/dicts.py
@@ -6,7 +6,14 @@
 #
 # Make up three entries of various values.
 
-waypoints = [
-]
+waypoint1 = dict([('latitude', '25* 15\' 10.08" N' ), ('longitude', '55* 21\' 51.84" E'), ('name', 'OMDB')])
+waypoint2 = dict([('latitude', '33* 56\' 33.13" N'), ('longitude', '118* 24\' 29.07" W'), ('name', 'KLAX')])
+waypoint3 = dict([('latitude', '41* 58\' 42.97" N' ), ('longitude', '87* 54\' 17.43" W'), ('name', 'KORD')])
+
+
+waypoints = [waypoint1, waypoint2, waypoint3]
+
+for waypoint in waypoints:
+    print(waypoint)
 
 # Write a loop that prints out all the field values for all the waypoints
