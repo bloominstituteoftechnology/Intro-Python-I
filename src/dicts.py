8428923e00d4b25e41773ec64daedb55bfb3603b
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-07-10 06:01:42 +0800

    Implemented dicts.py.

--- src/dicts.py
+++ src/dicts.py
@@ -7,6 +7,23 @@
 # Make up three entries of various values.
 
 waypoints = [
+    dict1 = {
+        "lat": 33,
+        "long": 33,
+        "name": "waypoint_one"
+    },
+    dict2 = {
+        "lat": 32,
+        "long": 32,
+        "name": "waypoint_two"
+    },
+    dict3 = {
+        "lat": 31,
+        "long": 31,
+        "name": "waypoint_three"
+    }
 ]
+
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
