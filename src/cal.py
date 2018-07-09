088465ae6f71f234b1958c9a1b1edb7e35a3997e
Usman Javed <ujaved931@gmail.com>
2018-07-10 02:51:39 +0500

    Finished bignum/lists/printf working on rest

--- src/cal.py
+++ src/cal.py
@@ -1,12 +1,18 @@
 # Use the 'calendar' module to draw calendars to the console
 # https://docs.python.org/3.6/library/calendar.html
+import sys
 #
 # Use the sys module to look for command line arguments in the `argv` list
 # variable.
+from sys import argv
 #
 # If the user specifies two command line arguments, month and year, then draw
 # the calendar for that month.
+import calendar
+from calendar import argv
 
+import datetime
+from datetime import date
 # Stretch goal: if the user doesn't specify anything on the command line, show
 # the calendar for the current month. See the 'datetime' module.
 
