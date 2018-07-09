3f1468a9c6b6fa7fcf1d0933fb214f15ec958422
unknown <sam.park.email@gmail.com>
2018-07-09 19:46:21 -0400

    obj.py

--- src/obj.py
+++ src/obj.py
@@ -1,23 +1,31 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
-class LatLon(object):
-  def __init__(self, lat, lon):
-    self.lat = lat
-    self.lon = lon
+class LatLon:
+    def __init__(self, lat=0, lon=0):
+        self.lat = lat
+        self.lon = lon
+        
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon.
 class Waypoint(LatLon):
-  def __init__(self, name, lat, lon):
-      LatLon.__init__(self, lat, lon)
-      self.name = name
-      print(name, lat, lon)
+    def __init__(self, name, lat=0, lon=0):
+        super().__init__(lat, lon)
+        self.name = name
+
+    def __str__(self):
+        return "<Waypoint '{}' {:f},{}>".format(self.name, self.lat, self.lon)
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
-#class Geocache:
- # def __init__(Waypoint, difficulty, size):
-  #    self.difficulty = difficulty
-   #   self.size = size
+class Geocache(Waypoint):
+    def __init__(self, name, difficulty, size, lat=0, lon=0):
+        super().__init__(name, lat, lon)
+        self.difficulty = difficulty
+        self.size = size
+
+    def __str__(self):
+        return "<Geocache '{}' {} {} {:f},{}>".format(self.name, self.difficulty, self.size, self.lat, self.lon)
+
 # Make a new waypoint "Catacombs", 41.70505, -121.51521
 w = Waypoint("Catacombs", 41.70505, -121.51521)
 
@@ -25,10 +33,10 @@ w = Waypoint("Catacombs", 41.70505, -121.51521)
 #
 # Without changing the following line, how can you make it print into something
 # more human-readable?
-if __name__ == '__main__':
-    print(w)
+print(w)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
+g = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
-#print(g)
+print(g)
