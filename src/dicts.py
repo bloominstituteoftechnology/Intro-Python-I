ef96edbe48355968ad84ce865c55d44595a1dece
Lavell Burton <taylormade800@gmail.com>
2018-07-09 15:59:00 -0700

    finishedthe code challenges up to the stretch goals

--- src/dicts.py
+++ src/dicts.py
@@ -7,7 +7,12 @@
 # Make up three entries of various values.
 
 waypoints = [
+    "lat:" "the latitude",
+    "lon:" "the longitude",
+    "name:" "the waypoint name"
 ]
 
 
 # Write a loop that prints out all the field values for all the waypoints
+for i in waypoints:
+    print(i) 
