cfc35517c67ad0dab2e46ed3da61d5c38ae93302
Marco A Guzman <marcoguzman16@gmail.com>
2018-07-09 22:21:17 -0700

    # comp.py

--- src/comp.py
+++ src/comp.py
@@ -8,10 +8,9 @@ y = [int(i) for i in x if int(i) % 2 == 0 ]
 
 print(y)
 
-"""
+
 #x = input("Enter comma-separated numbers: ").split(',')
 #Enter comma-separated numbers: 2,3,4,5,2,76,8,4,2,4,5
 #>>> y = [int(i) for i in x if int(i) % 2 == 0 ]
 #>>> print(y)
 #[2, 4, 2, 76, 8, 4, 2, 4]
-"""
