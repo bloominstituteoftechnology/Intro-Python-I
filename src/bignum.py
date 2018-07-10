469a9ca63d935aa5f9efc7e50147ecbf48ff4e4e
Thuy <tpham045@hotmail.com>
2018-07-10 16:44:47 +0000

    feat: use math.log for bignum

--- src/bignum.py
+++ src/bignum.py
@@ -1,10 +1,4 @@
 # Print out 2 to the 65536 power
-i = 0
-res = 0
-while pow(2,i) < 65536:
-  i += 1
-
-if pow(2,i) == 65536:
-    print(i)
-else:
-    print('Cannot find')
+import math
+    
+print(math.log(65536,2))
