5da2d396773943171b262acb82fcc3264ab2714e
Xang Thao <xangthao24@yahoo.com>
2018-07-10 14:31:16 -0700

    Got my input working with my controller

--- src/adv.py
+++ src/adv.py
@@ -56,7 +56,7 @@ earlier adventurers. The only exit is to the south.""",
 
 # Write a class to hold player information, e.g. what room they are in currently
 class Player: 
-    def __init__(self, name, current_position):
+    def __init__(self, name):
         self.name = name
         self.current_room = outside
     
@@ -66,13 +66,68 @@ class Player:
 # Main
 #
 
+class Controller:
+    def __init__(self, player, rooms):
+        self.player = player
+        self.rooms = rooms
+    
+    def move_north(self):
+        print("move north")
+
+    def move_south(self):
+        print("move south")
+
+    def move_east(self):
+        print("move east")
+
+    def move_west(self):
+        print("move west")
+
+    def start(self):
+        player_move = input("Please enter n for north s for south w for west and e for east and q for quit")
+        while(player_move != "q"):
+            if(player_move == "n"):
+                self.move_north()
+                player_move = input("Please enter n for north s for south w for west and e for east and q for quit")
+            elif(player_move == "s"):
+                self.move_south()
+                player_move = input("Please enter n for north s for south w for west and e for east and q for quit")
+            elif(player_move == "w"):
+                self.move_west()
+                player_move = input("Please enter n for north s for south w for west and e for east and q for quit")
+            elif(player_move == "e"):
+                self.move_east()
+                player_move = input("Please enter n for north s for south w for west and e for east and q for quit")
+            else:
+                player_move = input("Please enter n for north s for south w for west and e for east and q for quit")
+
+            
+newGame = Controller("Xang", "test")
+newGame.start()
+
+
+
+
 class Room: 
-    def __init__(self, name, description):
+    def __init__(self, name, description, n_to, s_to, e_to, w_to):
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
 
 class Board: 
     def __init__(self, player, rooms_obj):
         self.player = player
-        self.rooms_obj = rooms_obj
+        self.rooms = rooms_obj
+        # self.rooms_obj["outside"] = rooms_obj["outside"]
+        # self.rooms_obj["foyer"] = rooms_obj["foyer"]
+        # self.rooms_obj["overlook"] = rooms_obj["overlook"]
+        # self.rooms_obj["narrow"] = rooms_obj["narrow"]
+        # self.rooms_obj["treasure"] = rooms_obj["treasure"]
+    
+
     
 
 
