8101b94a18de6f10962fcdc6e31a343c860f4888
Herbert Hodgson <herberthodgson@hotmail.com>
2018-07-10 19:07:49 -0400

    what has been accomplished so far in adv.py

--- src/adv.py
+++ src/adv.py
@@ -2,6 +2,12 @@
 # typing "n", "w", "s", or "e" for north, west, south, and east.
 
 # These are the existing rooms. Add more as you see fit.
+compass = {
+    "n": "n_to",
+    "w": "w_to",
+    "s": "s_to",
+    "e": "e_to"
+}
 
 rooms = {
     "outside": {
@@ -55,20 +61,48 @@ earlier adventurers. The only exit is to the south.""",
 """
 
 # Write a class to hold player information, e.g. what room they are in currently
+class Player:
+    def __init__(self, player_name, current_room):
+        self.name = player_name
+        self.location = rooms[current_room]
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+new_player = Player("Billy", "outside")
 
 # Write a loop that:
-#
+
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
-#
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+def prompt():
+    print('What would you like to do?')
+    action = input("> ")
+    acceptable_actions = ["move, quit, q"]
+    while action.lower() not in acceptable_actions:
+        print("Unknown action, try again.")
+        action = input("> ")
+    if action.lower() == 'quit' || 'q':
+        break
+    elif action.lower() == 'move':
+        player_move(action.lower())
+
+def player_move(action):
+    ask = "Where would you like to go?\n"
+    dest = input(ask)
+    if dest == compass["n"]:
+        destination = rooms[new_player.location][dest]
+        movement_handler(destination)
+
+def movement_handler(destination):
+    print("You have moved to {}.".format(destination))
+    new_player.location = destination
