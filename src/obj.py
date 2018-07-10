71b7ff3009d28a1b5be79aaefa90758cf556c00a
Christina Kopecky <kopecky12112@gmail.com>
2018-07-10 13:53:02 -0700

    figured out the rest of obj.py

--- src/obj.py
+++ src/obj.py
@@ -1,19 +1,11 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
 
-# class MyParentClass():
-#     def __init__(self, x, y):
-#     pass
-
-# class SubClass(MyParentClass):
-#     def __init__(self, x, y):
-#     super().__init__(x, y)
-
-
 class LatLon:
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
+        pass
 
 
 
@@ -21,43 +13,40 @@ class LatLon:
 # # constructor. It should inherit from LatLon.
 
 class Waypoint(LatLon):
-    def __init__(self, lat, lon, name):
+    def __init__(self, name, lat, lon):
         super().__init__(lat, lon)
         self.name = name
     
     def __str__(self):
-        return "Cache {} is located at {}, {}".format(x, y, z)
-    
+        return "Cache %s is located at %.5f latitude, %.5f longitude" % (self.name, self.lat, self.lon)
 
 
     
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # # `size`, `lat`, and `lon` to the constructor. Wphat should it inherit from?
-x = "Catacombs"
-y = 44.05137
-z = -121.41556
-a = 1.2
-b = 3
+
 
 class Geocache(Waypoint):
-    def __init__(self, name, difficulty, size, lat, lon):
+    def __init__(self, difficulty, size, name, lat, lon):
         super().__init__(name, lat, lon)
         self.difficulty = difficulty
         self.size = size
+
     def __str__(self):
-        return "Cache {0} is located at {1}, {2} and is {3} in size and {4} in difficulty".format(x, y, z, a, b)
+        return "Cache {} is located at {} latitude, {} longitude and is {} in size and {} in difficulty".format(self.name, self.lat, self.lon, self.size, self.difficulty)
     
 # # Make a new waypoint "Catacombs", 41.70505, -121.51521
+w = Waypoint("Catacombs",44.05137, -121.41556)
 
-w = Waypoint(44.05137, -121.41556, "Catacombs")
 # # Print it
 # #
 # # Without changing the following line, how can you make it print into something
 # # more human-readable?
 print(w)
 
+
 # # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
-g = Geocache(44.052137, -121.41556, "Newberry Views", 1.5, 2).__str__()
+g = Geocache("2", "1.5", "Newberry Views", 44.052137, -121.41556)
+
 # # Print it--also make this print more nicely
 print(g)
-
