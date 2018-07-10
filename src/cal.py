413e00e68ce07562afc17b7a6edd24c31fef6cf2
sagardesai90 <saagardesai@gmail.com>
2018-07-09 23:21:18 -0500

    calendar

--- src/cal.py
+++ src/cal.py
@@ -14,3 +14,17 @@
 # docs for the calendar module closely.
 
 import sys
+import calendar
+import datetime
+
+cal = calendar.TextCalendar(calendar.SUNDAY)
+if len(sys.argv) == 3:
+    year = int(sys.argv[1])
+    month = int(sys.argv[2])
+else:
+    date = datetime.datetime.now()
+    year = date.year
+    month = date.month
+
+fin = cal.formatmonth(year, month)
+print(fin)
