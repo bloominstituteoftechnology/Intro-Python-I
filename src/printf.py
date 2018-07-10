121ed93b1f9427289c3b195311d2c329d6ebe8e3
tramanedhall <tramanedhall@gmail.com>
2018-07-10 16:33:35 -0400

    adv.py WIP

--- src/printf.py
+++ src/printf.py
@@ -5,6 +5,7 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print("%d %.2f %s" %(x,y,z))
 
 # Use the 'format' string method to print the same thing
+print('z is {2}, y is {1:.2f}, x is {0}'.format(x,y,z))
