a459bebf2bf7ef83d73361218ad3f4234e68de99
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-07-10 13:38:35 -0700

    fix: obj.py

--- src/obj.py
+++ src/obj.py
@@ -13,6 +13,8 @@ class Waypoint (LatLon):
         super().__init__(lat, lon) # new method
         self.name = name
 
+    def __str__(self):
+        return '\nthe %s are located at Longitude: %.2f and latitude: %.2f\n' % (self.name, self.lon, self.lat)
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
@@ -22,9 +24,13 @@ class Geocache (Waypoint):
         self.difficulty = difficulty
         self.size = size
 
+    def __str__(self):
+        return '\nthe %s cache (size: %s, diff: %.1f) is located at Longitude: %.2f and latitude: %.2f\n' % (self.name, self.size, self.difficulty, self.lon, self.lat)
+
+
 # Make a new waypoint "Catacombs", 41.70505, -121.51521
-newWaypoint = Waypoint("Catacombs", 41.70505, -121.51521)
-w = newWaypoint
+w = Waypoint("Catacombs", 41.70505, -121.51521)
+
 # Print it
 #
 # Without changing the following line, how can you make it print into something
@@ -32,7 +38,7 @@ w = newWaypoint
 print(w)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
-newGeocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
-g = newGeocache
+g = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
+
 # Print it--also make this print more nicely
 print(g)
