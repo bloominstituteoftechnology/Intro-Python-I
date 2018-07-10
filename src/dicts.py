b8f4ddaaf47a4de0d4a3377a251a9a8c983bd076
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-07-10 13:25:50 -0400

    Modified dicts.py file.

--- src/dicts.py
+++ src/dicts.py
@@ -7,9 +7,16 @@
 # Make up three entries of various values.
 
 waypoints = [
-    {'lat': 37.6979039}, 
-    {'lon': -121.925388}, 
-    {'name': 'Lambda School Office'}
+    {
+        'lat': 37.6979039, 
+        'lon': -121.925388, 
+        'name': 'Lambda School Office'
+    }, 
+    {
+        'lat': 37.421542, 
+        'lon': -122.084011, 
+        'name': 'Google Mountain View Office'
+    }
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
