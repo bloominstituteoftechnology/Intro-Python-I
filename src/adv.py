69b8b6c64dd0c1ad42ad9c007defaa466530bb8d
Matthias Bakken <matthias.bakken@gmail.com>
2018-07-10 15:22:58 -0700

    completed mvp of adv.py and adding more funcitonality

--- src/adv.py
+++ src/adv.py
@@ -3,11 +3,14 @@
 
 # These are the existing rooms. Add more as you see fit.
 
+import textwrap
+
 rooms = {
     "outside": {
         "name": "Outside Cave Entrance",
         "description": "North of you, the cave mouth beckons.",
         "n_to": "foyer",
+        "contents": ['stuff', 'things', 'other stuff', 'more things']
     },
 
     "foyer": {
@@ -16,6 +19,7 @@ rooms = {
         "n_to": "overlook",
         "s_to": "outside",
         "e_to": "narrow",
+        "contents": []
     },
 
     "overlook": {
@@ -24,6 +28,7 @@ rooms = {
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm.""",
         "s_to": "foyer",
+        "contents": []
     },
 
     "narrow": {
@@ -31,6 +36,7 @@ the distance, but there is no way across the chasm.""",
         "description": "The narrow passage bends here from west to north. The smell of gold permeates the air.", 
         "w_to": "foyer",
         "n_to": "treasure",
+        "contents": []
     },
 
     "treasure": {
@@ -39,6 +45,7 @@ the distance, but there is no way across the chasm.""",
 chamber. Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""",
         "s_to": "narrow",
+        "contents": []
     },
 
 }
@@ -55,6 +62,21 @@ earlier adventurers. The only exit is to the south.""",
 """
 
 # Write a class to hold player information, e.g. what room they are in currently
+class Player:
+    def __init__(self, initRoom):
+        self.currentRoom = initRoom
+        self.health = 100
+        self.inventory = ["6ft rope", "ration", "ration", "ration", "flint & steel", "small dagger"]
+
+def movement(move, currentRoom):
+    key = move + "_to"
+    if key not in rooms[currentRoom]:
+        print("\nChoose a different direction.\n")
+        return currentRoom
+
+    room = rooms[currentRoom][key]
+
+    return room
 
 #
 # Main
@@ -62,13 +84,43 @@ earlier adventurers. The only exit is to the south.""",
 
 # Make a new player object that is currently in the 'outside' room.
 
+Johnny = Player('outside')
+
 # Write a loop that:
 #
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
+
+
 # * Waits for user input and decides what to do.
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+completed = False
+
+while not completed:
+    # * Prints the current room name
+    print("\n" + rooms[Johnny.currentRoom]['name'] + "\n")
+    
+    # * Prints the current description (the textwrap module might be useful here).
+    for line in textwrap.wrap(rooms[Johnny.currentRoom]['description']):
+        print(line)
+    
+    print("\nContents of room: ")
+    for item in rooms[Johnny.currentRoom]['contents']:
+        print(item)
+        
+
+    attempt = input("\nChoose n, s, e or w for direction or i for inventory and q to quit: \n").lower()
+
+    if attempt == 'q':
+        completed = True
+    elif attempt in ['n', 's', 'e', 'w']:
+        Johnny.currentRoom = movement(attempt, Johnny.currentRoom)
+    elif attempt == 'i':
+        print("\nMy inventory contains: ")
+        for item in Johnny.inventory:
+            print(item)
+    else:
+        print("\nYou have entered an invalid option.\n")
