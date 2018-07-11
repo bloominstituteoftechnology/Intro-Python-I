7c28eaf577b3b88d696a181c819f00c6eb2a03ef
Danny Vail <danny_vail@outlook.com>
2018-07-10 21:53:38 -0400

    Completed text adventure game MVP - Basic functionality. Player can move around with n, s, e, w and can exit game with q

--- src/adv.py
+++ src/adv.py
@@ -1,7 +1,13 @@
-# Write a text adventure that allows the player to move from room to room by
-# typing "n", "w", "s", or "e" for north, west, south, and east.
-
-# These are the existing rooms. Add more as you see fit.
+""" template room to copy into code
+    "room": {
+        "name": "",
+        "description": "",
+        "n_to": "",
+        "s_to": "",
+        "e_to": "",
+        "w_to": "",
+    },
+"""
 
 rooms = {
     "outside": {
@@ -28,7 +34,7 @@ the distance, but there is no way across the chasm.""",
 
     "narrow": {
         "name": "Narrow Passage",
-        "description": "The narrow passage bends here from west to north. The smell of gold permeates the air.", 
+        "description": "The narrow passage bends here from west to north. The smell of gold permeates the air.",
         "w_to": "foyer",
         "n_to": "treasure",
     },
@@ -43,32 +49,65 @@ earlier adventurers. The only exit is to the south.""",
 
 }
 
-""" template room to copy into code
-    "room": {
-        "name": "",
-        "description": "",
-        "n_to": "",
-        "s_to": "",
-        "e_to": "",
-        "w_to": "",
-    },
-"""
 
-# Write a class to hold player information, e.g. what room they are in currently
+class GameObject:
+    def __init__(self, play):
+        self.play = play
 
-#
-# Main
-#
+    def print_title(self, title):
+        title_sep = ''.center(len(title), '-')
+        print('\n{}\n{}'.format(title, title_sep))
+    
+    def print_description(self, description):
+        print('{}\n'.format(description))
+    
+    def quit_game(self):
+        self.play = False
+        print("\nGoodbye")
 
-# Make a new player object that is currently in the 'outside' room.
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+class Player(GameObject):
+    def __init__(self, location, play):
+        super().__init__(play)
+        self.location = location
+        self.command = ''
+        self.commands = {
+            "n": self.move,
+            "s": self.move,
+            "e": self.move,
+            "w": self.move,
+            "q": self.quit_game
+        }
+    
+    def get_command(self):
+        self.command = input("\nWhat do you want to do? ")
+        self.process_command()
+    
+    def process_command(self):
+        try:
+            self.commands[self.command]()
+        except KeyError:
+            print("\nNot sure what you mean.")
+    
+    def look(self):
+        self.print_title(rooms[self.location]["name"])
+        self.print_description(rooms[self.location]["description"])
+        self.print_title("Available Directions")
+
+    def move(self):
+        direction = self.command + '_to'
+        try:
+            self.location = rooms[self.location][direction]
+        except KeyError:
+            print("\nWhoops! That's not a valid direction.")
+
+
+def game_loop():
+    player = Player("outside", True)
+    print("\n\n*** And so our story begins... ***\n\n")
+    while player.play:
+        player.look()
+        player.get_command()
+
+
+game_loop()
