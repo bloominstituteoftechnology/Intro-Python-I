93d8ee1d32acfc47ac221bd0016274b125fb4041
FireInjun <mk@fireinjun.com>
2018-07-15 00:28:41 -0400

    completed and added an additional way to work with strings

--- src/printf.py
+++ src/printf.py
@@ -5,6 +5,10 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print("My son is %d years old and my daughter is %f! And I %s" % (x, y, z))
 
 # Use the 'format' string method to print the same thing
+print("My son is {} years old and my daughter is {}! And I {}".format(x, y, z))
+
+# Use the new f string format to print out the same thing. Amazing I got it to work!
+print(f"My son is {x} years old and my daughter is {y} ! And I {z}")
