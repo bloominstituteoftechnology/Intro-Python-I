21ab93c45cf742ab8c0d0c2f560bab1ebf098084
FireInjun <mk@fireinjun.com>
2018-07-15 12:51:11 -0400

    continuing review

--- src/obj.py
+++ src/obj.py
@@ -1,19 +1,30 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
 
-Class LatLon:
-        def __init__(self, lat, lon)
-            self.lat = lat
-            self.lon = lon
-            
+
+class LatLon:
+    def __init__(self, lat=0, lon=0):
+        self.lat = lat
+        self.lon = lon
+
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon.
-
+class Waypoint(LatLon):
+    def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
-
-
+class Geocache(Waypoint):
+    def __init__(self, name, difficulty, size, lat, lon)
+    self.name = name
+    self.difficulty = difficulty
+    self.size = size
+    self.lat = lat
+    self.lon = lon
 # Make a new waypoint "Catacombs", 41.70505, -121.51521
 
 
