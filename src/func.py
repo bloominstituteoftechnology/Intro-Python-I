55f548e9ef22220998ee5280fd18329fb075cd13
FireInjun <mk@fireinjun.com>
2018-07-15 06:27:26 -0400

    complete and added number check

--- src/func.py
+++ src/func.py
@@ -4,3 +4,18 @@
 num = input("Enter a number: ")
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
+
+
+def even_odd(num):
+    ans1 = "Even!"
+    ans2 = "Odd!"
+    default = f"{num} is not a number!"
+    if (num).isdigit() == False:
+        return default
+    elif int(num) % 2 == 0:
+        return ans1
+    else:
+        return ans2
+
+
+print(even_odd(num))
