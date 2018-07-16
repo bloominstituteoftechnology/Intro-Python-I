6bddf3cd05aa972306a9f3e010a81842d51d7d27
FireInjun <mk@fireinjun.com>
2018-07-16 16:27:39 -0400

    corrected to float and int

--- src/printf.py
+++ src/printf.py
@@ -11,4 +11,4 @@ print("My son is %d years old and my daughter is %f! And I %s" % (x, y, z))
 print("My son is {} years old and my daughter is {}! And I {}".format(x, y, z))
 
 # Use the new f string format to print out the same thing. Amazing I got it to work!
-print(f"My son is {x} years old and my daughter is {y} ! And I {z}")
+print(f"My son is {x:i} years old and my daughter is {y} ! And I {z:f}")
