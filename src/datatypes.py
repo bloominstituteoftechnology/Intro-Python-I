edf664a4b062f2948a93dde20a42ea28dd25aca3
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-07-23 15:53:15 -0400

    Robert Hogan. Completed datatypes.py. Used str() and int() tools to complete the problem.

--- src/datatypes.py
+++ src/datatypes.py
@@ -2,7 +2,11 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
-print(x + y)
+print(x + int(y))
 
 # Write a print statement that combines x + y into the string value 57
-print(x + y)
+my_x = str(x)
+if my_x + y == "57":
+    print(my_x + y)
+else:
+    print("Test failed. We are not both strings")
