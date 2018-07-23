47f752cd76f358c656beed9d7dfc02831ef8a2ce
tanking <tanking@github.com>
2018-07-23 19:56:43 -0400

    Took out .split

--- src/comp.py
+++ src/comp.py
@@ -23,7 +23,7 @@ print(y)
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ").split(',')
+x = input("Enter comma-separated numbers: ")
 
 # What do you need between the square brackets to make it work?
 y = []
