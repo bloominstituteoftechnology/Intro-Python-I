16205d59e1153dd3732a4e48c38ad8330a8977d6
Eric Andrade <eric.andrade@gmail.com>
2018-07-23 18:30:32 -0400

    slice partially done, but a couple of pops behaving strangely

--- src/slice.py
+++ src/slice.py
@@ -1,26 +1,29 @@
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+print(a[1])
 
 # Output the second-to-last element: 9
-print()
+print(a[-2])
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+print(a[-3:])
 
 # Output the two middle elements in the array: [1, 7]
-print()
+print(a.pop((len(a) -1) //2))
+
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+a.pop(0)
+print(a)
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+a.pop(-1)
+print(a)
 
 # For string s...
 
 s = "Hello, world!"
 
-# Output just the 8th-12th characters: "world"
-print()
+# Output just the 8th-12th characters: "world"ß
+print(s[7:12])
