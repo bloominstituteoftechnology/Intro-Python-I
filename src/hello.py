729f8ceb32365dd4593696b3b13d3fb3e87a752b
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-07-23 15:27:22 -0400

    Robetrt Hogan. Initial commit. Completed hello.py, working on a few methods for solution.

--- src/hello.py
+++ src/hello.py
@@ -1,1 +1,16 @@
 # Write Hello, world
+
+#method 1 - print
+print("Hello, world")
+
+#method 2 - variable and print
+greeting = "Hello, World"
+print(greeting)
+
+#method 3 - function call
+def say_words(word1, word2):
+    print(word1 + word2)
+
+say_words("Hello ,", "world")
+
+#method 4
