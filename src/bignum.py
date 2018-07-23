c519d4a13988ea6e1c3ebbc58d8785c50abb17ac
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-23 15:43:14 -0400

    Completed bignum

--- src/bignum.py
+++ src/bignum.py
@@ -1,1 +1,12 @@
 # Print out 2 to the 65536 power
+
+# Built In
+def big_num():
+    print(pow(2, 65536))
+
+# Library
+# import math
+# def big_num():
+#     print(math.pow(2, 65536))
+    
+big_num()
