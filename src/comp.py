97bdd7f0074c0c6e0837ea667d690f78d43a9caf
JustinSHong <jhong2009@gmail.com>
2018-07-23 14:18:11 -0700

    in comp.py: wrote list comprehension to produce the cubes of numbers 0-9

--- src/comp.py
+++ src/comp.py
@@ -7,9 +7,9 @@ print ("list with integers from 1 - 5 {0}".format(y))
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [n ** 3 for n in range(10)]
 
-print(y)
+print("list with cubes of the numbers 0 - 9 {0}".format(y))
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
