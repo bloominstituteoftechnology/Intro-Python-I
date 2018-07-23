5e5594f76f4b4bdf0390834cd47febfbd2a0ddee
JustinSHong <jhong2009@gmail.com>
2018-07-23 13:03:29 -0700

    in modules.py: print operating system identifying information using os module

--- src/modules.py
+++ src/modules.py
@@ -24,7 +24,15 @@ print("serial part of Python version number is", sys.version_info.serial)
 # Module "os"
 #
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
-
+# Print name of the operating system dependent module imported
+print(os.name)
+# Print information identifying current operating system
+print("object containing machine's current os info:", os.uname())
+print("sysname:", os.uname().sysname)
+print("name of machine on network:", os.uname().nodename)
+print("os release:", os.uname().release)
+print("os version:", os.uname().version)
+print("hardware identifier:", os.uname().machine)
 # Print the current process ID
 print()
 
