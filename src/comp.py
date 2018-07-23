0d5742ae28ef43a898eb944d4c7d3b95aa7eb728
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2018-07-23 16:28:31 -0500

    Finished comp.py

--- src/comp.py
+++ src/comp.py
@@ -1,13 +1,13 @@
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [item for item in range(1, 6)]
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [item ** 3 for item in range(0, 10)] # item **3 gets evaluated everytime the loop runs
 
 print(y)
 
@@ -16,8 +16,8 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
-
+y = [letter.upper() for letter in a]
+ 
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
@@ -26,7 +26,7 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
-
+y = [int(item) for item in x if int(item) % 2 == 0]
+#split was causing it to be returned as a string but we need to coerse it to an int
 print(y)
 
