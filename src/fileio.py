d8f938fab53d1cb7219b1ad0bb63c88d01e57fc8
JustinSHong <jhong2009@gmail.com>
2018-07-23 14:55:58 -0700

    in fileio.py: printed lines in foo.txt

--- src/fileio.py
+++ src/fileio.py
@@ -1,9 +1,11 @@
+import io
 # Use open to open file "foo.txt" for reading
-
+with open('foo.txt', mode='r', newline = '\n') as file:
+	lines = file.read()
 # Print all the lines in the file
-
+print(lines)
 # Close the file
-
+# the with keyword closes file after successful write
 
 # Use open to open file "bar.txt" for writing
 
