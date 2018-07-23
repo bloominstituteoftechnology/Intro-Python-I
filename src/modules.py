cf77313a95dedac768db0f32e6f21affcec993e6
Devon Bacon <devon.bacon@gmail.com>
2018-07-23 12:49:04 -0700

    modules.py - printed process ID, working directory, and login name

--- src/modules.py
+++ src/modules.py
@@ -22,11 +22,11 @@ print(sys.version)
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-print()
+print(os.geteuid())
 
 # Print the current working directory (cwd):
-print()
+print(os.getcwd())
 
 # Print your login name
-print()
+print(os.getlogin())
 
