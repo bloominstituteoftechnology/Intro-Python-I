db75196fc853eeb259c098cf25392dc7b621213f
Devon Bacon <devon.bacon@gmail.com>
2018-07-23 14:08:47 -0700

    fileio.py - read lines of foor.txt and printed to console

--- src/fileio.py
+++ src/fileio.py
@@ -1,9 +1,12 @@
 # Use open to open file "foo.txt" for reading
+file = open('foo.txt', 'rU')
 
 # Print all the lines in the file
+for line in file:
+  print(line)
 
 # Close the file
-
+file.close()
 
 # Use open to open file "bar.txt" for writing
 
