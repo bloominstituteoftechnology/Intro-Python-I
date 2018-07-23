b03e9c83f4fa69bcddd011bec161bb4903289d80
JustinSHong <jhong2009@gmail.com>
2018-07-23 15:29:34 -0700

    used calendar module to draw calendars based on month and year given

--- src/cal.py
+++ src/cal.py
@@ -14,3 +14,37 @@
 # docs for the calendar module closely.
 
 import sys
+import calendar
+
+argc = len(sys.argv) # number of arguments given to the command line
+
+if argc == 2:
+	# not enough args were given
+	month = None
+	year = int(sys.argv[1])
+elif argc == 3:
+	# month and year are given
+	month = int(sys.argv[1])
+	year = int(sys.argv[2])
+else:
+	# error check for incorrect inputs
+	print("usage: cal.py [month] year")
+	sys.exit(1)
+
+# set Sunday to be the first day of the week
+calendar = calendar.TextCalendar(6) # generate a string formatted calendar
+
+if month != None:
+	calendar.prmonth(year, month) # print month's calendar
+else:
+	calendar.pryear(year) # print calendar for the whole year
+
+
+
+
+
+
+
+
+
+
