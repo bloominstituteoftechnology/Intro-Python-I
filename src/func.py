3c299defbff545e4ffd1dc8ab4b9bbcefc961c41
jaivonmassena <jaivonmassena@gmail.com>
2018-07-23 20:23:40 +0200

    finished datatypes, fileio, func hello and lists

--- src/func.py
+++ src/func.py
@@ -1,6 +1,7 @@
 # Write a function is_even that will return true if the passed in number is even.
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
+num = int(input("Enter a number: "))
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
+print("Even!") if num % 2 == 0 else print("Odd")
