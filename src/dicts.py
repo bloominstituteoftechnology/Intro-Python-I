2a4fe0db194bc6d7f56453c49f621ee6551a9444
Mark Marchant <jtla3@yahoo.com>
2018-07-23 13:33:29 -0700

    dicts.py done

--- src/dicts.py
+++ src/dicts.py
@@ -25,5 +25,11 @@ waypoints = [
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for i in waypoints:
+    print(i["lat"], i["lon"], i["name"])
 
-# Add a new waypoint to the list
+Add a new waypoint to the list
+waypoints.append({"lat": 36, "lon": -112, "name": "Grand Canyon"})
+
+for i in waypoints:
+    print(i["lat"], i["lon"], i["name"])
