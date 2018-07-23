271742b563339f2b6ee90082a8cd7485bb0143c6
JustinSHong <jhong2009@gmail.com>
2018-07-23 14:16:42 -0700

    in comp.py: wrote a list comprehension to produce [1,2,3,4,5]

--- src/comp.py
+++ src/comp.py
@@ -1,8 +1,8 @@
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [n for n in range(1,6)]
 
-print (y)
+print ("list with integers from 1 - 5 {0}".format(y))
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
