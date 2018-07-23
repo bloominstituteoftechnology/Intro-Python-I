690ed545a19470333948d21ed7a0546923712a5f
tanking <tanking@github.com>
2018-07-23 17:03:36 -0400

    Completed printf

--- src/printf.py
+++ src/printf.py
@@ -5,6 +5,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print("x is %d, y is %.2f, z is %s" % (x, round(y, 2), z))
 
 
 # Use the 'format' string method to print the same thing
+print("x is", format(x) + ",", "y is", format(round(y, 2)) + ",", "z is", format(z))
