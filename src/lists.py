10f20656d1d6cbea13c40683dac895bd7c9fb59b
JustinSHong <jhong2009@gmail.com>
2018-07-23 13:47:53 -0700

    in lists.py: modified x using append(), extend(), remove(), insert()

--- src/lists.py
+++ src/lists.py
@@ -7,23 +7,23 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# [command here]
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# [command here]
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# [command here]
+x.remove(8)
+# del x[x.index(8)]
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# [command here]
+x.insert(x.index(10), 99)
 print(x)
 
 # Print the length of list x
-# [command here]
 print(len(x))
 
 # Using a for loop, print all the element values multiplied by 1000
