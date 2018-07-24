6a2a4b227296e89ae58f94afda9fbd84998b11a2
Nedu Robert <nedumrobert@gmail.com>
2018-07-25 00:13:36 +0100

    Completed modules

--- src/modules.py
+++ src/modules.py
@@ -6,7 +6,7 @@ import os
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-
+print(sys.argv)
 
 # Print out the plaform from sys:
 print(sys.platform)
