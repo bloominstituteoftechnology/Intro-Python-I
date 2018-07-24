fb6497781a463055c05df343b863aa4bbee8bdda
totsuya <supertatortots@gmail.com>
2018-07-24 05:27:06 -0400

    completed fileio.py

--- src/fileio.py
+++ src/fileio.py
@@ -1,12 +1,26 @@
 # Use open to open file "foo.txt" for reading
 
+file = open('foo.txt', 'r')
+
 # Print all the lines in the file
+
+for line in file:
+  print(line)
 
 # Close the file
 
+file.close()
 
 # Use open to open file "bar.txt" for writing
 
+file = open('bar.txt', 'w')
+
 # Use the write() method to write three lines to the file
 
+file.write('I like spicy foods. \n')
+file.write('I also like Python! \n')
+file.write( 'It is a bit of a pain with indentation though...\n')
+
 # Close the file
+
+file.close()
