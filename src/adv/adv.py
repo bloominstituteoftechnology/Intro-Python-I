d8b0ff9a828a3e04e3ef2a35849b12022f6d31f0
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2018-07-24 15:39:39 -0500

    Worked on adv.py got player and room created working on location

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -1,25 +1,18 @@
+import os
 from room import Room
-
+from player import Player 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    # outside: {name: "Outside cave enterance", description: "North of you, the cave mouth beckons", n_to: {POINTS TO FOYER}}
+    'outside':  Room("Outside Cave Entrance","North of you, the cave mount beckons"),
+    'foyer':    Room("Foyer","Dim light filters in from the south. Dusty passages run north and east."),
+    'overlook': Room("Grand Overlook","A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."),
+    'narrow':   Room("Narrow Passage","The narrow passage bends here from west to north. The smell of gold permeates the air."),
+    'treasure': Room("Treasure Chamber","You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
+    # Add my own room
 }
+#debugging
 
 
 # Link rooms together
@@ -36,10 +29,22 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
+print(room['outside'].s_to)
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player('Jackee', room['outside'])
+direction = None
 # Write a loop that:
+while(True):
+    #print current room name
+    #debug 
+    os.system('cls' if os.name == 'nt' else 'clear')
+   
+    print(
+        '\n You are currently here:', player.location.name,
+        '\n Description:', player.location.description)
+    direction = input('\nWhich way would you like to go?\n(N, E, S, W) pick one:').lower()
+    player.move_to(direction)
+    
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
