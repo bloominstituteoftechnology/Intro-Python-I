2d1c3a43262896de60307acb899692c8282e8f54
jeffrey <jeffreyneilflynn@gmail.com>
2018-07-24 18:22:53 -0400

    room class cleanup

--- src/adv/room.py
+++ src/adv/room.py
@@ -8,9 +8,3 @@ class Room:
     self.s_to = None
     self.w_to = None
     self.e_to = None
-<<<<<<< HEAD
-
-  def __repr__(self):
-    return "%s" % (self.name)
-=======
->>>>>>> 0ab68d6bc3a3d95fde8b9dc6c86efe2fa46f8f91
