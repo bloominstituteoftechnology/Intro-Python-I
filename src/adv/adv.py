a60977a9fd356aa8e6ddb8517f031aa6bbfdbbb7
Cruise Brantley <cruise.brantley@gmail.com>
2018-07-24 15:25:33 -0500

    Classes set up, sets up initial player state, print's current position

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,6 +39,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player('outside')
 
 # Write a loop that:
 #
@@ -49,3 +51,7 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+# while True:
+print(room.get(player.position()))
+
