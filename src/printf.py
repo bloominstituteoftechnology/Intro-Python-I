87630df7f407e552690c497584eba13a4c66b4bb
totsuya <supertatortots@gmail.com>
2018-07-24 04:44:46 -0400

    finished printf

--- src/printf.py
+++ src/printf.py
@@ -6,5 +6,8 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print('%d %f %s' % (x, y, z))
 
 # Use the 'format' string method to print the same thing
+
+print('{} {} {}'.format(x, y, z))
