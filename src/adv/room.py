bb57ccfdcb01b01e0af1a245bedd92fa379e8ec2
Justin M <jekm321@gmail.com>
2018-07-24 18:25:29 -0400

    loop implemented, error proofed? xD

--- src/adv/room.py
+++ src/adv/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, room_name = "", room_description = ""):
+        self.room_name = room_name
+        self.room_description = room_description
+
+    def __repr__(self):
+        return "{} \nRoom Description: {}".format(self.room_name, self.room_description)
+
+    def __str__(self):
+        return "{} \nRoom Description: {}".format(self.room_name, self.room_description)
