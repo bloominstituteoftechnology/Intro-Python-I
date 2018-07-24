c7b6cb8dca1c3a80c3fa149e415ee96436936792
cgoez <goez2001@gmail.com>
2018-07-23 23:36:12 -0700

    fileio.py

--- src/fileio.py
+++ src/fileio.py
@@ -1,12 +1,14 @@
 # Use open to open file "foo.txt" for reading
-
 # Print all the lines in the file
-
 # Close the file
-
+with open("foo.txt") as x:
+    read_lines = x.read()
+    print(read_lines)
+x.closed
 
 # Use open to open file "bar.txt" for writing
-
 # Use the write() method to write three lines to the file
-
 # Close the file
+with open("bar.txt", "w+") as y:
+    write_lines = y.write("Python\nIs\nNeat\n")
+y.closed
