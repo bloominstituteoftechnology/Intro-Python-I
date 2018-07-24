09f3a8672720f4081b2e15b3b0248abe1b7be8cf
totsuya <supertatortots@gmail.com>
2018-07-24 05:05:09 -0400

    finished slice

--- src/slice.py
+++ src/slice.py
@@ -1,26 +1,28 @@
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+print(a[1])
 
 # Output the second-to-last element: 9
-print()
+print(a[-2])
+#a[4] would not work due to needing always the second last element
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+print(a[-3:])
+#using the : basically means "x to the end", so in this scenario, it means "starting from the third to last element, keep counting until the end"
 
 # Output the two middle elements in the array: [1, 7]
-print()
+print(a[2:4])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+print(a[1:])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+print(a[:-1])
 
 # For string s...
 
 s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
-print()
+print(s[7:12])
