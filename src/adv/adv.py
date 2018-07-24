7a4af5011d865cbf74c7b9c733e2309114e8e2ad
Moises Dobarganes <designerexpert@gmail.com>
2018-07-24 18:37:38 -0400

    Added inventory to rooms

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -9,14 +9,18 @@ from player import Player
 
 room = {
     #outside:   { name: "Outside Cave Entrance", description: "North of you, the cave mount beckons", n_to: {POINTS TO FOYER}}
-    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "Dandelions grow on the outside of the cave entrance. North of you, the cave mount beckons", inventory=['dandelions']),
     'foyer':    Room("Foyer","Dim light filters in from the south. Dusty passages run north and east."),
     'overlook': Room("Grand Overlook","A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."),
     'narrow':   Room("Narrow Passage","The narrow passage bends here from west to north. The smell of gold permeates the air."),
     'treasure': Room("Treasure Chamber","You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
     # Add your own room here
     'corridor': Room("Main Corridor", "You are located at a narrow corridor. West of you a dim light can be seen. East of you is the foyer"),
-    'gallery': Room("Grand Gallery", "The passage opens up to a very large gallery with rocks hanging from above, and a piercing hole in the center where light rays seep into the darkness of the caverns. At first the room appeared as a dead end because of the blinding light hiding the shadowy corners. To the South lies the same narrow corridor. But as your eyes adjust to the darkness of the caverns you notice on the far East of the room there seems to be a shining golden light coming from a hole in the ground leading into another cavity."),
+    'gallery': Room(
+                    "Grand Gallery", 
+                    "The passage opens up to a very large gallery with rocks hanging from above, and a piercing hole in the center where light rays seep into the darkness of the caverns. At first the room appeared as a dead end because of the blinding light hiding the shadowy corners. To the South lies the same narrow corridor. But as your eyes adjust to the darkness of the caverns you notice on the far East of the room there seems to be a shining golden light coming from a hole in the ground leading into another cavity.",
+                    inventory=['Sword','Lamp','Amulet']
+                    ),
 }
 
 # Link rooms together
@@ -45,6 +49,7 @@ while(True):
     direction = None
     print(
         '\nYou are located at:','\n'.join(wrap(player.location.name, width=50)),
-        '\nDescription:', '\n'.join(wrap(player.location.description, width=50)))
+        '\nDescription:', '\n'.join(wrap(player.location.description, width=50)),
+        '\n\nItems at this location:', '\n'.join(wrap(', '.join(player.location.inventory), width=50)))
     direction = input("\nIn what direction do you wish to proceed\n(N,E,W,S) or (Q: for quitting) pick one: ").lower()
     player.move_to(direction)
--- src/adv/room.py
+++ src/adv/room.py
@@ -1,8 +1,12 @@
 class Room:
-    def __init__(self, name, description, n_to=None, e_to=None, w_to=None, s_to=None,):
+    def __init__(self, name, description, n_to=None, e_to=None, w_to=None, s_to=None, inventory=None):
         self.name = name
         self.description = description
         self.n_to = n_to
         self.e_to = e_to
         self.w_to = w_to
         self.s_to = s_to
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
