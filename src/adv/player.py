f756aaed3c21400a52269e2527441d9a46216a49
Anthony <anthony@anthonys-mbp.lan>
2018-07-24 11:47:23 -0400

    Created Player Class
    Added a class that accepts the player name and the room they are in. Created a method on the class that will return a string indicating the player name and room when invoked.

--- src/adv/player.py
+++ src/adv/player.py
@@ -4,3 +4,10 @@ class Player:
     def __init__(self, playerName, room):
         self.playerName = playerName
         self.room = room
+
+    def playerDetails(self):
+        return 'Player {} is in the {}'.format(self.playerName, self.room)
+
+player_1 = Player('Anthony', 'Bedroom')
+
+print(player_1.playerDetails())
