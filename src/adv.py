7fcb924a45830f66e73dffd177861ff9cd27791e
ajf412 <aaronjfaulkner@yahoo.com>
2018-07-23 22:31:43 -0400

    NOW mvp is met.

--- src/adv.py
+++ src/adv.py
@@ -3,63 +3,8 @@
 
 # These are the existing rooms. Add more as you see fit.
 import textwrap
-
-rooms = {
-    "outside": {
-        "name": "Outside Cave Entrance",
-        "description": "North of you, the cave mouth beckons.",
-        "n_to": "foyer",
-    },
-
-    "foyer": {
-        "name": "Foyer",
-        "description": "Dim light filters in from the south. Dusty passages run north and east.",
-        "n_to": "overlook",
-        "s_to": "outside",
-        "e_to": "narrow",
-    },
-
-    "overlook": {
-        "name": "Grand Overlook",
-        "description": """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""",
-        "s_to": "foyer",
-    },
-
-    "narrow": {
-        "name": "Narrow Passage",
-        "description": "The narrow passage bends here from west to north. The smell of gold permeates the air.", 
-        "w_to": "foyer",
-        "n_to": "treasure",
-    },
-
-    "treasure": {
-        "name": "Treasure Chamber",
-        "description": """You've found the long-lost treasure
-chamber. Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""",
-        "s_to": "narrow",
-    },
-
-}
-
-""" template room to copy into code
-    "room": {
-        "name": "",
-        "description": "",
-        "n_to": "",
-        "s_to": "",
-        "e_to": "",
-        "w_to": "",
-    },
-"""
-
-# Write a class to hold player information, e.g. what room they are in currently
-class Player:
-    def __init__(self, location, hp):
-        self.location = location
-        self.hp = hp
+from room import rooms
+from player import Player
 
 #
 # Main
---
+++ src/player.py
@@ -0,0 +1,4 @@
+class Player:
+  def __init__(self, location, hp):
+    self.location = location
+    self.hp = hp
