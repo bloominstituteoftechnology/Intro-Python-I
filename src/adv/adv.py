105e7fa5dae96265ace6fe797cd8b13dc8c998dd
Moises Dobarganes <designerexpert@gmail.com>
2018-07-24 18:21:26 -0400

    ADV Stretch - Added 2 rooms: connected rooms added

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -15,6 +15,8 @@ room = {
     'narrow':   Room("Narrow Passage","The narrow passage bends here from west to north. The smell of gold permeates the air."),
     'treasure': Room("Treasure Chamber","You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
     # Add your own room here
+    'corridor': Room("Main Corridor", "You are located at a narrow corridor. West of you a dim light can be seen. East of you is the foyer"),
+    'gallery': Room("Grand Gallery", "The passage opens up to a very large gallery with rocks hanging from above, and a piercing hole in the center where light rays seep into the darkness of the caverns. At first the room appeared as a dead end because of the blinding light hiding the shadowy corners. To the South lies the same narrow corridor. But as your eyes adjust to the darkness of the caverns you notice on the far East of the room there seems to be a shining golden light coming from a hole in the ground leading into another cavity."),
 }
 
 # Link rooms together
@@ -23,6 +25,11 @@ room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
+room['foyer'].w_to = room['corridor']
+room['corridor'].e_to = room['foyer']
+room['corridor'].n_to = room['gallery']
+room['gallery'].s_to = room['corridor']
+room['gallery'].e_to = room['treasure']
 room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
