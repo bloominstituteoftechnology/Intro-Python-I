d95ccf333b226d765e32922bac40cf548b74c390
K <kkhaag@gmail.com>
2018-07-24 16:37:46 -0600

    Mostly working

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -7,18 +8,18 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+                     passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+                     into the darkness. Ahead to the north, a light flickers in
+                     the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+                     to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+                     chamber! Sadly, it has already been completely emptied by
+                     earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -38,9 +39,41 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player1 = Player(room['outside'])
 
 # Write a loop that:
 #
+complete = False
+
+while not complete:
+
+    print(player1.currRoom.room_name)
+    print(player1.currRoom.description)
+    
+    user_action = raw_input("Choose a direction to go (n, e, s, w) ")
+
+    if user_action is 'q':
+        print("\n goodbye \n")
+        # complete = True
+        break
+    
+    directions = {
+        "n": player1.currRoom.n_to,
+        "e": player1.currRoom.e_to,
+        "s": player1.currRoom.s_to,
+        "w": player1.currRoom.w_to
+    }
+
+
+    if directions[user_action] is None:
+        print("\nYou cannot go that way\n")
+
+    else:
+        player1.setRoom(directions[user_action])
+
+print()
+
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
--- src/adv/player.py
+++ src/adv/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, currRoom):
+        self.currRoom = currRoom
+
+    def setRoom(self, new_room):
+        self.currRoom = new_room
