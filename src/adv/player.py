a02ba7cfbf866d5244544372b4e2217d3e8010c7
Devon Bacon <devon.bacon@gmail.com>
2018-07-24 13:42:19 -0700

    player.py - added move method to Player class

--- src/adv/player.py
+++ src/adv/player.py
@@ -6,4 +6,25 @@ class Player:
     self.name = name
     self.current_room = current_room
 
+  def move(direction):
+    if direction == 'north' or direction == 'n':
+      if hasattr(self.current_room, 'n_to'):
+        self.current_room = self.current_room.n_to
+
+    elif direction == 'east' or direction == 'e':
+      if hasattr(self.current_room, 'e_to'):
+        self.current_room = self.current_room.e_to
+
+    elif direction == 'south' or direction == 's':
+      if hasattr(self.current_room, 's_to'):
+        self.current_room = self.current_room.s_to
+      
+    elif direction == 'west' or direction == 'w':
+      if hasattr(self.current_room, 'w_to'):
+        self.current_room = self.current_room.w_to
+
+    else:
+      print('I\'m sorry, it looks like there is no path in that direction.')
+    
+    print(self.current_room.description)
     
