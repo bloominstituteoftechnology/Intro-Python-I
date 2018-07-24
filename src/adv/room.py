8c7b72b6e0370d5a00f884b3dbda87b9e0589aa8
totsuya <supertatortots@gmail.com>
2018-07-24 15:26:28 -0400

    created basic room class

--- src/adv/room.py
+++ src/adv/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+  
+  def examine (self):
+    return "Name of room:[ {} ] {}".format(self.name, self.description)
