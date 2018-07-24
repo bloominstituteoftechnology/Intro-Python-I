e2b973f7816598160b4c12254c91dd74c72072ce
sagdish <forsi1@yahoo.com>
2018-07-24 15:43:11 -0700

    working on first day objectives

--- src/modules.py
+++ src/modules.py
@@ -6,26 +6,25 @@ import os
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-
+for arg in sys.argv:
+    print(f'CL argument: {arg}')
 
 # Print out the plaform from sys:
-print()
+print(f'platform: {sys.platform}')
 
 # Print out the Python version from sys:
-print()
-
-
+print(f'Python version: {sys.version}')
 
 # Module "os"
 #
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-print()
+print(f'current ID: {os.getgid()}')
 
 # Print the current working directory (cwd):
-print()
+print(f'current working directory: {os.getcwd()}')
 
 # Print your login name
-print()
+print(f'name of the user: {os.getlogin()}')
 
