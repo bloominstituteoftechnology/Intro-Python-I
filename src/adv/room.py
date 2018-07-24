0b4338f8be565da77e76dfbdfd8a2bc181807925
2940cristian <2940cristian@gmail.com>
2018-07-24 10:14:50 -0700

    Implemented room.py

--- src/adv/room.py
+++ src/adv/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, roomName, summary):
+        self.roomName = roomName
+        self.summary = summary
+
+
+    def printRoom(self):
+        return f"The {self.roomName} is {self.summary}"
+
+
+test = Room("Kitchen", "is for cooking food")
+
+print(test.printRoom())
