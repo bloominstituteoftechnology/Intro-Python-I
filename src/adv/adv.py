408239f3bae28a274b5f823fa39aadfbe7a4463d
jeffrey <jeffreyneilflynn@gmail.com>
2018-07-24 18:40:47 -0400

    added command option for mac users

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -31,7 +31,7 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
-system("cls")
+system("cls" or "clear")
 
 player_name = input("Enter a player name: ")
 
