46072ef9f215ad65f417cf711fe015527915202e
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-23 20:10:07 -0400

    Completed comp.py

--- src/comp.py
+++ src/comp.py
@@ -1,6 +1,7 @@
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
+[y.append(i) for i in range(1, 6)]
 
 print (y)
 
@@ -8,6 +9,7 @@ print (y)
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+[y.append(pow(i, 3)) for i in range(0, 10)]
 
 print(y)
 
@@ -17,16 +19,18 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
+[y.append(word.upper()) for word in a]
 
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ")
+x = input("Enter comma-separated numbers: ").split(", ")
 
 # What do you need between the square brackets to make it work?
 y = []
+[y.append(even) for even in x if (int(even) % 2 == 0)]
 
 print(y)
 
