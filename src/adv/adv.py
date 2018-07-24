3de997c08133a4f1c8f25f6f86c1152650e04587
Cruise Brantley <cruise.brantley@gmail.com>
2018-07-24 18:01:17 -0500

    finished formatting and works!

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -1,23 +1,24 @@
 from room import Room
 from player import Player
+import os
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'outside':  Room("outside the Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("in the Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("at the Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("in the Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("in the Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -39,7 +40,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player('outside')
+player = Player(room['outside'])
 
 # Write a loop that:
 #
@@ -51,7 +52,38 @@ player = Player('outside')
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+direction = None
+os.system("cls")
+while True:
+    flag = 0
+    print(player.room)
 
-# while True:
-print(room.get(player.position()))
-
+    # switch statement... kind of
+    direction = input("Where would you like to go? ")
+    if direction == "north":
+        if player.room.n_to:
+            os.system("cls")
+            choice = player.room.n_to
+            flag = 1
+    if direction == "south":
+        if player.room.s_to:
+            os.system("cls")
+            choice = player.room.s_to
+            flag = 1
+    if direction == "east":
+        if player.room.e_to:
+            os.system("cls")
+            choice = player.room.e_to
+            flag = 1
+    if direction == "west":
+        if player.room.w_to:
+            choice = player.room.w_to
+            flag = 1
+    if direction == "exit":
+        os.system("cls")
+        break
+    if flag == 0:
+        os.system("cls")
+        print("You shall not pass!\n")
+    if flag == 1:
+        player.room = choice
