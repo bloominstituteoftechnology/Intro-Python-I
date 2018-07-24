8720b632e21c8457d3c46463ff136054d7ea3f0e
Yasin Shuman <email@yasin.io>
2018-07-23 18:53:15 -0700

    fileio done

--- src/fileio.py
+++ src/fileio.py
@@ -1,12 +1,14 @@
 # Use open to open file "foo.txt" for reading
-
+userFile = open('foo.txt', 'r')
 # Print all the lines in the file
-
+print(userFile.read())
 # Close the file
 
-
+userFile.close()
 # Use open to open file "bar.txt" for writing
-
+userFile2 = open('bar.txt', 'w')
 # Use the write() method to write three lines to the file
-
+userFile2.write(
+    'Ride with the mob\nAlhamdulallah\nCheck in with me, and do ya job')
 # Close the file
+userFile2.close()
