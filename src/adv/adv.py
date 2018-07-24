e4c22057ca104cccb1ae79fe82e5f9e4649eec83
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-24 19:40:51 -0400

    Added new name input to make the game more personal

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -39,7 +39,8 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player('Kevin', 'outside')
+name = input("Enter your name:\n")
+player = Player(name, 'outside')
 
 # Write a loop that:
 #
