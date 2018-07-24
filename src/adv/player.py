da4c9bee5ad2038fe38880bdbffec947f0cb28cb
totsuya <supertatortots@gmail.com>
2018-07-24 15:41:13 -0400

    added wip player class

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+  def __init__(self, room)
+   self.room = room
+
+  def playerLocation(self):
+    return "You are at {}!".format(self.name)
+
+  def playerMove(self):
+
+
+playerCharacter = Player("outside")
