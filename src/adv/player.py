772545fd0d61828fd19431d32bc71ab42ef9ad0c
Jared Cuffe <jcuffe@gmail.com>
2018-07-24 13:46:59 -0700

    Player class implementation

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, playerName, currentRoom):
+        self.playerName = playerName
+        self.currentRoom = currentRoom
+
+    def __str__(self):
+        return '{} is in the {}.'.format(self.playerName, self.currentRoom)
