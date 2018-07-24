64750b8084b422199b4f0f570ed2af9ef75e45a4
JustinSHong <jhong2009@gmail.com>
2018-07-24 13:57:55 -0700

    in adv.py: simplified names of directions to single char

--- src/adv.py
+++ src/adv.py
@@ -8,15 +8,15 @@ rooms = {
     "outside": {
         "name": "Outside Cave Entrance",
         "description": "North of you, the cave mouth beckons.",
-        "n_to": "foyer",
+        "n": "foyer",
     },
 
     "foyer": {
         "name": "Foyer",
         "description": "Dim light filters in from the south. Dusty passages run north and east.",
-        "n_to": "overlook",
-        "s_to": "outside",
-        "e_to": "narrow",
+        "n": "overlook",
+        "s": "outside",
+        "e": "narrow",
     },
 
     "overlook": {
@@ -24,14 +24,14 @@ rooms = {
         "description": """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm.""",
-        "s_to": "foyer",
+        "s": "foyer",
     },
 
     "narrow": {
         "name": "Narrow Passage",
         "description": "The narrow passage bends here from west to north. The smell of gold permeates the air.", 
-        "w_to": "foyer",
-        "n_to": "treasure",
+        "w": "foyer",
+        "n": "treasure",
     },
 
     "treasure": {
@@ -39,7 +39,7 @@ the distance, but there is no way across the chasm.""",
         "description": """You've found the long-lost treasure
 chamber. Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""",
-        "s_to": "narrow",
+        "s": "narrow",
     },
 
 }
@@ -48,10 +48,10 @@ earlier adventurers. The only exit is to the south.""",
     "room": {
         "name": "",
         "description": "",
-        "n_to": "",
-        "s_to": "",
-        "e_to": "",
-        "w_to": "",
+        "n": "",
+        "s": "",
+        "e": "",
+        "w": "",
     },
 """
 
