e5e9bb7839723ffe271050dc6f53310417d4aded
2940cristian <2940cristian@gmail.com>
2018-07-24 10:19:32 -0700

    Player class done

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, player, room):
+        self.player = player
+        self.room = room
+
+    def printPlayer(self):
+        return f"{self.player} is in the {self.room}"
+
+
+test = Player("Bob", "kitchen")
+
+print(test.printPlayer())
