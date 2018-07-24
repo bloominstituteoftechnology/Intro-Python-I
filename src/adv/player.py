a277ba82358b891d15bb8403a62928de3fce6e5b
Anthony <anthony@anthonys-mbp.lan>
2018-07-24 11:43:24 -0400

    Added Room Class
    Created Room class that accepts a room name and a description, and gave it a method that will return the room name and description in a statement when invoked.

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,1 +1,6 @@
 # Write a class to hold player information, e.g. what room they are in currently.
+
+class Player:
+    def __init__(self, playerName, room):
+        self.playerName = playerName
+        self.room = room
--- src/adv/room.py
+++ src/adv/room.py
@@ -1,2 +1,15 @@
-#Implement a class to hold room information. This should have name and
+# Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, roomName, description):
+       self.roomName = roomName
+       self.description = description
+
+    # Method to print roomName and description 
+    def roomDetails(self):
+        return 'The {} is {}'.format(self.roomName, self.description)
+
+room_1 = Room('Bathroom', 'Blue')
+
+print(room_1.roomDetails())
