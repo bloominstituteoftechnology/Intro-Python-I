f8d89a7084da979c8f6ed0589395398027d74712
Devon Bacon <devon.bacon@gmail.com>
2018-07-24 13:10:25 -0700

    adv.py - imported Player from player.py for later use

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,6 +39,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player()
 
 # Write a loop that:
 #
