fca82715bbb17ca0cfee715e74d71976accd208e
cgoez <goez2001@gmail.com>
2018-07-24 14:38:01 -0700

    printf.py fix string format

--- src/printf.py
+++ src/printf.py
@@ -7,8 +7,10 @@ z = "I like turtles!"
 # x is 10, y is 2.25, z is "I like turtles!"
 print("x is %s," % x + " y is %.2f," %y + " z is '%s'" % z)
 
-
-# Use the 'format' string method to print the same thing
 data = (x, y, z)
 format_string = "x is %s, y is %.2f, z is '%s'"
 print(format_string % data)
+
+
+# Use the 'format' string method to print the same thing
+print(f"x is {x}, y is {round(y, 2)}, z is '{z}'")
