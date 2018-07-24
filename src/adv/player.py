7b927f33e9eca96d21879275dbaf9a79112803ef
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-07-24 18:54:07 -0400

    working on text game

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class playerInfo:
+   def __init__(self, room):
+        self.room = room
+
+    def position(self):
+        return self.room
--- src/adv/room.py
+++ src/adv/room.py
@@ -1,2 +1,9 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class room:
+  def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return "You are currently " + self.name + '\n' + self.description]
