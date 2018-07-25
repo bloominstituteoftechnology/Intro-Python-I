55a9d8ade30b29167930a9e39fcf461e5cc6e259
Jacob Leon Lyerla <umasour@yahoo.com>
2018-07-25 15:44:25 -0700

    added in a bag size

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,8 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self,playerName,start,inventory):
+    def __init__(self,playerName,start,inventory,bagSize):
         self.current = start;
         self.name = playerName;
         self.inventory =inventory
+        self.bagSize = bagSize
 
