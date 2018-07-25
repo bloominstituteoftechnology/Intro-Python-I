6fb8846d02eb8d009d1277ee16b01d49fb0be6c6
JustinSHong <jhong2009@gmail.com>
2018-07-25 12:18:21 -0700

    set up player class with room, inventory, name data

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+	def __init__(self, playername, room):
+		self.room = room
+		self.inventory = []
+		self.name = playername
