e006260738a8a90ba49fdd001d192bfd1c5a3929
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-07-25 16:28:03 -0700

    adv.py - Implemented commands for taking items/inventory

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -10,25 +10,18 @@ room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons", [Item("Lantern", "A Lantern to light your way")]),
 
-    'foyer':    Room("\nFoyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("\nFoyer", """Dim light filters in from the south. Dusty passages run north and east."""),
 
-    'overlook': Room("\nGrand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("\nGrand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("\nNarrow Passage", """The narrow passage bends here from west
-to north and south. The smell of gold permeates the air."""),
+    'narrow':   Room("\nNarrow Passage", """The narrow passage bends here from west to north and south. The smell of gold permeates the air."""),
 
-    'treasure': Room("\nTreasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south, but you smell something from the North.""", [Item("Shield", "A shield made of sturdy steel")])),
+    'treasure': Room("\nTreasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south, but you smell something from the North.""", [Item("Shield", "A shield made of sturdy steel")]),
     # Rm I added on for fun
-    'dungeon': Room("\nThe Dungeon", """You've entered the dark, smelly dungeon of doom!
-Now you are cursed for 10 million years and forever the
-caregiver for these smelly pugs. Exit to the east.""", [Item("Satchel", "A satchel made of pig skin to carry your loot")])),
+    'dungeon': Room("\nThe Dungeon", """You've entered the dark, smelly dungeon of doom! Now you are cursed for 10 million years and forever the
+caregiver for these smelly pugs. Exit to the east.""", [Item("Satchel", "A satchel made of pig skin to carry your loot")]),
     # Rm I added on for fun
-    'pugcave': Room("\nPugs Cave", """Oh No!!! You've awoken the pug beasts... RUN for your lives to the only exit north!""", [Item("Fang", "A smelly pug fang for goodluck")])),
+    'pugcave': Room("\nPugs Cave", """Oh No!!! You've awoken the pug beasts... RUN for your lives to the only exit north!""", [Item("Fang", "A smelly pug fang for goodluck")]),
 }
 
 
@@ -72,29 +65,50 @@ print("\nWelcome, %s!" % (player.playerName)) # Welcomes player by name they ent
 # If the user enters "q", quit the game.
 
 def dircheck(attr):
-    if(hasattr(player.room, attr)):
+    if hasattr(player.room, attr):
         player.room = getattr(player.room, attr)
     else:
         print("\nYou Shall Not Pass\n")
 
 while True:
-    print(player.room.name)
+    print("\n* " + player.room.name + " *")
     print(textwrap.fill(player.room.description, 50))
-    cmd = input("\nWhich Direction Would You Like To Go? \n(n, s, e, w or q to quit):")
-    if cmd == "q": # Q = quit
-        break
-    elif cmd in ["n", "e", "s", "w"]:
-        dircheck(cmd + "_to")
-    # elif cmd == "N": # N = North
-    #     dircheck("n_to")
-    # elif cmd == "E": # E = East
-    #     dircheck("e_to")
-    # elif cmd == "S": # S = South
-    #     dircheck("s_to")
-    # elif cmd == "W": # W = West
-    #     dircheck("w_to")
+    print("Items: " + (",".join([item.name for item in player.room.items]) or "None") + "\n")
+    
+    cmd = input("\nWhich Direction Would You Like To Go? \n(n, s, e, w or q to quit): ").strip().lower()
+    cmds = cmd.split(" ")
+    if len(cmds) == 1:
+        if cmd == "q" or cmd == "quit":
+            break
+        elif cmd in ["n", "e", "s", "w"]:
+            dircheck(cmd + "_to")
+        elif cmd == "i" or cmd == "inventory":
+            print("\nInventory:\n" + "\n".join([item.name + " - " + item.description for item in player.inventory]))
+        else:
+            print("\nSorry, Wrong Command - Try Again") 
+    elif len(cmds) == 2:
+        verb = cmds[0]
+        obj = cmds[1]
+        if verb == "get" or verb == "take":
+            if obj in [item.name.lower() for item in player.room.items]:
+                for i, o in enumerate(player.room.items):
+                    if o.name.lower() == obj:
+                        player.inventory.append(o)
+                        del player.room.items[i]
+                        break
+            else: 
+                print("\nSorry - Item Not Found In This Room")
+        elif verb == "drop":
+            if obj in [item.name.lower() for item in player.inventory]:
+                for i, o in enumerate(player.inventory):
+                    if o.name.lower() == obj:
+                        player.room.items.append(o)
+                        del player.inventory[i]
+                        break
+            else:
+                print("\nSorry - Item Not Found In Your Inventory")
     else:
-        print("\nWrong Way - Try Again\n")
+        print("\nSorry, Wrong Command - Try Again")
 
 
 # Instructor Solve------------------------------------
