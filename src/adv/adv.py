41aa2182201a432c5ec7e1f71bd2232a00b3babf
Justin M <jekm321@gmail.com>
2018-07-25 17:47:55 -0400

    updated logic path and instructions for inspecting items

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -18,19 +18,19 @@ room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons."),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north 
-and east. """, [item["flower"], item["stick"], item['torch']]),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. 
+Dusty passages run north and east. """, [item["flower"], item["stick"], item['torch']]),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", [item["rope"], item["plank"]]),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, 
+falling into the darkness. Ahead to the north, a light flickers in the distance, 
+but there is no way across the chasm.""", [item["rope"], item["plank"]]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", [item["coin"], item["sword"]]),
+chamber! Sadly, it has already been completely emptied by earlier adventurers. 
+The only exit is to the south.""", [item["coin"], item["sword"]]),
 }
 
 
@@ -69,12 +69,13 @@ print('Loading...')
 print('Now Playing: \'Room Explorer.txt\'')
 print('You will be starting out outside. In order to move around type in (n)orth (s)outh (e)ast or (w)est.')
 print('Typing info will allow you to see what items are in the room.')
-print('To keep track of your current items, typing (i)nventory will remind you what you have.')
 print('If you wish to pick up or drop an item, type (t)ake or (d)rop followed by item name.')
+print('To keep track of your current items, typing (i)nventory will remind you what you have.')
+print('Once an item is in your inventory, typing (ins)pect will read you its description')
 print('When you are done exploring, type q to exit.')
 while(playing):
     print(f"\n{character}")
-    request = input("What would you like to do?\n").lower().split(' ')
+    request = input("What would you like to do?\n\n").lower().split(' ')
 
     if len(request) == 1:
         if request[0] == 'q' or request[0] == 'quit':
@@ -113,7 +114,6 @@ while(playing):
                 print('\nInvalid Input')
     elif len(request) == 2:
         if request[0] == 'take' or request[0] == 't':
-            print(request)
             taken_item = currentLocation.item_found(request[1])
             if taken_item:
                 character.pick_up(taken_item)
@@ -125,6 +125,12 @@ while(playing):
             if dropped_item:
                 currentLocation.item_dropped(dropped_item)
                 print(f"\n{dropped_item} has been dropped!")
+            else:
+                print('\nInvalid Input')
+        elif request[0] == 'inspect' or request[0] == 'ins':
+            descript = character.inspect(request[1])
+            if descript:
+                print(f'\n{descript}')
             else:
                 print('\nInvalid Input')
         else:
