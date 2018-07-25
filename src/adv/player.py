449c2f9a50cdbe6e0d71117ace601e1ce2d7b369
Yasin Shuman <email@yasin.io>
2018-07-24 17:40:09 -0700

    trying a simpler approach

--- src/adv/player.py
+++ src/adv/player.py
@@ -9,13 +9,3 @@ class Player:
     def __init__(self, name, location):
         self.name = name
         self.location = location
-
-    def move_to(self, direction):
-        cardinal = direction + '_to'
-    # if hasattr(self.location, 'cardinal'):
-    #     #move towards it --->
-    #     self.location = self.location.__dict__[cardinal]
-    # else:
-    #     os.system('cls' if os.name == 'nt' else 'clear')
-    #     print('\nPick a different option, You cannot go that way...')
-    #     sleep(5)
--- src/adv/room.py
+++ src/adv/room.py
@@ -6,7 +6,3 @@ class Room:
     def __init__(self, name, description, s_to=None, n_to=None, e_to=None, w_to=None):
         self.name = name
         self.description = description
-        self.s_to = s_to
-        self.n_to = n_to
-        self.e_to = e_to
-        self.w_to = w_to
