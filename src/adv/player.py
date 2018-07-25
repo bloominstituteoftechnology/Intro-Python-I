852b89404aa8ecd7f92900413d50f8bd605447df
Jacob Leon Lyerla <umasour@yahoo.com>
2018-07-25 14:06:11 -0700

    set up some stuff for adv mostly just made my console pretty

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,7 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self,playerName,start):
+    def __init__(self,playerName,start,inventory):
         self.current = start;
         self.name = playerName;
+        self.inventory =inventory
 
