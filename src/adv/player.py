2e4b23a06cfc30492f5554d7a1256660c105bd25
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-25 16:43:14 -0400

    Separated creating an item and placing into the room. Extended an inventory to the player to add items found in the cave

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,7 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+#
+# Add capability to add `Item`s to the player's inventory. The inventory can
+# also be a `list` of items "in" the player, similar to how `Item`s can be in a
+# `Room`.
+
+from room import Room
 
 class Player():
     def __init__(self, name, room):
         self.name = name
         self.room = room
+        self.inventory = []
+    
+    def toInventory(self, item):
+        inventory.append(item)
