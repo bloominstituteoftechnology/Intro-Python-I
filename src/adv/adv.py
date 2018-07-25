9d16805f4b9a72988d4e67df7e2c9d6451a68c95
Shawn Stewart <s.stewart0220@gmail.com>
2018-07-25 19:13:21 -0400

    Created a message formatting function and added commands
    Created formatMsg() to give the messages a pretty border. Added a 'help' command to output all possible commands. Added 'i' command to output the player's inventory. Added 'q' command to quit the game.

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -1,6 +1,9 @@
 from room import Room
 from player import Player
+from commandslist import Commands
+import math
 import textwrap
+import os
 
 # Declare all the rooms
 
@@ -40,8 +43,57 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player1 = Player(room['outside'])
+player1 = Player(input("Enter a username: ").strip()[0:20], room['outside'])
 GamePlaying = True
+message = None
+
+# help functions
+def formatMsg(msg):
+	res = ""
+	border = ""
+
+	# is a list
+	if type(msg) is list:
+		# list is of dicts
+		if type(msg[0]) is dict:
+			print(len(msg[0].values()))
+
+		# longest in the list
+		length = len(max(msg, key=len)) + 6
+
+		#sizes border
+		for n in range(length + 2):
+			border += "*"
+
+		res += border
+		for line in msg:
+			res += "\n*"
+			# separator
+			if line == '=':
+				res += " "
+				for s in range(length - 2):
+					res += '='
+				res += " "
+			# formats line
+			else:
+				spaceAmt = length - len(line)
+				for s in range(math.ceil(spaceAmt / 2)):
+					res += " "
+				res += line
+				for s in range(math.floor(spaceAmt / 2)):
+					res += " "
+			res += "*\n"
+		res += border
+
+		return res
+
+	# single message
+	else:
+		for n in range(len(msg) + 9):
+			border += "*"
+		
+		res = border + "\n*   " + msg + "   *\n" + border
+		return res
 
 # Write a loop that:
 #
@@ -49,37 +101,47 @@ GamePlaying = True
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 while GamePlaying == True:
-	# print("\n%s\n\n%s" % (player1.location.name, player1.location.desc))
+	os.system('cls' if os.name == 'nt' else 'clear')
 	desc = textwrap.wrap(player1.location.desc)
 
-	print("\n****************************************")
+	if message != None:
+		print(message)
 	print("\n%s\n" % player1.location.name)
 	for line in desc:
 		print(line)
-	print("\n****************************************")
 
-	direction = input("\nEnter direction: ").lower()
+	# Input from user
+	userInput = input("\nEnter direction: ").strip().lower()
+
+	print("\n===========================================================\n")
 
 	# If the user enters a cardinal direction, attempt to move to the room there.
 	# Print an error message if the movement isn't allowed.
-	options = ['n', 's', 'e', 'w']
-	getRoom = [
-		player1.location.n_to,
-		player1.location.s_to,
-		player1.location.e_to,
-		player1.location.w_to
-	]
 
-	if len(direction) == 1 and direction in options:
-		if getRoom[options.index(direction)] != None:
-			print("\nYou move to the %c" % direction)
-			player1.location = getRoom[options.index(direction)]
+	# help / commands
+	if userInput == 'h' or userInput == 'help' or userInput == 'commands':
+		message = formatMsg(Commands)
+	
+	# move player
+	elif len(userInput) == 1 and userInput in ['n', 's', 'e', 'w']:
+		# is movable to
+		if getattr(player1.location, userInput + '_to') != None:
+			player1.location = getattr(player1.location, userInput + '_to')
+			message = formatMsg("%s moved to the %c" % (player1.name, userInput))
+		
+		#is not movable to
 		else:
-			print("\nInvalid: There's nothing that direction")
-	elif direction == 'q':
-		print("\nQuitting the game...")
-		GamePlaying = False
-	else:
-		print("\nERROR: You can only move n, s, e, or w.")
+			message = formatMsg("You can't move %c" % userInput)
 
-	# If the user enters "q", quit the game.
+	# display inventory
+	elif userInput == 'i':
+		message = formatMsg(player1.inventory)
+
+	# quit game
+	elif userInput == 'q' or userInput == 'quit' or userInput == 'exit':
+		print("Quitting the game...")
+		GamePlaying = False
+
+	# invalid input
+	else:
+		message = formatMsg("Invalid input: use 'help' for commands")
