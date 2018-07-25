9bc3b43fe8049971e66c407f0d5d7669dc07cfb8
Natalie <natalieschennum@gmail.com>
2018-07-25 14:38:16 -0700

    Item functions added to player

--- src/adv/player.py
+++ src/adv/player.py
@@ -3,7 +3,20 @@
 
 
 class Player:
-    def __init__(self, playerMame, playerRoom): 
-        self.room = startRoom
+    def __init__(self, startRoom):
+        self.curRoom = startRoom
+        self.items = []
 
-  
+    def addItem(self, item):
+         self.items.append(item)
+
+    def removeItem(self, name):
+
+        item = self.findItem(name)
+        self.items.remove(item)
+
+    def findItem(self, name):
+        for item in self.items:
+            if item.name == name:
+                return item
+        return None
