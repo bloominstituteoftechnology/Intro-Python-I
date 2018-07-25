420064d654a578ead9d8742288b596ec40cf8bff
Jared Cuffe <jcuffe@gmail.com>
2018-07-25 14:22:43 -0700

    Created Item class

---
+++ src/adv/item.py
@@ -0,0 +1,7 @@
+class Item:
+    def __init__(self, name, description):
+        self.name = name    
+        self.description = description
+
+    def __str__(self):
+        return self.description
