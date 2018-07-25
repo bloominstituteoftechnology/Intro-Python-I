262f8cd1cda95b41d09df39149ce5e80857b4ad6
Harrison Brock <harrison@harrisonbrock.com>
2018-07-25 17:00:01 -0400

    CODE: player.py -- Added find and remove item

--- src/adv/player.py
+++ src/adv/player.py
@@ -8,3 +8,14 @@ class Player:
 
     def addItem(self, item):
          self.items.append(item)
+
+    def removeItem(self, name):
+
+        item = self.findItem(name)
+        self.items.remove(item)
+
+    def findItem(self, name):
+        for item in self.items:
+            if item.name == name:
+                return item
+        return None
