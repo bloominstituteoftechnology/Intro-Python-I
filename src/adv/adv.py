185406edf701e5e97980ac871034d7888f6b7b00
nickbrenn <nbren@tutanota.com>
2018-07-25 16:11:13 -0400

    Added old adv.py stuff but have to fix it to make it work

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -1,8 +1,9 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
-room = {
+rooms = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
@@ -24,20 +25,23 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+rooms['outside'].n_to = rooms['foyer']
+rooms['foyer'].s_to = rooms['outside']
+rooms['foyer'].n_to = rooms['overlook']
+rooms['foyer'].e_to = rooms['narrow']
+rooms['overlook'].s_to = rooms['foyer']
+rooms['narrow'].w_to = rooms['foyer']
+rooms['narrow'].n_to = rooms['treasure']
+rooms['treasure'].s_to = rooms['narrow']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+
+startingRoom = rooms['outside']
+player1 = Player(startingRoom)
 
 # Write a loop that:
 #
@@ -49,3 +53,41 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+playing = True
+
+while(playing is True):
+    print(player1.room)
+    direction = input("Enter a direction (n/w/e/s): ")
+    if direction+"_to" == "n_to":
+        # print("n_to")
+        for room in rooms:
+            # print(room)
+            # print(rooms[room])
+            if room == player1.room['n_to']:
+                player1.room = rooms[room]
+                break
+    elif direction+"_to" == "w_to":
+        # print("w_to")
+        for room in rooms:
+            if room == player1.room['w_to']:
+                player1.room = rooms[room]
+                break
+    elif direction+"_to" == "s_to":
+        # print("s_to")
+        for room in rooms:
+            if room == player1.room['s_to']:
+                player1.room = rooms[room]
+                break
+    elif direction+"_to" == "e_to":
+        # print("e_to")
+        for room in rooms:
+            if room == player1.room['e_to']:
+                player1.room = rooms[room]
+                break
+    elif direction == "q":
+        print("Game Ogre")
+        playing = False
+    else:
+        print("Invalid input")
+        break
--- src/adv/player.py
+++ src/adv/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, room):
+        self.room = room
