2f977ed35fc111e233e301d3056a7358f90ae586
Shawn Stewart <s.stewart0220@gmail.com>
2018-07-25 18:57:41 -0400

    Added more attributes to the player class
    Added a name, health, strength, capacity, and inventory attribute.

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,5 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-  def __init__(self, room):
+  def __init__(self, name, room):
+    self.name = name
     self.location = room
+    self.health = 100
+    self.strength = 1
+    self.capacity = 25
+    self.inventory = [
+      {'item': 'knife', 'weight': 1}
+    ]
