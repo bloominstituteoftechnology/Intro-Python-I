5138243cd8fbd8dd6ba12ed50eff08e13b3f39f1
2940cristian <2940cristian@gmail.com>
2018-07-25 09:53:15 -0700

    Added items to room.py

--- src/adv/room.py
+++ src/adv/room.py
@@ -2,19 +2,24 @@
 # description attributes.
 
 class Room:
-    def __init__(self, roomName, summary):
+    def __init__(self, roomName, summary, items):
         self.roomName = roomName
         self.summary = summary
         self.n_to = None
         self.s_to = None
         self.w_to = None
         self.e_to = None
+        self.items = items
 
 
     def printRoom(self):
         return f"The {self.roomName} is {self.summary}"
 
+    def printItems(self):
+        return f"The room has these items: {self.items}"
 
-# test = Room("Kitchen", "is for cooking food")
+    
+# test = Room("Kitchen", "is for cooking food", ["Sword", "Staff", "Knife"])
 
 # print(test.printRoom())
+# print(test.printItems())
