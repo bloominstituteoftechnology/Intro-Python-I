014bf77b477449d14e38436a7b9ac511b92ba8b7
Jared Cuffe <jcuffe@gmail.com>
2018-07-26 14:37:30 -0700

    Added helpers, refactored input handlers, encapsulated printing tasks

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -18,27 +18,23 @@ items = {
 
 # Declare the rooms
 rooms = {
-    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
-    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east.", [items['dagger']]),
-    'overlook': Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.", [items['locket']]),
-    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air."),
-    'treasure': Room("Treasure Chamber", "You've found the long-lost treasure chamber! The only exit is to the south.", [items['jewel']]),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons", True),
+    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east.", True, [items['dagger']]),
+    'overlook': Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.", True, [items['locket']]),
+    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air.", False),
+    'treasure': Room("Treasure Chamber", "You've found the long-lost treasure chamber! The only exit is to the south.", False, [items['jewel']]),
 }
 
 # Link rooms together
 
-rooms['outside'].n_to = rooms['foyer']
-rooms['foyer'].s_to = rooms['outside']
-rooms['foyer'].n_to = rooms['overlook']
-rooms['foyer'].e_to = rooms['narrow']
-rooms['overlook'].s_to = rooms['foyer']
-rooms['narrow'].w_to = rooms['foyer']
-rooms['narrow'].n_to = rooms['treasure']
-rooms['treasure'].s_to = rooms['narrow']
-
-# Flags
-show_inventory = False
-show_help = False
+rooms['outside'].connections['n'] = rooms['foyer']
+rooms['foyer'].connections['s'] = rooms['outside']
+rooms['foyer'].connections['n'] = rooms['overlook']
+rooms['foyer'].connections['e'] = rooms['narrow']
+rooms['overlook'].connections['s'] = rooms['foyer']
+rooms['narrow'].connections['w'] = rooms['foyer']
+rooms['narrow'].connections['n'] = rooms['treasure']
+rooms['treasure'].connections['s'] = rooms['narrow']
 
 
 #
@@ -46,89 +42,154 @@ show_help = False
 #
 
 def process_command(command):
-    command = command.split(" ")
-    commands = {
-        "n": move,
-        "w": move,
-        "e": move,
-        "s": move,
-        "move": move,
-        "get": take_item,
-        "take": take_item,
+    tokens = [token.lower() for token in command.split(" ")]
+
+    # Commands with no arguments
+    simple_commands = {
+        "n": lambda: move("n"),
+        "w": lambda: move("w"),
+        "e": lambda: move("e"),
+        "s": lambda: move("s"),
         "q": quit_game,
         "quit": quit_game,
-        "i": inventory,
-        "inventory": inventory,
+        "i": print_inventory,
+        "inventory": print_inventory,
+        "h": print_help,
+        "help": print_help,
+        "score": print_score
+    }
+
+    # Commands with one or more arguments
+    complex_commands = {
+        "move": move,
+        "inspect": inspect_item,
+        "get": take_item,
+        "take": take_item,
         "drop": drop_item,
-        "h": help,
-        "help": help
     }
-    if command[0] in commands:
-        commands[command[0]](command)
 
-def move(command):
-    room = player.current_room
-    directions = {
-        "n": room.n_to,
-        "north": room.n_to,
-        "e": room.e_to,
-        "east": room.e_to,
-        "s": room.s_to,
-        "south": room.s_to,
-        "w": room.w_to,
-        "west": room.w_to
-    }
-    dir = command[0] if len(command) is 1 else command[1]
-    if dir in directions and directions[dir]:
-        player.current_room = directions[dir]
+    # Handle 'it' as a stand-in for an item
+    if len(tokens) is 2 and tokens[1] == "it":
+        tokens[1] = player.last_item
+
+    if tokens[0] in simple_commands:
+        simple_commands[tokens[0]]()
+    elif len(tokens) > 1 and tokens[0] in complex_commands:
+        complex_commands[tokens[0]](tokens[1])
     else:
-        print("You cannot go that way")
+        prompt("Invalid command. Enter 'help' for a list of commands.")
 
-def quit_game(command):
-    print("Thanks for playing!")
+def move(direction):
+    # the first letter of the direction is all we need
+    dir = direction[0]
+    if dir in player.room.connections:
+        player.room = player.room.connections[dir]
+    else:
+        prompt("You cannot go that way")
+
+def take_item(item_name):
+    if player.room.has_item(item_name):
+        prompt(f"You take the {item_name} from the {player.room.name}.")
+        player.take_item_from_room(item_name, player.room)
+        player.last_item = item_name
+    else:
+        prompt(f"There are no {item_name}s here to take")
+
+def drop_item(item_name):
+    if player.has_item(item_name):
+        prompt(f"You drop the {item_name} in the {player.room.name}")
+        player.drop_item_in_room(item_name, player.room)
+        player.last_item = item_name
+    else:
+        prompt(f"You have no {item_name}s to drop")
+
+def inspect_item(item_name):
+    item = None
+    for location in [player, player.room]:
+        if location.has_item(item_name):
+            item = location.get_item(item_name)
+    
+    if item:
+        text = tabulate(str(item).capitalize(), "\n", item.description, bullet="")
+    else:
+        text = f"No {item_name}s in {player}'s inventory or this room"
+    
+    prompt(text)
+
+def quit_game():
+    prompt("Thanks for playing!")
     exit()
 
-def take_item(command):
-    item = items.get(command[1])
-    if item and item in player.current_room.items:
-        player.current_room.items.remove(item)
-        player.inventory.append(item)
+#
+# Display methods
+#
+
+def print_turn():
+    clear()
+    print(f"{player} is in the {player.room.name}\n")
+    print(player.room)
+    if player.room.items:    
+        item_text = tabulate(
+            "\nYou see the following in this room:\n",
+            *list(map(str, player.room.items)))
+
+        print(item_text)
+
+def print_inventory():
+    if player.items:
+        inventory_text = tabulate(
+            f"{player.name} is carrying:\n",
+            *map(str, player.items))
     else:
-        print("There are no {}s here to take".format(command[1]))
+        inventory_text = f"{player.name} is not carrying anything."
 
-def drop_item(command):
-    item = items.get(command[1])
-    if item and item in player.inventory:
-        player.inventory.remove(item)
-        player.current_room.items.append(item)
-    else:
-        print("You have no {}s to drop".format(command[1]))
+    prompt(inventory_text)
 
-def inventory(command):
-    global show_inventory 
-    show_inventory = True
 
-def help(command):
-    global show_help
-    show_help  = True
+def print_help():
+    help_text = tabulate(
+        "Available commands:\n",
+        "move < north | east | south | west >",
+        "take < item >",
+        "drop < item >",
+        "inspect < item >",
+        "quit",
+        "inventory",
+        "help")
+
+    prompt(help_text)
+
+def print_score():
+    score_text = tabulate(
+        f"{player.name}'s score:\n",
+        f"Items: {player.score['items']}",
+        f"Monsters: {player.score['monsters']}",
+        f"Total: {player.get_score()}")
+
+    prompt(score_text)
+
+def clear():
+    os.system('cls' or 'clear')
+
+def pause():
+    input("\nPress Enter to continue...")
+
+def prompt(message):
+    clear()
+    print(message)
+    pause()
+
+def tabulate(*lines, bullet="-"):
+    join_string = f"\n  {bullet} "
+    return join_string.join(lines)
 
 #
 # Main loop
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player("Leon", rooms['outside'])
+name = input("Welcome to the Dungeon of Diogenes! Who are you? ")
+player = Player(name, rooms['outside'])
 while True:
-    os.system('cls')
-    print(player)
-    print(player.current_room.description)
-    if show_inventory:
-        print("{} is carrying:".format(player.playerName))
-        for item in player.inventory:
-            print("A {}".format(item))
-        show_inventory = False
-    for item in player.current_room.items:
-        print("You see a {}".format(item))
-
-    command = input("Enter choice: ")
-    process_command(command)
+    print_turn()
+    process_command(input("\nEnter choice (or help): "))
