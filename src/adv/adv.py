4ea739eee0108b41287f783c2d016d70efb18278
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-07-26 16:32:42 -0400

    Robert Hogan. Have working command prompt again, and a more readable prompt.

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -1,7 +1,11 @@
+# importing room file into adv
 from room import Room
+# import player file into adv
+from player import Player 
 
 # Declare all the rooms
-
+# an object called room holding a location string, and an instance of room
+# each one has a value of a room object. imported above.
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
@@ -23,7 +27,9 @@ earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
-
+# setting up possible movements between rooms.
+# can use these to create controls. 
+# .x_to is a reference to another room. 
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -39,6 +45,16 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+# a new instance of the player class. Passed with the params
+# playerName = "Falcorn"
+#startRoom = room['outside']
+main_player = Player("Falcorn", room['outside'])
+
+#start game a line down 
+print("\n")
+#player name should display at the top. 
+print("Your player: " + main_player.name + " defender of the alliance \n")
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +65,18 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+# setting a flag. We can use this to make stuff work!
+done = False
+
+while not done:
+    current_room = main_player.room 
+    #room is what startRoom is set as. 
+    #self.room = startRoom
+    print(f'{current_room.name}\n{current_room.description}\n')
+
+#ask for user input
+#this is going to take all the white space off
+#and convert the command to lowercase
+    command = input("Command> ").strip().lower()
+    
--- src/adv/player.py
+++ src/adv/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+#player class 
+class Player:
+    def __init__(self, playerName, startRoom):
+        self.room = startRoom
+        self.name = playerName
