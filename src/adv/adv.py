412c44cb9d7be8cca918da07dea30adec15eb283
Devon Bacon <devon.bacon@gmail.com>
2018-07-26 13:27:38 -0700

    adv.py - extracted code into respective class files

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -1,67 +1,10 @@
-from room import Room
-from player import Player
-from item import Item
+import room
+import player
+import item
 from os import system
 import textwrap
 
-# Create all the items
-
-item = {
-  'lever': Item('lever', """It looks as though it was broken off at the base.
-Maybe it goes to something."""),
-}
-
-# tools = Item('tools', "These could be used to fashion some thing handy")
-
-# Declare all the rooms
-
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty 
-passages run north and east.""", """You see a wall to the west that seems to 
-have very uniform gaps in the shape of a doorway. Upon further inspection there
-appears to be a gab in the wall with a slot inside. It must be a mehcanism of some sort"""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", """As you inspect the area, you see a strange
-metal object near the edge of the cliff.
-It appears to be a lever.""",[item['lever']]),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'workshop': Room("Smith's Workshop", """This hidden room has scraps strewn out
-across the floor and containers along portions of the walls.""", """Around the corner seems
-to be a functional workbench. Maybe we could use this to craft something useful"""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
-
-
-# Link rooms together
-
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
-
-
-#
-# Main
-#
-
-# Make a new player object that is currently in the 'outside' room.
-
-player = Player('Devon', room['overlook'])
+## Main
 
 # Write a loop that:
 #
@@ -74,19 +17,14 @@ player = Player('Devon', room['overlook'])
 #
 # If the user enters "q", quit the game.
 
-turn = 0
+# Establish global variables for displaying information after console refresh
 action = None
 description = None
 
 while(True):
   system('clear')
-  turn += 1
 
-  # print('\n{}:\n{}\n'.format(player.room.name,
-  #   textwrap.fill(player.room.description, 50)))
-
-  print('\n{}:\n{}\n'.format(player.room.name,
-    textwrap.fill(player.room.description, 50)))
+  print(f'\n{player.room.name}:\n{textwrap.fill(player.room.description, 50)}\n')
 
   if action:
     print(action + '\n')
