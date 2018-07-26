c558a84f20b3f23e1f6aa278b502187e551f0bd9
jeffrey <jeffreyneilflynn@gmail.com>
2018-07-26 14:32:47 -0400

    lightsource subclass

--- src/adv/item.py
+++ src/adv/item.py
@@ -9,3 +9,8 @@ class Treasure(Item):
   def __init__(self, name, description, value):
     super().__init__(name, description)
     self.value = value
+
+
+class LightSource(Item):
+  def __init__(self, name, description):
+    super().__init__(name, description)
