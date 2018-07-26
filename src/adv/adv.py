b3d96e879ac465066e17c74b180a8435780490e8
nickbrenn <nbren@tutanota.com>
2018-07-26 15:37:20 -0400

    Modified can_see to handle different actions and removed comments

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -3,14 +3,10 @@ from player import Player
 from item import Item
 from item import Treasure
 from item import LightSource
-# import json
-# import codecs
 import copy
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 copy = copy.copy
-
-# Declare all the rooms
 
 items = {
     'coins': Treasure("coins", "a pouch of coins", 100),
@@ -39,8 +35,6 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""", 'treasure', [copy(items['coins']), copy(items['coins']), copy(items['coins'])], False),
 }
 
-# Link rooms together
-
 rooms['outside'].n_to = rooms['foyer']
 rooms['foyer'].s_to = rooms['outside']
 rooms['foyer'].n_to = rooms['overlook']
@@ -50,12 +44,6 @@ rooms['narrow'].w_to = rooms['foyer']
 rooms['narrow'].n_to = rooms['treasure']
 rooms['treasure'].s_to = rooms['narrow']
 
-#
-# Main
-#
-
-# Make a new player object that is currently in the 'outside' room.
-
 startingRoom = 'outside'
 
 
@@ -63,23 +51,14 @@ def saveGame(player):
     saveFile = open("saves/%s.txt" % player.name, "w+")
     saveFile.write("%s" % player.room.key)
     inventoryData = "//["
-
     for i in player.inventory:
-        # print(vars(i).items())
-        # pp.pprint("%s" % vars(i).values())
-        # print("type is %s", type(i).__name__)
-        # pp.pprint("%s" % repr(vars(i)))
-        # pp.pprint("%s" % repr(vars(i).values()))
-        # pp.pprint("%s".strip('[]') % list(vars(i).values()))
         itemString = str(list(vars(i).values())).strip('[]')
-        # print(itemString)
         inventoryData += "%s(%s)," % (type(i).__name__, itemString)
-
     inventoryData += "]"
     saveFile.write("%s" % inventoryData)
     saveFile.write("//")
     roomData = ""
-    for key, room in rooms.items():
+    for room in rooms.values():
         roomData += "["
         for item in room.items:
             itemString = str(list(vars(item).values())).strip('[]')
@@ -88,24 +67,6 @@ def saveGame(player):
     saveFile.write("%s//%d" % (roomData, player.score))
     saveFile.close()
     print("Game saved.")
-
-
-# def jsonSave(player):
-    # saveFile = open("saves/%s.txt" % player.name, "w+")
-    # saveFile.write("[%s,\n%s]" % (json.dumps(player), json.dumps(rooms)))
-    # saveFile.close()
-    # inventoryData = ''
-    # for i in player.inventory:
-    #     itemString = str(list(vars(i).values())).strip('[]')
-    #     inventoryData += "%s(%s)," % (type(i).__name__, itemString)
-    # player.inventory = "[%s]" % inventoryData
-    # player.room = player.room.key
-    # with open("saves/%s.json" % player.name, "wb") as f:
-    #     json.dump(repr(vars(player)), codecs.getwriter(
-    #         'utf-8')(f), ensure_ascii=True)
-    # print("Game saved.")
-    # print(player, 'utf-8')
-    # pp.pprint(vars(player))
 
 
 def loadGame(playerName):
@@ -117,10 +78,7 @@ def loadGame(playerName):
     playerScore = int(lines[3])
     loadFile.close()
     for index, room in enumerate(rooms.items()):
-        # print(index)
         rooms[room[0]].items = eval(roomData[index])
-    # print("startingRoom: %s\ninventoryData: %s\nroomData: %s\n" %
-    #       (startingRoom, inventoryData, roomData))
     return Player(playerName, rooms[startingRoom], eval(inventoryData), playerScore)
 
 
@@ -137,31 +95,23 @@ else:
     print("Restart the program and choose whether to load or start a new game.")
     exit()
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
 
 playing = True
 helpString = 'Type n/s/e/w to move in a direction.\nType "i" or "inventory" to check your inventory.\nType "look" or "search" to look around for items.\nType "t", "take", "g" or "get" and the name of the item you want to take to pick up an item.\nType "d" or "drop" and the name of the item in your inventory that you want to drop.\nType "score" to see your score.\nType "q" or "quit" to quit and save.\nType "save" to save the game without quitting.'
 
 
-def can_see(player, look=False):
+def can_see(player, action='default'):
     if player.room.is_light is True or next((i for i in player.room.items if isinstance(i, LightSource)), None) or next((i for i in player.inventory if isinstance(i, LightSource)), None):
-        if look == False:
+        if action == 'default':
             print("\n" + player.room.description)
         return True
     else:
-        if look == False:
+        if action == 'default':
             print("\nIt's too dark for you to see.")
-        else:
+        elif action == 'look':
             print("It's too dark for you to see.")
+        elif action == 'take':
+            print("Good luck finding that in the dark!")
         return False
 
 
@@ -192,7 +142,7 @@ while(playing is True):
         else:
             print("You can't go that way.")
     elif command == "look" or command == "search":
-        if can_see(player, True):
+        if can_see(player, 'look'):
             print("You scan your surroundings.")
             for i in curRoom.items:
                 print("You find %s." % i.description)
@@ -202,14 +152,18 @@ while(playing is True):
         print("You check your inventory.")
         for i in player.inventory:
             print("You have %s." % i.description)
+        if len(player.inventory) < 1:
+            print("You aren't carrying anything.")
     elif command == "t" or command == "take" or command == "g" or command == "get":
-        item = next((i for i in curRoom.items if i.name == secondCommand), None)
-        if item != None:
-            item.on_take(player)
-            player.inventory.append(item)
-            curRoom.items.remove(item)
-        else:
-            print("There isn't any of that item that you can pick up.")
+        if can_see(player, 'take'):
+            item = next(
+                (i for i in curRoom.items if i.name == secondCommand), None)
+            if item != None:
+                item.on_take(player)
+                player.inventory.append(item)
+                curRoom.items.remove(item)
+            else:
+                print("There isn't any of that item that you can pick up.")
     elif command == "d" or command == "drop":
         item = next(
             (i for i in player.inventory if i.name == secondCommand), None)
