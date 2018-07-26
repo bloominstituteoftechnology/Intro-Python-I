09463f3daba4ad0c5656a3c9feead66acd646b82
Devon Bacon <devon.bacon@gmail.com>
2018-07-25 21:16:36 -0700

    room.py - added a few attributes to help with items and actions

--- src/adv/room.py
+++ src/adv/room.py
@@ -1,8 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+default_look = 'You don\'t see anything of particular interest'
+
 class Room:
-  def __init__(self, name, description):
+  def __init__(self, name, description, look = default_look, items=[]):
     self.name = name
     self.description = description
-    self.items = []
+    self.look = look
+    self.items = items
+    self.inspected = False
