4e8cf4cdd3e861e85ab42eeaf8f61555052ec790
Jared Cuffe <jcuffe@gmail.com>
2018-07-26 14:35:41 -0700

    Added class methods to simplify item manipulation

--- src/adv/room.py
+++ src/adv/room.py
@@ -1,15 +1,30 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+from lightsource import LightSource
+import textwrap
 
 class Room:
-    def __init__(self, name, description, items=[]):
+    def __init__(self, name, description, illuminated, items=[]):
         self.name = name
         self.description = description
-        self.n_to = None
-        self.s_to = None
-        self.w_to = None
-        self.e_to = None
+        self.connections = {}
+        self.illuminated = illuminated
         self.items = items
 
+    def has_item(self, item_name):
+        return item_name in [item.name for item in self.items]
+
+    def get_item(self, item_name):
+        return [item for item in self.items if item.name == item_name][0]
+
+    def take_item(self, item_name):
+        item = self.get_item(item_name)
+        self.items.remove(item)
+        return item
+
+    def put_item(self, item):
+        self.items.append(item)
+
+    def is_illuminated(self):
+        return self.illuminated or len([item for item in self.items if isinstance(item, LightSource)]) > 0
+
     def __str__(self):
-        return self.name
+        return textwrap.fill(self.description)
