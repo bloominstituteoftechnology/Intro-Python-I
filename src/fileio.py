ae6082a2b071ae91549e7a876331f7b56b11c3bb
dresean <williamsdresean@gmail.com>
2018-07-26 18:10:07 -0400

    more stuff completed

--- src/fileio.py
+++ src/fileio.py
@@ -1,9 +1,11 @@
 # Use open to open file "foo.txt" for reading
-
+import os
+foo = open('foo.txt')
 # Print all the lines in the file
-
+for lines in foo: 
+  print(lines)
 # Close the file
-
+foo.close()
 
 # Use open to open file "bar.txt" for writing
 
