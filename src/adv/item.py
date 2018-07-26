9a8712c91de2cee2f6dc0d1f9d94021abfbc6e28
Jared Cuffe <jcuffe@gmail.com>
2018-07-26 15:17:18 -0700

    Added on_take method

--- src/adv/item.py
+++ src/adv/item.py
@@ -1,8 +1,15 @@
+from printing import prompt
+
 class Item:
     def __init__(self, name, adjectives, description):
         self.name = name
         self.adjectives = adjectives  
         self.description = description
+        self.uncollected = True
+
+    def on_take(self, player):
+        self.uncollected = False
+        prompt(f"{player.name} collects the {self.name} from the {player.room.name}")
 
     def __str__(self):
         adjectives = ", ".join(self.adjectives)
