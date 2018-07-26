d245450cd55829d8b8f1f72c848c9a1a899cf315
JustinSHong <jhong2009@gmail.com>
2018-07-26 15:16:10 -0700

    changed treasure values to nums. created on_take and on_drop handlers

--- src/adv/item.py
+++ src/adv/item.py
@@ -11,7 +11,23 @@ class Item:
 	def showDescription(self):
 		return self.description
 
+	def on_take(self, entity, item_name):
+		# called when player picks up an item
+		# Item uses this to run more code when picked up
+		return "{0} picked up the {1} and put in in their inventory". format(entity.name, item_name)
+
+	def on_drop(self, entity, item_name):
+		# called when player drops an item
+		return "{0} dropped the {1} in the {2}".format(entity.name, item_name, entity.room.name)
+
 class Treasure(Item):
 	def __init__(self, name, description, value):
 		super().__init__(name, description)
 		self.value = value
+
+	def on_take(self, entity, treasure_name):
+		# get value of Treasure added to player's score
+		# occurs on first pick up only
+		entity.score += self.value
+		self.value = 0
+		return "{0} picked up the {1}, a rare treasure.".format(entity.name, treasure_name) 
