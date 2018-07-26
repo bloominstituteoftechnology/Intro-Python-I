929266750040585c2192c876e9e50f3df89b4475
Henry K <mykim1891@hotmail.com>
2018-07-26 16:00:36 -0700

    Implemented is_light attributes to all rooms

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -17,33 +17,33 @@ room = {
                 "BrokenSword",
                 "The tip of the sword is broken. It can probably still slay some beasts.",
             ),
-        ],
+        ], True
     ),
     "foyer": Room(
         "Foyer",
         """Dim light filters in from the south. Dusty
 passages run north and east.""",
-        [],
+        [], True
     ),
     "overlook": Room(
         "Grand Overlook",
         """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm.""",
-        [],
+        [], False
     ),
     "narrow": Room(
         "Narrow Passage",
         """The narrow passage bends here from west
 to north. The smell of gold permeates the air.""",
-        [],
+        [], False
     ),
     "treasure": Room(
         "Treasure Chamber",
         """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""",
-        [],
+        [], False
     ),
 }
 
