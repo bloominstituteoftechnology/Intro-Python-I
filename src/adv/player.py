3b1623d120e14d77efb8ed81b33499d2da4f079e
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-07-26 15:55:39 -0400

    Robert Hogan. Functionality was lost. Restarting from the beginning.

--- src/adv/player.py
+++ src/adv/player.py
@@ -1,7 +1,2 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-
-class Player:
-    def __init__(self, playerName, startRoom):
-        self.startRoom = startRoom
-        self.playerName = playerName
--- src/adv/room.py
+++ src/adv/room.py
@@ -1,9 +1,2 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
-from item import Item
-
-class Room(Item):
-    def __init__(self, name, description, item_name, item_description):
-        Item.__init__(self, item_name, item_description)
-        self.name = name
-        self.description = description
