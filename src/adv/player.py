a2b5a3aa7a247c305cea7b457959645e1f3772fa
totsuya <supertatortots@gmail.com>
2018-07-26 02:29:19 -0400

    added inventory for player

--- src/adv/player.py
+++ src/adv/player.py
@@ -2,10 +2,10 @@
 # currently.
 
 class Player:
-    def __init__(self, playerName, startRoom):
+    def __init__(self, playerName, startRoom, inventory):
         self.room = startRoom
         self.name = playerName
-        self.inventory = {}
+        self.inventory = []
 
     # def pickUpItem(self, item)
     #   self.item
