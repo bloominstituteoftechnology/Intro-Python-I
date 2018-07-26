f276c4ef1cd2e4bca7d6e43f8c3b5268622bb88f
Natalie <natalieschennum@gmail.com>
2018-07-26 08:31:15 -0700

    Working on pickup

--- src/adv/adv.py
+++ src/adv/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 import textwrap
+import item from Item
 
 # Declare all the rooms
 
@@ -20,8 +21,15 @@ to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",
 }
+
+# Create items and add to rooms
+
+item1 = Item("A rose.", "Smells lovely.")
+item2 = Item("A single small diamond.", "Sparkles.")
+room['outside'].addItem(item1)
+room['treasure'].addItem(item2)
 
 
 # Link rooms together
@@ -39,8 +47,10 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
+
 # Make a new player object that is currently in the 'outside' room.
 player = Player("Natalie", room['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -52,16 +62,29 @@ player = Player("Natalie", room['outside'])
 #
 # If the user enters "q", quit the game.
 
+def pickup(self, player, item):
+        try:
+            self.items.remove(item)
+            player.addItem(item)
+            return "Picked up " + item.name + '\n'
+        except:
+            return "There is no " + item.name + '\n'
+
 done = False
 
 while not done: 
     curRoom = player.room
 
-    prettyDesc = textwrap.fill(curRoom.desription)
+    prettyDesc = textwrap.fill(curRoom.description.items)
 
-    print(f'{curRoom.name}\n{curRoom.description}')
+    print(f'{curRoom.name}\n{prettyDesc.description}')
 
-    command = input("Command> ").strip().lower() #takes off white space and returns lowercase
+    if len(player.curRoom.items) > 0:
+        print("\nYou see:")
+        for i in player.curRoom.items:
+            print("     " + str(i.name))
+            
+    command = input("Command> ").strip().lower().split() #takes off white space and returns lowercase
 
     if command == 'q' or command == 'quit' or command == 'exit':
         done = True
@@ -69,11 +92,20 @@ while not done:
     elif command in ["s", "n", "e", "w"]:
         dirAttr = command + "_to"
 
-        if hasattr(curRoom, dirattr): 
+        if hasattr(curRoom, dirAttr): 
           player.room = getattr(curRoom, dirAttr)
 
         else: 
             print("You can't go that way.")
+    elif len(s) == 2:
+        if s[0] in ["get", "take", "pickup"]:
+            item = player.curRoom.findItem(s[1])
+
+            if item is None:
+                print("Item not found")
+            else:
+                player.addItem(item)
+                player.curRoom.removeItem(item.name)
         
     else: 
         print("I don't understand that command.")
