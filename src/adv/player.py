30aee902fc096949b7156a7e5bc8d7005674dea0
Jared Cuffe <jcuffe@gmail.com>
2018-07-26 14:35:17 -0700

    Now handling item manipulation in Player class methods

--- src/adv/player.py
+++ src/adv/player.py
@@ -2,10 +2,33 @@
 # currently.
 
 class Player:
-    def __init__(self, playerName, currentRoom):
-        self.playerName = playerName
-        self.current_room = currentRoom
-        self.inventory = []
+    def __init__(self, name, room):
+        self.name = name
+        self.room = room
+        self.items = []
+        self.last_item = ''
+        self.score = {
+            'items': 0,
+            'monsters': 0
+        }
+
+    def has_item(self, item_name):
+        return item_name in [item.name for item in self.items]
+    
+    def get_item(self, item_name):
+        return [item for item in self.items if item.name == item_name][0]
+
+    def take_item_from_room(self, item_name, room):
+        item = room.take_item(item_name)
+        self.items.append(item)
+
+    def drop_item_in_room(self, item_name, room):
+        item = self.get_item(item_name)
+        self.items.remove(item)
+        self.room.put_item(item)
+
+    def get_score(self):
+        return sum(self.score.values())
 
     def __str__(self):
-        return '{} is in the {}.'.format(self.playerName, self.current_room)
+        return self.name
