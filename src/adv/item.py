efbc4f0aabde8ed0c924e46f235b6be6a716ed9b
Jared Cuffe <jcuffe@gmail.com>
2018-07-26 16:58:31 -0700

    on_* methods now return a bool to signal success

--- src/adv/item.py
+++ src/adv/item.py
@@ -10,7 +10,12 @@ class Item:
     def on_take(self, player):
         self.uncollected = False
         prompt(f"{player} collects the {self} from the {player.room}")
+        return True
+
+    def on_drop(self, player):
+        prompt(f"{player} drops the {self} in the {player.room}")
+        return True
 
     def __str__(self):
         adjectives = ", ".join(self.adjectives)
-        return f"a {adjectives} {self.name}"
+        return f"{adjectives} {self.name}"
