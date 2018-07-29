40212077dd899918069d9832cf0679ad1bdd0fdd
Devon Bacon <devon.bacon@gmail.com>
2018-07-29 13:47:30 -0700

    game.py - added to items, weapons and added npc object

--- src/adv/game.py
+++ src/adv/game.py
@@ -2,21 +2,6 @@ from room import Room
 from item import Item, Consumable, Equippable, Armor, Weapon, Accessory, MagicItem
 from being import Player, NPC, Animal, Monster
 
-# Create all the items
-item = {
-  'lever': Item('lever', """It looks as though it was broken off at the base.
-Possibly part of a larger contraption."""),
-  'small potion': Consumable('small potion', 'Heals user for 1-3 hp.', 'potion', 5), 
-  'medium potion': Consumable('medium potion', 'Heals user for 2-6 hp.', 'potion', 5),
-  'large potion': Consumable('large potion', 'Heals user for 2-6 hp.', 'potion', 5)
-}
-
-weapon = {
-  'human natural': Weapon('human natural', 'Flying fists and feet of fury!', 1),
-  'steel sword': Weapon('steel sword', 'Tried and true with a keen edge.', 2),
-  'iron hammer': Weapon('iron hammer', 'You could not ask for a more sturdy companion.', 3)
-}
-
 # Declare all the rooms
 room = {
     'outside':  Room("Outside Cave Entrance",
@@ -29,7 +14,7 @@ the wall with a slot inside. It must be a mehcanism of some sort."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", None, [item['lever']]),
+the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
@@ -43,15 +28,39 @@ useful."""),
 chamber! Sadly, it has already been completely emptied by earlier adventurers. The 
 only exit is to the south."""),
 
-    'arena': Room("Arena", """This is the place where champions are made. Best of luck""")
+    'arena': Room("Arena", """This is the place where champions are made. Best of luck!""")
 }
 
-# Make the player object
-player = Player(room['outside'], 'Devon', 'human', 20, [weapon['iron hammer'], item['medium potion']])
+# Create all the items
+item = {
+  'lever': Item('lever', """It looks as though it was broken off at the base.
+Possibly part of a larger contraption."""),
+  'small potion': Consumable('small potion', 'Heals user for 1-3 hp.', 'potion', 5), 
+  'medium potion': Consumable('medium potion', 'Heals user for 2-6 hp.', 'potion', 5),
+  'large potion': Consumable('large potion', 'Heals user for 2-6 hp.', 'potion', 5)
+}
 
-# Create and place any othe 
-orc = NPC(room['arena'], 'Random', 'orc', 40, [weapon['steel sword'], item['small potion']])
+weapon = {
+  # 'medium humanoid natural': Weapon('natural weapons', 'Flying fists and feet of fury!', 1),
+  'steel sword': Weapon('steel sword', 'Tried and true with a keen edge.', 2),
+  'iron hammer': Weapon('iron hammer', 'You could not ask for a more sturdy companion.', 3)
+}
 
+# Create all the beings
+npc = {
+  'Orc:Vesh': NPC(room['arena'], 'Vesh', 'Orc', 40, [item['small potion']])
+}
+
+player = Player(room['arena'], 'Devon', 'Human', 20, [weapon['iron hammer'], item['medium potion']])
+
+# Equip any weapons
+npc['Orc:Vesh'].equip_slots['weapon'] = weapon['steel sword']
+player.equip_slots['weapon'] = weapon['iron hammer']
+
+# Add items and occupants to rooms
+room['outside'].inventory.append(item['lever'])
+
+room['arena'].occupants += [player, npc['Orc:Vesh']]
 
 # Link rooms together
 room['outside'].n_to = room['foyer']
@@ -62,12 +71,3 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
-
-
-
-
-
-
-
-
-
