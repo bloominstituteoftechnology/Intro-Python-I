266f14d8dd091701750db7150a30b4c9471b3099
unknown <alexisreyes90@gmail.com>
2018-07-31 00:03:41 -0400

    up to lists.py completed

--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -6,5 +6,5 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-
 # Use the 'format' string method to print the same thing
+print("x is %d, y is %f, z is %s" % (x, y, z))
