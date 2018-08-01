0f237f8fa217bb7dd64f972d938a3dffebce7241
Bhavik Ravani <ravanibhavik@gmail.com>
2018-08-01 00:36:26 -0400

    Completed day-1-toy.

--- src/day-1-toy/obj.py
+++ src/day-1-toy/obj.py
@@ -11,28 +11,37 @@ class LatLon:
 
 class Waypoint(LatLon):
     def __init__(self, name, lat, lon):
-        super(self, lat, lon)
+        LatLon.__init__(self, lat, lon)
         self.name = name
+
+    def __str__(self):
+        return "Waypoint -> Name: {}, Latitude: {}, Longitude: {}" \
+                .format(self.name, self.lat, self.lon)
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 class Geocache(Waypoint):
-    def __init__(self, name, difficulty, size, lat, lon):
-        super(self, name, lat, lon)
-        self.difficulty = difficulty
+    def __init__(self, name, diff, size, lat, lon):
+        Waypoint.__init__(self, name, lat, lon)
+        self.diff = diff
         self.size = size
 
+    def __str__(self):
+        return "Geocache -> Name: {}, Difficulty: {}, Size: {}, Latitude: {}," \
+               "Longitude: {}".format(self.name, self.diff, self.size, self.lat, \
+               self.lon)
+
 # Make a new waypoint "Catacombs", 41.70505, -121.51521
-wp = Waypoint("Catacombs", 41.70505, -121.51521)
+w = Waypoint("Catacombs", 41.70505, -121.51521)
 # Print it
-print(wp)
+print(w)
 
 # Without changing the following line, how can you make it print into something
 # more human-readable?
-# print(w)
+print(w)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
-
+g = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 # Print it--also make this print more nicely
-# print(g)
+print(g)
