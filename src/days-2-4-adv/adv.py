fd501f40d05614025af25361898102abcbbd8a9a
Bhavik Ravani <ravanibhavik@gmail.com>
2018-08-02 00:04:02 -0400

    Removed unnecessary comments.

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -75,9 +75,3 @@ while True:
     else:
         print("Movement is not allowed")
 
-
-# If the user enters a cardinal direction, attempt to move to the room there.
-
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
