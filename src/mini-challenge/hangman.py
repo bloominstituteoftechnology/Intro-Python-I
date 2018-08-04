1e70d9983bb695db04f825263002480bbd9be428
Thuy <tpham045@hotmail.com>
2018-08-04 19:25:59 +0000

    fix: hangman game bug
    1. import random
    2. incorrect file name
    3. index out of range words
    4. global curWord
    5. guessing letter twice
    6. strikes should be converted to string type

--- src/mini-challenge/hangman.py
+++ src/mini-challenge/hangman.py
@@ -7,6 +7,8 @@
 # from a set of win/loss messages...basically, make it better!
 
 # Initial setup
+import random
+
 bodies = [ " ------\n |    |\n |    O\n |\n |\n |\n |\n |\n---", 
 " ------\n |    |\n |    O\n |    |\n |    |\n |\n |\n |\n---", 
 " ------\n |    |\n |    O\n |    |\n |    |\n |   / \n |\n |\n---", 
@@ -15,24 +17,25 @@ bodies = [ " ------\n |    |\n |    O\n |\n |\n |\n |\n |\n---",
 " ------\n |    |\n |  O\n |   \|/\n |    |\n |   / \ \n |\n |\n---" ]
 strikes = 0
 words = [None]
-file = open("word.txt", "r")
+file = open("words.txt", "r")
 for line in file:
     words.append(line)
 file.close()
-targetWord = words[random.randint(0, 100)]
+targetWord = words[random.randint(0, len(words))]
 lettersLeft = len(targetWord)-1
 length = len(targetWord)-1
 curWord = "_" * length
-alphabet = [chr(65+x) for x in range(1, 26) ]
+alphabet = [chr(65+x) for x in range(0, 26) ]
 
 # Draw body based on # of incorrect guesses
-def drawBody()
+def drawBody():
     print(bodies[strikes])
 
 # Replace blanks with correctly guessed letters
 def fillLetters( letter ):
     for i in range(len(targetWord)-1):
         if( targetWord[i : i+1]) == letter:
+            global curWord
             curWord = curWord[0: i] + letter + curWord[i: ]
             global lettersLeft
             lettersLeft -= 1
@@ -54,15 +57,21 @@ printWord(alphabet)
 # Gameplay loop
 while strikes < 5 and lettersLeft > 0:
     letter = input("\nPlease guess a letter...")
-    if letter in targetWord:
-        print("Great!")
-        fillLetters(letter)
+    
+    if letter.upper() not in alphabet:
+        print("YOU ALREADY GUESSED THAT LETTER !!!!!")
     else:
-        strikes += 1
-        print( strikes + " / 5 strikes" )
-    printWord(curWord)
-    drawBody()
-    alphabet.remove(letter.upper())
+        if letter in targetWord:
+            print("Great!")
+            fillLetters(letter)
+        else:
+            strikes += 1
+            print( str(strikes) + " / 5 strikes" )
+            
+        printWord(curWord)
+        drawBody()
+        alphabet.remove(letter.upper())
+    
     print("Letters left:")
     printWord(alphabet)
 
