94c06455fdbc52e601dc3fc7f590dc46b1e971d0
Frank Faustino <nawzyah@gmail.com>
2018-08-05 02:04:38 -0700

    Went a little overboard with the Hangman mini-challenge 😂

--- src/mini-challenge/hangman.py
+++ src/mini-challenge/hangman.py
@@ -1,73 +1,83 @@
-# There are at least 10 "mistakes" in this Hangman game
-# These could include syntax errors, logical errors, spelling errors...
-# Find the mistakes and fix the game!
-#
-# STRETCH GOAL: If you fix all the errors, can you find a way to improve 
-# it? Add a cheat code, more error handling, chose randomly
-# from a set of win/loss messages...basically, make it better!
+import random
+import sys
+import time
+import curses
 
-# Initial setup
-bodies = [ " ------\n |    |\n |    O\n |\n |\n |\n |\n |\n---", 
-" ------\n |    |\n |    O\n |    |\n |    |\n |\n |\n |\n---", 
-" ------\n |    |\n |    O\n |    |\n |    |\n |   / \n |\n |\n---", 
-" ------\n |    |\n |    O\n |    |\n |    |\n |   / \ \n |\n |\n---", 
-" ------\n |    |\n |    O\n |   \|\n |    |\n |   / \ \n |\n |\n---",
-" ------\n |    |\n |  O\n |   \|/\n |    |\n |   / \ \n |\n |\n---" ]
-strikes = 0
-words = [None]
-file = open("word.txt", "r")
-for line in file:
+
+try:
+  bodies = [" ------\n |    |\n |    😄\n |\n |\n |\n |\n |\n---",
+          " ------\n |    |\n |    😥\n |    |\n |    |\n |\n |\n |\n---",
+          " ------\n |    |\n |    😰\n |    |\n |    |\n |   / \n |\n |\n---",
+          " ------\n |    |\n |    😩\n |    |\n |    |\n |   / \ \n |\n |\n---",
+          " ------\n |    |\n |    😫\n |   \|\n |    |\n |   / \ \n |\n |\n---",
+          " ------\n |    |\n |    💀\n |   \|/\n |    |\n |   / \ \n |\n |\n---"]
+  strikes = 0
+  words = [None]
+  file = open("words.txt", "r")
+  for line in file:
     words.append(line)
-file.close()
-targetWord = words[random.randint(0, 100)]
-lettersLeft = len(targetWord)-1
-length = len(targetWord)-1
-curWord = "_" * length
-alphabet = [chr(65+x) for x in range(1, 26) ]
+  file.close()
+  target_word = words[random.randint(0, len(words) - 1)]
+  letters_left = len(target_word) - 1
+  length = len(target_word) - 1
+  current_word = "—" * length
+  alphabet = [chr(65 + x) for x in range(0, 26)]
 
-# Draw body based on # of incorrect guesses
-def drawBody()
+  def draw_body():
     print(bodies[strikes])
 
-# Replace blanks with correctly guessed letters
-def fillLetters( letter ):
-    for i in range(len(targetWord)-1):
-        if( targetWord[i : i+1]) == letter:
-            curWord = curWord[0: i] + letter + curWord[i: ]
-            global lettersLeft
-            lettersLeft -= 1
 
-# Add spaces when displaying letters / blanks for readability
-def printWord( word ):
-    prntWord = ""
+  def fill_letters(letter):
+    global current_word
+    global letters_left
+
+    for i in range(length):
+      if target_word[i] == letter:
+        current_word = current_word[0: i] + letter + current_word[i + 1:]
+        letters_left -= 1
+
+  def print_word(word):
+    output = ""
     for letter in word:
-        prntWord += letter + " "
-    print(prntWord)
+      output += letter + " "
+    # print(f"\n\n👉 {output} 👈")
+    print(f"\nThe word has {length} letters: {output}\n\n")
 
-# Begin game
-print( "Welcome to Hangmn!" )
-printWord(curWord)
-drawBody()
-print("Letters left:")
-printWord(alphabet)
+  print("\n\nWelcome to Hangman! 🕴")
+  print_word(current_word)
+  draw_body()
+  print("Letters left:")
+  print_word(alphabet)
 
-# Gameplay loop
-while strikes < 5 and lettersLeft > 0:
-    letter = input("\nPlease guess a letter...")
-    if letter in targetWord:
-        print("Great!")
-        fillLetters(letter)
+  while strikes < 5 and letters_left > 0:
+    print(target_word)
+    print(alphabet)
+    letter = input("\nPlease guess a letter: ")
+
+    if letter.upper() not in alphabet:
+      print(f"\n❌ {letter} is not a valid letter! Try again! \n")
     else:
+      if letter in target_word:
+        print("👌 Great!")
+        fill_letters(letter)
+      else:
         strikes += 1
-        print( strikes + " / 5 strikes" )
-    printWord(curWord)
-    drawBody()
-    alphabet.remove(letter.upper())
-    print("Letters left:")
-    printWord(alphabet)
+        print(strikes, " / 5 strikes")
 
-# Game over, print outcome
-if lettersLeft < 0:
+      print_word(current_word)
+      draw_body()
+      alphabet.remove(letter.upper())
+
+    print("Letters left:")
+    print_word(alphabet)
+
+  if letters_left < 0:
     print("YOU WIN!!")
-else:
-    print("YOU LOSE...word was " + targetWord)
+  else:
+    print("YOU LOSE...word was " + target_word)
+
+
+
+except KeyboardInterrupt:
+  print('\n\n Goodbye! 👋\n')
+  sys.exit()
