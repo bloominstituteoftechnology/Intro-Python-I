fc1ff4aabf67a2e218007f40644788956741a9f8
darthMinimus <irishseanvaldivia@gmail.com>
2018-08-06 22:07:39 -0500

    Initial commit for tonight's additions to adv.py

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,10 +1,17 @@
 from room import Room
+from item import Item
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
 class Player(object):
-    def __init__(self, name, room):
+    def __init__(self, name, room, items):
         self.name = name
         self.room = room
+        self.items = ['torch', 'flint']
+
+    def check_items(self):
+        for i in self.items:
+            print('\t i \n')
+
 
 
