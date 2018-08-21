c64b52ca56fa9732386d14df5f2c1e2baf655c38
JohnJustinn <jjustinn85@gmail.com>
2018-08-21 01:40:19 -0400

    Dictionary Task Completed

--- src/day-1-toy/dicts.py
+++ src/day-1-toy/dicts.py
@@ -26,4 +26,13 @@ waypoints = [
 
 # Write a loop that prints out all the field values for all the waypoints
 
+for i in waypoints :
+    print (i['lat'], i['lon'], i['name'])
+
+
 # Add a new waypoint to the list
+
+waypoints.append({'lat': 51, 'lon': -100, 'name': 'that final place'})
+
+for i in waypoints :
+    print (i['lat'], i['lon'], i['name'])
