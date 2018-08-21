48f4dd98835579a629d8ae2c470ca066c97197a8
JohnJustinn <jjustinn85@gmail.com>
2018-08-21 11:40:38 -0400

    FileIO Task Completed

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,27 @@
 # Use open to open file "foo.txt" for reading
 
+file = open("foo.txt", "r")
+
 # Print all the lines in the file
 
+print(file.readlines())
+
 # Close the file
+
+file.close()
 
 
 # Use open to open file "bar.txt" for writing
 
+file = open("bar.txt", "w")
+
 # Use the write() method to write three lines to the file
 
+file.write("This is the first line \n")
+file.write("This is the second line \n")
+file.write("This is the third line \n")
+
+
 # Close the file
+
+file.close()
