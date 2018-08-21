0efe1ca47ce1eb33a087db06bf48cab1baba2f26
groov1234 <c.groover@outlook.com>
2018-08-21 15:25:21 -0400

    new player object

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,4 +1,7 @@
+import textwrap
 from room import Room
+from player import Player
+
 
 # Declare all the rooms
 
@@ -38,6 +41,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player(room['outside'])
 
 # Write a loop that:
 #
