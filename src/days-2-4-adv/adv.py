be8a2f4c03e489fe3a9ab5d9f8a006e05c6ad49b
groov1234 <c.groover@outlook.com>
2018-08-22 18:48:39 -0400

    refactoring for items/moves/clarity

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,6 +1,10 @@
 import textwrap
+
 from room import Room
+
 from player import Player
+
+from item import Item
 
 
 # Declare all the rooms
@@ -24,52 +28,54 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['outside'].connect(room['foyer'], "n")
+room['foyer'].connect(room['overlook'], "n")
+room['foyer'].connect(room['narrow'], "e")
+room['narrow'].connect(room['treasure'], "n")
 
+i1 = Item("Rock", "This is a rock")
+room['outside'].addItem(i1)
 #
 # Main
 #
-def moveRoom(d, curRoom):
-    attrib = d + '_to'
 
-    if hasattr(curRoom, attrib):
-        return getattr(curRoom, attrib)
-
-    print("Dead End!")
-
-    return curRoom
 # Make a new player object that is currently in the 'outside' room.
-player = Player(room['outside'])
-
 # Write a loop that:
 #
 # Prints the current room name
-
-done = False
-
-while not done:
-
-    # Checks if player has joined
-        
-        # Prints the current room's description using textwrap module
-        print("\n{}".format(player.curRoom.name))
-        
-        for line in textwrap.wrap(player.curRoom.description):
-            print(line)
-        done = True
-
+# Checks if player has joined
 # Waits for user input and decides what to do.
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+directions = ["n", "s", "e", "w"]
+
+player = Player(input("\nWhat is your name? "), room['outside'])
+
+print("\nHello, %s!" % player.name)
+
+error = ""
+
+while True:
+    print(player.curRoom)
+    if error != "":
+        print (error)
+        error = ""
+    inp = input(" >")
+    inputs = inp.split()
+    numArgs = len(inputs)
+    if numArgs == 1:
+        if inp in directions:
+            player.mRooms(inp)
+        elif inp == "q":
+            print ("You have left the room!")
+            break
+        else:
+            error = "Command not found..."
+    else:
+        if inputs[0] == "look":
+            print (player.curRoom.lookItem(inputs[1]))
