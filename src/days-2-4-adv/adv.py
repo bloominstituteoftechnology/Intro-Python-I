5b9e7b0775b3b650a73a87e3b749432357c8e522
holly <holly.giang@penguin>
2018-08-22 03:51:20 +0000

    Create new player object & quit command

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+import textwrap 
 
 # Declare all the rooms
 
@@ -38,14 +40,21 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player("Khan", room["outside"])
 
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+
+
+
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
+
 # If the user enters "q", quit the game.
+if command == "q" or command == "quit" or command == "exit":
+    done = True 
