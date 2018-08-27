2a55f66d6ea3c14c9fcaae1501b64c32d00dbbc0
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-27 11:54:49 -0700

    hello

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -4,30 +4,34 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-#def f1(...
+def f1(a, b):
+    return a+b
 
 print(f1(1, 2))
+
 
 # Write a function f2 that takes any number of iteger arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
-# def f2(...
+def f2(*arg):
+    return sum(arg)
 
+print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
-print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
-
-#def f3(...
+# 
+def f3(*arg, num = 1):
+    return sum(arg)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
