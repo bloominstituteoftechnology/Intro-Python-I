63cd01f6c73c9c720e42333afde8e5c5b837c740
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-27 16:01:12 -0700

    obj

--- src/day-1-toy/obj.py
+++ src/day-1-toy/obj.py
@@ -1,21 +1,38 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
-        
+class LatLon():
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon        
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon.
+class Waypoint():
+    def __init__(self, name, lat, lon):
+        LatLon.__init__(self, lat, lon) 
+        self.name = name
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
+class Geocache():
+    def __init__(self, name, difficulty, size, lat, lon):
+        Waypoint.__init__(self, name, lat, lon) 
+        self.difficulty = difficulty
+        self.size = size
 
 # Make a new waypoint "Catacombs", 41.70505, -121.51521
-
+newPoint = Waypoint("Catacombs", 41.70505, -121.51521)
 # Print it
+print(newPoint)
 #
 # Without changing the following line, how can you make it print into something
 # more human-readable?
+from pprint import pprint
+pprint(newPoint)
 print(w)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
+geoPoint = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
+print(geoPoint)
 print(g)
