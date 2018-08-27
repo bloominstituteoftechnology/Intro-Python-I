aa99c2aa58fcadf016207b5a4e3569ce7f4f12bc
Daniel Chamorro <dan@dcitllc.com>
2018-08-27 16:08:34 -0400

    printf

--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -5,6 +5,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print("x is %a, y is %a, z is %a" % (x, y, z))
 
 
 # Use the 'format' string method to print the same thing
+print('x is {}, y is {}, z is "{}"'.format(x, y, z))
