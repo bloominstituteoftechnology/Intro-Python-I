fad32a34464f1f85604839929571f971d7c6cec8
Vu Cao <cdhvu@yahoo.com>
2018-08-27 14:20:55 -0700

    Completed args

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -4,14 +4,21 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-#def f1(...
+
+def f1(a, b):
+    return a+b
+
 
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of iteger arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
+from functools import reduce
 
-# def f2(...
+
+def f2(*args):
+    return reduce((lambda x, y: x+y), args)
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +28,15 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
-#def f3(...
+
+def f3(a, b=1):
+    return a+b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -35,13 +44,16 @@ print(f3(8))     # Should print 9
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints ouf the keys and values like so:
-#
+
 # key: foo, value: bar
 # key: baz, value: 12
-#
+
 # Google "python keyword arguments".
 
-#def f4(...
+def f4(**kwargs):
+    for k, v in kwargs.items():
+        print(f'{k}: {v}')
+
 
 # Should print
 # key: a, value: 12
@@ -60,4 +72,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
