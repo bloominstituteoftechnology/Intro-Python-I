cadaf9e81d674cb80cad400f88544a384d65b193
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-27 15:13:43 -0700

    Complete more exercises

--- src/day-1-toy/dicts.py
+++ src/day-1-toy/dicts.py
@@ -21,9 +21,36 @@ waypoints = [
         "lat": 43,
         "lon": -122,
         "name": "a third place"
+    },
+        {
+        "lat": 33,
+        "lon": -121,
+        "name": "a fourth"
+    }, 
+    {
+        "lat": 31,
+        "lon": -123,
+        "name": "fifth place"
+    }, 
+    {
+        "lat": 32,
+        "lon": -122,
+        "name": "sixth place"
     }
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for point in waypoints:
+    f"Latitude: {point['lat']}"
+    f"Longitude: {point['lon']}"
+    f"Name: {point['name']}"
 
 # Add a new waypoint to the list
+
+waypoints.append({
+    "lat": "23",
+    "lon": "32",
+    "name": "seventh place"
+})
+
+waypoints
