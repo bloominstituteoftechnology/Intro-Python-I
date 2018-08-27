6d74b32c94ddf4787747947cccd6653d9754f48c
Marco A Guzman <marcoguzman16@gmail.com>
2018-08-27 15:18:25 -0700

    sum with *args

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -11,7 +11,12 @@ print(f1(1, 2))
 # Write a function f2 that takes any number of iteger arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
-# def f2(...
+
+def f2(*numbers):
+  a = 0
+  for n in numbers:
+    a += n
+  return a
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
