47b774beefa2ef29b9802d71669c52fcb6f68550
Vu Cao <cdhvu@yahoo.com>
2018-08-27 12:12:10 -0700

    Completed tuples and slice

--- src/day-1-toy/tuples.py
+++ src/day-1-toy/tuples.py
@@ -22,11 +22,15 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # Write a function that prints all the values in a tuple
 
-# def print_tuple(...
+def print_tuple(t):
+    for i in t:
+        print(i)
+
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+#Need to have a comma
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
