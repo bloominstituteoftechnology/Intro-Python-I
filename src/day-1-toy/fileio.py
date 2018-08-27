f394d090a4247413b5a635b1c91890058fc9256a
Tyson Gushiken <tysonism@gmail.com>
2018-08-27 15:43:26 -0700

    Create fileio script

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,16 @@
 # Use open to open file "foo.txt" for reading
-
+file = open("foo.txt", "r") 
 # Print all the lines in the file
-
+content = file.read()
+print(content)
 # Close the file
-
+file.close()
 
 # Use open to open file "bar.txt" for writing
-
+file = open("foo.txt", "w") 
 # Use the write() method to write three lines to the file
-
+file.write('Hello\n')
+file.write('world,\n')
+file.write('Python')
 # Close the file
+file.close()
