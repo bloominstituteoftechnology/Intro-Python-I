7c30f1817968de92104ed97d2c054543f5805313
Marco A Guzman <marcoguzman16@gmail.com>
2018-08-27 12:23:45 -0700

    added args.py,fileio,foo,modules,scope,slice,tuples.py for cs11 intro to python

---
+++ src/day-1-toy/fileio.py
@@ -0,0 +1,12 @@
+# Use open to open file "foo.txt" for reading
+
+# Print all the lines in the file
+
+# Close the file
+
+
+# Use open to open file "bar.txt" for writing
+
+# Use the write() method to write three lines to the file
+
+# Close the file
