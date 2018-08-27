afc651f71f2fb03bcc4135da199b7cacab7eee49
Adrian Adames <adrianadames@gmail.com>
2018-08-27 16:28:57 -0400

    Started modules.py, but didn't finish. Will come back to later.

--- src/day-1-toy/datatypes.py
+++ src/day-1-toy/datatypes.py
@@ -2,7 +2,7 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
-print(x + y)
+print(x + int(y)) ## why do i get an error when I use int(x) here?
 
 # Write a print statement that combines x + y into the string value 57
-print(x + y)
+print(str(x) + y)
