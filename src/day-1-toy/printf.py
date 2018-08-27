667503e60544032b759f5acfed4ecd080c60fa11
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-08-27 17:52:56 -0400

    modules, printf, lists

--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -6,5 +6,8 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print(f"x is {x}, y is {round(y, 2)}, z is {z}")
 
 # Use the 'format' string method to print the same thing
+
+print("x is {}, y is {}, z is {}".format(x, round(y, 2), z))
