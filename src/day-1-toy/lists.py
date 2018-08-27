421f5792a9919d413525d943d2d1018f2306a5b8
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-08-27 16:46:26 -0700

    slice scope printf lists

--- src/day-1-toy/lists.py
+++ src/day-1-toy/lists.py
@@ -7,19 +7,19 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# [command here]
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# [command here]
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# [command here]
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# [command here]
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
@@ -27,3 +27,5 @@ print(x)
 print(len(x))
 
 # Using a for loop, print all the element values multiplied by 1000
+for num in x:
+   print(num*1000)
--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -5,6 +5,6 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print("%d %f %s" % (x,y,z))
 
 # Use the 'format' string method to print the same thing
