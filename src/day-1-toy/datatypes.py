8dc62eb01be8693e1eae4395435cbb6c35762039
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-27 13:33:54 -0600

    Finished all prompts

--- src/day-1-toy/datatypes.py
+++ src/day-1-toy/datatypes.py
@@ -2,7 +2,9 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
-print(x + y)
+print(x + float(y))
+print(type(x + int(y)))
 
 # Write a print statement that combines x + y into the string value 57
-print(x + y)
+print(str(x) + y)
+print(type(str(x) + y))
