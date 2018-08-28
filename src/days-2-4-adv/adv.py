2ba354ffb0a3dac1d77c78e76003cac222bf394e
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-28 13:16:21 -0500

    Player

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,5 +1,5 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
@@ -38,7 +38,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+Ahmed = Player(room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, location):
+        self.location = location
