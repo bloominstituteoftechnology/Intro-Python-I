fe1c725384b89d3da92a5db0414097c90345592b
Marco A Guzman <marcoguzman16@gmail.com>
2018-08-28 15:00:26 -0700

    player class progress

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,4 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__()
+    def __init__(self, name, spells, health, room):
+        self.name = name
+        self.spells = spells
+        self.health = health
+        self.room = room
