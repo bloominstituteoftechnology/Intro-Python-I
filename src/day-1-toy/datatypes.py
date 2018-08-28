9b40819b4369476569581ee7c92e1a1d244da161
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-08-28 15:03:59 -0500

    completed datatypes.py

--- src/day-1-toy/datatypes.py
+++ src/day-1-toy/datatypes.py
@@ -3,6 +3,8 @@ y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
 print(x + y)
+5 + int("7")
 
 # Write a print statement that combines x + y into the string value 57
 print(x + y)
+str(5) + "7"
