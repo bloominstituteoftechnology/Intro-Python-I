84d0ebf402e36ecb8a2f99a2cd9d6198e95602fc
chainchompa <chainchompa@gmail.com>
2018-08-28 08:58:16 -0400

    completed up to cal plus stretch

--- src/day-1-toy/func.py
+++ src/day-1-toy/func.py
@@ -1,6 +1,11 @@
 # Write a function is_even that will return true if the passed in number is even.
-
+def is_even(num):
+    if num % 2 == 0:
+        print('Even!')
+    else:
+        print('Odd!')
 # Read a number from the keyboard
-num = input("Enter a number: ")
+num = int(input("Enter a number: "))
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
+is_even(num)
