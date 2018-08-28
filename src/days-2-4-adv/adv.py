5f764743c0d41c81490b2634b8b7475f317e1c20
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-08-28 19:27:59 -0400

    Day 2 MVP completelat: 43, git push

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,22 +1,23 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'outside':  Room("outside the cave entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("in the foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("at the grand overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("in the narrow passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("inside the treasure chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -39,6 +40,8 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player(room['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +52,23 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+print('\nWelcome to TAG, the text adventure game!')
+print('\nFollow the prompts on screen.\n~Enter \'Where am I?\' for your current location\n~Enter \'q\' to quit')
+while True:
+    print('\n ~ ~ ~')
+    print('\nYou are currently %s'  % (player.room.name))
+    print('\n... %s' % (player.room.description))
+
+    inp = input('\nWhich direction do you want to go? (n/e/s/w): ')
+    if inp == 'q':
+        print('\nQuitting application...\n')
+        break
+    elif inp in ['n', 's', 'e', 'w']:
+        if hasattr(player.room, inp + '_to'):
+            player.room = getattr(player.room, inp + '_to')
+        else:
+            print('\nTHOUGH SHALL NOT PASS. TRY A DIFFERENT DIRECTION.')
+    elif inp != ['n', 's', 'e', 'w']:
+        print("\nINVALID INPUT, PLEASE ENTER \'n\', \'e\', \'s\' or \'w\'")
+        
