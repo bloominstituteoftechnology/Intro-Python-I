efb2abffd86da4a4c2d8d991e67dd6114e966892
Mariana Cerda <raymond_grc@yahoo.com>
2018-08-28 17:34:10 -0500

    add room class

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -24,21 +24,13 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player("Raymond", room["outside"])
 # Write a loop that:
 #
 # * Prints the current room name
