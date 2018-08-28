3462c29328ce24c1774e29d818a96e6f3f1a6e50
nickolaussmith <nickolaussmith721@gmail.com>
2018-08-28 16:55:40 -0600

    Working on adv.py

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,9 +39,12 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+Nick = Player(room['outside'])
+
 
 # Write a loop that:
 #
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
