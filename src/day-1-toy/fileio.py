0c950c0e2a121545ec946a4d1b4dc97a704dfe19
Randy Calderon <randycweb@gmail.com>
2018-08-28 00:51:06 -0700

    Finished fileio

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,16 @@
 # Use open to open file "foo.txt" for reading
-
+f = open('foo.txt')
 # Print all the lines in the file
-
+print(*f)
 # Close the file
-
+f.close()
 
 # Use open to open file "bar.txt" for writing
-
+bar = open('bar.txt', 'w')
 # Use the write() method to write three lines to the file
-
+bar.write('I know that is is all over\nBut I want to keep going\nI am getting sleepy now')
+print(*bar)
 # Close the file
+bar.close()
+
+#ask why I was getting issues on this on run, though file was created
