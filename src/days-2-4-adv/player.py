666b321bba0d24c94b2a167bb3c0a5413b52ecb0
Katie Gorbell <katiegorbell@gmail.com>
2018-08-28 15:38:35 -0500

    created player and room class

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player(Room):
+    def __init__(self, name, curRoom):
+        super().__init__(curRoom)
+        self.name = name
