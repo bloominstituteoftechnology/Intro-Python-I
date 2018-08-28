00869ca8a04b863f657d4d54f02208724ea28ed6
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-28 09:28:09 -0600

    Finished all prompts

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -4,14 +4,18 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-#def f1(...
+f1 = lambda x, y: x + y
 
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of iteger arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
-# def f2(...
+def f2(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +25,24 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
-#def f3(...
+def f3(*args, value=1):
+    total = 0
+    if len(args) == 1:
+        for num in args:
+            total += num
+        total += value
+        return total
+    else:
+        for num in args:
+            total += num
+        return total
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -41,7 +56,11 @@ print(f3(8))     # Should print 9
 #
 # Google "python keyword arguments".
 
-#def f4(...
+def f4(**kwargs):
+    print("/nData type of argument:", type(kwargs))
+
+    for key, value in kwargs.items():
+        print('key: {}, value: {}'.format(key, value))
 
 # Should print
 # key: a, value: 12
@@ -60,4 +79,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
