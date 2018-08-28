f42a339a7dd3ced1b197865dce50461a8ce15c0e
JacobLayton <jlayjlay3@gmail.com>
2018-08-27 22:09:03 -0700

    Tuples complete

--- src/day-1-toy/tuples.py
+++ src/day-1-toy/tuples.py
@@ -21,12 +21,14 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 
 # Write a function that prints all the values in a tuple
-
+def print_tuple(n):
+    for i in n:
+        print(i)
 # def print_tuple(...
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
