e23541a3992cf449f73c170d77748bda8d95fbf4
TangledTessellations <mcevoya@oregonstate.edu>
2018-08-27 17:37:25 -0700

    Finish MVP

--- src/day-1-toy/scope.py
+++ src/day-1-toy/scope.py
@@ -2,15 +2,15 @@
 # Good reading: https://www.programiz.com/python-programming/global-local-nonlocal-variables
 
 # When you use a variable in a function, it's local in scope to the function.
-x = 12
+x = [12]
 
 def changeX():
-    x = 99
+    x[0] = 99
 
 changeX()
 
 # This prints 12. What do we have to modify in changeX() to get it to print 99?
-print(x)
+print(x[0])
 
 
 # This nested function has a similar problem.
@@ -19,6 +19,7 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
 
     inner()
