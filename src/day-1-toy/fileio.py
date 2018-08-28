bce74cfb4c145ed0b61b569fdc0b930bda6c8c45
JacobLayton <jlayjlay3@gmail.com>
2018-08-28 13:31:48 -0700

    Fileio complete

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,15 @@
 # Use open to open file "foo.txt" for reading
-
+file = open("foo.txt")
 # Print all the lines in the file
-
+for l in file:
+  print(l)
 # Close the file
-
+file.close()
 
 # Use open to open file "bar.txt" for writing
-
+bar_file = open("bar.txt", "w")
 # Use the write() method to write three lines to the file
+bar_file.write("He's on the scene\nUh he's the funky duck\nHe's the first to leave")
 
 # Close the file
+bar_file.close()
