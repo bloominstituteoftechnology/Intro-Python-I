cb362f418234b64f3a2012e700cc0b99b5a8da7a
emaquino44 <prexy44@yahoo.com>
2018-08-28 16:05:25 -0700

    finished room.py

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, room):
+        self.name = name
+        self.room = room
+        self.inventory = []
+        self.xp = 0
+
+    def moveTo(self, direction):
+        self.direction = direction
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
