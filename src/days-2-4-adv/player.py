6556aab2183a6f2cd02e6d73d95dc68ddab5a9e4
Simon <Simon_Freelance@hotmail.com>
2018-08-28 17:49:08 -0400

    Initial Player class creation

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,34 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, age, height, weight, hp, attack, defense, inventory):
+        self.name = name
+        self.age = age
+        self.height = str(height) + ' centimeters' if height != 1 else str(height) + ' centimeter'
+        self.weight = str(weight) + ' pounds' if weight != 1 else str(weight) + ' pound'
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.inventory = inventory
+        self.currentRoom = None
+    
+    def __str__(self):
+        print('Name: {},\nAge: {},\nHeight: {},\nWeight: {}'.format(self.name, self.age, self.height, self.weight))
+    def go_direction(direction):
+        print(direction)
+    
+    def check_hp(self):
+        print(self.hp)
+    
+    def get_current_location(self):
+        print(self.currentRoom)
+    
+    def set_location(self, room):
+        self.currentRoom = room
+    
+    def check_inventory(self):
+        print(self.inventory)
+
+
+# name, age, height, weight, hp, attack, defense, inventory
+player1 = Player('John', 14, 1, 180, 12, 5, 4, {})
