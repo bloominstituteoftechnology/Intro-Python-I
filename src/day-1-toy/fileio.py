15ceb34a8a8007e7d201ea58f962283ac9fdc76e
Simon <Simon_Freelance@hotmail.com>
2018-08-27 22:13:33 -0400

    Finished fileio exercises and generated bar.txt file

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,14 @@
 # Use open to open file "foo.txt" for reading
-
+f = open("foo.txt", 'r')
 # Print all the lines in the file
-
+for line in f:
+    print(line)
 # Close the file
-
+f.close()
 
 # Use open to open file "bar.txt" for writing
-
+barFile = open("bar.txt", 'w')
 # Use the write() method to write three lines to the file
-
+barFile.write('This is a sentence\n I am another sentence\n Goodbye')
 # Close the file
+barFile.close()
