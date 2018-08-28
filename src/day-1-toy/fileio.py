81d3e592d2d250ed29ede1d489e84878c3ea1c41
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-28 11:09:55 -0600

    Finished all prompts

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,15 @@
 # Use open to open file "foo.txt" for reading
-
+foo = open('foo.txt', 'r')
 # Print all the lines in the file
-
+lines = foo.readlines()
+for line in lines:
+    print(line)
 # Close the file
-
+foo.close()
 
 # Use open to open file "bar.txt" for writing
-
+bar = open('bar.txt', 'w')
 # Use the write() method to write three lines to the file
-
+bar.write('Line1\nLine2\nLine3\n')
 # Close the file
+bar.close()
--- src/day-1-toy/lists.py
+++ src/day-1-toy/lists.py
@@ -28,4 +28,4 @@ print(len(x))
 
 # Using a for loop, print all the element values multiplied by 1000
 for i in x:
-    print(i+1000)
+    print(i*1000)
