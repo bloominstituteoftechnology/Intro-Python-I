015f7d60e46be9fd197eb355c44df4a07d91fa18
Daniel Chamorro <dan@dcitllc.com>
2018-08-28 15:53:26 -0400

    args

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -18,6 +18,20 @@ print(f1(1, 2))
 
 # def f2(...
 
+
+# def f2(*args):
+#     sum = 0
+#     for num in args:
+#         sum += num
+#     return sum
+
+# You can use SUM instead
+
+
+def f2(*args):
+    return sum(args)
+
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -26,13 +40,25 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # def f3(...
+
+
+# def f3(required, *args):
+#     if required:
+#         return required + 1
+#     if args:
+#         return sum(args)
+
+
+def f3(a, num=1):
+    return a + num
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -47,6 +73,11 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # def f4(...
+
+def f4(**kwargs):
+    for key in kwargs:
+        print("key: {}, value: {}".format(key, kwargs[key]))
+
 
 # Should print
 # key: a, value: 12
@@ -65,4 +96,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
