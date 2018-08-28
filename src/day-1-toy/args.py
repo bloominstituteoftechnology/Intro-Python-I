ac81e58c51138246353b431683d7c1f6ac34a4b2
Erin Meredith <ekmeredith@att.net>
2018-08-27 22:59:15 -0400

    completed args

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -23,7 +23,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
- #   print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -68,4 +68,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-#f4(d.items())
+f4(**d)
