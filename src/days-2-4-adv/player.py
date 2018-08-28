8dcf69cb271d36d91e4ad9fc767411c8b32eb4ee
Adrian Adames <adrianadames@gmail.com>
2018-08-28 16:27:07 -0400

    First commit for day 2 of Intro-Python project (basic text adventure game).

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,4 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+#Put the player class in player.py based on what you see in `adv.py`.
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,2 +1,4 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+#Put the Room class in room.py based on what you see in `adv.py`.
