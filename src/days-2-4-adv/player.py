d5f96fc1268733a4dea293e93d6d1d56f80dfc34
chainchompa <chainchompa@gmail.com>
2018-08-28 16:21:51 -0400

    created player class

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,6 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init(self, name, location):
+        self.name = name
+        self.location = location
