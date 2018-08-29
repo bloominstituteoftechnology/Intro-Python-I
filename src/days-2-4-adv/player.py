66a7f124d43210d6f9ea5855c5334fed8d5e9be8
snowcoding <vimalshah77@gmail.com>
2018-08-29 13:48:24 -0700

    Added some comments on another way to implement the Player class

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -4,3 +4,14 @@
 class Player():
   def __init__(self,room):
     self.room = room
+
+
+# class Player:
+#     def __init__(self, startLocation):
+#         self.location = startLocation #instance variable
+#     def change_location(self, new_location):
+#         self.location = new_location
+#     def __repr__(self):
+#         return "Current Location: {}".format(self.location)
+#     def __str__(self):
+#         return "Current Location: {}".format(self.location) 
