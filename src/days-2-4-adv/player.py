b72ce84d8090664c06cec9ae000ff33a9738d57c
Daniel Chamorro <dan@dcitllc.com>
2018-08-28 20:54:10 -0400

    Added Player class

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player():
+    def __init__(self, current_room):
+        self.current_room = current_room
