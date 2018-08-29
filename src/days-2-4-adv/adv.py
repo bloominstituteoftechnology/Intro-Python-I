303861143f11758584365d33ad224252de3f97ac
Daniel Chamorro <dan@dcitllc.com>
2018-08-28 21:24:23 -0400

    Added Plaer Object

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -40,6 +40,8 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player(room["outside"])
+print(player)
 # Write a loop that:
 #
 # * Prints the current room name
