fec6633db0f79ac2334e2305a927e453596d2a21
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-29 16:11:57 -0600

    Added Item contructor, added capabilities to Room constructor and Player constructor along with the grab function

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -5,21 +5,21 @@ from player import Player
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ['coin',]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ['coin', 'coin']),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", ['coin',]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", ['treasure', 'coin', 'coin']),
 }
 
 
@@ -34,35 +34,58 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
-# Main
-#
+# Error function and Win Function
+def printErrorString(errorString):
+    print(f'\x1b[1;31;40m\n{errorString}\x1b[0m\n')
+def printWinString(winString):
+    print(f'\033[92m\n{winString}\033[0m\n')
 
-# Make a new player object that is currently in the 'outside' room.
-p1 = Player("Adventurer #1", room['outside'])
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-print('\nWelcome %s to the text adventure game!')
-print('\nFollow prompts on screen.\n~Enter \'q\' to quit\n~Enter \'player\' for name and location')
+validItems = ['coin', 'treasure']
+
+# User creates their player
+p1 = Player((input('What is your name? ')), room['outside'], ['flashlight'])
+# 
+# Welcome message and instructions
+print(f'\nWelcome {p1.name} to the text adventure game!')
+print('\nFollow prompts on screen.\n  ~Enter \'q\' to quit\n  ~Enter \'player\' for account info\n  ~You start with a flashlight')
+# Loops starts
 while True:
+    # Prints the current room name, description, and items
     print('\n ====================================================================================')
-    print('\n%s\n%s' % (p1.location.name, p1.location.description))
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-    inp = input('\nWhich direction (n/s/e/w): ')
-    if inp == 'q':
-        print('Quitting application...')
+    print(f'\033[95m\n{p1.location.name}\033[0m\n{p1.location.description}')
+    print(f'\033[1m\nRoom contains: {p1.location.itemList}\033[0m') if len(p1.location.itemList) > 0 else None
+    # User input is received below
+    inputList = input('\nWhich direction (n/s/e/w): ').split(' ')
+    # Response 'q'
+    if inputList[0] == 'q':
+        printErrorString('\nQuitting application...\n')
         break
-    elif (inp in ['n', 's', 'e', 'w']):
-        if hasattr(p1.location, inp + '_to'):
-            p1.location = getattr(p1.location, inp + '_to')
+    # Response 'n', 's', 'e', 'w'
+    elif (inputList[0] in ['n', 's', 'e', 'w']):
+        if hasattr(p1.location, inputList[0] + '_to'):
+            p1.location = getattr(p1.location, inputList[0] + '_to')
         else:
-            print('\nThis move is not allowed')
-    elif inp == 'player':
-        print('\nName: %s Current Location: %s' % (p1.name, p1.location.name))
+            printErrorString('\nThis move is not allowed')
+    # Response 'grab' + item
+    elif inputList[0] == 'grab':
+        if len(inputList) == 1:
+            printErrorString('\nSpecify an item to grab')
+        elif (inputList[1] in validItems):
+            if inputList[1] in p1.location.itemList:
+                p1.inventory.append(inputList[1])
+                print(f'Grabbed: {inputList[1]}')
+                p1.location.itemList.remove(inputList[1])
+            else:
+                printErrorString('\nThat item doesn\'t exist here')
+        else:
+            printErrorString('\nInvalid entry, try again')
+    # Response 'player'
+    elif inputList[0] == 'player':
+        print(f'\nName: {p1.name} Inventory: {p1.inventory}')
+    # Catch all error
     else:
-        print('\nInvalid entry, try again')
-# If the user enters "q", quit the game.
+        printErrorString('\nInvalid entry, try again')
+    # Checks if player has 'treasure' in inventory
+    if 'treasure' in p1.inventory:
+        printWinString(f'\n{p1.name} you found the treasure and won the game!')
+        break
