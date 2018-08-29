9b56198deaae64b06d6100092a43f047381ec60f
Flintbean <ajmerando1@yahoo.com>
2018-08-28 19:06:49 -0700

    8 excersises done

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,11 +1,14 @@
-# Use open to open file "foo.txt" for reading
-
-# Print all the lines in the file
-
+foo = open("src/day-1-toy/foo.txt", "r")
+print(foo.read())
 # Close the file
-
+foo.close()
 
 # Use open to open file "bar.txt" for writing
+bar = open("src/day-1-toy/bar.txt", "w")
+lines = ["line1\n", "line2\n", "line3\n"]
+bar.writelines(lines)
+bar.close()
+
 
 # Use the write() method to write three lines to the file
 
--- src/day-1-toy/obj.py
+++ src/day-1-toy/obj.py
@@ -1,13 +1,43 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
-        
+
+
+class LatLon(object):
+    lat = 0
+    lon = 0
+
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon.
+
+
+class Waypoint(LatLon):
+    name = ''
+
+    def __init__(self, name, lat, lon):
+        LatLon.__init__(self, lat, lon)
+        self.name = name
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
+
+class Geocache(Waypoint):
+    difficulty = ''
+    size = ''
+
+    def __init__(self, name, difficulty, size, lat, lon):
+        Waypoint.__init__(self, name, lat, lon)
+        self.difficulty = difficulty
+        self.size = size
+    def __str__(self):
+        return str(self.difficulty)
+
+
 # Make a new waypoint "Catacombs", 41.70505, -121.51521
+w = Waypoint("Catacombs", 41.70505, -121.51521)
 
 # Print it
 #
@@ -16,6 +46,6 @@
 print(w)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
-
+g = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 # Print it--also make this print more nicely
 print(g)
