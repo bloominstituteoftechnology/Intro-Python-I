cc585f673c0922428e7f77ca81b963870bf243a9
Nicky Chen <nicky.chen0@gmail.com>
2018-08-29 14:32:15 -0700

    adding items

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -5,21 +5,21 @@ from player import Player
 # Which rooms are empty?? Who knows?
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", ["rocks", "paper", "gum"]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ["smoke"]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ["chandelier", "pants"]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", ["pen"]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", ["newspaper", "treasure"]),
 }
 
 
@@ -57,18 +57,121 @@ allowed = ['north', 'south', 'east', 'west']
 """print(player.room)
 player = Player(room['outside'].n_to)
 print(player.room)"""
+suppressRoomPrint = False
 
 while True:
-    print("----------------Treasure Game----------------")
-    print("You are at {}.".format(player.room.getDescription()))
+    if suppressRoomPrint:
+        suppressRoomPrint = False
+    else:
+        print("----------------Treasure Game----------------")
+        print("You are at {}.".format(player.room.getDescription()))
     message = input("\nWhere do you want to go? (north/south/east/west): ")
     if message == 'quit':
         print("Game over")
         break
     if message not in allowed:
-        print("\nPlease only choose north, south, east, or west.\n")
-    if hasattr(player.room, message[0] + '_to'):
-        # print("Worked!")
-        player.room = getattr(player.room, message[0] + '_to')
-    elif hasattr(player.room, message[0] + '_to') == False:
-        print("That doesn't lead anywhere, please choose another direction.")
+        #print("\nPlease only choose north, south, east, or west.\n")
+        newMessage = message.split(" ")
+        if len(newMessage) > 1:
+            if (newMessage[0] == "take" or newMessage[0] == "get"):
+                print("take or get")
+            elif (newMessage[0] == "drop"):
+                pass
+        else:
+            print("Not a valid option, please choose another.")
+    else:
+        if hasattr(player.room, message[0] + '_to'):
+            # print("Worked!")
+            player.room = getattr(player.room, message[0] + '_to')
+        elif hasattr(player.room, message[0] + '_to') == False:
+            print("That doesn't lead anywhere, please choose another direction.")
+            suppressRoomPrint = True
+
+
+"""
+# Game variables
+suppressRoomPrint = False
+validDirections = ["n", "s", "e", "w"]
+
+
+
+############
+# Command functions
+############
+commands = {}
+commands["n"] = moveCommand
+commands["s"] = moveCommand
+commands["e"] = moveCommand
+commands["w"] = moveCommand
+commands["l"] = lookCommand
+commands["look"] = lookCommand
+
+commandsHelp = {}
+commandsHelp["n"] = "Move North"
+commandsHelp["s"] = "Move South"
+commandsHelp["e"] = "Move Eath"
+commandsHelp["w"] = "Move West"
+commandsHelp["l"] = "Look somewhere"
+commandsHelp["look"] = "Look somewhere"
+
+
+def moveCommand(player, *args):
+    newRoom = player.location.getRoomInDirection(args[0])
+    if newRoom == None:
+        printErrorString("You cannot move in that direction!")
+    else:
+        player.change_location(newRoom)
+    return False
+
+def lookCommand(player, *args):
+
+    if not (args[0] == "l" or args[0] == "look"):
+        printErrorString("That is not a look command")
+    elif len(args) == 1:
+        return False
+    elif args[1] in validDirections:
+        lookRoom = player.location.getRoomInDirection(args[1])
+        if lookRoom is not None:
+            print (lookRoom)
+        else:
+            printErrorString("There is nothing in that direction.")
+        return True
+
+#######
+# Util
+#######
+
+def printErrorString(errorString):
+    print(f'\x1b[1;31;40m\n{errorString}\x1b[0m\n')
+    global suppressRoomPrint
+    suppressRoomPrint = True
+
+# Room.connect(<Room>, <direction>)
+
+
+
+######
+# Start Game loop here
+######
+
+
+player = Player( input("\nWhat is your name?: ") , room['outside'])
+
+print (f"Welcome, {player.name}!\n")
+
+while True:
+    if suppressRoomPrint:
+        suppressRoomPrint = False
+    else:
+        print (player.location)
+    inputList = input(">>> ").split(" ")
+    if inputList[0] == "q":
+        break
+    elif inputList[0] in commands:
+        suppressRoomPrint = commands[inputList[0]](player, *inputList)
+    elif inputList[0] == "help":
+        for command in commandsHelp:
+            print (f"{command} - {commandsHelp[command]}")
+    else:
+        printErrorString("I do not understand that command")
+"""
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -3,9 +3,10 @@
 
 
 class Room():
-    def __init__(self, name, description):
+    def __init__(self, name, description, items=[]):
         self.name = name
         self.description = description
+        self.items = items
 
     def getDescription(self):
         return ("{}.\n{}.".format(self.name, self.description))
