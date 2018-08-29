ba93511c990a462e79667c65575349d0eaba1132
Alec <afroenygam@gmail.com>
2018-08-29 17:47:32 -0400

    Functionality added to Room classes to allow them to hold items, and display held items.

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -7,21 +7,31 @@ import textwrap
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",
+                     *[Item("Rock", """Who knows how useful this could be!"""),
+                     Item("Grass", """A nutritious treat should you get hungry in your travels"""),
+                     Item("Key", """Who knows what doors this may open?""")]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", *[Item("Umbrella", """This will protect against
+suppressive  waterpower""")]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", *[Item("Axe",
+"""Heeeerrre's Johnny!"""), Item("Native American Artifact", """Legend says this
+artifact allows the possessor to communicate telepathically with the ghost of
+Scatman Crothers""")]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", *[Item("Torch", """Batteries
+included"""), Item("Wet Mud", """Gross!""")]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", *[Item("Empty Treasure Chest",
+"A big box that once held great treasure"), Item("Scribbled Note", """A note
+containing a famous director's explanation of a mysterious hotel room...""")]),
 }
 
 
@@ -79,9 +89,13 @@ while(True):
             print('...{}\n'.format(player1.location.description))
         else:
             print('\nNone has traveled catacorner to the three dimensions...invalid input try again...')
-    elif inp == 'i':
+    elif inp == 'inventory':
         for item in player1.inventory:
             print('{} {}'.format(item.name, item.description))
+    elif inp == "look":
+        print('\nThis room contains...')
+        for item in player1.location.items:
+            print('\n-- {}: {}--\n'.format(item.name, item.description))
     else:
         print('None has traveled catacorner to the three dimensions...invalid input try again...')
 
