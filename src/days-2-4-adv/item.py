d3ec5508ae4deb1182379eebe858bc2aba8537ae
sean chavez <seanchavez@seanchavezsMBP.home>
2018-08-30 15:59:35 -0700

    Add LightSource subclass to Item; add super() to Treasure; add on_drop to Item

--- src/days-2-4-adv/item.py
+++ src/days-2-4-adv/item.py
@@ -6,8 +6,12 @@ class Item:
     def on_take(self, player):
         pass
 
+    def on_drop(self, player):
+        pass
+
 class Treasure(Item):
     def __init__(self, name, description, value):
+        super().__init__(name, description)
         self.value = value
         self.picked_up = False
 
@@ -15,4 +19,7 @@ class Treasure(Item):
         if not self.picked_up:
             player.score += self.value
             self.picked_up = True
-        
+
+class LightSource(Item):
+    def __init__(self, name, description): 
+        super().__init__(name, description)      
