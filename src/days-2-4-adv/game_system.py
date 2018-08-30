e0280619dd466bb199c7501a67df3908dc72b734
Simon <Simon_Freelance@hotmail.com>
2018-08-30 18:28:49 -0400

    Made a more in-depth help guide

--- src/days-2-4-adv/game_system.py
+++ src/days-2-4-adv/game_system.py
@@ -2,13 +2,52 @@ class Game_System:
     @staticmethod
     def display_help():
         print("""
-        Type in a command:
-
-        Directions[n,s,e,w]
-
-        Get info: [look for items, look for enemies, check inventory]
-
-        Battle: [a [Enemy]: attack [Enemy]]
-
-        Other commands: [q: quit]
+    'n': partial(player.go_direction, 'n'),
+    's': partial(player.go_direction, 's'),
+    'e': partial(player.go_direction, 'e'),
+    'w': partial(player.go_direction, 'w'),
+    'help': display_help,
+    'hp': player.get_hp,
+    'health': player.get_hp,
+    'my': {
+        'health': player.get_hp,
+        'hp': player.get_hp
+    },
+    'q': quit,
+    'quit': quit,
+    'loc': player.get_current_location,
+    'look':{
+        'for': {
+            'items': player.look_for_items,
+            'enemies': player.look_for_enemies
+        },
+        'around': player.get_current_location
+    },
+    'where': {
+        'am': {
+            'i': player.get_current_location
+        },
+        'is': {
+            'this': {
+                'place': player.get_current_location
+            }
+        }
+    },
+    'check':{
+         'inventory': player.get_inventory,
+    },
+    'inventory': player.get_inventory,
+    'get': {
+        'item': player.add_item,
+        'hp': player.get_hp,
+        'current': {
+            'location': player.get_current_location
+        }
+    },
+    'grab': {
+        'item': player.add_item
+    },
+    'drop': {
+        'item': player.drop_item
+    }
         """)
