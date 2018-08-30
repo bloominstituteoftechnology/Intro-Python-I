df68d35b7754161f955fe8af6adc44a95d6b3cec
snowcoding <vimalshah77@gmail.com>
2018-08-29 23:25:59 -0700

    Added removeItem and displayItems in player class

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,11 +1,6 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
-# class Player():
-#   def __init__(self,room):
-#     self.room = room
-
-
 class Player:
   def __init__(self, start_room):
     self.room = start_room #instance variable
@@ -13,7 +8,16 @@ class Player:
 
   def addItem(self,item):
     self.items.append(item)
-    
+  
+  def removeItem(self, item):
+    if len(self.items) > 0:
+      for i in self.items:
+        if i.name == item:
+          self.items.remove(i)
+    else:
+      print('Item not available to remove!')
+
+
   def set_room(self, new_room):
     self.room = new_room
 
@@ -21,3 +25,10 @@ class Player:
     return "Current Location: {}".format(self.room)
   def __str__(self):
     return "Current Location: {}".format(self.room) 
+
+  def displayItems(self):
+    if (self.items):
+      for i in self.items:
+        print('\x1b[1;33;40m' + i.name + '\x1b[0m')
+    else:
+      print('')
