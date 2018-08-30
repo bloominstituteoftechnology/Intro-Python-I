5e81e2a5f7829f7025dd863d03f9195487d0c412
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-08-30 18:13:58 -0400

    Day 4 MVP and Stretch Goals done

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -2,25 +2,39 @@
 # currently.
 
 from item import itemList
+from item import LightSource
 
 class Player:
-    def __init__( self, name, location, items):
+    def __init__( self, name, location, items, score=0):
         self.location = location
         self.name = name
         self.items = list(items)
-        
+        self.score = score
+
     def change_location(self, new_location):
         self.location = new_location
 
     def player_inventory(self):
         if len(self.items) == 0:
-            print("No items available") 
+            print("No items available")
         elif len(self.items) >= 1:
             item_list = "\n"
             for item in self.items:
-                item_list += f"{item.name}\n"            
-            print(f"\033[94m\nPlayer's Inventory:{item_list}\033[0m") 
+                item_list += f"{item.name}\n"
+            print(f"\033[94m\nPlayer's Inventory:{item_list}\033[0m")
             input("Press Enter to Exit Inventory")
+
+    def inspect(self, item):
+        if not item:
+            print("Select an item, dumbass")
+        elif item.lower() in list(map(lambda item : item.name.lower() , self.location.items + self.items)):
+            print("Name: {}\nDescription: {}".format(itemList[item].name, itemList[item].description))
+            if hasattr(itemList[item], 'value'):
+                print("\nValue: {}".format(itemList[item].value))
+            input("Press Enter to exit description")
+        else:
+            print("Man, I don't know what you're talking about")
+
     def move(self, input):
         newRoom = self.location.getRoomInDirection(input)
         if newRoom == None:
@@ -30,11 +44,18 @@ class Player:
     def get(self, input):
         if not input:
             print("Select an item, dumbass")
+        elif self.location.is_Light == False:
+            print("Good luck finding that in the dark!")
         else:
             if input.lower() in list(map(lambda item : item.name.lower() , self.location.items)):
-                print('Item added to inventory!')
-                self.items.append(itemList[input])
-                self.location.items.remove(itemList[input])
+                itemList[input].on_take()
+                if hasattr(itemList[input], 'value'):
+                    self.score += itemList[input].value
+                    self.items.append(itemList[input])
+                    self.location.items.remove(itemList[input])
+                else:
+                    self.items.append(itemList[input])
+                    self.location.items.remove(itemList[input])
             else:
                 print("No Item by that name")
 
@@ -44,7 +65,12 @@ class Player:
         else:
             if input.lower() in list(map(lambda item : item.name.lower() , self.items)):
                 print('Item dropped from inventory!')
-                self.items.remove(itemList[input])
-                self.location.items.append(itemList[input])
+                if hasattr(itemList[input], 'value'):
+                    self.score -= itemList[input].value
+                    self.items.remove(itemList[input])
+                    self.location.items.append(itemList[input])
             else:
                 print("No Item by that name")
+
+    def get_score(self):
+        print(f"\nCurrent Score: {self.score}")
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -2,9 +2,11 @@
 # description attributes.
 
 from item import itemList
+from item import LightSource
+from player import Player
 
 class Room:
-    def __init__(self, title, description, items):
+    def __init__(self, title, description, items, is_Light):
         self.title = title
         self.description = description
         self.items = list(items)
@@ -12,6 +14,7 @@ class Room:
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.is_Light = is_Light
 
     def getRoomInDirection(self, direction):
         if direction =="n":
@@ -24,15 +27,30 @@ class Room:
             return self.w_to
         else:
             return 'None'
-            
+
+    def check_for_light(self, player):
+        lighted = False
+        for item in player.items:
+            if type(item) == LightSource:
+                lighted = True
+        for item in self.items:
+            if type(item) == LightSource:
+                lighted = True
+        if lighted:
+            self.is_Light = True
+            return player.location.print_items()
+        else:
+            self.is_Light = False
+            return "\nCan't see, It's pitch black!"
+
     def print_items(self):
         if len(self.items) == 0:
             return "No items available"
         elif len(self.items) >= 1:
             item_list = "\n"
             for item in self.items:
-                item_list += f"{item.name}\n"            
-            return item_list   
+                item_list += f"{item.name}\n"
+            return item_list
 
     def connectRooms(self, destinationRoom, direction):
         if direction == "n":
@@ -52,21 +70,21 @@ class Room:
 
 roomlist = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons",  [itemList['torch'], itemList['shield']] ),             
+                     "North of you, the cave mount beckons",  [itemList['torch'], itemList['shield']], True),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", [itemList['coin']]),
+passages run north and east.""", [itemList['coin']], True),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", [itemList['telescope']]),
+the distance, but there is no way across the chasm.""", [itemList['telescope']], True),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", [itemList['sword']]),
+to north. The smell of gold permeates the air.""", [itemList['sword']], False),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", [itemList['coin'], itemList['coin'], itemList['chest']]),
+earlier adventurers. The only exit is to the south.""", [itemList['diamond'], itemList['coin'], itemList['chest']], False),
 
-    'dungeon': Room("Dungeon Chamber", """You've stumbled into a trap!""", [itemList['hat'], itemList['hat'], itemList['hat']])
+    'dungeon': Room("Dungeon Chamber", """You've stumbled into a trap!""", [itemList['hat'], itemList['hat'], itemList['ruby']], False)
 }
