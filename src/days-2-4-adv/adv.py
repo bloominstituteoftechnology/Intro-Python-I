4ba9d5a6578c00a4ce9fc6a9f12c4b06931fe1fd
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-08-30 15:52:33 -0600

    Refactored format of adv.py

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -7,21 +7,30 @@ room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons", [], True),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", ['coin', 'hat'], True),
+    'foyer':    Room("Foyer", 
+                    """Dim light filters in from the south. Dusty passages run north and east.""",
+                    ['coin', 'hat'],
+                    True),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", ['coin', 'coin', 'lamp'], True),
+    'overlook': Room("Grand Overlook",
+                    """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.""",
+                    ['coin', 'coin', 'lamp'],
+                    True),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", ['coin', 'shovel'], True),
+    'narrow':   Room("Narrow Passage",
+                    """The narrow passage bends here from west to north. The smell of gold permeates the air.""",
+                    ['coin', 'shovel'],
+                    True),
 
-#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-# chamber! Sadly, it has already been completely emptied by
-# earlier adventurers. The only exit is to the south.""", ['coin', 'coin', 'treasure'], False),
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Treasures are worht 10 pts. The only exit is to the south.""", ['coin', 'coin', 'treasure'], False),
+    'treasure': Room("Treasure Chamber",
+                    """You've found the long-lost treasurechamber! Treasures are worht 10 pts. The only exit is to the south.""", 
+                    ['coin', 'coin', 'treasure'],
+                    False),
+
+#   'treasure': Room("Treasure Chamber",
+#                   """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.""",
+#                   ['coin', 'coin', 'treasure'],
+#                   False),
 }
 
 
