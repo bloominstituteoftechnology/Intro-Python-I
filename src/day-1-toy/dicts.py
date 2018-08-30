8d35805b8ad9e241e0bb0597eb995409175aeeae
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-08-30 13:55:25 -0500

    completed dicts.py

--- src/day-1-toy/dicts.py
+++ src/day-1-toy/dicts.py
@@ -8,22 +8,32 @@
 
 waypoints = [
     {
-        "lat": 43,
-        "lon": -121,
-        "name": "a place"
+        "lat": 715,
+        "lon": 1955,
+        "name": "Disneyland"
     }, 
     {
-        "lat": 41,
-        "lon": -123,
-        "name": "another place"
+        "lat": 19,
+        "lon": 65,
+        "name": "Disneyworld"
     }, 
     {
-        "lat": 43,
-        "lon": -122,
-        "name": "a third place"
+        "lat": 2006,
+        "lon": 9,
+        "name": "Humble"
     }
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
 
+for city in waypoints:
+    print('{}: latitude = {}, longitude = {}'.format(city["name"], city["lat"], city["lon"]))
+
 # Add a new waypoint to the list
+
+waypoints.append({
+    "lat": 23.45,
+    "lon": -123,
+    "name": "Bombadil's House"
+})
+print(waypoints)
