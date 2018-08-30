efaf676fe5cfe618d0f3f074e776ec0ad2337f54
chainchompa <chainchompa@gmail.com>
2018-08-29 20:22:45 -0400

    created items class, added to room

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -5,21 +5,22 @@ from player import Player
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",
+                     []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
 
 
@@ -42,7 +43,8 @@ room['treasure'].s_to = room['narrow']
 player = Player(input("Please enter your name: "), room['outside'])
 incorrectMessage = '\nThere is no path in that direction'
 
-print(f'\nHello {player.name}')
+# print(f"\nHello {player.name}")
+print("Hello %s!" % player.name)
 # Write a loop that:
 #
 # * Prints the current room name
@@ -50,8 +52,11 @@ print(f'\nHello {player.name}')
 # * Waits for user input and decides what to do.
 #
 while True:
-    print(f'\nLocation: {player.location.name}')
-    print(f'{player.location.description}')
+    # print(f'\nLocation: {player.location.name}')
+    print("\nLocation: %s" % player.location.name)
+    print("%s\n" % player.location.description)
+
+    # print(f'{player.location.description}')
 
     direction = str(input('Which direction do you walk?  '))
 
@@ -60,21 +65,25 @@ while True:
     elif direction.lower() == 'n' or direction.lower() == 'north':
         if player.location.n_to:
             player.location = player.location.n_to
+            print('You decide to head north')
         else:
             print(incorrectMessage)
     elif direction.lower() == 's' or direction.lower() == 'south':
         if player.location.s_to:
             player.location = player.location.s_to
+            print('You decide to head south')
         else:
             print(incorrectMessage)
     elif direction.lower() == 'w' or direction.lower() == 'west':
         if player.location.w_to:
             player.location = player.location.w_to
+            print('You decide to head west')
         else:
             print(incorrectMessage)
     elif direction.lower() == 'e' or direction.lower() == 'east':
         if player.location.e_to:
             player.location = player.location.e_to
+            print('You decide to head west')
         else:
             print(incorrectMessage)
 # If the user enters a cardinal direction, attempt to move to the room there.
