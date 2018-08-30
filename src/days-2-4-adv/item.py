46f053cedc1dd0911505e59ed75ddfc53dc5bd02
Alec <afroenygam@gmail.com>
2018-08-30 17:40:48 -0400

    On take methods updated

--- src/days-2-4-adv/item.py
+++ src/days-2-4-adv/item.py
@@ -7,11 +7,22 @@ class Item:
         self.description = description
     def __str__(self):
         return '{}: {}'.format(self.name, self.description)
+    def on_take(self):
+        print(f'\x1b[1;27;42m -You have obtained {self.name}!- \x1b[0m')
+        return 0
 
 class Treasure(Item):
-    def __init__(self, name, description, value):
+    def __init__(self, name, description, value, notTakenBefore=True):
         super().__init__(name, description)
         self.value = value
-
+        self.notTakenBefore = notTakenBefore
     def __str__(self):
         return '(Treasure!) {}: {}, Value: {}\n'.format(self.name, self.description, self.value)
+    def on_take(self):
+        if self.notTakenBefore:
+            print(f'\x1b[1;27;42m -Treasure obtained! You took {self.name}! {self.value} points to Gryffindor!- \x1b[0m')
+            self.notTakenBefore = False
+            return self.value
+        else:
+            print(f'\x1b[1;27;42m -Slimy fingered, ornery knave, lazy bum! You dropped your {self.name} and tried to pick it up again. No points for you!- \x1b[0m')
+            return 0
