e94bb722dd6aeb16466e33253c5204b3d25dbbcc
Alec <afroenygam@gmail.com>
2018-08-29 22:08:08 -0400

    Extra code deleted from room.py

--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -25,26 +25,3 @@ class Room:
             return self.w_to
         else:
             return None
-
-'''
-    def connect(self, Room, direction):
-        if self.name == "Outside Cave Entrance" and direction == n:
-            self.name = "Foyer"
-            self.description = """Dim light filters in from the south. Dusty
-            passages run north and east."""
-            print(self.name)
-        elif self.name == "Foyer":
-            self.name = "Grand Overlook"
-            self.description = """A steep cliff appears before you, falling
-            into the darkness. Ahead to the north, a light flickers in
-            the distance, but there is no way across the chasm."""
-            print(self.name)
-        elif self.name == "Narrow Passage":
-            self.name = "Treasure Chamber"
-            self.description = """You've found the long-lost treasure
-            chamber! Sadly, it has already been completely emptied by
-            earlier adventurers. The only exit is to the south."""
-            print(self.name)
-        else:
-            print("Thwack! You just hit a brick wall, choose another direction")
-'''
