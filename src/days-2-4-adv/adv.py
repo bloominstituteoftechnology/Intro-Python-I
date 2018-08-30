36cf80452572a3b8e51060f66328424ec099a117
Jasper <joshtietz11@gmail.com>
2018-08-30 18:04:24 -0500

    missed some commits for the game part'

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,37 +1,72 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", 'N'),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", 'S', 'N', 'E'),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm. There is a chest near the door to the south""", 'S'),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", 'W', 'N'),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers, but you might find something if you look around. The only exit is to the south.""", 'S'),
 }
 
 
+# Add items to rooms
+
+room['overlook'].addItem('key' ,"You pick up the key, best to hold on to it, it might come in handy later.")
+room['treasure'].addItem('rock', 'You pick up the jagged rock, maybe you can throw it at something?')
+room['treasure'].addItem('note', 'You pick up old note, "captivated by the view over the cliff, you never even notice what\'s above you"')
+
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+def connectRooms(r1, to, r2):
+    # r1 goes (N, S, E, W, U, or D) to r2
+    r1.connect(r2, to)
+    if to is 'N':
+        r2.connect(r1, "S")
+    elif to is 'E':
+        r2.connect(r1, "W")
+    elif to is 'S':
+        r2.connect(r1, "N")
+    elif to is 'W':
+        r2.connect(r1, "E")
+    elif to is 'U':
+        r2.connect(r1, "D")
+    elif to is 'D':
+        r2.connect(r1, "U")
+
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+
+connectRooms(room['outside'], 'N', room['foyer'])
+
+# room['overlook'].s_to = room['foyer']
+# room['foyer'].n_to = room['overlook']
+
+connectRooms(room['overlook'], 'S', room['foyer'])
+
+# room['narrow'].w_to = room['foyer']
+# room['foyer'].e_to = room['narrow']
+
+connectRooms(room['narrow'], 'W', room['foyer'])
+
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
+
+connectRooms(room['narrow'], 'N', room['treasure'])
+
 
 #
 # Main
@@ -49,3 +84,38 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+direction = [ 'N', 'E', 'S', 'W', 'U', 'D' ]
+hideRoomDesciption = 'false'
+
+player = Player( input("\nWhat is your name?: ") , room['outside'])
+
+print (f'\nWelcome, {player.name}!\n')
+
+while 'true':
+    if hideRoomDesciption is 'true':
+        hideRoomDesciption = 'false'
+    else:
+        print(f'\n{player.roomDescription()}\n')
+
+    inputList = input("\nWhat would you like to do?: ").upper().split(' ')
+
+    if inputList[0] == 'Q':
+        break
+
+    elif inputList[0] in direction:
+        if player.move(inputList[0]) == 'false':
+            hideRoomDesciption = 'true'
+            print('\nYou cannot go that way!')
+
+    elif inputList[0] == 'SEARCH' or 'OPEN':
+        hideRoomDesciption = 'true'
+        if len(player.location.items) == 0:
+            print('you don\'t find anything')
+        elif len(player.location.items) == 1:
+            for item in player.location.items:
+                print(f'You find a {item}\n')
+        elif len(player.location.items) > 1:
+            print('You find some items: ')
+            for item in player.location.items:
+                print(item)
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,2 +1,26 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room(object):
+    def __init__(self, name, description, *paths):
+        self.name = name
+        self.description = description
+        self.paths = paths
+        self.items = {}
+
+    def connect(self, room, direction):
+        if direction == 'N':
+            self.north = room
+        elif direction == 'E':
+            self.east = room
+        elif direction == 'S':
+            self.south = room
+        elif direction == 'W':
+            self.west = room
+        elif direction == 'U':
+            self.up = room
+        elif direction == 'D':
+            self.down = room
+
+    def addItem(self, name, description):
+        self.items[name] = description
