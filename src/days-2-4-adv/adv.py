9bfa33d11d283c6ce851600ece5b20948833ca1f
Simon <Simon_Freelance@hotmail.com>
2018-08-30 18:27:08 -0400

    Added more commands in player command list

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -89,6 +89,7 @@ while restart == 'r':
 player.set_location(room['outside'])
 Dialogue.greet_player(player.name)
 
+# These commands are traversed with a tree algorithm
 playerCommands = {
     'n': partial(player.go_direction, 'n'),
     's': partial(player.go_direction, 's'),
@@ -96,16 +97,48 @@ playerCommands = {
     'w': partial(player.go_direction, 'w'),
     'help': Game_System.display_help,
     'hp': player.get_hp,
+    'health': player.get_hp,
+    'my': {
+        'health': player.get_hp,
+        'hp': player.get_hp
+    },
     'q': quit,
+    'quit': quit,
+    'loc': player.get_current_location,
     'look':{
         'for': {
             'items': player.look_for_items,
             'enemies': player.look_for_enemies
         },
+        'around': player.get_current_location
+    },
+    'where': {
+        'am': {
+            'i': player.get_current_location
+        },
+        'is': {
+            'this': {
+                'place': player.get_current_location
+            }
+        }
     },
     'check':{
          'inventory': player.get_inventory,
     },
+    'inventory': player.get_inventory,
+    'get': {
+        'item': player.add_item,
+        'hp': player.get_hp,
+        'current': {
+            'location': player.get_current_location
+}
+    },
+    'grab': {
+        'item': player.add_item
+    },
+    'drop': {
+        'item': player.drop_item
+    }
 }
 
 
