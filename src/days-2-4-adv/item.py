5c083c461550193984da1e4a1f95db9edcd8c289
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-30 17:14:33 -0500

    class Tresure(Item)

--- src/days-2-4-adv/item.py
+++ src/days-2-4-adv/item.py
@@ -5,3 +5,9 @@ class Item:
 
     def __str__(self):
         return self.name
+
+
+class Treasure(Item):
+    def __init__(self, name, description, value):
+        super().__init__(name, description)
+        self.value = value
