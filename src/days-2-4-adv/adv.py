6e219b5f603103a9b2a3910fb023d88c52a7da12
Gabriel Duquette <gabrielduquette@gmail.com>
2018-08-30 00:40:34 -0400

    refactored and centralized

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,6 +1,26 @@
-from room import Room
-from player import Player
-from item import Item
+class Room:
+    def __init__(self, name, description, items=[], adjacent_rooms={}):
+        self.name = name
+        self.description = description
+        self.items = items
+        self.adjacent_rooms = adjacent_rooms
+    def add_item(self, item):
+        self.items.append(item)
+    def remove_item(self, item):
+        del self.items[self.items.index(item)]
+
+class Player:
+    def __init__(self, room, items=[]):
+        self.room = room
+        self.items = items
+    def get(self, item):
+        self.items.append(item)
+    def drop(self, item):
+        del self.items[self.items.index(item)]
+
+class Item:
+    def __init__(self, name):
+        self.name = name
 
 item = {
     'knife': Item('knife'),
@@ -10,76 +30,69 @@ item = {
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                "North of you, the cave mount beckons",
-                [item['knife'], item['shield']]),
+                    "North of you, the cave mount beckons",
+                    [item['knife'], item['shield']],
+                    {'n': 'foyer', 's': None, 'w': None, 'e': None}),
 
     'foyer':    Room("Foyer",
-                """Dim light filters in from the south. Dusty passages run north and east."""),
+                    """Dim light filters in from the south. Dusty passages run north and east.""",
+                    [],
+                    {'n': 'overlook', 's': 'outside', 'w': None, 'e': 'narrow'}),
 
     'overlook': Room("Grand Overlook",
-                """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+                    """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.""",
+                    [],
+                    {'n': None, 's': 'foyer', 'w': None, 'e': None}),
 
     'narrow':   Room("Narrow Passage",
-                """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+                    """The narrow passage bends here from west to north. The smell of gold permeates the air.""",
+                    [],
+                    {'n': 'treasure', 's': None, 'w': 'foyer', 'e': None}),
 
     'treasure': Room("Treasure Chamber",
-                """You've found the long-lost treasure chamber! Sadly, it has already been almost completely emptied by earlier adventurers. The only exit is to the south.""",
-                [item['coin']]),
+                    """You've found the long-lost treasure chamber! Sadly, it has already been almost completely emptied by earlier adventurers. The only exit is to the south.""",
+                    [item['coin']],
+                    {'n': None, 's': 'narrow', 'w': 'foyer', 'e': None})
 }
-
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
 
 player = Player(room['outside'])
 
 while True:
     print("\nYou are in the %s. %s" % (player.room.name, player.room.description))
     if player.room.items:
-        print(f"Inside the room, you see the following items: {', '.join(item.name for item in player.room.items)}")
-    inp = input(">>> ").split(" ")
-    if inp[0] == "q":
-        print("\nYOU HAVE DIED.")
-        break
-    elif inp[0] == "get" and not player.room.items:
-        print("There is nothing for you to take.")
-    elif inp[0] == "drop" and not player.items:
-        print("You have no items.")
-    elif inp[0] == "get":
-        player.get(item[inp[1]])
-        player.room.remove_item(item[inp[1]])
-    elif inp[0] == "drop":
-        player.drop(item[inp[1]])
-        player.room.add_item(item[inp[1]])
-    elif inp[0] == "items":
-        if player.items:
-            print(f"You have the following items: {', '.join(item.name for item in player.items)}")
+        print(f"\nInside the room, you see the following items: {', '.join(item.name for item in player.room.items)}")
+    inp = input("\n>>> ").split(" ")
+    if len(inp) == 1 or len(inp) == 2 and not inp[1]:
+        command, target = inp[0], None
+        if command == "q":
+            print("\nYOU HAVE DIED.")
+            break
+        elif command == "items":
+            if player.items:
+                print(f"\nYou have the following items: {', '.join(item.name for item in player.items)}")
+            else:
+                print("\nYou have no items.")
+        elif command in ['n', 's', 'e', 'w']:
+            if player.room.adjacent_rooms[command] == None:
+                print("\nYou cannot move that way.")
+            else:
+                new_room = player.room.adjacent_rooms[command]
+                player.room = room[new_room]
         else:
-            print("You have no items.")
-    elif inp[0] == "n":
-        if player.room.n_to:
-            player.room = player.room.n_to
+            print ("\nI did not recognize that command.")
+    elif len(inp) == 2:
+        command, target = inp
+        if command == "get" and not player.room.items:
+            print("\nThere is nothing for you to take.")
+        elif command == "drop" and not player.items:
+            print("\nYou have no items.")
+        elif command == "get":
+            player.get(item[target])
+            player.room.remove_item(item[target])
+        elif command == "drop":
+            player.drop(item[target])
+            player.room.add_item(item[target])
         else:
-            print("\nThat way is closed to you.")
-    elif inp[0] == "s":
-        if player.room.s_to:
-            player.room = player.room.s_to
-        else:
-            print("\nThat way is closed to you.")
-    elif inp[0] == "e":
-        if player.room.e_to:
-            player.room = player.room.e_to
-        else:
-            print("\nThat way is closed to you.")
-    elif inp[0] == "w":
-        if player.room.w_to:
-            player.room = player.room.w_to
-        else:
-            print("\nThat way is closed to you.")
+            print ("\nI did not recognize that command.")
     else:
         print ("\nI did not recognize that command.")
--- src/days-2-4-adv/item.py
+++
@@ -1,3 +0,0 @@
-class Item:
-    def __init__(self, name):
-        self.name = name
