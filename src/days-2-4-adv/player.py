c4b5a45baff1a7b46415911a22153af0743cfd2b
snowcoding <vimalshah77@gmail.com>
2018-08-30 15:33:08 -0700

    Modified addItem()
    - Check to see if item was a Treasure
    - If so, update the score
    - Set is_taken to True

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,43 +1,60 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from item import Treasure
 
 class Player:
-  def __init__(self, start_room):
-    self.room = start_room #instance variable
-    self.items = []
-    self.score = 0
-  
-  def __repr__(self):
-    return "Current Location: {}".format(self.room)
-  def __str__(self):
-    return "Current Location: {}".format(self.room) 
 
-  def set_room(self, new_room):
-    self.room = new_room
+    # Intialization
+    def __init__(self, start_room):
+        self.room = start_room  # instance variable
+        self.items = []
+        self.score = 0
 
-  def setScore(self, score):
-    self.score = score
+    # Get it to print nicely
+    def __repr__(self):
+        return "Current Location: {}".format(self.room)
 
-  def addItem(self,item):
-    self.items.append(item)
-  
-  def removeItem(self, item):
-    if len(self.items) > 0:
-      for i in self.items:
-        if i.name == item:
-          self.items.remove(i)
-    else:
-      print('Item not available to remove!')
+    def __str__(self):
+        return "Current Location: {}".format(self.room)
 
-  def getItemsList(self):
-    if (self.items):
-      return [i.name for i in self.items]
-    else:
-      return []
+    def setRoom(self, new_room):
+        self.room = new_room
 
-  def displayItems(self):
-    if (self.items):
-      for i in self.items:
-        print('\x1b[1;35;40m' + i.name + '\x1b[0m')
-    else:
-      print('\x1b[1;35;40m' + "You don't have any items!" + '\x1b[0m')
+    def setScore(self, score):
+        self.score = score
+
+    def addItem(self, item):
+
+        # Check if it's a treasure
+        if isinstance(item, Treasure):
+
+            # Check if the treasure has not already been taken
+            if not item.is_taken:
+                self.score += item.value
+                print(f'You just scored {item.value} points!')
+                item.is_taken = True
+            else:
+                print(f'You already had this item before... no points')
+
+        self.items.append(item)
+
+    def removeItem(self, item):
+        if len(self.items) > 0:
+            for i in self.items:
+                if i.name == item:
+                    self.items.remove(i)
+        else:
+            print('Item not available to remove!')
+
+    def getItemsList(self):
+        if (self.items):
+            return [i.name for i in self.items]
+        else:
+            return []
+
+    def displayItems(self):
+        if (self.items):
+            for i in self.items:
+                print('\x1b[1;35;40m' + i.name + '\x1b[0m')
+        else:
+            print('\x1b[1;35;40m' + "You don't have any items!" + '\x1b[0m')
