1efffeab1a827803ce7c46d72b5d57663d5d812b
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-08-30 18:39:14 -0400

    Added on_take and on_drop to items and dropped boolean to treasure

--- src/days-2-4-adv/items.py
+++ src/days-2-4-adv/items.py
@@ -6,14 +6,22 @@ class Items:
         self.name = name
         self.description = description
 
+    # these prints for on_take() and on_drop basically repeat the print on if inp[0] == 'take' or 'drop'
+    def on_take(self):
+        print('You\'ve picked up the %s' % (self.name))
+
+    def on_drop(self):
+        print('You\'ve dropped the %s' % (self.name))
+
 def __repr__(self):
     return f"{self.name} - {self.description}"
 
 class Treasure(Items):
-    def __init__(self, name, description, value):
+    def __init__(self, name, description, value, dropped):
         self.name = name
         self.description = description
         self.value = value
+        self.dropped = False
 
 class Lightsource(Items):
     def __init__(self, name, description):
