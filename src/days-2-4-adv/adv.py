4e624a8300cb02cf46fdeae26936220fdee7c99f
Austin Merando <ajmerando1@yahoo.com>
2018-08-30 17:07:08 -0700

    items functional, and food can be eaten

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,29 +1,39 @@
 from os import system, name
 import random
-from room import Room
-from player import Player
-# Declare all the rooms
 
-suppressRoomPrint = False
+from room import Room
+from item import Item
+from item import Weapon
+from item import LightSource
+from item import Food
+from player import Player
+
 validDirections = ["n", "s", "e", "w"]
+
+items = {
+    'sword': Weapon("Sword", "A rusty but useful sword", 15),
+    'refined_sword': Weapon("Sharpened Sword", "A big, brutish sword that has a red tint reflecting off the blade", 50),
+    'torch': LightSource("Torch", "A torch that has been lit recently...", 20),
+    'bread': Food("Bread", "A half eaten loaf", 2, 20),
+}
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", None),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", items['bread']),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", items['sword']),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", items['torch']),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers, except for one item. The only exit is to the south.""", items['refined_sword']),
 }
 
 
@@ -38,29 +48,91 @@ room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
 
+def item_check(p_location):
+    if p_location.itemDrop:
+        inp = input("\nTheres an item in this room:\n{} \nWould you like to pick it up?: ".format(
+            p_location.itemDrop))
+
+        if inp == 'y' or inp == 'yes':
+            player.addItem(p_location.itemDrop)
+            p_location.itemDrop = None
+        else:
+            print("\nYou get ready to make your next move...\n")
+
+    if p_location.droppedItem:
+        inp = input("\nThe {} you dropped earlier is in here, did you want to pick it up?: ".format(
+            p_location.droppedItem.name))
+        if inp == 'y' or inp == 'yes':
+            player.addItem(p_location.droppedItem)
+            p_location.droppedItem = None
+        else:
+            print("\nYou get ready to make your next move...\n")
+
+    elif not p_location.itemDrop and not p_location.droppedItem:
+        print("\nRoom looks empty...\n")
+
+
 def move_check(p_location):
     if p_location:
         system('cls')
         player.change_location(p_location)
-        print(player.location.weaponDrop)
 
     else:
         print("\nCan't go that way!\n")
 
+
+def drop_item(item):
+    item_grab = player.getItem(item)
+    if item_grab:
+        player.removeItem(item_grab)
+        player.location.droppedItem = item_grab
+    else:
+        print("\nThat item isn't in your inventory\n")
+
+
+system('cls')
 player = Player((input("What is your name: ")), room['outside'])
 
-
 while True:
-    inp = input("What would you like to do next: ")
-    if inp == 'q':
+
+    inputList = input(">>> ").split(" ")
+
+    if inputList[0] == 'q':
         break
-    elif inp == 'n':
-        move_check(player.location.n_to)
-    elif inp == 'e':
-        move_check(player.location.e_to)
-    elif inp == 's':
-        move_check(player.location.s_to)
-    elif inp == 'w':
-        move_check(player.location.w_to)
+    elif inputList[0] == 'look':
+        item_check(player.location)
+
+    elif inputList[0] == 'inv':
+        player.listInventory()
+
+    elif inputList[0] == 'stats':
+        player.playerInfo()
+
+    elif inputList[0] == 'eat':
+        if len(inputList) is 1:
+            print('\nChoose an item to eat\n')
+        elif player.getItem(inputList[1]) is not None:
+            food = player.getItem(inputList[1])
+            player.eatItem(food)
+        else:
+            print("\nThats not in inventory\n")
+
+    elif inputList[0] == 'drop':
+        if len(inputList) is 1:
+            print('\nChoose an item to drop\n')
+        else:
+            drop_item(inputList[1])
+
+    elif inputList[0] == 'move':
+        if len(inputList) is 1:
+            print('\nPick a location to move to\n')
+        elif inputList[1] == 'n':
+            move_check(player.location.n_to)
+        elif inputList[1] == 'e':
+            move_check(player.location.e_to)
+        elif inputList[1] == 's':
+            move_check(player.location.s_to)
+        elif inputList[1] == 'w':
+            move_check(player.location.w_to)
     else:
-        print('\nInvalid entry, please type n, e, s, w\n')
+        print('\nInvalid entry, please type: move (n, e, s, w,) look, eat (item), drop (item), stats, inv, q to quit\n')
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -5,15 +5,16 @@ import random
 
 class Room:
 
-    def __init__(self, title, desc):
+    def __init__(self, title, desc, itemDrop):
         self.title = title
         self.desc = desc
-        self.itemDrop = None
-        self.weaponDrop = None
+        self.droppedItem = None
+        self.itemDrop = itemDrop
         self.n_to = None
         self.e_to = None
         self.s_to = None
         self.w_to = None
+
     def __str__(self):
             return f"\n\n--{self.title}-- \n{self.desc} \n"
      
