e916786a69e5cc202c717ab86bae9a44e5dec300
Charles Cortinas <crcortinas@gmail.com>
2018-10-01 17:01:37 -0500

    Day 1
    Finished printf

--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -6,5 +6,8 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print("x is %(x)d, y is %(y)f, z is %(z)s" % {"x": x, "y": y, "z": z})
 
 # Use the 'format' string method to print the same thing
+
+print('x is {0}, y is {1}, z is {2}'.format(x, y, z))
