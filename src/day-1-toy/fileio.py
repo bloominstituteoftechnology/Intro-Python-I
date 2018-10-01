c55c3051856210109c7ea0ef6d6cdacdec4260b7
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-10-01 13:35:53 -0700

    finish cal, fileio, obj, scope

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,16 @@
 # Use open to open file "foo.txt" for reading
-
+f = open('foo.txt', 'r')
 # Print all the lines in the file
-
+print(f.read())
 # Close the file
-
+f.close()
 
 # Use open to open file "bar.txt" for writing
-
+o = open('bar.txt', 'w')
 # Use the write() method to write three lines to the file
+o.write('Testing 0 \n')
+o.write('Testing 1 \n')
+o.write('Testing 2 \n')
 
 # Close the file
+o.close()
