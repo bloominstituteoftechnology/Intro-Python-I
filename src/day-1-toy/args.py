18f17caa5bb053b58b9c3022efc7b032c7e6aada
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-01 15:44:04 -0400

    args.py

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 #def f1(...
+def f1(a, b):
+    """adds two numbers together"""
+    return a + b
 
 print(f1(1, 2))
 
@@ -12,6 +15,16 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # def f2(...
+def f2(*argv):
+    """returns the sum of all numbers passed in"""
+    count = 0
+    for arg in argv:
+        if type(arg) is list:
+            for i in arg:
+                count += 1
+        else:
+            count += arg
+    return count
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -28,6 +41,10 @@ print(f2(a))    # Should print 22
 # arguments. Google "python default arguments" for a hint.
 
 #def f3(...
+def f3(a, b = 1):
+    """returns sum of all numbers passed in,
+    unless only one number, in which case add 1"""
+    return a + b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +59,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 #def f4(...
+def f4(**kwargs):
+    for key in kwargs:
+        print(f'key {key}, value: {kwargs[key]}')
 
 # Should print
 # key: a, value: 12
@@ -60,4 +80,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
