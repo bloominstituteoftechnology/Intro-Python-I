474dbdaff17efc894ce86cf62dc9075c20b99e3b
William <kwontaewoongah@gmail.com>
2018-10-01 15:28:17 -0700

    finished cal with stretch

--- src/day-1-toy/cal.py
+++ src/day-1-toy/cal.py
@@ -3,13 +3,24 @@
 #
 # Use the sys module to look for command line arguments in the `argv` list
 # variable.
-#
+import sys
+import calendar
+import datetime
+arguments=sys.argv
 # If the user specifies two command line arguments, month and year, then draw
 # the calendar for that month.
+if len(arguments)==3:
+    month=arguments[1]
+    year=arguments[2]
+    print(calendar.month(int(year),int(month)))
+else:
+    date=datetime.date.today().isoformat().split('-')
+    month=date[1]
+    year=date[2]
+    print(calendar.month(int(year),int(month)))
 
 # Stretch goal: if the user doesn't specify anything on the command line, show
 # the calendar for the current month. See the 'datetime' module.
-
 # Hint: this should be about 15 lines of code. No loops are required. Read the
 # docs for the calendar module closely.
 
