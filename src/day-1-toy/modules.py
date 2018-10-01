2eaf9522bae3ef7c31aef8436946f89327739681
Erik Kimsey <erik@erikkimsey.com>
2018-10-01 16:04:33 -0400

    Completed programs through modules.py

--- src/day-1-toy/modules.py
+++ src/day-1-toy/modules.py
@@ -6,26 +6,27 @@ import os
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-
+print(sys.argv)
 
 # Print out the platform from sys:
-print()
+print(sys.platform)
 
 # Print out the Python version from sys:
-print()
+print(sys.implementation)
+print(sys.version_info)
 
 
 
-# Module "os"
-#
-# See the docs for the OS module: https://docs.python.org/3.7/library/os.html
+# # Module "os"
+# #
+# # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
-# Print the current process ID
-print()
+# # Print the current process ID
+print(os.getpid())
 
-# Print the current working directory (cwd):
-print()
+# # Print the current working directory (cwd):
+print(os.getcwd())
 
-# Print your login name
-print()
+# # Print your login name
+print(os.getlogin())
 
