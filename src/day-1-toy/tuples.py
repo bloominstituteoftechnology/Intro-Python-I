363f69df5ec6a99b89814b308ba267911625318c
codejoncode <jonathanjholloway@gmail.com>
2018-10-01 17:33:43 -0500

    finished tuples.py

--- src/day-1-toy/tuples.py
+++ src/day-1-toy/tuples.py
@@ -23,7 +23,13 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function that prints all the values in a tuple
 
 # def print_tuple(...
-
+def print_tuple(tup):
+    try:
+        if len(tup) > 1:
+            for value in tup:
+                print(value)
+    except:
+        print(tup)
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
