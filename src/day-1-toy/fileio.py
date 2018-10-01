defe6d1eb25eba5f560901127b38b2b4b2eb6908
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-01 15:46:34 -0400

    fileio.py

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,21 @@
 # Use open to open file "foo.txt" for reading
+f = open('foo.txt', 'r')
 
 # Print all the lines in the file
+print(f.read())
 
 # Close the file
-
+f.close()
 
 # Use open to open file "bar.txt" for writing
+b = open('bar.txt', 'w')
 
 # Use the write() method to write three lines to the file
+b.write("""
+Colorless hillside
+A spanish, cunning fly stands
+in spite of the pond
+""")
 
 # Close the file
+b.close()
