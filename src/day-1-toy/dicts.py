ac792bf4c5a1dfa88ad14da80edc98b7213b1290
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-10-01 17:59:48 -0500

    dicts

--- src/day-1-toy/dicts.py
+++ src/day-1-toy/dicts.py
@@ -11,12 +11,12 @@ waypoints = [
         "lat": 43,
         "lon": -121,
         "name": "a place"
-    }, 
+    },
     {
         "lat": 41,
         "lon": -123,
         "name": "another place"
-    }, 
+    },
     {
         "lat": 43,
         "lon": -122,
@@ -26,4 +26,11 @@ waypoints = [
 
 # Write a loop that prints out all the field values for all the waypoints
 
+for x in waypoints:
+    for y in x.values():
+        print(y)
+#     print(x.values())
+# for place in waypoints:
+#     for k, v in place.items():
+#         print('{}: {}'.format(k, v))
 # Add a new waypoint to the list
