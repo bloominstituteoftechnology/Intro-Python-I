fb74d667085c211e340d0174f52b878ba11f56bb
Josue Peralta <jperal77@gmail.com>
2018-10-01 17:46:11 -0500

    completed lists.py, modules.py, printf.py

--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -5,6 +5,6 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print("x is %2d, y is %4.2f, z is %15s" % (x, y, z))
 
 # Use the 'format' string method to print the same thing
