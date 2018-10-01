51d2fdcc694c48b9d9196e37187e831c96327600
chainchompa <chainchompa@gmail.com>
2018-10-01 18:38:24 -0400

    completed lists, printf, slice, and tuples

--- src/day-1-toy/tuples.py
+++ src/day-1-toy/tuples.py
@@ -9,7 +9,7 @@ def dist(a, b):
     """Compute the distance between two x,y points."""
     x0, y0 = a  # Destructuring assignment
     x1, y1 = b
-    
+
     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
 
 a = (2, 7)   # <-- x,y coordinates stored in tuples
@@ -23,10 +23,14 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function that prints all the values in a tuple
 
 # def print_tuple(...
+def print_tuple(tuple):
+  for x in tuple:
+    print(x)
+
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
