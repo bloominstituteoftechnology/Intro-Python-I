40260d4966ed988517504ee930b7214f703bd05b
codejoncode <jonathanjholloway@gmail.com>
2018-10-01 15:26:20 -0500

    Completed the args.py functions

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 #def f1(...
+def f1(x,y):
+    return x + y
 
 print(f1(1, 2))
 
@@ -12,7 +14,11 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # def f2(...
-
+def f2(*args):
+    total = 0
+    for arg in args:
+        total+= arg; 
+    return total 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -21,13 +27,18 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 #def f3(...
+def f3(x, y=None):
+    if y is None:
+        return x + 1
+    else:
+        return x + y
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,7 +53,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 #def f4(...
-
+def f4(**kwargs):
+    for key in kwargs:
+        print(f"key: {key}, value: {kwargs[key]}")
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -60,4 +73,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
