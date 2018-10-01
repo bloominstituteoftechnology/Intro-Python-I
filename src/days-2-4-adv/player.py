9f1e027bbe7d1c937f55fdbcf956dece95c0d445
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-01 17:22:58 -0400

    adding item support

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,8 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player():
-    def __init__(self, c_room):
+    def __init__(self, c_room, items=[]):
         self.c_room = c_room
+        self.items = items
 
     def __update_room__(self, room):
         self.c_room = room
+
+    def __get_item__(self, item):
+        self.items.append(item)
+
+    def __list_items__(self):
+        for i in self.items:
+            print(i)
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,6 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room():
-    def __init__(self, location, description):
+    def __init__(self, location, description, items=[]):
         self.location = location
         self.description = description
+        self.items = items
+
+    def __add_item__(self, item):
+        self.items.append(item)
