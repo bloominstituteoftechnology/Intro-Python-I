4174e95fb1225bfe0790f11680b39cf202f88879
sca4cs <shannon.atkinson.ca@gmail.com>
2018-10-01 17:50:48 -0400

    completed through comp.py

--- src/day-1-toy/modules.py
+++ src/day-1-toy/modules.py
@@ -6,7 +6,7 @@ import os
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-
+print(sys.argv)
 
 # Print out the platform from sys:
 print(sys.platform)
@@ -21,11 +21,11 @@ print(sys.version_info)
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-print()
+print(os.getpid())
 
 # Print the current working directory (cwd):
-print()
+print(os.getcwd())
 
 # Print your login name
-print()
+print(os.getlogin())
 
