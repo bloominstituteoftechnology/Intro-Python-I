5df665d609e663c9843a48eea1ab0533e564dfa5
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-01 17:15:52 -0500

    finished Func
    I am almost half way done, I finshed Func, tested out each of the functions that were created and each worked.

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,17 @@
 # Use open to open file "foo.txt" for reading
+file = open("foo.txt")
 
 # Print all the lines in the file
-
+for line in file: 
+      print(line)
 # Close the file
 
-
+file.close()
 # Use open to open file "bar.txt" for writing
-
+file = open("bar.txt", "r")
 # Use the write() method to write three lines to the file
-
+file.write(""" working on assignment
+Filling out each answer
+Pushing each commit""")
 # Close the file
+file.close()
