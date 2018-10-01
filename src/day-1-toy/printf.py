14088b3e7f5efe973269f7f80fe2a1b58734810d
Naaz Shaikh <naazhome@Naazs-MBP-3.hsd1.or.comcast.net>
2018-10-01 13:37:03 -0700

    lists and printf done

--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -2,9 +2,14 @@ x = 10
 y = 2.24552
 z = "I like turtles!"
 
-# Using the printf operator (%), print the following feeding in the values of x,
-# y, and z:
+# Using the printf operator (%), print the following feeding in the values of x, y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print("%d %.2f %s" %(x,y,z))
 
 # Use the 'format' string method to print the same thing
+#you can put placeholders in the curly like i did
+print ('z is {2}, y is {1:.2f}, x is {0}'.format(x,y,z))
+
+#just another way
+print (f'x is {x},  y is {y}, z is {z}')
