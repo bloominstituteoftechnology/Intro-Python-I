14926972083b7999815c18fff79c872dc30cf9ce
codejoncode <jonathanjholloway@gmail.com>
2018-10-01 17:53:22 -0500

    created classes for player and room

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class PlayerInfo(object):
+    def __init__(self, name, currentRoom):
+        self.name = name
+        self.currentRoom = currentRoom 
+    def __repr__(self):
+        return f"{self.name} currently in room {self.currentRoom}"
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,2 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class RoomInfo (object):
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    def __repr__(self):
+        return f"Room Name: {self.name}, Room description: {self.description}"
