ac750c500cae874492506faa91305cac99f64328
codejoncode <jonathanjholloway@gmail.com>
2018-10-01 16:31:10 -0500

    started working on fileio.py and completed hello.py

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,17 @@
+import os
 # Use open to open file "foo.txt" for reading
-
+o = open(os.path.abspath("foo.txt"), "r")
 # Print all the lines in the file
-
+print(o.read())
 # Close the file
-
+o.close()
 
 # Use open to open file "bar.txt" for writing
-
+o = open("bar.txt", "w")
 # Use the write() method to write three lines to the file
-
+add = [1,2,3]
+for num in add:
+    o.write(str(num))
+    o.write("\n")
 # Close the file
+o.close()
