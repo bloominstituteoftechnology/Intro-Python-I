6de664848fba69c974013fee45fb597e079ae144
William <kwontaewoongah@gmail.com>
2018-10-01 16:12:00 -0700

    added some detail to obj.py

--- src/day-1-toy/obj.py
+++ src/day-1-toy/obj.py
@@ -11,7 +11,7 @@ class Waypoint(LatLon):
         super().__init__(lat,lon)
         self.name=name
     def __str__(self):
-        return '{}, {}, {}'.format(self.name,self.lat,self.lon)
+        return 'name:{}, lat:{}, lon:{}'.format(self.name,self.lat,self.lon)
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
@@ -21,7 +21,7 @@ class Geocache(Waypoint):
         self.difficulty=difficulty
         self.size=size
     def __str__(self):
-        return '{}, {}, {}, {}, {}'.format(self.name,self.difficulty,self.size,self.lat,self.lon)
+        return 'name:{}, difficulty:{}, size:{}, lat:{}, lon:{}'.format(self.name,self.difficulty,self.size,self.lat,self.lon)
 # Make a new waypoint "Catacombs", 41.70505, -121.51521
 w=Waypoint("Catacombs", 41.70505, -121.51521)
 # Print it
