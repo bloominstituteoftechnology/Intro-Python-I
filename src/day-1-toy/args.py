ecf0e30eef003e2309de1e480ab48d85c34573e6
tulf <timzulf@gmail.com>
2018-10-01 19:29:41 -0400

    slice done, args done, guessing works!

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -13,7 +13,7 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 def f2(*args):
-
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -23,13 +23,14 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
-#def f3(...
+def f3(a, b=None):
+    return a+b if b is not None else a+1
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -43,7 +44,9 @@ print(f3(8))     # Should print 9
 #
 # Google "python keyword arguments".
 
-#def f4(...
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print("key: {}, value: {}".format(key, value))
 
 # Should print
 # key: a, value: 12
@@ -62,4 +65,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
