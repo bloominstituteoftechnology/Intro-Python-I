f185f0592d7c30bacc07731dcc5e9fa55020a54a
AlexanderCaves <ahcaves@gmail.com>
2018-10-02 15:56:48 -0700

    made class constructors for Room and Player

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player(Room):
+    def __init__(self, location, name, description):
+        Room.__init__(self, name, description)
+        self.location = location
+        
+    def printP(self):
+        return self.location, self.name, self.description
