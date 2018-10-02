ec47c28d6623165c638d983b9b7f8e08b3d47af8
AI2EJAY <JHammett1314@gmail.com>
2018-10-01 21:36:58 -0500

    lists done

--- src/day-1-toy/lists.py
+++ src/day-1-toy/lists.py
@@ -8,22 +8,24 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # [command here]
-print(x)
+print(x.append(4), x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # [command here]
-print(x)
+print(x.extend(y), x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # [command here]
-print(x)
+print(x.remove(x[4]),  x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # [command here]
-print(x)
+print(x.insert(len(x)-1, 99), x)
 
 # Print the length of list x
 # [command here]
 print(len(x))
 
 # Using a for loop, print all the element values multiplied by 1000
+for i in x: 
+    print(i * 1000)
