a9901c7579a5f6c8b550f39bce5bb7f2cb44d9ae
vanceleon <vanceleon44@gmail.com>
2018-10-01 23:01:41 -0400

    lists

--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -2,13 +2,12 @@ x = 10
 y = 2.24552
 z = "I like turtles!"
 
-# Using the printf operator (%), print the following feeding in the values of x,
-print(x%2)
-print(y%2)
-print(str(z) % 2)
+# Using the printf operator (%), print the following feeding in the values of    x,
 # y, and z:
+print("Integer: %d, Float: %f, String: %s" %(x,y,z))
 
 # x is 10, y is 2.25, z is "I like turtles!"
-
+# print()
 
 # Use the 'format' string method to print the same thing
+print('x is {0}, y is {1}, z is {2}'.format(x, y, z))
