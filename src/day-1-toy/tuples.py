54832fc37fd418c086401a75bd169ab934aaafff
vanceleon <vanceleon44@gmail.com>
2018-10-01 23:29:57 -0400

    latest

--- src/day-1-toy/tuples.py
+++ src/day-1-toy/tuples.py
@@ -23,6 +23,9 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function that prints all the values in a tuple
 
 # def print_tuple(...
+def print_tuple(t):
+    # for x in t:
+    print('\n'.join(map(str,t)))
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
