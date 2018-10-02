c00e845481abce14dfde93aafdda5fc016fcd4de
Charlie Winslow <cwinslow22@yahoo.com>
2018-10-02 17:08:46 -0500

    added a Player class and started working on adv.py
    updated the Pipfile to use python3.6

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,7 +39,8 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+name = input('What do ye call yourself?')
+plyr = Player(name, room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +51,12 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+#
+
+
+while True:
+    print(f'You are currently located {plyr.current_room}')
+    cmd = input('How should we proceed? \n')
+    if cmd ==  'q':
+        break
+    
