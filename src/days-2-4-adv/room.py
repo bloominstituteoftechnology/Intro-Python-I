d0f3b4ab01685f6bb8ec9c012036de4b6ab300a0
Erik Kimsey <erik@erikkimsey.com>
2018-10-02 17:54:05 -0400

    Created Room class init and attributes and hasItem method

--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,3 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+import random
 
+
+class Room:
+  def __init__(self, room_type):
+    self.self = self
+    self.room_type = room_type
+    self.containsItem = self.hasItem()
+
+  def hasItem():
+    return bool(random.getrandbits(1))
