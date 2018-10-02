18bedcff521ddb6010a01123d87b30fc87e7e6e7
chainchompa <chainchompa@gmail.com>
2018-10-02 16:13:24 -0400

    added player and room class

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, location=None):
+        self.name = name
+        self.location = location
+
+    def __str__(self):
+        return self.name
+        
+    def get_location(self.location):
+        return self.location
