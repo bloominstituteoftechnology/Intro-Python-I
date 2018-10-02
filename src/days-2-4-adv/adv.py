6e9f2cde0d665b672291232c82c8ec76852e3be9
LaikaFusion <newrad4@gmail.com>
2018-10-02 17:11:00 -0400

    formating

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -2,27 +2,29 @@ from room import Room
 from player import Player
 
 
-class AdventureDone(Exception): pass
+class AdventureDone(Exception):
+    pass
 
 # Declare all the rooms
 
+
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons",'outside'),
+                     "North of you, the cave mount beckons", 'outside'),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""",'foyer'),
+passages run north and east.""", 'foyer'),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""",'overlook'),
+the distance, but there is no way across the chasm.""", 'overlook'),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""",'narrow'),
+to north. The smell of gold permeates the air.""", 'narrow'),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""",'treasure'),
+earlier adventurers. The only exit is to the south.""", 'treasure'),
 }
 
 # Link rooms together
@@ -54,40 +56,43 @@ currentPlayer = Player(playerName)
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
 def TextEval(text):
     if text == "q":
         raise AdventureDone
-    elif text =="n":
-      if hasattr(room[currentPlayer.room], 'n_to'):
-          currentPlayer.room = room[currentPlayer.room].n_to.key
-          return False
-      else:
-        print("Invalid Direction")
-        return True
-    elif text =="w":
-      if hasattr(room[currentPlayer.room], 'w_to'):
-          currentPlayer.room = room[currentPlayer.room].w_to.key
-          return False
-      else:
-        print("Invalid Direction")
-        return True
-    elif text =="e":
-      if hasattr(room[currentPlayer.room], 'e_to'):
-          currentPlayer.room = room[currentPlayer.room].e_to.key
-          return False
-      else:
-        print("Invalid Direction")
-        return True
-    elif text =="s":
-      if hasattr(room[currentPlayer.room], 's_to'):
-          currentPlayer.room = room[currentPlayer.room].s_to.key
-          return False
-      else:
-        print("Invalid Direction")
-        return True
+    elif text == "n":
+        if hasattr(room[currentPlayer.room], 'n_to'):
+            currentPlayer.room = room[currentPlayer.room].n_to.key
+            return False
+        else:
+            print("Invalid Direction")
+            return True
+    elif text == "w":
+        if hasattr(room[currentPlayer.room], 'w_to'):
+            currentPlayer.room = room[currentPlayer.room].w_to.key
+            return False
+        else:
+            print("Invalid Direction")
+            return True
+    elif text == "e":
+        if hasattr(room[currentPlayer.room], 'e_to'):
+            currentPlayer.room = room[currentPlayer.room].e_to.key
+            return False
+        else:
+            print("Invalid Direction")
+            return True
+    elif text == "s":
+        if hasattr(room[currentPlayer.room], 's_to'):
+            currentPlayer.room = room[currentPlayer.room].s_to.key
+            return False
+        else:
+            print("Invalid Direction")
+            return True
     else:
-      print("Invalid Direction")
-      return True
+        print("Invalid Direction")
+        return True
+
 
 print(f"Welcome {currentPlayer.name}, pick a direction or use q to quit")
 
@@ -95,11 +100,12 @@ try:
     while True:
         awaitValidDirection = True
         print("\n")
-        print(f"{currentPlayer.name} is located in {room[currentPlayer.room].name}")
+        print(
+            f"{currentPlayer.name} is located in {room[currentPlayer.room].name}")
         print(room[currentPlayer.room].description)
         while awaitValidDirection:
-          playerInput = input("Please select a direction:")
-          awaitValidDirection = TextEval(playerInput.lower())
+            playerInput = input("Please select a direction:")
+            awaitValidDirection = TextEval(playerInput.lower())
 
 except AdventureDone:
     print('See you next adventure')
