87d3fc88d25d809b01b88e36d707ab948ac14509
AI2EJAY <JHammett1314@gmail.com>
2018-10-02 11:35:13 -0500

    Fixed printf

--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -5,7 +5,12 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-print ("x is %s, y is %s, z is %s " %(x, y, z))
+
+# print("x is %s, y is %s, z is %s "%(x, y, z))
+# print('x is {x}, y is {y}, z is "{z}"'.format(x = 10, y=2.24552, z="I like turtles!"))
+
+print('x is {0}, y is {1:.2f}, z is "{2}"'.format(x, y, z))
 
 # Use the 'format' string method to print the same thing
-print(f"x is {x}, y is {y}, z is {z} ")
+print(f'x is {x}, y is {y:.2f}, z is "{z}"')
+
