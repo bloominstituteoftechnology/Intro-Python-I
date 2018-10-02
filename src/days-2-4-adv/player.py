e87624d3e2aa97ccb63460aa69ed96d59833078d
Sukhada Gholba <sukhadagholba@gmail.com>
2018-10-02 16:04:22 -0700

    created a player class that takes current room name as an argument

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+import textwrap
+
+class Player():
+        def __init__(self,room):
+                self.room=room
+
+        def room_name(self):
+                return f'Your current room is: {self.room}'
+        
