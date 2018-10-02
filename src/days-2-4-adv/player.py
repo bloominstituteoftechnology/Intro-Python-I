4a45090c8de123b6ab4179b0a47bdd3aa59069c1
Das MA <das@linux-rd5e.frontierlocal.net>
2018-10-02 15:50:28 -0700

    Added get_item, drop_item check_inventory methods to Player.py

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,9 +1,27 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, room):
+    def __init__(self, name, room, inventory):
         self.name = name
         self.room = room
+        self.inventory = inventory
+
+    def get_item(self, item):
+        self.inventory.append(item)
+    
+    def drop_item(self, item):
+        if len(self.inventory) is 0:
+            print('Oops, your inventory is totally empty!')
+        else:
+            del self.inventory[index]
+
+    def check_inventory(self):
+        print('Your inventory contains: ')
+        if len(self.inventory) is not 0:
+            for i, item in enumerate(self.inventory):
+                print(str(i) + '->' + item)
+        else:
+            print('You are broke. Nothing in the bag!!!')
         
     def getRoom(self):
         print(75 * '#')
