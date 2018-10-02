f94460310902bfc3a4754225e640aa09ce64882c
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-02 14:40:01 -0500

    started day 2
    started the character and the room classes

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -49,3 +49,4 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player(): 
+    def _init_(self, name, currentRoom):
+        self.name = name
+        self.currentRoom = currentRoom
