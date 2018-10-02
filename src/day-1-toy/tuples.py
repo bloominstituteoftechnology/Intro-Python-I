62dd699faa6e551e7ae545d1600342be18a0c150
jordan-massingill <jordan_massingill@hotmail.com>
2018-10-02 11:02:15 -0500

    Progressing through files
    Completed up to fileio.py

--- src/day-1-toy/tuples.py
+++ src/day-1-toy/tuples.py
@@ -26,12 +26,10 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 def print_tuple(tup):
     for arg in tup:
         print(arg)
-    else:
-        print(tup[0])
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
