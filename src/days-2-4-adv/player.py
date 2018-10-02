878582bc8272e7a048d0e632d1d2a294041d6860
Arthur Bates <aabatesj@gmail.com>
2018-10-02 17:46:04 -0400

    Day 2 initial commit
    beginnings of player and room classes

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,25 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(player, name):
+        player.name = name
+
+    def greet(player):
+        print(f"{player.name} greeted you")
+
+    # not @classmethod: call a method on an instance
+    # player = Player(...)
+    # player.create(...)
+    #
+    # @classmethod: call a method on a class
+    # Player.create(...)
+    @classmethod
+    def create(cls, name, kind):
+        if kind == "hunter":
+            return PlayerType1(name)
+        elif kind == "mage":
+            return PlayerType2(name)
+        else:
+            # cls = Player
+            return cls(name)
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,2 +1,26 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(room, name, description):
+        room.name = name
+        room.description = description
+
+    # not @classmethod: call a method on an instance
+    # room = Room(...)
+    # room.create(...)
+    #
+    # @classmethod: call a method on a class
+    # Room.create(...)
+    @classmethod
+    def create(cls, name, kind):
+        if kind == "outside":
+            return OutsideRoom(name, description)
+        elif kind == "foyer":
+            return FoyerRoom(name, description)
+        elif kind == "overlook":
+            return OverlookRoom(name, description)
+        elif kind == "narrow":
+            return NarrowRoom(name, description)
+        elif kind == "treasure":
+            return TreasureRoom(name, description)
