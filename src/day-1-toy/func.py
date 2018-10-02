83404e8566da2b88b864bceca79c8203cb88a548
Noah-Amar <ne.amar@outlook.com>
2018-10-01 19:56:30 -0500

    bignum, datatypes

--- src/day-1-toy/func.py
+++ src/day-1-toy/func.py
@@ -1,6 +1,14 @@
 # Write a function is_even that will return true if the passed in number is even.
-
+def is_even(num):
+    if num%2==0:
+        return True
 # Read a number from the keyboard
 num = input("Enter a number: ")
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
+
+def is_even_or_odd():
+    if num%2==0:
+        print ('Even!')
+    elif num%2!=0:
+        print ('Odd')
