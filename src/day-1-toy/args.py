d25f6c683dae5de233a364c9163d2d2aa3edb1e0
Das MA <das@linux-rd5e.frontierlocal.net>
2018-10-02 10:00:49 -0700

    Little change

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -22,7 +22,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-# print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,7 +1,7 @@
 from room import Room
 
 # Declare all the rooms
-
+ 
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
