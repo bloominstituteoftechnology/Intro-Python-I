e68b6887d75c0aa3236fa29a464a89d4ef94c104
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-02 17:25:49 -0400

    Last commit in error, this one has treasure

--- src/days-2-4-adv/item.py
+++ src/days-2-4-adv/item.py
@@ -5,3 +5,8 @@ class Item():
 
     def __str__(self):
         return f'{self.name}: {self.description}'
+
+class Treasure(Item):
+    def __init__(self, name, description, value):
+        super().__init__(name, description)
+        self.value = value
