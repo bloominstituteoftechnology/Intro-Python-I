683575a22cf4ec8d34f7e80605fb5ce38b8bd803
LaikaFusion <newrad4@gmail.com>
2018-10-02 15:54:41 -0400

    added keys to rooms dict

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,25 +1,27 @@
 from room import Room
 from player import Player
 
+class AdventureDone(Exception): pass
+
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",'outside'),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""",'foyer'),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",'overlook'),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""",'narrow'),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",'treasure'),
 }
 
 # Link rooms together
@@ -51,11 +53,21 @@ currentPlayer = Player(playerName)
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-# def TextEval(text):
+print(room[currentPlayer.room].n_to.name)
+def TextEval(text):
+    if text == "q":
+        raise AdventureDone
+    elif text =="n":
+        room[currentPlayer.room].n_to
 
-print(f"Welcome {currentPlayer.name}")
-while True:
-    print(f"{currentPlayer.name} is located in {room[currentPlayer.room].name}")
-    print(room[currentPlayer.room].description)
-    print(room["outside"].n_to.name)
-    break
+print(f"Welcome {currentPlayer.name}, pick a direction or use q to quite")
+try:
+    while True:
+      print(f"{currentPlayer.name} is located in {room[currentPlayer.room].name}")
+      print(room[currentPlayer.room].description)
+      playerInput = input("Please select a direction:")
+      TextEval(playerInput.lower())
+      break
+
+except AdventureDone:
+    print('See you next adventure')
