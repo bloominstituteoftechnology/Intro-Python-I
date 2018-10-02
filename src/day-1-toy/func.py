57062d661829e4f798e6ac628441b5465d4881a8
codejoncode <jonathanjholloway@gmail.com>
2018-10-02 11:52:15 -0500

    I had return true else false instead of print even or print odd

--- src/day-1-toy/func.py
+++ src/day-1-toy/func.py
@@ -2,11 +2,11 @@
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
-
+num = int(num) 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 def is_even(x):
     if x % 2 == 0:
-        return True
+        print("Even!")
     else:
-        return False
+        print("Odd!")
 print(is_even(num))
