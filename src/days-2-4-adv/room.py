a61c1fc3659ce06c6d361021ba1c2b951a71f8ba
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-10-02 14:40:01 -0500

    started day 2
    started the character and the room classes

--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,2 +1,7 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room():
+    def _init_(self,name,description):
+        self.name = name
+        self.description = description
