ecf7b9ecd5a2694358066ae44dff0c4de7b8ee05
Munsu <dark_specineff2002@yahoo.com>
2018-10-02 13:50:28 -0700

    mvp

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -18,6 +18,15 @@ print(f1(1, 2))
 
 # def f2(...
 
+
+def f2(*x):
+    count = 0
+
+    for num in x:
+        count += num
+    return count
+
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -26,13 +35,18 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # def f3(...
+
+
+def f3(x, y=1):
+    return x + y
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -47,6 +61,11 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # def f4(...
+def f4(**gazorp):
+
+    for key in gazorp:
+        print("key: %s, value: %s" % (key, gazorp[key]))
+
 
 # Should print
 # key: a, value: 12
@@ -65,4 +84,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,20 @@
 # Use open to open file "foo.txt" for reading
+traceOn = open("foo.txt")
 
 # Print all the lines in the file
+for line in traceOn:
+    print(line)
 
 # Close the file
-
+traceOn.close()
 
 # Use open to open file "bar.txt" for writing
+traceOn = open("bar.txt", "w")
 
 # Use the write() method to write three lines to the file
+traceOn.write("""Line One
+Line Two
+Line Three""")
 
 # Close the file
+traceOn.close()
