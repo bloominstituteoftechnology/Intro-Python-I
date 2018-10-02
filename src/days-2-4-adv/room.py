21699ee1afeb74a4a39ba98bde1680fba111d0ff
Sukhada Gholba <sukhadagholba@gmail.com>
2018-10-02 16:05:00 -0700

    created a room class that takes room name and description as arguments

--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,2 +1,35 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+import textwrap
+
+class Room():
+        def __init__(self, name, text):
+                self.name=name.split(' ', 1)[0].lower()     
+                self.text=text
+                self.next_d=None
+
+        def text_descr(self):
+                return f'{textwrap.fill({self.text}, 35)}'       
+
+        def connecting(self, direction, next_destination):
+                if self.name =='outside' and direction =='n':
+                        self.next_d=next_destination.name
+                elif self.name =='foyer' and direction =='s':
+                        self.next_d=next_destination.name
+                elif self.name =='foyer' and direction =='n':
+                         self.next_d=next_destination.name
+                elif self.name =='foyer' and direction =='e':
+                         self.next_d=next_destination.name
+                elif self.name =='overlook' and direction =='s':
+                         self.next_d=next_destination.name
+                elif self.name =='narrow' and direction =='w':
+                         self.next_d=next_destination.name
+                elif self.name =='treasure' and direction =='s':
+                         self.next_d=next_destination.name
+                else:
+                         self.next_d=self.name
+                    
+    
+                    
+    
+
