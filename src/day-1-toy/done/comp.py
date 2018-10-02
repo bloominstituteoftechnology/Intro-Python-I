6e30fb442091df89996953d21ed2387de400baec
mkerbleski <mkerbles@gmail.com>
2018-10-02 17:06:23 -0600

    complete 'comp.py'

--- src/day-1-toy/done/comp.py
+++ src/day-1-toy/done/comp.py
@@ -26,7 +26,7 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?     
-y = [int(i) for i in x if i % 2 == 0 ]
+y = [ i for i in x if int(i) % 2 == 0 ]#add int
 
 print(y)
 
