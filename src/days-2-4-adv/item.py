04c142a6eeb2608bf8d013ba943dfeef476dbb9e
Grant Reighard <grant.reighard@gmail.com>
2018-10-03 19:19:44 -0400

    Finished Day 4 tasks.

--- src/days-2-4-adv/item.py
+++ src/days-2-4-adv/item.py
@@ -3,13 +3,23 @@ class Item:
         self.name = name
         self.description = description
     def on_take(self, player, room):
-        player.inventory.append(self)
-        room.inventory.remove(self)
-        print("You have taken the %s." % self.name)
+        if room.is_light:
+            player.inventory.append(self)
+            room.inventory.remove(self)
+            print("You have taken the %s." % self.name)
+        else:
+            print("Good luck finding that in the dark!\n")
     def on_drop(self, player, room):
-        player.inventory.remove(self)
-        room.inventory.append(self)
-        print("You have dropped the %s." % self.name)
+        if not isinstance(self, Lightsource):
+            player.inventory.remove(self)
+            room.inventory.append(self)
+            print("You have dropped the %s." % self.name)
+        else:
+            reallyDrop = input("It's not wise to drop your source of light! Do you really want to do so? Y/N ")
+            if reallyDrop.upper() == "Y":
+                player.inventory.remove(self)
+                room.inventory.append(self)
+                print("You have dropped the %s." % self.name)
 
 class Treasure(Item):
     def __init__(self, name, description, value):
@@ -17,10 +27,13 @@ class Treasure(Item):
         self.value = value
         self.takenAlready = False
     def on_take(self, player, room):
-        self.takenAlready = True
-        player.inventory.append(self)
-        room.inventory.remove(self)
-        print("You have taken the %s." % self.name)
+        if room.is_light:
+            self.takenAlready = True
+            player.inventory.append(self)
+            room.inventory.remove(self)
+            print("You have taken the %s." % self.name)
+        else:
+            print("Good luck finding that in the dark!\n")
 
 class Lightsource(Item):
     def __init__ (self, name, description):
