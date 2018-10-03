506c1d0f6c07b482c0d4951a0f3cec450c70fc3c
William <kwontaewoongah@gmail.com>
2018-10-03 15:52:14 -0700

    wrote on_take function for item and overrode in treasure

--- src/days-2-4-adv/item.py
+++ src/days-2-4-adv/item.py
@@ -4,7 +4,16 @@ class Item:
         self.description=description
     def __str__(self):
         return f'name:{self.name}\ndescription:{self.description}'
+    def on_take(self):
+        return None
 class Treasure(Item):
     def __init__(self,name,description,value):
         super().__init__(name,description)
         self.value=value
+        self.picked_up=false
+    def on_take(self):
+        if self.picked_up==false:
+            self.picked_up=True
+            return 1
+        else:
+            return 0
