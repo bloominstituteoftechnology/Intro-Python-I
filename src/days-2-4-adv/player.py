a7247e1282ef00a113cd12851eb0696072d2d4ab
Carey <careycbaldwin@gmail.com>
2018-10-03 17:09:25 -0400

    add items parameters to players and rooms

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,11 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player():
-    def __init__(self, name, currentRoom):
+    def __init__(self, name, currentRoom, items):
         self.name = name
         self.currentRoom = currentRoom
+        self.items = []
 
-    # def travel(self, direction)
-    #     nextRoom = self.currentRoom.
-
+    # deal with items
+    def addItem(self, item):
+        self.items.append(item)
 
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -2,13 +2,14 @@
 # description attributes.
 
 class Room():
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
         self.n_to = None
         self.e_to = None
         self.s_to = None
         self.w_to = None
+        self.items = []
 
     def __str__(self):
         return f"\n\n{self.name}\n\n   {self.description}\n"
