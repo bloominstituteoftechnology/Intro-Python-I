c4f8cbc9507d3704fa6f5b72461d8dd542b33d55
William <kwontaewoongah@gmail.com>
2018-10-03 16:16:42 -0700

    changed illuminated to is_light

--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,11 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self,name,description,illuminated):
+    def __init__(self,name,description,is_light):
         self.name=name
         self.description=description
         self.inventory=[]
-        self.illuminated=illuminated
+        self.is_light=is_light
         self.n_to=None
         self.s_to=None
         self.e_to=None
