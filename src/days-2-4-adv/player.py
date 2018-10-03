a96b34d8f5badc0cc13f723eae81eb07da61e4fd
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-10-03 17:38:00 -0500

    Add and Remove
    having trouble with my add and remove, they are not working at the moment.

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -6,3 +6,12 @@ class Player:
     def __init__(self, currentRoom, items):
         self.currentRoom = currentRoom
         self.items = items
+    def add(self, item):
+        self.items.append(item)
+    def remove(self, item):
+        if len(self.items) > 0:
+            for i in self.items:
+                if i.name == item:
+                    self.items.remove(i)
+        else:
+            print("item not avalible")
