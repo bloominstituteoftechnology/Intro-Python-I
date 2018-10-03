8e97fefcc8eafb244c076a75ee6238836d91e1c6
codejoncode <jonathanjholloway@gmail.com>
2018-10-02 19:48:35 -0500

    added items to each room some rooms have an empty array because there are no items in the room

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -6,25 +6,25 @@ import random
 # Declare all the rooms
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [items["spells"]]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",[items["gun"]]),
     'prison': Room('Prison', """You will have to fight your way out of the prison if you wish to go north.
-     If you wish not to fight run left or right"""),
-    'coward': Room("Cowards Forest", """You coward! You ran from the fight. Now the goblins have been alerted.  """),
-    'kitchen': Room('Kitchen', """Welcome to the kitchen """)
+     If you wish not to fight run left or right""", [items["bat"]]),
+    'coward': Room("Cowards Forest", """You coward! You ran from the fight. Now the goblins have been alerted.  """, [items["shovel"]]),
+    'kitchen': Room('Kitchen', """Welcome to the kitchen """, [items["sword"]])
 
 }
 # Link rooms together
