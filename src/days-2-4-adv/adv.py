0efb4f464c09fd7586776c1e3934a107d664a389
Kairi <kairijb@gmail.com>
2018-10-03 18:56:46 -0500

    room now updates successfully when moving

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,37 +1,38 @@
-from room import Room
+from room import Room,room
 from player import Player
+import time
 # Declare all the rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+# room = {
+#     'outside':  Room("Outside Cave Entrance",
+#                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+# passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+# into the darkness. Ahead to the north, a light flickers in
+# the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+# to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
+#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+# chamber! Sadly, it has already been completely emptied by
+# earlier adventurers. The only exit is to the south."""),
+# }
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+# room['foyer'].n_to = room['overlook']
+# room['foyer'].e_to = room['narrow']
+# room['overlook'].s_to = room['foyer']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
 
 #
 # Main
@@ -39,7 +40,7 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
-player = Player(room.get("outside", 'outside'), ["flashlight", "first-aid kit", "rock"])
+player = Player("Kairi", room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
@@ -52,32 +53,36 @@ player = Player(room.get("outside", 'outside'), ["flashlight", "first-aid kit", 
 # If the user enters "q", quit the game.
 
 while True:
-    player.printRoom(player.room)
-    cmd = input("-What do you want to do? You can: move 'n', move 'e', move 's', move 'w', press 'q' to quit \n-Pess 'i' to view item \n -> " )
+    print (f"You are at: {player.room.name}")
+    print (player.room.description)
+    cmd = input(f"-What do you want to do, {player.name}? You can: move 'n', move 'e', move 's', move 'w', press 'q' to quit  \n-Pess 'i' to view items and 'd' to drop items  \n-> ")
     if cmd == "q":
+        game = False
         break
     elif cmd == "n":
         if hasattr(player.room, 'n_to'):
-            player.room(player.room.n_to)
+           player.room = player.room.n_to
         else:
             print ("There is no room that way")
     elif cmd == "e":
         if hasattr(player.room, 'e_to'):
-            player.room(player.room.e_to)
+            player.room = player.room.e_to
         else:
             print ("There is no room that way")
     elif cmd == "s":
         if hasattr(player.room, 's_to'):
-            player.room(player.room.s_to)
+           player.room = player.room.s_to
         else:
             print ("There is no room that way")
     elif cmd == "w":
         if hasattr(player.room, 'w_to'):
-            player.room(player.room.w_to)
+           player.room = player.room.w_to
         else:
             print ("There is no room that way")
     elif cmd == "i":
         player.printItems(player.items)
+    elif cmd == "d":
+        player.drop(player.items)
     else:
         print("Invalid choice")
         cmd
--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,13 +1,19 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player():
-    def __init__ (self, room, items):
+    def __init__ (self, name, room, items=["flashlight", "first-aid kit", "rock"]):
         self.room = room
         self.items = items
+        self.name = name
     def printRoom(self, room):
         print (self.room)
     def printItems(self, items):
         for item in items:
             print (item)
+    def drop(self, items):
+        drop = input("What items do you want to drop: " for item in items)
+        for item in self.items:
+            if item != drop:
+                self.items.append(item)
 
         
