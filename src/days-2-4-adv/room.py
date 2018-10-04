b4df1e89342bcdea4f28b05ab0ac84965274e082
AlexanderCaves <ahcaves@gmail.com>
2018-10-04 05:13:17 -0700

    day three; room class functions added

--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,9 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
+        self.items = items
         self.n_to = None
         self.s_to = None
         self.e_to = None
@@ -21,3 +22,17 @@ class Room:
             return self.w_to
         else:
             return None
+
+    def room_items(self):
+        if len(self.items) > 0:
+            print('This room contains: ')
+            for item in self.items:
+                print(item.name, '--', item.description)
+        else:
+            print('There is nothing else in here.')
+
+    def dropped_item(self, item):
+        self.items.append(item)
+
+    def delete_item(self, index):
+        del self.items[index]
