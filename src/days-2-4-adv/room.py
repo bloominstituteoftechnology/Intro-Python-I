8a68fa73ad2eac22d6b65f83a0354a11c1dfa185
cubsfan46350 <cubsfan.46350@msn.com>
2018-10-04 17:22:48 -0500

    added item list to room class

--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -5,6 +5,7 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.items = []
         self.n_to = None
         self.s_to = None
         self.e_to = None
