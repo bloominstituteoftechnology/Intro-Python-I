fccc6d1755a46500109faa1c3eb0a6f3ddbe042e
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-10-04 10:03:26 -0500

    items class, and imported items into player and rooms

--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,5 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import item
+
+items = {
+   'sword': Item("Sword", """Double-edged, and guilded"""),
+
+    'pen': Item("Pen", """The pen is mightier than the sword"""),
+
+    'axe': Item("Axe", """Axe first, take names later""")
+}
+
+
 
 class Room():
     def __init__(self, name. description)
