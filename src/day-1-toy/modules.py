42231b665123f62fb1e1fa3787a89d324d06fbd5
Lauren <llwor94@gmail.com>
2018-10-12 20:52:00 -0700

    sys in module folder

--- src/day-1-toy/modules.py
+++ src/day-1-toy/modules.py
@@ -6,13 +6,13 @@ import os
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-
+print(str(sys.argv))
 
 # Print out the platform from sys:
-print()
+print(sys.platform)
 
 # Print out the Python version from sys:
-print()
+print(sys.version)
 
 
 
