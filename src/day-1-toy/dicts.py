ed65a3bf95112a1d97554ac4c60f6b2f3b456eca
Csillag61 <csillagok@gmail.com>
2018-11-05 18:26:46 -0500

    finished day1

--- src/day-1-toy/dicts.py
+++ src/day-1-toy/dicts.py
@@ -11,12 +11,12 @@ waypoints = [
         "lat": 43,
         "lon": -121,
         "name": "a place"
-    }, 
+    },
     {
         "lat": 41,
         "lon": -123,
         "name": "another place"
-    }, 
+    },
     {
         "lat": 43,
         "lon": -122,
@@ -25,5 +25,8 @@ waypoints = [
 ]
 
 # Write a loop that prints out all the field values for all the waypoints
+for wp in waypoints:
+    print(wp['name'], wp['lat'], wp['lon'])
 
 # Add a new waypoint to the list
+waypoints.append({"lat": 44, "lon": -123, "name": "added"})
--- src/day-1-toy/modules.py
+++ src/day-1-toy/modules.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 
@@ -6,14 +7,14 @@ import os
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-
+for arg in sys.argv:
+    print(arg)
 
 # Print out the platform from sys:
-print()
-
+print(sys.platform)
 # Print out the Python version from sys:
-print()
 
+print(sys.version)
 
 
 # Module "os"
@@ -21,11 +22,10 @@ print()
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-print()
+print(os.getpid())
 
 # Print the current working directory (cwd):
-print()
+print(os.getcwd())
 
 # Print your login name
-print()
-
+print(os.getlogin())
