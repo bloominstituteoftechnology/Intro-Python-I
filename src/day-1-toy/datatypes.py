5b1913887981727762f39c45ddb9801eafc714fb
verydecent <wnjhwng@gmail.com>
2018-11-05 12:48:21 -0800

    datatypes.py

--- src/day-1-toy/datatypes.py
+++ src/day-1-toy/datatypes.py
@@ -2,7 +2,13 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
-print(x + y)
+print(x + int(y))
+# y variable is a string
+# convert it into an int to add x integer 5 
+# add 5 + 7 .. 12
 
 # Write a print statement that combines x + y into the string value 57
-print(x + y)
+print(str(x) + y)
+# x variable is an integer
+# convert it into a string
+# concatenate/append y variable to x .. "57"
