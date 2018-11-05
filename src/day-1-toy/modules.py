d5a4959cebae6d36c9890e87f5a1ba30ff64b37b
travis-russell <russell.travis@yahoo.com>
2018-11-05 15:10:05 -0600

    sys

--- src/day-1-toy/modules.py
+++ src/day-1-toy/modules.py
@@ -9,11 +9,10 @@ import os
 
 
 # Print out the platform from sys:
-print()
+print(sys.platform)
 
 # Print out the Python version from sys:
-print()
-
+print(sys.version)
 
 
 # Module "os"
@@ -28,4 +27,3 @@ print()
 
 # Print your login name
 print()
-
