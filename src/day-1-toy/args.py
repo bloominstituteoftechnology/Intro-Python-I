2672a3e8f65c704edc4785217f4e7be55db7e653
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-05 15:58:36 -0800

    commit before standup

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -23,14 +23,16 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
-# What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+# What thing do you have to add to make this work? a => *a
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
-#def f3(...
+def f3(*arg):
+    if len(arg) == 1:
+        return arg + 1
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
