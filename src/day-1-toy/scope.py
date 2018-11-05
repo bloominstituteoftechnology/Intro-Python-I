69a4d1055644429f6dbe59c01dd5b97541d4d4b6
iAmAdamReid <adam@adamreid.me>
2018-11-05 14:39:29 -0800

    scope

--- src/day-1-toy/scope.py
+++ src/day-1-toy/scope.py
@@ -5,11 +5,12 @@
 x = 12
 
 def changeX():
+    global x
     x = 99
 
 changeX()
 
-# This prints 12. What do we have to modify in changeX() to get it to print 99?
+# This prints 12. What do we have to modify in changeX() to get it to print 99? USE THE GLOBAL X DEFINED OUTSIDE THE FUNCTION SCOPE
 print(x)
 
 
@@ -19,12 +20,13 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
 
     inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
-    # 999? Google "python nested function scope".
+    # 999? Google "python nested function scope". USE THE NONLOCAL SCOPE IN THE NESTED FUNCTION
     print(y)
 
 outer()
