db63659a4f6f635fed1786ddd496c3d41dc2c417
decagondev <decagondevelopment@gmail.com>
2018-11-05 22:22:52 +0000

    (MVP) : obj - Waypoint class adds name to LatLon class

--- src/day-1-toy/obj.py
+++ src/day-1-toy/obj.py
@@ -13,6 +13,12 @@ class LatLon:
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon.
 
+
+class Waypoint(LatLon):
+    def __init__(self, lat, lon, name):
+        super().__init__(lat, lon)
+        self.name = name
+
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
