c80c7fd263f6d114ac951c61f4c081bfc8fc25b5
decagondev <decagondevelopment@gmail.com>
2018-11-05 21:27:45 +0000

    (MVP) : comp - Use a list comprehension to create a list containing only the _even_ elements

--- src/day-1-toy/comp.py
+++ src/day-1-toy/comp.py
@@ -16,7 +16,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = y = [x.upper() for x in a]
 
 print(y)
 
@@ -26,6 +26,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [num for num in x if int(num) % 2 == 0]
 
 print(y)
