26e1f245ba055205cada8d7cbbd102cd08bc2efb
decagondev <decagondevelopment@gmail.com>
2018-11-05 21:40:56 +0000

    (MVP) : func - Write a function is_even that will return true if the passed in number is even

--- src/day-1-toy/func.py
+++ src/day-1-toy/func.py
@@ -1,4 +1,10 @@
 # Write a function is_even that will return true if the passed in number is even.
+def is_even(num):
+    if int(num) % 2 == 0:
+        return True
+    else:
+        return False
+
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
