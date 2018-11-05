9a318f16d6c9671c7a93c5a487e5f62486fd192e
Anthony Greb <agreb255@gmail.com>
2018-11-05 18:39:04 -0500

    got some done

--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -5,7 +5,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print()
 # Use the 'format' string method to print the same thing
-
+print()
 # Finally, print the same thing using an f-string
+print()
