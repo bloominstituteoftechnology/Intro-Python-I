ef833b8a8448a61749a56b97a26d2d3cea91dc04
marshall <marshall@marshalls-MacBook-Air.local>
2018-11-04 20:45:18 -0800

    fixed a couple of typos

--- src/day-1-toy/func.py
+++ src/day-1-toy/func.py
@@ -2,10 +2,11 @@
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
-
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 def is_even(num):
-  if num % 2 == 0:
+  if int(num) % 2 == 0:
     print("Even!")
   else:
     print("Odd")
+    
+is_even(num)
