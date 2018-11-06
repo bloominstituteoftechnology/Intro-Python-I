b4c4c4c7bc616d102f681710328dd3c66164ba29
Adrian Adames <adrianadames@gmail.com>
2018-11-06 00:51:20 -0500

    Corrected piece of code so it outputs number and not the number as a string.

--- src/day-1-toy/comp.py
+++ src/day-1-toy/comp.py
@@ -27,7 +27,7 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # # What do you need between the square brackets to make it work?
 
-y = [n for n in x if int(n) % 2 == 0]
+y = [int(n) for n in x if int(n) % 2 == 0]
 
 print(y)
 
