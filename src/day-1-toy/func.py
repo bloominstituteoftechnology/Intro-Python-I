0fdb5c5bb40f3f671d7af2a94846cb60dfba9f73
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-11-05 16:01:53 -0800

    halfway done

--- src/day-1-toy/func.py
+++ src/day-1-toy/func.py
@@ -1,6 +1,18 @@
 # Write a function is_even that will return true if the passed in number is even.
+def is_even(num):
+    if (n % 2 == 0):
+        print("true")
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
+#print(if num % 2 == "even" else "odd")
+
+#unfinished
+if num % 2 == 0:
+    print('Even!')
+    
+odds = [a for a in num if a not in evens]
+
+if (len.evens = 1) print('Even!')
--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -2,10 +2,15 @@ x = 10
 y = 2.24552
 z = "I like turtles!"
 
+#unfinished
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+text = ('x is %i, y is %.2f, z is "%s"' %(x, y, z))
+print(text)
 
 # Use the 'format' string method to print the same thing
+print ('x is {}, y is {:.2f}, z is "{}"'.format(x, y, z))
 
 # Finally, print the same thing using an f-string
+f'x is {x}, y is {y}, z is "{z}"'
