f96f98443595714d2feb3c68e61d6fe7beac888a
verydecent <wnjhwng@gmail.com>
2018-11-06 09:53:00 -0800

    args.py, cal.py

--- src/day-1-toy/args.py
+++ src/day-1-toy/args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 #def f1(...
+def f1(a, b):
+  return a + b
 
 print(f1(1, 2))
 
@@ -12,6 +14,11 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # def f2(...
+def f2(*args):
+  total = 0
+  for num in args:
+    total += num
+  return total
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +28,23 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
-#def f3(...
+def f3(a, b=1):
+  a + b
+
+# def f3(*anyvalue):
+#   if anyvalue == 1:
+#     return anyvalue + 1
+#   else:
+#     total = 0
+#     for num in anyvalue:
+#       total +=num
+#     return total
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +59,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 #def f4(...
+def f4(*args):
+  for key, value in args.items():
+    print(f"key: {key}, value: {value}")
 
 # Should print
 # key: a, value: 12
@@ -60,4 +80,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
