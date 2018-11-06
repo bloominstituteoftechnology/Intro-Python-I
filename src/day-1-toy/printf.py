53a30c6fe9fd9d8b9c32b3aef76f2e95fb06becc
Timothy Hoang <timh1203@gmail.com>
2018-11-05 22:45:07 -0500

    Complete printf.py

--- src/day-1-toy/printf.py
+++ src/day-1-toy/printf.py
@@ -7,5 +7,7 @@ z = "I like turtles!"
 # x is 10, y is 2.25, z is "I like turtles!"
 
 # Use the 'format' string method to print the same thing
+print('x is {0}, y is {1}, z is "{2}"'.format(x, round(y, 2), z))
 
 # Finally, print the same thing using an f-string
+print(f'x is {x}, y is {round(y, 2)}, z is "{z}"')
