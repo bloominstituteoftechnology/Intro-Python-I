be08ffcdde7e4a679bb8f848d0934d436b4e16bb
BrianARuff <thelonewolf198927@gmail.com>
2018-11-06 18:49:33 -0500

    Player and Room

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,23 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+from room import Room
+
+
+class Player(Room):
+
+    def __init__(self, player_name, room_name, room_description, player_hp=100, player_attack=10):
+        self.player_name = player_name
+        super().__init__(room_name, room_description)
+        self.player_hp = player_hp
+        self.player_attack = player_attack
+
+    def __str__(self):
+        return "<Player - Player Name: %s, Player HP: %s, Player Attack: %s, Player Room: %s," \
+               " Player's Room Description: %s" % (
+        self.player_name, self.player_hp, self.player_attack, self.room_name, self.room_description)
+
+
+p = Player('asdf', 'asdf', 'asdf')
+
+print(p)
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,2 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+
+    def __init__(self, room_name, room_description):
+        self.room_name = room_name
+        self.room_description = room_description
+
+    def __str__(self):
+        return "<Room - Name: %s, Description: %s" % (self.room_name, self.room_description)
