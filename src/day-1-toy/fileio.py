2d8d34f8a459d11fb3daa7e91d804388346f5d28
jlaluces123 <jlalucescareer123@gmail.com>
2018-11-05 19:54:45 -0800

    MVP: calendar and fileio done

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -1,12 +1,20 @@
 # Use open to open file "foo.txt" for reading
-
+file_name = open("foo.txt", "r")
 # Print all the lines in the file
+print (file_name.read())
+print("-----------------------------------------------------------------")
 
 # Close the file
+file_name.close()
 
 
 # Use open to open file "bar.txt" for writing
+file_write = open("bar.txt", "w")
+
 
 # Use the write() method to write three lines to the file
+multiple_lines = ["line one\n", "line two\n", "line three\n"]
+file_write.writelines(multiple_lines)
 
 # Close the file
+file_write.close()
