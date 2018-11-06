508f43330449ac60cebfa26aeb77b6ba3c7d2c24
Luisan <luisangelsc@gmail.com>
2018-11-06 10:41:45 -0600

    tuples.py

--- src/day-1-toy/tuples.py
+++ src/day-1-toy/tuples.py
@@ -8,7 +8,7 @@ import math
 def dist(a, b):
     """Compute the distance between two x,y points."""
     x0, y0 = a  # Destructuring assignment
-    x1, y1 = b
+    x1, y1 = b 
     
     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
 
@@ -22,11 +22,13 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # Write a function that prints all the values in a tuple
 
-# def print_tuple(...
+def print_tuple(t):
+    for i in t:
+        print(i)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
