0b3dc47b501abc941dd54a10f444f497e173d227
Timothy Hoang <timh1203@gmail.com>
2018-11-06 09:03:51 -0500

    Add comments to fileio.py

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -4,16 +4,29 @@ file = open('foo.txt', 'r')
 # Print all the lines in the file
 print(file.read())
 
+"""
+Alternative:
+for line in file:
+    print(line)
+"""
+
 # Close the file
 file.close()
 
 # Use open to open file "bar.txt" for writing
-file2 = open('foo.txt', 'w')
+file2 = open('bar.txt', 'w')
 
 # Use the write() method to write three lines to the file
 file2.write( "Python is a great language.\n")
 file2.write( "Javascript is a great language.\n")
 file2.write( "Java is a great language.\n");
+
+"""
+Alternative:
+file2.write("""Line 1
+Line 2
+Line 3""")
+"""
 
 # Close the file
 file2.close()
