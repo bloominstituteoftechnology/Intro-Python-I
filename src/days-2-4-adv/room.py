040d42a5dd203eb4ec1512b9024951fb2c8f67b1
Nicocchi <nicocchilover@gmail.com>
2018-11-06 16:13:29 -0500

    added comments to Room class

--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,8 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+"""Return a room object
+
+Room holds the room name, description and values of the
+rooms in their cardinal direction
+"""
+
 
 class Room:
+    # Initialize the properties of the class
     def __init__(self, name, description):
         self.name = name
         self.description = description
@@ -11,6 +18,7 @@ class Room:
         self.e_to = ''
         self.w_to = ''
 
+    # Return a formatted value of the Room class
     def __str__(self):
         return f"Name: {self.name}, Description: {self.description}"
 
