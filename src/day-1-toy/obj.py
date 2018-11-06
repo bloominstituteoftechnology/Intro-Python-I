e5b36e80d4bdd8a2d4bafb3949c90c9c73845dab
benny <oseguerabenny27@gmail.com>
2018-11-05 16:53:12 -0800

    Finished obj.py

--- src/day-1-toy/obj.py
+++ src/day-1-toy/obj.py
@@ -1,21 +1,34 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
+class Latlon(lat,lon):
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
         
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon.
+class Waypoint(Latlon):
+    def __init__(self,name,lat,lon):
+        super().__init__(lat,lon)
+        self.name = name
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
+class Geocache(Waypoint):
+    def __init__(self,name,difficulty,size,lat,lon):
+        super().__init__(name,lat,lon)
+        self.size = size
+        self.difficulty = difficulty
 
 # Make a new waypoint "Catacombs", 41.70505, -121.51521
-
+new_waypoint = Waypoint("Catacombs", 41.70505, -121.51521)
 # Print it
-#
+print(w)
 # Without changing the following line, how can you make it print into something
 # more human-readable?
-print(w)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
+g = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
 print(g)
