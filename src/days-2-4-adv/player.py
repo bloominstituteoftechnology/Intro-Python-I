8892d65222a8fc0d680f511ad7ad60b9e4abedc8
decagondev <decagondevelopment@gmail.com>
2018-11-06 21:34:51 +0000

    (MVP DAY 3) : implemented a basic get and drop method in the player class

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -7,3 +7,13 @@ class Player:
         self.room = room
         self.items = [] if items is None else items
         self.gold = 0
+
+    # class methods
+
+    # get method to pick up an item [ making use of append() method ]
+    def get(self, item):
+        self.items.append(item)
+
+    # drop method to drop an item [ making use of the del keyword and suing the index() method ]
+    def drop(self, item):
+        del self.items[self.items.index(item)]
