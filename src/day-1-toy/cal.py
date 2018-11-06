46db3a2c45f2ff65f57e44e4f9fe32eb63b73405
Carlos Vargas <cornielleandres@gmail.com>
2018-11-05 19:22:16 -0500

    Complete cal for day 1.

--- src/day-1-toy/cal.py
+++ src/day-1-toy/cal.py
@@ -1,9 +1,9 @@
 # Use the 'calendar' module to draw calendars to the console
 # https://docs.python.org/3.6/library/calendar.html
-#
+
 # Use the sys module to look for command line arguments in the `argv` list
 # variable.
-#
+
 # If the user specifies two command line arguments, month and year, then draw
 # the calendar for that month.
 
@@ -14,3 +14,16 @@
 # docs for the calendar module closely.
 
 import sys
+import calendar
+
+cal = calendar.Calendar()
+
+if len(sys.argv) > 2:
+	year = int(sys.argv[1])
+	month = int(sys.argv[2])
+else:
+	import datetime
+	year = datetime.date.today().year
+	month = datetime.date.today().month
+
+print(cal.monthdayscalendar(year, month))
