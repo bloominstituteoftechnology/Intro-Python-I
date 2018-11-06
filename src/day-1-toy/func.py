99343235bb05721d60d7547e5968437756420319
Luisan <luisangelsc@gmail.com>
2018-11-06 11:22:39 -0600

    args.py

--- src/day-1-toy/func.py
+++ src/day-1-toy/func.py
@@ -1,6 +1,14 @@
 # Write a function is_even that will return true if the passed in number is even.
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
+num = int(input("Enter a number: "))
+
+def is_even(num):
+    if num%2:
+        print('Odd!')
+    else:
+        print('Even!')
+
+is_even(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
