6fda437d9a348dcbb48a13f71696feeec74d16d6
mparedes003 <michelle.g.paredes@gmail.com>
2018-11-06 13:18:48 -0800

    Added class Player to hold player information.

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+  def __init__(self, start_location):
+    self.location = start_location
+
+  def change_location(self, new_location):
+    self.location = new_location
+
+  def __repr__(self):
+    return f"Current Location: {self.location}"
+
+  def __strs__(self):
+    return f"Current Location: {self.location}"
