0e1ead3429d61656856a7ed48c0725a5709b31fe
Timothy Hoang <timh1203@gmail.com>
2018-11-06 08:56:38 -0500

    Add comments to func.py

--- src/day-1-toy/func.py
+++ src/day-1-toy/func.py
@@ -1,9 +1,20 @@
 # Write a function is_even that will return true if the passed in number is even.
+def is_even(x):
+  return x % 2 == 0
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
+num = int(num)
+
+if is_even(num):
+  print("Even!")
+else:
+  print("Odd!")
+
+"""
+Alternative:
 def oddEven(n):
   if int(n) % 2 == 0:
     print("Even!")
@@ -11,3 +22,4 @@ def oddEven(n):
     print("Odd")
 
 oddEven(num)
+"""
