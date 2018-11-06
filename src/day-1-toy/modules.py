3211de5d7591de2edfb8306ae2bf77af410e7e52
Adrian Adames <adrianadames@gmail.com>
2018-11-06 00:08:11 -0500

    Finished working on the modules.py toy problems. Added documentation from python as comments for a couple of the methods used.

--- src/day-1-toy/modules.py
+++ src/day-1-toy/modules.py
@@ -6,13 +6,18 @@ import os
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-print(sys.argv[0])
+
+# sys.argv:  The list of command line arguments passed to a Python script. argv[0] is the script name (it is operating system dependent whether this is a full pathname or not). If the command was executed using the -c command line option to the interpreter, argv[0] is set to the string '-c'. If no script name was passed to the Python interpreter, argv[0] is the empty string.
+for i in sys.argv:
+    print(i)
 
 # Print out the platform from sys:
-print()
+# 
+# sys.platform: This string contains a platform identifier that can be used to append platform-specific components to sys.path, for instance.
+print(sys.platform)
 
 # Print out the Python version from sys:
-print()
+print(sys.version_info)
 
 
 
@@ -21,11 +26,11 @@ print()
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-print()
+print(os.getpid())
 
 # Print the current working directory (cwd):
-print()
+print(os.getcwd())
 
 # Print your login name
-print()
+print(os.getlogin())
 
