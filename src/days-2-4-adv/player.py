ae4a6010d861893f84c681a388b701e68bd143d1
IlanL75 <anatoly.leytman@gmail.com>
2018-11-06 15:13:02 -0800

    day two

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, title, room):
+        self.title = title
+        self.room = room
