df87d537dc048842f2738fd875e70a32844fcc46
travis-russell <russell.travis@yahoo.com>
2018-11-07 17:53:54 -0600

    mage and warrior

--- src/days-2-4-adv/warrior.py
+++ src/days-2-4-adv/warrior.py
@@ -4,8 +4,16 @@ from player import Player
 class Warrior(Player):
     def __init__(self):
         self.main_weapon = "Barbed sword"
-        self.health = 140
+        self.hp = 140
         self.rage = 0
+        self.armor = 100
 
     def thrash(self, target):
-        target.health -= 20
+        target.health -= (20 + (100 / target.armor * 5))
+
+    def eye_for_an_eye(self, target):
+        if self.hp > 20:
+            target.hp -= 20
+            self.hp -= 20
+        else:
+            print("You'll kill yourself!")
