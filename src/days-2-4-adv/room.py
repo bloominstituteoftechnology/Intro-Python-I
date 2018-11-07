53d78ea5518d333c3d2419e1a7dbb5853ee82d69
Csillag61 <csillagok@gmail.com>
2018-11-07 16:26:13 -0500

    class Room refactored

--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -2,3 +2,16 @@
 # description attributes.
 
 
+from item import LightSource
+
+
+class Room:
+    def __init__(self, name, desc, lit=True):
+        self.name = name
+        self.desc = desc
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+        self.items = []
+        self.lit = lit
