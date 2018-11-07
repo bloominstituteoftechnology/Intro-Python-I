07cac3864dc12c6aec009395bf4ffaf799eddb3e
travis-russell <russell.travis@yahoo.com>
2018-11-07 17:42:50 -0600

    warrior

--- src/days-2-4-adv/warrior.py
+++ src/days-2-4-adv/warrior.py
@@ -1,1 +1,11 @@
 from player import Player
+
+
+class Warrior(Player):
+    def __init__(self):
+        self.main_weapon = "Barbed sword"
+        self.health = 140
+        self.rage = 0
+
+    def thrash(self, target):
+        target.health -= 20
