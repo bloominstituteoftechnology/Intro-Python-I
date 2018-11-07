be8fd8f801ee2267391a01303ea33877fdae94f5
Terrance Danckert <tbarnett2020@gmail.com>
2018-11-07 17:58:57 -0500

    Day 3...Going through and trying to refeactor things

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -5,12 +5,22 @@ class Player:
   def __init__(self, name, start_room):
     self.name = name
     self.location = start_room
+    self.inventory = []
   def changeLocation(self, new_room):
     self.location = new_room
   def __repr__(self):
     return "Current Location: {}\n".format(self.location.name)
   def __str__(self):
     return "Current Location: {}\n".format(self.location.name)
+  def addItem(self, item):
+    self.inventory.append(item)
+  def removeItem(self, item):
+    if len(self.inventory) > 0:
+      for i in self.inventory:
+        if i == item:
+          self.inventory.remove(i)
+    else:
+      print('that item is not in your inventory')
 
 
 
