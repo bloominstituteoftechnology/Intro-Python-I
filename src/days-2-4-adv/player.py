fd9b7973690d1c254137873eb8a9fc71612d317b
James Cassidy <cassidyjamesw@gmail.com>
2018-11-07 15:54:08 -0500

    day 2

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, current):
+        self.name = name
+        self.current = current
+
+    def move_to(self, direction):
+        if direction:
+            self.current = self.current.to_new(direction)
+        else:
+            print("You can't go that way.")
