af1e2f28108c562ca8ee83851db2f118e443722d
Tsai Huang <tsaihuangsd@gmail.com>
2018-11-06 16:01:07 -0800

    Add Day 2 MVP
    Added completed adv.py, room.py, and player.py

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+import textwrap
 
 # Declare all the rooms
 
@@ -36,8 +38,13 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+def main():
+    new_player_name = input('Please enter your name: ')
+
 
 # Make a new player object that is currently in the 'outside' room.
+    new_player = Player(new_player_name, room['outside'])
+
 
 # Write a loop that:
 #
@@ -49,3 +56,76 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+    while True:
+        # display current room info
+        print(f"You are in the {new_player.room.name}")
+        print(textwrap.wrap(new_player.room.description))
+
+        # promp for next move
+        print("What do you want to do next?")
+        print("('n': go north")
+        print("('s': go south")
+        print("('e': go east")
+        print("('w': go west")
+        print("('q': quit game")
+        next_move = input("")
+
+        # type check next_move input
+        try:
+            next_move = str(next_move) 
+        except ValueError:
+            print("Please enter an appropriate letter.")
+        
+        if len(next_move) == 1:
+
+            #handle quit
+            if next_move == 'q':
+                print("Thank you for playing.  Goodbye!")
+                break
+
+            elif next_move == 'n':
+                if hasattr(new_player.room, 'n_to'):
+                    new_player.room = new_player.room.n_to
+                    if new_player.room.name == 'Treasure Chamber':
+                        print("You won!!")
+                        break
+                else:
+                    print("This move is not allowed.  Please try again.")
+            
+            elif next_move == 's':
+                if hasattr(new_player.room, 's_to'):
+                    new_player.room = new_player.room.s_to
+                    if new_player.room.name == 'Treasure Chamber':
+                        print("You won!!")
+                        break
+                else:
+                    print("This move is not allowed.  Please try again.")
+            elif next_move == 'e':
+                if hasattr(new_player.room, 'e_to'):
+                    new_player.room = new_player.room.e_to
+                    if new_player.room.name == 'Treasure Chamber':
+                        print("You won!!")
+                        break
+                else:
+                    print("This move is not allowed.  Please try again.")
+            elif next_move == 'w':
+                if hasattr(new_player.room, 'w_to'):
+                    new_player.room = new_player.room.w_to
+                    if new_player.room.name == 'Treasure Chamber':
+                        print("You won!!")
+                        break
+                else:
+                    print("This move is not allowed.  Please try again.")
+            
+            # elif        or next_move == 's' or next_move == 'e' or next_move == 'w':
+                # print('here')
+                # break
+
+                # move_str = next_move + '_to'
+                # if hasattr(new_player.room, move_str):
+                #     new_player.room = new_player.room[move_str]
+        else:
+            print("Please enter an appropriate letter.")
+
+main()
