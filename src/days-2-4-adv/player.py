6b82bcfb8df447ced3ad9e9c127b24b11f985056
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-11-07 18:20:06 -0500

    typo

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,7 +1,5 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-# Write a class to hold player information, e.g. what room they are in
-# currently.
 class Player:
     def __init__(self, name, location, inventory=[]):
         self.name = name
