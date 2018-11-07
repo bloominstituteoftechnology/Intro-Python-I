c9ad1393d6f9237756ff0dc515cf59ffedaebb40
decagondev <decagondevelopment@gmail.com>
2018-11-07 18:59:00 +0000

    (STRETCH) : added random to the enemy class

--- src/days-2-4-adv/enemy.py
+++ src/days-2-4-adv/enemy.py
@@ -1,7 +1,8 @@
+import random
 from item import Item
 
 class Enemy:
-    def __init__(self, room, name, items=None, health=10):
+    def __init__(self, room, name, items=None, health=100):
         self.room = room
         self.name = name
         self.items = [] if items is None else items
@@ -13,7 +14,7 @@ class Enemy:
     def take_damage(self, dmg):
       if self.alive:
         print("the " + self.name + " took " + str(dmg) + " damage")
-        self.health -= dmg
+        self.health -= (dmg + random.randrange(1, 20))
         if self.health <= 0:
           print("the " + self.name + " died from the battle")
           self.alive = False
