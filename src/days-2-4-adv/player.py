3ee5cc26eeb6b30ddec5110dbbc0033e29b9512c
David Doswell <davidoliverdoswell@icloud.com>
2018-11-07 15:56:34 -0800

    set up guided files from guided projects for later

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,2 +1,26 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+class Player:
+    def __init__(self, currentRoom):
+        self.currentRoom = currentRoom
+        self.health = 100
+        self.inventory = []
+        self.movement_speed = 10
+
+    def pickup_item(self, item):
+        self.inventory.append(item)
+
+    def try_move(self, direction):
+
+
+        # Try to move in a direction or print error message
+
+        d = direction + "_to"
+
+        if not hasattr(self.currentRoom, d):
+            print("You can't go that way")
+            return self.currentRoom
+        else:
+            self.currentRoom = self.currentRoom[]
---
+++ src/days-2-4-adv/warrior.py
@@ -0,0 +1,8 @@
+from player import Player
+
+class Warrior(Player):
+    def __init__(self):
+        self.main_weapon = "Sword"
+        self.second_weapon = "Shield"
+        self.aggro_level = 10
+        self.rage = 0
