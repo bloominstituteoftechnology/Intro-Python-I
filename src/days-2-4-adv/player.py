638d222b6712a24c265be12103a62bfb554090c8
thomaskisner <tjkisner@gmail.com>
2018-11-08 18:45:40 -0500

    struggled adding items

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -1,5 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, room):
-        self.room = room 
+    def __init__(self, name, room, inventory): 
+        self.name = name
+        self.room = room
+        self.inventory = inventory
+
+    
+    def __repr__(self):
+        for item in self.inventory:
+            print(str(item.item_name)) 
+            print(str(item.description))
+            return
+            
+            
+
