0a892de314affbd04d9a471390172522b0bcab87
Tsai Huang <tsaihuangsd@gmail.com>
2018-11-08 17:01:56 -0800

    Fixed bugs in checking if the room is lit and added condition to disallow player to get item in a dark room

--- src/days-2-4-adv/data.py
+++ src/days-2-4-adv/data.py
@@ -20,11 +20,11 @@ room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons", [item['scroll'], item['dictionary']], True),
 
-    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east.", [item['ring'], item['lamp']], True),
+    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east.", [item['ring'], item['lamp']]),
 
     'overlook': Room("Grand Overlook", "A steep cliff appears before you,falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.", []),
 
-    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air.",[]),
+    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air.",[item['diamond']]),
 
     'treasure': Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.",[item['holy_grail']]),
 }
--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -9,12 +9,14 @@ class Player:
         self.current_room = current_room
         self.player_items = player_items
         self.score = score
-
+    
+    # handle add item from room to player's inventory
     def take_item(self, item_object):
         item_object.on_take(self)
         self.player_items.append(item_object)
         self.current_room.room_items.remove(item_object)
 
+    # handle drop item from player's inventory to room
     def drop_item(self, item_object):
         item_object.on_drop()
         self.current_room.room_items.append(item_object)
