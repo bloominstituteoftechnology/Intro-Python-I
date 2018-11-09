12c6b3dc27bb9dfb00e245beedd275f47fdc83eb
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-08 16:32:28 -0800

    standup commit. working on create items, take_item, drop_item

--- src/days-2-4-adv/player.py
+++ src/days-2-4-adv/player.py
@@ -3,5 +3,16 @@
 
 class Player:
     def __init__(self, room):
-        self.name = ""
-        self.room = room
+        self.current_room = room #starting/current room of the player
+        self.health = 100
+        self.inventory = []
+
+    
+    def try_move(self, direction):
+        key = direction + "_to"
+        if not hasattr(self.current_room, key):
+            print("You can't go that way!")
+            return self.current_room
+        else:
+            return getattr(self.current_room, key)
+        
