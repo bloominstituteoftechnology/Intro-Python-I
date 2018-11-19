d613052f2ad4fca0a4b5890dac58aff44b8d5da0
Allen Hai <allenhai03@gmail.com>
2018-11-18 22:58:17 -0600

    write dicts

--- src/day-1-toy/dicts.py
+++ src/day-1-toy/dicts.py
@@ -26,4 +26,12 @@ waypoints = [
 
 # Write a loop that prints out all the field values for all the waypoints
 
+for wp in waypoints:
+    for v in list((wp.values())):
+        print(v)
+
 # Add a new waypoint to the list
+
+waypoints.append({'lat': 34, 'lon': 32, 'name': 'taco bell'})
+
+print(waypoints)
