86cc9dddfd6f80d44f1a10382c7eb85ea7181fb7
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-25 15:16:23 -0800

    lists complete

--- src/day-1-toy/lists.py
+++ src/day-1-toy/lists.py
@@ -6,24 +6,31 @@ y = [8, 9, 10]
 
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
-# Change x so that it is [1, 2, 3, 4]
-# [command here]
-print(x)
+# 1. Change x so that it is [1, 2, 3, 4]
+x.append(4)
+print('--- 1 ---')
+print(x, '\n')
 
-# Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# [command here]
-print(x)
+# 2. Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
+x += y
+print('--- 2 ---')
+print(x, '\n')
 
-# Change x so that it is [1, 2, 3, 4, 9, 10]
-# [command here]
-print(x)
+# 3. Change x so that it is [1, 2, 3, 4, 9, 10]
+x.remove(8)
+print('--- 3 ---')
+print(x, '\n')
 
-# Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# [command here]
-print(x)
+# 4. Change x so that it is [1, 2, 3, 4, 9, 99, 10]
+x.insert(5, 99)
+print('--- 4 ---')
+print(x, '\n')
 
-# Print the length of list x
-# [command here]
+# 5. Print the length of list x
+print('--- 5 ---')
 print(len(x))
 
 # Using a for loop, print all the element values multiplied by 1000
+print('--- 6 ---')
+for i in x:
+    print(i * 1000)
