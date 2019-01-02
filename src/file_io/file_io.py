40c58195e8dcfe7a4231fd8d39a296e39f953973
Sean Chen <skypemaster007@gmail.com>
2019-01-02 12:15:23 -0800

    Move days 3 and 4 content into its own repo and move each file into its own dir

--- src/file_io/file_io.py
+++ src/file_io/file_io.py
@@ -1,11 +1,11 @@
-# Use open to open file "foo.txt" for reading
+# Use the `open` function to open the file "foo.txt" for reading
 
 # Print all the lines in the file
 
 # Close the file
 
 
-# Use open to open file "bar.txt" for writing
+# Use the `open` function to open the file "bar.txt" for writing
 
 # Use the write() method to write three lines to the file
 
