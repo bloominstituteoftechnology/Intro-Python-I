7df2573a15b411f6de7b4bd8d7dab8e4d01f8c1f
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-03 18:28:42 -0800

    assured values were converted into integer values to print out the result of the operation

--- src/datatypes.py
+++ src/datatypes.py
@@ -14,7 +14,8 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+integer = int(x) + int(y)
+print(integer)
 
 # Write a print statement that combines x + y into the string value 57
 
