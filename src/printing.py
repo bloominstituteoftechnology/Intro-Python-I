f4fd3c26c9cfb0254ed25d901141f0b3b0191751
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-03 19:49:28 -0800

    cleaned up code

--- src/printing.py
+++ src/printing.py
@@ -12,11 +12,12 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-print("x is %i, y is %r, z is %s" % (x, y, z))
+print("x is %i, y is %.5f, z is %s" % (x, y, z))
 
 # Use the 'format' string method to print the same thing
 print('x is {}, y is {}, z is {}'.format(x, y, z))
-print('x is {1:1.5f}, y is {2}, z is {0}'.format(x, y, z))
+
+print('x is {0}, y is {1}, z is {2}'.format(x, y, z))
 
 # Finally, print the same thing using an f-string
 print(f'x is {x}, y is {y}, z is {z}')
