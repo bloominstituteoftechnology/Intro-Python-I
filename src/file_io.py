34e6b255b930adaceb26700714d8b4ddefe6c27d
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-03 23:37:43 -0800

    completed args & file_io problems

--- src/file_io.py
+++ src/file_io.py
@@ -9,10 +9,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+file = open(
+    '/Users/chivo/lambda-school/week-sixteen/Intro-Python-I/src/foo.txt', 'r')
+print(file.read())
+file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+created = open(
+    '/Users/chivo/lambda-school/week-sixteen/Intro-Python-I/src/bar.txt', 'w')
+created.write('OMG LOL HELLO KITTY')
+created.close()
