4eaf6ba5d1de62bf4eb693b2d0b39611cdc73503
Conner Alegre <cga0@pm.me>
2019-01-07 14:07:10 -0700

    Update args.py

--- src/args.py
+++ src/args.py
@@ -4,14 +4,24 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+def f1(x, y):
+    return x + y
 
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+def f2(*argv):
+    count = 0
+    if type(argv[0]) is list:
+        for num in argv[0]:
+            count += num
+    else:
+        for arg in argv:
+            if type(arg) is int:
+                count += arg
+    return count
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -27,7 +37,11 @@ print(f2(a))    # Should print 22
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+def f3(x, y = 0):
+    if y == 0:
+        return x + 1
+    else:
+        return x + y
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -40,8 +54,10 @@ print(f3(8))     # Should print 9
 # key: baz, value: 12
 #
 # Google "python keyword arguments".
-
-# YOUR CODE HERE
+def f4(*argv, **kwargs):
+    if len(argv) == 1:
+        print(argv[0])
+    print(str(kwargs))
 
 # Should print
 # key: a, value: 12
