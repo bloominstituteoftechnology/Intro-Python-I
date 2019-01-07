e9aefb5e95b7a35b7eac54e29a3d5014e9fcbdc5
Kevin Sooter <kevin.m.sooter@gmail.com>
2019-01-07 14:32:11 -0600

    Completed

--- src/classes.py
+++ src/classes.py
@@ -1,29 +1,46 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
+class LatLon:
+  def __init__(self, lat, lon):
+    self.lat = lat
+    self.lon = lon
 
-# YOUR CODE HERE
-        
+  def __str__(self):
+    return "Latitude: {}, Longitude: {}".format(self.lat, self.lon)
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
-# constructor. It should inherit from LatLon. Look up the `super` method.
+# constructor. It should inherit from LatLon.
+class Waypoint(LatLon):
+  def __init__(self, name, lat=0, lon=0):
+    super().__init__(lat, lon)
+    self.name = name
 
-# YOUR CODE HERE
+  def __str__(self):
+    return "<Waypoint '{}' {:f},{}>".format(self.name, self.lat, self.lon)
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
+class Geocache(Waypoint):
+  def __init__(self, name, difficulty, size, lat=0, lon=0):
+    super().__init__(name, lat, lon)
+    self.difficulty = difficulty
+    self.size = size
+  
+  def __str__(self):
+        return "<Geocache '{}' {} {} {:f},{}>".format(self.name, self.difficulty, self.size, self.lat, self.lon)
 
-# YOUR CODE HERE
+# Make a new waypoint "Catacombs", 41.70505, -121.51521
+l = LatLon(22, 44)
+w = Waypoint("Catacombs", 41.70505, -121.51521)
 
-# Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
-
-# YOUR CODE HERE
-
+# Print it
 # Without changing the following line, how can you make it print into something
-# more human-readable? Hint: Look up the `object.__str__` method
-print(waypoint)
+# more human-readable?
+print(l)
+print(w)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
-
-# YOUR CODE HERE
+g = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
-print(geocache)
+print(g)
