b7d913d423830396010c37394c982bfdcf769df4
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-07 14:09:32 -0700

    Finish comprehensions.py

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -10,14 +10,14 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [boring for boring in range(1, 6)]
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [cubey ** 3 for cubey in range(10)]
 
 print(y)
 
@@ -26,7 +26,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [uppey.upper() for uppey in a]
 
 print(y)
 
@@ -36,6 +36,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [evensteven for evensteven in x if int(evensteven) % 2 == 0] # No type coercion :(
 
 print(y)
