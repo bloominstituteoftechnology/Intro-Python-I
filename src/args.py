726f7d9a8bae186384eb26dc454b0ab8f63b256b
Linh Bouniol <paulalinh89@gmail.com>
2019-01-07 15:13:09 -0800

    args, bignum, hello

--- src/args.py
+++ src/args.py
@@ -15,9 +15,10 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*args):
-    a = 0
+    x = 0
     for num in args:
-        a += num
+        x += num
+    return x
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -27,13 +28,18 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(num1, num2 = None):
+    if num2 == None:
+        return num1 + 1
+    else:    
+        return num1 + num2
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -48,6 +54,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f'key: {key}, value: {value}')
 
 # Should print
 # key: a, value: 12
@@ -66,4 +75,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
