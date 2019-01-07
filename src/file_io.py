d5213f271726f7854c8138940da9006f41f1eaed
Samantha Gatt <smfgatt@gmail.com>
2019-01-07 18:27:31 -0500

    Completes scopes and file_io

--- src/file_io.py
+++ src/file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt') as f1:
+  print(f1.read())
+f1.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+contents = '''
+  Hello this is the first line.
+
+  And the third
+
+  Finally the fifth!
+  '''
+
+with open('bar.txt', 'w+') as f2:
+  f2.write(contents)
+f2.closed
+
+with open('bar.txt') as f3:
+  if f3.read() == contents:
+    print("The contents of bar.txt match what was written to it!")
+
+import os
+os.remove('bar.txt')
+print('bar.txt has been deleted')
