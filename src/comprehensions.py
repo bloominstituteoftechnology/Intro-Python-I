9b409225ea24400ef594dbf7db3bee9bcf41bbd1
ezra davis davis <ezra.rr.davis@gmail.com>
2019-01-07 13:03:26 -0800

    comprehensions file completed

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -11,14 +11,16 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
+for n in range(1,6):
+  y.append(n)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for n in range(0,10):
+  y.append(n ** 3)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -27,6 +29,8 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
+for word in a: 
+  y.append(word.upper())
 
 print(y)
 
@@ -37,5 +41,8 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 y = []
-
+for n in x:
+  n = int(n)
+  if(n % 2 == 0):
+    y.append(n)
 print(y)
