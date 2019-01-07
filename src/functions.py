510f5c463ff343a5e465a4e64d6adb8786410650
Linh Bouniol <paulalinh89@gmail.com>
2019-01-07 15:44:19 -0800

    dictionaries, functions, lists

--- src/functions.py
+++ src/functions.py
@@ -1,12 +1,19 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(number):
+    if number %2 == 0:
+        return True
+    return False    
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
+#num = input("Enter a number: 5")
+num = int(6)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+if is_even(num):
+    print("Even!")
+else:
+    print("Odd")
