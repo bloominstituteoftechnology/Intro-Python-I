c73dc7a0810845cdcdcaa33611d1a470f432a076
Andrew <ghobrial.andrew@gmail.com>
2019-01-07 18:08:11 -0500

    completed printing.py

--- src/modules.py
+++ src/modules.py
@@ -10,7 +10,9 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+for argument in sys.argv:
+  print(argument)
+  
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 
