9eca61ff2a030276f6e9a445134d5adc48c1b9e5
Kevin <brackkevin@gmail.com>
2019-01-07 17:05:11 -0600

    datatypes.py complete

--- src/datatypes.py
+++ src/datatypes.py
@@ -14,8 +14,10 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+print(f'Combine x + y as integers: {x + int(y)}')
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print(f'Combine x + y as strings: {str(x) + y}')
