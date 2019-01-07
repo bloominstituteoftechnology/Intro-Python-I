8cf18c8e47c14a6d954b7550a09058d075cfde48
Kat Fries <katherynejf@gmail.com>
2019-01-07 17:19:56 -0500

    finished functions

--- src/functions.py
+++ src/functions.py
@@ -1,12 +1,16 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
-# YOUR CODE HERE
+def is_even(n):
+  return False if n % 2 else True
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
-# YOUR CODE HERE
-
+def is_even_printer():
+  if is_even(num) == True:
+    print('Even!')
+  else:
+    print('Odd')
+is_even_printer()
