b77f0b2dd884fd5b2ff11daa880285749e22ab73
Jeanette <jeanfern555@gmail.com>
2019-01-07 17:55:42 -0600

    completed file.io.py

--- src/file_io.py
+++ src/file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt', 'r') as foo:
+    print (foo.read())
+foo.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,12 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as bar:
+    bar.write('''
+    Two cartons of yogurt walk into a bar. 
+    The bartender, who is a tub of cottage cheese, 
+    says to them, "We don't serve your kind in here."
+    One of the yogurt cartons says to him, "Why not? 
+    We're cultured individuals.
+    ''')
+bar.closed
