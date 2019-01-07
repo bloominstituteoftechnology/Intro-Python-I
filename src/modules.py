61f93990038598d5e0fe08783f6d73f7fe7668c9
Kat Fries <katherynejf@gmail.com>
2019-01-07 15:25:46 -0500

    finished up to modules

--- src/modules.py
+++ src/modules.py
@@ -9,23 +9,24 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+print(sys.argv)           # ['modules.py']
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+print(sys.platform)       # darwin
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
-
+print(sys.version_info)   # sys.version_info(major=3, minor=7, micro=2, releaselevel='final', serial=0)
+print(sys.version)        #3.7.2 (default, Dec 27 2018, 07:35:52)
+                          # [Clang 10.0.0 (clang-1000.11.45.5)]
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+print(os.getpid())
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+print(os.getcwd())
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print(os.getlogin())
