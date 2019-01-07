2a812643f0857d8942c1d3a4b2fdc357cb24bccf
stdemicheli <stdemicheli@gmail.com>
2019-01-08 06:20:07 +0700

    Adds scopes, fileio, classes, and calendar exercises.

--- src/file_io.py
+++ src/file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open("foo.txt") as file:
+    print(file.read())
+file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar_file = open("bar.txt", "w")
+bar_file.write("Hi\n")
+bar_file.write("how\n")
+bar_file.write("are you?\n")
+bar_file.close()
