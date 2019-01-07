f91877f6bfa14edf484e4afe222c513dcf5ee6e1
michaelagard <mdagard@gmail.com>
2019-01-07 15:37:46 -0500

    add answer

--- src/datatypes.py
+++ src/datatypes.py
@@ -1,6 +1,6 @@
 """
 Python is a strongly-typed language under the hood, which means 
-that the types of values matter, especially when we're trying
+that it the types of values matter, especially when we're trying
 to perform operations on them. 
 
 Note that if you try running the following code without making any
@@ -12,6 +12,8 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
+
+print(x + int(y))
 
 # YOUR CODE HERE
 
