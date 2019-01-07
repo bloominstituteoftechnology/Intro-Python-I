6df8b84429bc3fe7f7021599b20e50b49f83479d
Ryan Walker <rytwalker@gmail.com>
2019-01-07 18:18:57 -0500

    completed scopes.py and file_io.py creating a bar.txt file

--- src/file_io.py
+++ src/file_io.py
@@ -1,6 +1,6 @@
 """
 Python makes performing file I/O simple. Take a look
-at how to read and write to files here: 
+at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
@@ -9,10 +9,25 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+# can do it this way...
+# with open('src/foo.txt') as f:
+#     print(f.read())
+# f.closed
+
+# ...Or this way
+foo_file = open('src/foo.txt', 'r')
+print(foo_file.read())
+foo_file.close()
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar_file = open('src/bar.txt', 'w')
+bar_file.write('This is the first line I am writing\n')
+bar_file.write('This is the second line I am writing\n')
+bar_file.write('This is the third line I am writing\n')
+bar_file.close()
--- src/scopes.py
+++ src/scopes.py
@@ -1,11 +1,15 @@
 # Experiment with scopes in Python.
-# Good reading: https://www.programiz.com/python-programming/global-local-nonlocal-variables
+# Good reading:
+# https://www.programiz.com/python-programming/global-local-nonlocal-variables
 
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
+
 def changeX():
+    global x
     x = 99
+
 
 changeX()
 
@@ -19,6 +23,7 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
 
     inner()
@@ -26,5 +31,6 @@ def outer():
     # This prints 120. What do we have to change in inner() to get it to print
     # 999? Google "python nested function scope".
     print(y)
+
 
 outer()
