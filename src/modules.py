fd10b018a23d4145a3e73a9a39ec869de53511c9
alejandrok93 <alejandrok93@gmail.com>
2019-01-07 14:23:24 -0800

    Completed modules and printing excercises

--- src/modules.py
+++ src/modules.py
@@ -27,9 +27,12 @@ import os
 
 # Print the current process ID
 # YOUR CODE HERE
+print(os.geteuid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print(os.curdir)
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
