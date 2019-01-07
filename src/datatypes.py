016252efe5ff491a44f7b05441566b65563ca1b6
jmbradley <jmbradley808@gmail.com>
2019-01-07 16:50:05 -0500

    datatypes completed

--- src/datatypes.py
+++ src/datatypes.py
@@ -15,7 +15,18 @@ y = "7"
 
 # YOUR CODE HERE
 
+x = 5
+y = "7"
+a = int(y)
+sum = a + x
+print (sum)
+12
+
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+
+x = 5
+y = "7"
+b =  str(x)
