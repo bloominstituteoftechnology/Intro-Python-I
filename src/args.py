1b5c9201382536e58f03f5fe094d5d55a3c79efe
Drew <drewsup123c@gmail.com>
2019-01-07 16:16:38 -0700

    stuck on args.py coming back to it

--- src/args.py
+++ src/args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(int1,int2):
+    return int(int1) + int(int2)
+
 
 print(f1(1, 2))
 
@@ -12,6 +15,12 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+def f2(*ints):
+    total = 0
+    for nums in ints:
+        total += nums
+    return total
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,20 +30,23 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))   # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(int1, int2 = 1):
+    return int(int1) + int(int2)
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
-# prints ouf the keys and values like so:
+# prints out the keys and values like so:
 #
 # key: foo, value: bar
 # key: baz, value: 12
@@ -42,6 +54,10 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    print(kwargs)
+    for value in kwargs:
+        return 'Key : %s, value: %s' %(value, value)
 
 # Should print
 # key: a, value: 12
