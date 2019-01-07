0d96eb6b947a6e03935853ab3c1bec4816a8170c
lajawanti <myemail>
2019-01-07 15:26:29 -0500

    bignum.py completed

--- src/bignum.py
+++ src/bignum.py
@@ -1,4 +1,8 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
-
+'''console.log(Math.pow(65536, 2));
+   4294967296 
+   the output is same...'''
 # YOUR CODE HERE
+
+print(65536 ** 2)
