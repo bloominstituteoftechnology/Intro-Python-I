fc390e8b9561c64518618a905fd2653715a31fa8
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2019-01-08 14:01:04 -0700

    printing

--- src/modules.py
+++ src/modules.py
@@ -1,3 +1,4 @@
+
 """
 In this exercise, you'll be playing around with the sys module,
 which allows you to access many system specific variables and
@@ -19,14 +20,15 @@ print(opsys)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-print(sys.version)
+print(sys.version) 
 
 
-import os
+
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+import os
 print(os.getpid())
 
 # Print the current working directory (cwd):
@@ -36,3 +38,7 @@ print(os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
 print(os.getlogin())
+
+import math 
+
+print('pi is', math.pi)
