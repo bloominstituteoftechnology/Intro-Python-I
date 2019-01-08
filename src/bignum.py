4823ce35d6060dd9ef351fe9d98a938835b0259f
lajawanti <myemail>
2019-01-08 16:03:17 -0500

    Some changes dictionaries.py completed

--- src/bignum.py
+++ src/bignum.py
@@ -1,8 +1,8 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
-'''console.log(Math.pow(65536, 2));
-   4294967296 
+'''console.log(Math.pow(2, 65536));
+   Infinity 
    the output is same...'''
 # YOUR CODE HERE
 
-print(65536 ** 2)
+print(2 ** 65536)
--- src/dictionaries.py
+++ src/dictionaries.py
@@ -35,10 +35,22 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+x = {
+        "lat": 55,
+        "lon": -155,
+        "name": "added place"
+    }
+
+waypoints.append(x)
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
+waypoints[0].update({"lon" : -130, "name" : "not a real place"})
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for i in waypoints:
+    print(i)
