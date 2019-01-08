1f59ae1721e3f09f21a55af79e14a5aa2bc54bf2
Lee Len Formento <leebatef@gmail.com>
2019-01-08 12:22:13 -0500

    modified modules.py

--- src/modules.py
+++ src/modules.py
@@ -8,10 +8,11 @@ level operating system functionality.
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
-# Print out the command line arguments in sys.argv, one per line:
+# # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-for command in sys.argv:
-    print(command)
+# for (let i = 0; i< 10; i++)
+for arg in range(sys.argv):
+    print(arg)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
