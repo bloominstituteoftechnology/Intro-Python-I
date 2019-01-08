18bc75fc95933f61edef73ccea2a82d63636ae7b
Eva <evalee904@gmail.com>
2019-01-08 17:02:48 -0600

    Calendar

--- src/cal.py
+++ src/cal.py
@@ -22,3 +22,14 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+num_args = len(sys.argv)
+
+if num_args == 1:
+  print(calendar.monthcalendar(datetime.now().year, datetime.now().month))
+elif num_args == 2:
+  print(calendar.monthcalendar(datetime.now().year, int(sys.argv[1])))
+elif num_args == 3:
+  print(calendar.monthcalendar(int(sys.argv[2]), int(sys.argv[1])))
+else:
+  print('Please specify a month or a year or both. No input will would output the current month')
