e84d7b1a4a8fd01ea10ff3de99c73b86d3516c1f
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-08 09:35:52 -0800

    completed cal.py

--- src/cal.py
+++ src/cal.py
@@ -1,18 +1,18 @@
 """
-The Python standard library's 'calendar' module allows you to 
+The Python standard library's 'calendar' module allows you to
 render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
 
 Write a program that accepts user input of the form
   `calendar.py month [year]`
 and does the following:
- - If the user doesn't specify any input, your program should 
+ - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
    module may be helpful for this.
  - If the user specifies one argument, assume they passed in a
    month and render the calendar for that month of the current year.
  - If the user specifies two arguments, assume they passed in
-   both the month and the year. Render the calendar for that 
+   both the month and the year. Render the calendar for that
    month and year.
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
@@ -23,17 +23,21 @@ import sys
 import calendar
 from datetime import datetime
 
-x = input('Enter date: MM/DD/YYYY')
 
-print(len(x))
-
-
-def calendarFunction(date):
-    cal = calendar.Calendar()
-    print(cal.itermonthdates(datetime.now().year, datetime.now().month))
-    if len(date) == 0:
-            print(datetime.now().month)
-            return datetime.now().month
+x = input('Enter date: MM/YYYY: ')
+month = datetime.today().month
 
 
-calendarFunction(x)
+def calendar_fun(y):
+    if len(y) == 0:
+        print(calendar.TextCalendar().formatmonth(2019, month))
+    elif 1 <= len(y) <= 2:
+        print(calendar.TextCalendar().formatmonth(2019, int(y)))
+    else:
+        userInput = x.split('/')
+        date = userInput[0]
+        year = userInput[1]
+        print(calendar.TextCalendar().formatmonth(int(year), int(date)))
+
+
+calendar_fun(x)
