73c750e167fd70313ea98f333eb7109a39489823
Vuk Radosavljevic <vukrado1@gmail.com>
2019-01-08 11:31:36 -0600

    tuples.py

--- src/modules.py
+++ src/modules.py
@@ -24,10 +24,10 @@ import os
 
 # Print the current process ID
 # YOUR CODE HERE
-print(os.getpid)
+print(os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-print(os.curdir)
+print(os.curdir())
 # Print out your machine's login name
 # YOUR CODE HERE
-print(os.getlogin)
+print(os.getlogin())
