bc4d16a10ad27ec99f236995985892b9a20558c5
ckirkwood94 <ckirkwood94@gmail.com>
2019-01-08 17:52:11 -0500

    Added arg check to print_tuple function

--- src/tuples.py
+++ src/tuples.py
@@ -1,16 +1,16 @@
 """
 Python tuples are sort of like lists, except they're immutable and
 are usually used to hold heterogenous data, as opposed to lists
-which are typically used to hold homogenous data. Tuples use 
-parens instead of square brackets. 
+which are typically used to hold homogenous data. Tuples use
+parens instead of square brackets.
 
 More specifically, tuples are faster than lists. If you're looking
-to just define a constant set of values and that set of values 
-never needs to be mutated, use a tuple instead of a list. 
+to just define a constant set of values and that set of values
+never needs to be mutated, use a tuple instead of a list.
 
 Additionally, your code will be safer if you opt to "write-protect"
 data that does not need to be changed. Tuples enforce immutability
-automatically. 
+automatically.
 """
 
 # Example:
@@ -36,7 +36,12 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function `print_tuple` that prints all the values in a tuple
 
 def print_tuple(a):
+    if isinstance(a, int):
+        print(a)
+        return
+
     for x in range(0, len(a)):
+
         print(a[x])
 
 
@@ -44,5 +49,5 @@ t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1,)  # What needs to be added to make this work?
+u = (1)  # What needs to be added to make this work?
 print_tuple(u)
