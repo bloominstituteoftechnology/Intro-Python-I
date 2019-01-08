719309c4c1936bbb78d47d5d1d17328333916789
Kevin <brackkevin@gmail.com>
2019-01-08 17:15:35 -0600

    scopes.py complete

--- src/scopes.py
+++ src/scopes.py
@@ -5,11 +5,13 @@
 x = 12
 
 def changeX():
+    global x
     x = 99
 
 changeX()
 
 # This prints 12. What do we have to modify in changeX() to get it to print 99?
+# ^^ add 'global x' inside the function
 print(x)
 
 
@@ -19,12 +21,19 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
 
     inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999? Google "python nested function scope".
+    # ^^ there are 3 ways
+    ## 1) we could make y a list: y = [120], then in the function: y[0] = 999
+    ## 2) we could add dot syntax: outer.y = 120, ind the function: outer.y = 999
+    ## 3) we could tell the inner function to bring in a nonlocal variable with
+        #the nonlocal keyword like I did above.
+        
     print(y)
 
 outer()
