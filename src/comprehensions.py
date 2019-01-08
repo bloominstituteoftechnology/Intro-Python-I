34e084173fe8f11312dcd22b3fb77032e3c02418
Steven O'Neill <oneillsteven957@gmail.com>
2019-01-08 16:49:48 -0600

    completed comprehensions, dictionaries, and functions

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -11,14 +11,16 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
+for n in range(5):
+    y.append(n + 1)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for n in range(10):
+    y.append(n ** 3)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -27,7 +29,8 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
-
+for n in a:
+    y.append(n.upper()) 
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
@@ -36,6 +39,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [int(n) for n in x if int(n) % 2 == 0]
 
 print(y)
