f927317efcbb0705fb7c1ead33734e961534338c
sean-one <basement365@gmail.com>
2019-01-08 13:56:52 -0800

    fixed lines 43-47 in dictionaries.py to help scale some

--- src/dictionaries.py
+++ src/dictionaries.py
@@ -40,9 +40,11 @@ print(waypoints)
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
-waypoints[0]['name'] = 'not a real place'
-waypoints[0]['lon'] = -130
-print(waypoints)
+for wp in waypoints:
+    if wp['name'] == 'a place':
+        wp['name'] = 'not a real place'
+        wp['lon'] = -130
+        print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
