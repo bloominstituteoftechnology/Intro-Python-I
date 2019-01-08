fde1ac0d75c7494e9c089aca2c5cf5e8a736ecda
Scott Bren <smbren@ncsu.edu>
2019-01-07 19:01:38 -0500

    finished slices exercise, working on dictionaries

--- src/args.py
+++ src/args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x, y):
+    return x + y
 
 print(f1(1, 2))
 
@@ -12,6 +14,10 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    x = 0
+    for num in args:
+        x += num
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
