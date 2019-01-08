bed29a2980e27ef093280d067c70518ef998ab33
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-01-08 16:43:17 -0500

    file_io.py complete

--- src/file_io.py
+++ src/file_io.py
@@ -9,6 +9,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt', 'r') as f:
+    read_data = f.read()
+    print(read_data)
+f.closed
+# saw a "True" here in some examples but not quite sure what the function was
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +21,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+# can do both of this with "r+" mode, but doing it in steps for reinforcement.
+# first, open bar.txt as write-only and add the text
+with open('bar.txt', 'w') as f:
+    write_data = f.write('Line one \n')
+    write_data2 = f.write('Line two \n')
+    write_data3 = f.write('Line three')
+f.closed
+
+# now open up bar.txt as read-only to return the text
+with open('bar.txt', 'r') as f:
+    read_data = f.read()
+    print(read_data)
+f.closed
