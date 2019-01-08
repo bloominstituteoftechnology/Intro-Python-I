86d431755a061d2156f1aba27957ebdd49bbcfdb
sean-one <basement365@gmail.com>
2019-01-08 11:51:56 -0800

    args.py finished

--- src/args.py
+++ src/args.py
@@ -16,12 +16,9 @@ print(f1(1, 2))
 def f2(*nums):
     result = 0
     for num in nums:
-        if type(num) == list:
-            for item in num:
-                print(type(item))
-                # result += item
         result += int(num)
     return result
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -30,13 +27,15 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(a, b=1):
+    return a + b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -51,6 +50,11 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    print(kwargs)
+    for k, v in kwargs.items():
+        # print(kwargs)
+        print(f'key: {k}, value: {v}')
 
 # Should print
 # key: a, value: 12
@@ -69,4 +73,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
