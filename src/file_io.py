a02e0a880d8535b697155c2033fb1fda85ea2b3c
lndubose <lndubose13@gmail.com>
2019-01-08 12:58:45 -0600

    Finish file_io.py

--- src/file_io.py
+++ src/file_io.py
@@ -1,6 +1,6 @@
 """
 Python makes performing file I/O simple. Take a look
-at how to read and write to files here: 
+at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
@@ -9,10 +9,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+f = open('foo.txt', 'r')
+
+print(f.read())
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.txt', 'w')
+
+f.write('Movie Quotes \n')
+f.write('\"Frankly, my dear, I don\'t give a damn.\" Gone With the Wind, 1939 \n')
+f.write('\"Toto, I\'ve got a feeling we\'re not in Kansas anymore.\" The Wizard of Oz, 1939 \n')
+
+f.close()
