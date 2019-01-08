13fede8383f86c350985f37f81bbe39d1c495359
Eva <evalee904@gmail.com>
2019-01-08 16:39:03 -0600

    File io

--- src/file_io.py
+++ src/file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt') as f1:
+  print(f1.read())
+f1.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+content = '''
+  I'm bad, and that's good. 
+  I will never be good, and that's not bad. 
+  There's no one I'd rather be than me. 
+  '''
+
+with open('bar.txt', 'w+') as f2:
+  f2.write(content)
+f2.closed
+
+with open('bar.txt') as f3:
+  if f3.read() == content:
+    print('Contents match')
