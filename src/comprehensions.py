72f13f5d4cf47719137792273f23108763041af4
Sumayyah Asgar <sasgar04@gmail.com>
2019-01-08 17:07:04 -0800

    functions, comprehension, and args

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -44,7 +44,7 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 
-y = [int(num) % 2 == 0]
+y = [num for num in x if int(num) % 2 == 0]
 print(y)
 
 
