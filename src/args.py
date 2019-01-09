3eeee270105e75abbce508673cfed00d6b7fc7ce
jmbradley <jmbradley808@gmail.com>
2019-01-09 10:31:55 -0500

    args f2

--- src/args.py
+++ src/args.py
@@ -6,12 +6,25 @@
 
 # YOUR CODE HERE
 
+
+def f1(num1, num2):
+    return num1 + num2
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*args):
+    if len(args) == 1:
+        return args[0]
+    else:
+        return args[-1] + f2(*args[:-1])
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
