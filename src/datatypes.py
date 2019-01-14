ec72a5b82d0210e205e72537418a7d8563557afb
AM_Myrick <amyrick.820@gmail.com>
2019-01-14 14:20:36 -0500

    completes modules and datatypes files

--- src/datatypes.py
+++ src/datatypes.py
@@ -19,7 +19,7 @@ def add_ints(num1, num2):
     What are the types in this problem?
         Ideally, integers.
     Do I have to convert the types?
-        If given anything other than integers, yes. This can be handled by convert all arguments that are passed in or
+        If given anything other than integers, yes. This can be handled by converting all arguments that are passed in or
         pushing all arguments through if statements to check for type, then converting based on that.
     
     Pseudocode:
@@ -32,3 +32,17 @@ add_ints(x, y)
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+def add_strs(str1, str2):
+    """
+    What are the types in this problem?
+        Strings.
+    Do I have to convert the types?
+        If given anything other than strings, yes. This can be handled by converting all arguments that are passed in or
+        pushing all arguments through if statements to check for type, then converting based on that.
+    
+    Pseudocode:
+        convert the arguments to strings
+        add the arguments together and return the result
+    """
+    print(str(str1) + str(str2))
+add_strs(x, y)
