f173bebfc29452a2c35a52b514975c89c1f3d4d3
AM_Myrick <amyrick.820@gmail.com>
2019-01-14 17:19:29 -0500

    completes args and functions

--- src/args.py
+++ src/args.py
@@ -5,14 +5,18 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(x, y):
+    return x + y
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-
+def f2(*nums):
+    if isinstance(nums[0], list):
+        return sum(nums[0])
+    return sum(nums)
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -28,7 +32,8 @@ print(f2(a))    # Should print 22
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3(x, y = 1):
+    return x + y
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -42,7 +47,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(**kwargs):
+    for k, v in kwargs.items():
+        print(f'key: {k}, value: {v}')
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -60,4 +67,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
