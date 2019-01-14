a1bbc014aa3a92c8132e2f213285850eb869eb6c
AM_Myrick <amyrick.820@gmail.com>
2019-01-14 14:03:43 -0500

    initial commit

--- src/datatypes.py
+++ src/datatypes.py
@@ -14,7 +14,20 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+def add_ints(num1, num2):
+    """
+    What are the types in this problem?
+        Ideally, integers.
+    Do I have to convert the types?
+        If given anything other than integers, yes. This can be handled by convert all arguments that are passed in or
+        pushing all arguments through if statements to check for type, then converting based on that.
+    
+    Pseudocode:
+        convert the arguments to integers
+        add the arguments together and return the result
+    """
+    print(int(num1) + int(num2))
+add_ints(x, y)
 
 # Write a print statement that combines x + y into the string value 57
 
