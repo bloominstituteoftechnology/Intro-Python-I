3bbd42c6bf44756adc829ae841ba312714caa3c6
AM_Myrick <amyrick.820@gmail.com>
2019-01-14 17:50:56 -0500

    completes scopes and file_io

--- src/file_io.py
+++ src/file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open("foo.txt") as f:
+    print(f.read())
+
+print(f.closed)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+l = open("bar.txt", "r+")
+
+l.write("Test\n")
+l.write("Test\n")
+l.write("Test\n")
+l.close()
