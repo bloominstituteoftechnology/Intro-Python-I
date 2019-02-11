782dccb7f6bdf7c423b4d2f66a5321cbba7296e8
JacobABrennan <jacobabrennan@gmail.com>
2019-02-11 17:11:41 -0500

    Finish modules.py

--- src/modules.py
+++ src/modules.py
@@ -7,25 +7,26 @@ level operating system functionality.
 
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
-
-# Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
-
-# Print out the OS platform you're using:
-# YOUR CODE HERE
-
-# Print out the version of Python you're using:
-# YOUR CODE HERE
-
-
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
+# Print out the command line arguments in sys.argv, one per line:
+print('-- Being Arguments --------')
+for argument in sys.argv:
+    print(argument)
+print('-- End Arguments ----------\n')
+
+# Print out the OS platform you're using:
+print('OS: '+os.name)
+
+# Print out the version of Python you're using:
+print('Version: '+sys.version)
+
 # Print the current process ID
-# YOUR CODE HERE
+print('Process ID: '+str(os.getpid()))
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+print('Working Directory: '+os.getcwd())
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print('Login Name: '+os.getlogin())
