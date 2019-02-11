1fcfaa947e24dfacfc88ee6e3bc057b0b9dd9d4a
TRIF3X <thewizardofcrypto@gmail.com>
2019-02-11 14:02:07 -0600

    datatypes done

--- src/datatypes.py
+++ src/datatypes.py
@@ -14,8 +14,12 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+# Use int to convert a string into an integer
+print(x+int(y))
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+# Use str to convert an integer into a string
+print(str(x)+y)
