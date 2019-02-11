db56eaa24dfb4f24fa592b6316d88358a8d7a7f0
Chris Cedeno <chrisrey95@gmail.com>
2019-02-11 16:15:54 -0500

    tuples.py

--- src/tuples.py
+++ src/tuples.py
@@ -36,6 +36,13 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # YOUR CODE HERE
 
+def print_tuple(tuple):
+    if isinstance(tuple, int):
+        print(tuple)
+    else:
+        for num in tuple:
+            print(num)
+
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
