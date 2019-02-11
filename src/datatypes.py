7fc294a6a834273803cd71ce1a5c5990d5dfd768
Wanda <doubleuevans@gmail.com>
2019-02-11 17:00:04 -0600

    completed 6/16

--- src/datatypes.py
+++ src/datatypes.py
@@ -12,10 +12,8 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
-
-# YOUR CODE HERE
+print(x + int(y))  # convert to integer
 
 
 # Write a print statement that combines x + y into the string value 57
-
-# YOUR CODE HERE
+print(str(x) + y)  # convert to string
--- src/printing.py
+++ src/printing.py
@@ -11,7 +11,21 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print(" %d " % x)
+print(" %f " % y)
+print(" %s " % z)
+
+# https://docs.python.org/3/library/stdtypes.html#old-string-formatting
 
 # Use the 'format' string method to print the same thing
+print('{x}, {y}, {z}'.format(x=x, y=y, z=z))
+print('{}'.format(x))
+print('{}'.format(y))
+print('{}'.format(z))
+
+# https://realpython.com/python-string-formatting/
 
 # Finally, print the same thing using an f-string
+print(f'{x}')
+print(f'{y}')
+print(f'{z}')
