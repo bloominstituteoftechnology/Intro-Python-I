05bf1fbeeeda377e0a021b85f4fa3c86de008457
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-11 16:19:27 -0500

    completes bignum.py

--- src/bignum.py
+++ src/bignum.py
@@ -1,4 +1,17 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
+
 # YOUR CODE HERE
+
+# Python
+print(2 ** 65536)
+# Output: massive integer with 19729 digits
+
+# JavaScript
+# Math.pow(2, 65536)
+# Output: Infinity
+
+# Notes
+# - in P3, int type is unbounded
+# - in P2, max 64-bit int (switches to long once exceeded)
