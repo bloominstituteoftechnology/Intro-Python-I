dc3f7770154607ebf4e72754cd3b74d838f8d049
kyranmccann <kyran.mccann@gmail.com>
2019-02-11 14:00:30 -0800

    convert string to int and int to string

--- src/datatypes.py
+++ src/datatypes.py
@@ -1,7 +1,7 @@
 """
-Python is a strongly-typed language under the hood, which means 
+Python is a strongly-typed language under the hood, which means
 that the types of values matter, especially when we're trying
-to perform operations on them. 
+to perform operations on them.
 
 Note that if you try running the following code without making any
 changes, you'll get a TypeError saying you can't perform an operation
@@ -14,8 +14,12 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+y = int(y, 10)
+print(x + y)
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+x = str(x)
+y = str(y)
+print(x + y)
