ba98ff5683995a972db111049ffc47547d0e0c4c
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-02-11 18:04:00 -0500

    completes slices.py

--- src/slices.py
+++ src/slices.py
@@ -9,29 +9,37 @@ overview: https://stackoverflow.com/a/509295
 Use Python's slice syntax to achieve the following:
 """
 
+# a[start:end]  items start through end-1
+# a[start:]     items start through the rest of the array
+# a[:end]       items from the beginning through end-1
+# a[:]          a copy of the whole array
+
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+print(a[1])
 
 # Output the second-to-last element: 9
-print()
+second_to_last = len(a) - 2
+print(a[second_to_last])
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+last_three = len(a) - 3
+print(a[last_three:])
 
 # Output the two middle elements in the array: [1, 7]
-print()
+print(a[2:4])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+print(a[1:])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+except_last = len(a) - 1
+print(a[:except_last])
 
 # For string s...
 
 s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
-print()
+print(s[7:12])
