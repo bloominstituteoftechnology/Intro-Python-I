59963db7152a29915cd50b62634ad62d7ae4b404
CookieMonsta89 <jlstan06@hotmail.com>
2019-02-11 15:03:27 -0500

    trying to figure out git flow

--- src/lists.py
+++ src/lists.py
@@ -8,22 +8,26 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
-print(x)
+print(x.append(4))
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
-print(x)
+print(x.extend(y))
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
-print(x)
+print(x.remove(8))
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
-print(x)
+print(x.insert(5, 99))
 
 # Print the length of list x
 # YOUR CODE HERE 
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for param in x:
+    result = param * 1000
+    print(result)
