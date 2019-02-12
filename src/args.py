c17cf5ed1196ab3a2f47ac2c28c4f0ed07fae263
qtn2932 <quan.nguyen199712@gmail.com>
2019-02-11 19:03:53 -0500

    args done, Still have question

--- src/args.py
+++ src/args.py
@@ -6,14 +6,20 @@
 
 # YOUR CODE HERE
 #Start code
-
-print(f1(1, 2))
+def f1 (n1,n2):
+    return n1+n2
+print(f1(1,2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-
+def f2 (*numbers):
+    sum=0
+    for number in numbers:
+        sum+=number
+    return sum
+print(f1(1,2))
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -22,14 +28,18 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+#    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3 (number1, number2=None):
+    if(number2!=None):
+        return number1+number2
+    else:
+        return number1+1
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -43,7 +53,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4 (**keywords):
+    for keyword in keywords:
+        print(f"key: {keyword}, value: {keywords[keyword]}")
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -61,4 +73,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+#f4(d)
