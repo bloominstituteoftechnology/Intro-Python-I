64467b1bf421e427f2358feae72fccb69dce4c77
Edward J <edjeong99@gmail.com>
2019-02-12 18:13:08 -0500

    finish scopes file_io

--- src/file_io.py
+++ src/file_io.py
@@ -9,10 +9,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
-
+f = open('foo.txt', 'r')
+print(f.read())
+f.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.txt', 'r+')
+f.write('This is a test\nWhat is a test\n if not this\n')
+f.close()
+f = open('bar.txt', 'r+')
+print(f.read())
+f.close()
