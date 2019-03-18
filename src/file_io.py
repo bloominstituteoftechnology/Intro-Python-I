7addb4fe27ec5a945dcab18c26876dd3a0b42480
yzhukovs <yzhukovs@bumnetworks.com>
2019-03-18 17:01:44 -0400

    added file_io

--- src/file_io.py
+++ src/file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open("foo.txt") as f:
+    print(f.read())
+f.closed    
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+arrcontant = '''
+I wish I had more time
+To learn Python 
+But I only have 1 week
+'''
+
+with open('bar.txt', 'w+') as ff:
+    ff.write(arrcontant)
+ff.closed
+
+with open('bar.txt') as fff:
+    if fff.read() == arrcontant:
+        print("File contains what you expect it to contain!")
