8e4e97f7e559762136d89f8765ba70d7844a64ae
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2019-03-18 14:00:30 -0500

    Finished functions

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,7 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
-# YOUR CODE HERE
+def iseven(x):
+    return x % 2 == 0
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -8,5 +9,10 @@ num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
-# YOUR CODE HERE
+def printevenodd(x):
+    if iseven(x):
+        print("Even!")
+    else:
+        print("Odd")
 
+printevenodd(num)
