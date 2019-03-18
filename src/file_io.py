96006d286fe73a28498805c2a3aeda98e9261f3f
ToMMaaY <thuynh629@gmail.com>
2019-03-18 15:46:38 -0700

    completed file_io

--- src/file_io.py
+++ src/file_io.py
@@ -8,11 +8,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+with open('foo.txt') as f:
+    read_data = f.read()
+    print(read_data)
+f.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+f = open('bar.txt', 'w+')
+f.write('Helllo there 1. \nHelllo there 2. \nHelllo there 3. \n')
+f.close()
