9f4d48242f6d7032f8fba31401278cba55dd48e1
Brad Mortensen <b.mort86@gmail.com>
2019-03-19 10:35:14 -0600

    updates comprehensions.py and classes.py

--- src/comprehensions.py
+++ src/comprehensions.py
@@ -10,18 +10,13 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-for x in range(5):
-  y.append(x+1)
+y = [x+1 for x in range(5)]
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-for x in range(10):
-  y.append(x**3)
-
+y = [x**3 for x in range(10)]
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
