14acfcc783d10c1fc7b16c3b781e0904ea2b60c6
BrandCam <BrandCam>
2019-03-26 18:06:13 -0500

    woops didnt fork lol

--- src/cal.py
+++ src/cal.py
@@ -22,3 +22,12 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+if len(sys.argv) == 1:
+  calendar.TextCalendar().prmonth(datetime.now().year, datetime.now().month)
+elif len(sys.argv) == 2:
+  calendar.TextCalendar().prmonth(datetime.now().year, int(sys.argv[1]))
+elif len(sys.argv) == 3:
+  calendar.TextCalendar().prmonth(int(sys.argv[2]), int(sys.argv[1]))
+else:
+  print("if no args given, it will print current month, one arg for month, 2 args for month and year(month then year) ")
