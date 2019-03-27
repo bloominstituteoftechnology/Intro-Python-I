1f404a6f38a38ace9550b43f72806f5759ce8a2b
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-03-26 19:15:33 -0500

    slices working

---
+++ src/rps.py
@@ -0,0 +1,15 @@
+import random
+
+wins = 0
+loses = 0
+ties= 0
+choices = ['r', 'p', 's']
+
+def compare_answers(player_choice, )
+
+while True: 
+    print(f"Score:" {wins} - {losses} - {ties"})
+    cmd = input("\nChose r/p/s: ")
+
+    ai_choice = choices[random.randrange(3)]
+    print(f"")
