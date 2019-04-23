b0e436e5517d4de9d5d81dda493ebd3116e5b733
leianne louis <leilouis112@gmail.com>
2019-04-23 15:27:32 -0400

    completed functions.py

--- src/slices.py
+++ src/slices.py
@@ -12,22 +12,22 @@ Use Python's slice syntax to achieve the following:
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print(a[slice(1,2)])
+print(a[1:2])
 
 # Output the second-to-last element: 9
-print(a[slice(4, 5)])
+print(a[4:5])
 
 # Output the last three elements in the array: [7, 9, 6]
-print(a[slice(3,6)])
+print(a[-3:])
 
 # Output the two middle elements in the array: [1, 7]
-print(a[slice(2,4)])
+print(a[2:4])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print(a[slice(1,6)])
+print(a[1:])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print(a[slice(0,5)])
+print(a[:-1])
 
 # For string s...
 
