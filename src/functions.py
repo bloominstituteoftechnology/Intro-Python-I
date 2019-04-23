173ca131cf3d1b8ccbb2f1362fe8811e18e2564a
Thomas Claydon <tomclaydon102@gmail.com>
2019-04-23 11:31:02 -0400

    Compleetd functions.py

--- src/functions.py
+++ src/functions.py
@@ -2,6 +2,14 @@
 
 # YOUR CODE HERE
 
+def is_even(number):
+    if number == 0:
+        return 0
+    elif number % 2 == 0:
+        return True
+    else:
+        return False
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -10,3 +18,10 @@ num = int(num)
 
 # YOUR CODE HERE
 
+result = is_even(num)
+if result == True:
+    print("Even!")
+elif result == 0:
+    print("Input was equal to Zero! Try again.")    
+else:
+    print("Odd")
