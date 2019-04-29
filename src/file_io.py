41181af3a18919640d6a1e309ca1a2d8059416f9
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-04-29 16:53:03 -0400

    Completed 'file_io' task

--- src/file_io.py
+++ src/file_io.py
@@ -10,9 +10,27 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+# f = open('foo.txt', 'r')
+# print(f.read())
+# f.close()
+
+with open('foo.txt') as f:
+    print(f.read())
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+# b = open('bar.txt', 'w')
+# b.write("The etymology of 'foo' is obscure.\n")
+# b.write("It's used in connection with 'bar'.\n")
+# b.write("Not to be confused with the military slang 'FUBAR'.\n")
+# b.close()
+
+with open('bar.txt', 'w') as b:
+    b.write("The etymology of 'foo' is obscure.\n")
+    b.write("It's used in connection with 'bar'.\n")
+    b.write("Not to be confused with the military slang 'FUBAR'.\n")
