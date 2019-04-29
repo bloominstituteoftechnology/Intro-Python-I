da0f7e42109feec3f4ff32ee51ec4916f3776510
audreywelch <welch.audreyl@gmail.com>
2019-04-29 17:22:22 -0500

    Updated lists assignment

--- src/lists.py
+++ src/lists.py
@@ -19,16 +19,19 @@ print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-
 x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
+x.insert(-1, 99)
 print(x)
 
 # Print the length of list x
-# YOUR CODE HERE 
+# YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for each_num in x:
+    print(each_num * 1000)
