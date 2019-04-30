2740719eeac9d8fcbf49253110dec57ee90c6706
Imon Ovbude <imonovbude@gmail.com>
2019-04-30 12:53:45 -0700

    completed functions.py

--- src/functions.py
+++ src/functions.py
@@ -2,11 +2,30 @@
 
 # YOUR CODE HERE
 
+
+def is_even(num):
+    if num % 2 == 0:
+        print('true')
+        return 'true'
+    else:
+        print('false')
+        return 'false'
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
-
+is_even(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+
+def even_odd(cb):
+    if cb == 'true':
+        print('Even!')
+    else:
+        print('Odd!')
+
+
+even_odd(is_even(num))
