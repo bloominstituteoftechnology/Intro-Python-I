317d4576b2d836f1addb874fbdec30cb44f4b790
Pascale Pierre <pascaleee@gmail.com>
2019-05-08 22:33:59 -0500

    completed scope

--- src/args.py
+++ src/args.py
@@ -15,8 +15,10 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*argv):
-        for arg in argv:
-    return arg + arg
+  sum = 0
+  for arg in argv:
+    sum = sum  + arg
+  return sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -26,7 +28,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+# print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
