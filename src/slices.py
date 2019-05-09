ed376ba038797302d264430e99c6f5a1c8c68dd5
cocoitali <cmburatto@gmail.com>
2019-05-08 21:03:56 -0600

    completed slices.py

--- src/slices.py
+++ src/slices.py
@@ -6,32 +6,37 @@ only a portion of a list or string.
 This Stack Overflow answer provides a brief but thorough
 overview: https://stackoverflow.com/a/509295
 
+a[start:stop]  # items start through stop-1
+a[start:]      # items start through the rest of the array
+a[:stop]       # items from the beginning through stop-1
+a[:]           # a copy of the whole arr
+
 Use Python's slice syntax to achieve the following:
 """
 
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+print(a[1])
 
 # Output the second-to-last element: 9
-print()
+print(a[-2])
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+print(a[-3:])
 
 # Output the two middle elements in the array: [1, 7]
-print()
+print(a[-4:-2])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+print(a[1:])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+print(a[:-1])
 
 # For string s...
 
 s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
-print()
+print(s[-7:-1])
