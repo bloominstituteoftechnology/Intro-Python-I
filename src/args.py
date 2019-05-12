f443f55334a07fb404465271a994487a23b3c374
cocoitali <cmburatto@gmail.com>
2019-05-11 21:07:59 -0600

    args.py complete

--- src/args.py
+++ src/args.py
@@ -3,15 +3,15 @@
 
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
-
-# YOUR CODE HERE
-
+def f1(x, y):
+    return x + y
+     
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
-
-# YOUR CODE HERE
+def f2(*args):
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -19,15 +19,15 @@ print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
-
-# What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+   
+# What thing do you have to add to make this work? *
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
-
-# YOUR CODE HERE
+def f3(x, y = 1):
+    return x + y
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -40,7 +40,11 @@ print(f3(8))     # Should print 9
 # key: baz, value: 12
 #
 # Google "python keyword arguments".
-
+def f4(**attributes):
+    print( ", ".join(
+        f"key: {param}, value: {value}"
+        for param, value in attributes.items()
+    ))
 # YOUR CODE HERE
 
 # Should print
@@ -60,4 +64,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
