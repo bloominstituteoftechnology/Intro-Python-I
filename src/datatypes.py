303c155ed208dd7a66336d931800943ae6fae65a
Eddy <edward.gonzalez9708@gmail.com>
2019-05-13 20:13:11 -0400

    Modified bignum.py and datatypes.py.

--- src/datatypes.py
+++ src/datatypes.py
@@ -1,3 +1,13 @@
+"""
+Python is a strongly-typed language under the hood, which means 
+that the types of values matter, especially when we're trying
+to perform operations on them.
+
+Note that if you try running the following code without making any
+changes, you'll get a TypeError saying you can't perform an operation
+on a string and an integer.
+"""
+
 x = 5
 y = "7"
 
