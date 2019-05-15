6006f1968e60bcbca17e5e97e1446c8ff577e1bb
Eddy <edward.gonzalez9708@gmail.com>
2019-05-15 14:54:07 -0400

    Completed cal.py

--- README.md
+++ README.md
@@ -83,8 +83,8 @@ The suggested order for going through each of the directories is:
 * `args` -- Arguments and Keyword Arguments
 * `scopes` -- Global, Local, and Non-Local scope
 * `file_io` -- Read and write from files
+* `cal` -- Experiment with module imports and implement a text-based calendar
 
-* `cal` -- Experiment with module imports
 * `obj` -- Classes and objects
 
 ## Day 2
--- src/cal.py
+++ src/cal.py
@@ -1,27 +1,43 @@
-# Use the 'calendar' module to draw calendars to the console
-# https://docs.python.org/3.6/library/calendar.html
-#
-# Use the sys module to look for command line arguments in the `argv` list
-# variable.
-#
-# If the user specifies two command line arguments, month and year, then draw
-# the calendar for that month.
+'''
+The Python standard library's 'calendar' module allows you to render a calendar to your terminal.
+https://docs.python.org/3.6/library/calendar.html
 
-# Stretch goal: if the user doesn't specify anything on the command line, show
-# the calendar for the current month. See the 'datetime' module.
+Write a program that accepts user input of the form `calendar.py month [year]` and does the following:
+ 
+ - If the user doesn't specify any input, your program should print the calendar for the current month. The 'datetime' module may be helpful for this.
+ 
+ - If the user specifies one argument, assume they passed in a month and render the calendar for that month of the current year.
+ 
+ - If the user specifies two arguments, assume they passed in both the month and the year. Render the calendar for that month and year.
+ 
+ - Otherwise, print a usage statement to the terminal indicating the format that your program expects arguments to be given. Then exit the program.
+'''
 
-# Hint: this should be about 15 lines of code. No loops are required. Read the
-# docs for the calendar module closely.
-
+import sys
 import calendar
-import datetime
+from datetime import date
 
-def cal(year, month): 
-    if (year and month): 
-        print(calendar.prmonth(int(year), int(month)))
-    else: 
-        now = datetime.datetime.now() 
-        print(calendar.prmonth(now.year, now.month))
+def cal(args):
+  today = date.today()
+  cal = calendar.TextCalendar()
+  
+  if len(args) == 1:
+    return cal.formatmonth(today.year, today.month)
+  elif len(args) == 2:
+    try:
+      if int(args[1]) >= 1 and int(args[1]) <= 12:
+        return cal.formatmonth(today.year, int(args[1]))
+      else: 
+        return 'The month should be a whole number between 1 and 12.'
+    except: 
+      return 'The month should be a whole number between 1 and 12.'
+  elif len(args) == 3:
+    try:
+      if int(args[1]) >= 1 and int(args[1]) <= 12: 
+        return cal.formatmonth(int(args[2]), int(args[1]))
+      else:
+        return 'The month should be a whole number between 1 and 12.'
+    except:
+      return 'The month should be a whole number between 1 and 12.\nThe year should be a whole number.'
 
-cal((input('Enter a year as yyyy: ')), (input('Enter a month as mm: ')))
-
+print(cal(sys.argv))
