828bf74d2ee3dc755f8c0302243c67ede2f5b1af
ckopecky <kopecky12112@gmail.com>
2019-05-15 13:00:28 -0700

    function.py

--- src/functions.py
+++ src/functions.py
@@ -6,6 +6,13 @@
 num = input("Enter a number: ")
 num = int(num)
 
+def is_even():
+    if(num % 2 == 0):
+        print("Even!")
+    else:
+        print("Odd!")
+
+is_even()
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
