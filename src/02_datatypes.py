e195e3bc4a0f3d921e82124e7125ea655bea3f2e
FreeKeyBoy <jjhutton81@gmail.com>
2019-05-22 15:39:30 -0700

    Completed 02.

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -15,7 +15,12 @@ y = "7"
 
 # YOUR CODE HERE
 
+print(x + int(y))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+
+print(str(x) + y)
+
+#I keep getting X is undefined
