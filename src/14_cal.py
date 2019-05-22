04a169311abcb9be09e9a588f24de0ef0aade5dd
Eric Whitcomb <eric.whitcomb@gmail.com>
2019-05-22 02:08:43 -0600

    Completed 14

--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,29 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+c = calendar.TextCalendar(calendar.SUNDAY)
+
+def help():
+    print("cmd: python cal.py [month] [year]")
+
+def main(args):
+    if len(args) == 0:
+        t = datetime.now()
+        s = c.formatmonth(t.year, t.month)
+        print(s)  
+    elif len(args) == 1:
+        if (args[0] == '-h' or args[0] == '--help'):
+            help()
+        else:
+            t = datetime.now()
+            s = c.formatmonth(t.year, int(args[0]))
+            print(s)
+    elif len(args) == 2:
+        s = c.formatmonth(int(args[1]), int(args[0]))
+        print(s)
+    else:
+        help()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
