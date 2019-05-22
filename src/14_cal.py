b72bfa9c4bf781836944f7de743b5c2621f96349
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-05-22 17:30:38 -0500

    14 complete

--- src/14_cal.py
+++ src/14_cal.py
@@ -2,7 +2,6 @@
 The Python standard library's 'calendar' module allows you to 
 render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
-
 Write a program that accepts user input of the form
   `calendar.py month [year]`
 and does the following:
@@ -22,3 +21,32 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+c = calendar.TextCalendar(calendar.SUNDAY)
+
+
+def help():
+    print("cmd: python cal.py [month] [year]")
+
+
+def main(args):
+    if len(args) == 0:
+        t = datetime.now()
+        s = c.formatmonth(t.year, t.month)
+        print(s)
+    elif len(args) == 1:
+        if (args[0] == '-h' or args[0] == '--help'):
+            help()
+        else:
+            t = datetime.now()
+            s = c.formatmonth(t.year, int(args[0]))
+            print(s)
+    elif len(args) == 2:
+        s = c.formatmonth(int(args[1]), int(args[0]))
+        print(s)
+    else:
+        help()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
