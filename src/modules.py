f61f9f7ee7dfea5a43ae741803cfd71a7f6e86d3
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-05-22 10:17:26 -0500

    intial commit

--- src/modules.py
+++ src/modules.py
@@ -5,27 +5,40 @@ methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
 
+import os
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print(sys.argv)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+if sys.platform.startswith("linux"):
+    print("You are on a Linux Machine")
+elif sys.platform.startswith("darwin"):
+    print("You are using a Mac Machine")
+elif sys.platform.startswith("win30"):
+    print("You need an upgrade")
+else:
+    print("You are a caveman")
 
-# Print out the version of Python you're using:
-# YOUR CODE HERE
+    # Print out the version of Python you're using:
+    # YOUR CODE HERE
+print('Your current version of Python is: ' + sys.version)
 
-
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print('Your current process user id is: ' + str(os.geteuid()) +
+      '. The current group id is: ' + str(os.getgid()) + '.')
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print('The working directory is: ' + str(os.getcwd()))
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print('Your login name is: ' + str(os.getlogin()))
