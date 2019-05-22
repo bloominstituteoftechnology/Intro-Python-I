d93c969e8bc009dbb3d7ceaa8a51ef5d8da5c765
FreeKeyBoy <jjhutton81@gmail.com>
2019-05-22 15:36:52 -0700

    Completed 01.

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,9 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+print(pow(2, 65536))
+
+# Or did you mean 2 to the 16th power which returns 65536?
+
+print(pow(2, 16))
