f5f650bc6a16fe8e32a22ceea9c6a6d63e6e7bd0
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-05-22 12:48:22 -0500

    08 complete

--- src/03_modules.py
+++ src/03_modules.py
@@ -16,11 +16,11 @@ for a in sys.argv:
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-print(sys.platform)
+print(sys.platform + '\n')
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-print(sys.version)
+print(sys.version + '\n')
 
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
@@ -30,8 +30,8 @@ print(os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-print(os.getcwd())
+print(os.getcwd() + '\n')
 
 # Print out your machine's login name
 # YOUR CODE HERE
-print(os.environ.get('USER'))
+print(os.environ.get('USER') + '\n')
