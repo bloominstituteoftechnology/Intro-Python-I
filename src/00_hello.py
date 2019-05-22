f57474eeb362ca222c51591616377966e7ae9258
alasalle <angela.lasalle91@gmail.com>
2019-05-21 23:19:07 -0400

    Hello world complete

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,14 @@
 # Print "Hello, world!" to your terminal
+
+print("Hello, world!")
+
+
+string = "Hello, world!"
+print(f"{string}")
+
+
+def display(st):
+    return f"{st}"
+
+
+print(display(string))
