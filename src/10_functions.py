312a55990cf87384513400529cdc0faaa8a684b5
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-05-22 13:13:12 -0500

    10 complete

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,18 @@
 # Write a function is_even that will return true if the passed-in number is even.
-
 # YOUR CODE HERE
+
+
+def isEven(n):
+    return not n % 2
+
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
 # YOUR CODE HERE
-
+if isEven(num):
+    print("is even.")
+else:
+    print("is odd.")
