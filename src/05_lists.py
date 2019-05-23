7ea42f8afeb042b29955ddc48d933f65e4c4ce7f
Jonathan Brock <jbrock1981@gmail.com>
2019-05-22 21:41:27 -0500

    fixed 03

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,10 +8,12 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
