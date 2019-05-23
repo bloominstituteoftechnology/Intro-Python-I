aef2db7766ddde306b0b6df044f1474e85619919
Alex Martin <atonymartin20@yahoo.com>
2019-05-22 20:48:52 -0500

    modules.py and printing.py completed

--- src/modules.py
+++ src/modules.py
@@ -15,20 +15,23 @@ for x in sys.argv:
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-print(sys.platform)
+print("OS Platform: " + sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-p
+print("Python Version: " + sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print("Current Process ID: " + os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print("Current Working Directory: " + os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print("Machine's Login Name: " + os.getlogin())
