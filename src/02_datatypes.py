b85e6722ccc8d3a3dbc2b78957f7c4d6db3fbb19
Emmanium <pradoe.ep@gmail.com>
2019-05-22 20:42:36 -0600

    completed modules 02 and 03

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -1,7 +1,7 @@
 """
-Python is a strongly-typed language under the hood, which means 
+Python is a strongly-typed language under the hood, which means
 that the types of values matter, especially when we're trying
-to perform operations on them. 
+to perform operations on them.
 
 Note that if you try running the following code without making any
 changes, you'll get a TypeError saying you can't perform an operation
@@ -14,8 +14,9 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+print(x + int(y))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print(str(x) + y)
