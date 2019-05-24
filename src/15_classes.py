8122c554f8b548f133a5aa856b396be19fc0108d
Jennifer King <jenbellpgh@gmail.com>
2019-05-24 00:19:25 -0400

    Finished 15!

--- src/15_classes.py
+++ src/15_classes.py
@@ -1,21 +1,71 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
 
-# YOUR CODE HERE
-        
+class LatLon:
+  lat = 0
+  lon = 0
+
+  def __init__(self, lat, lon):
+    self.lat = lat
+    self.lon = lon
+
+  def getLat(self):
+    return self.lat
+
+  def getLon(self):
+    return self.lon
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
-# YOUR CODE HERE
+class Waypoint(LatLon):
+  name = ""
+
+  def __init__(self, name, lat, lon):
+    self.name = name
+
+    super().__init__(lat, lon)
+
+  def __repr__(self):
+    return { 'name':self.name, 'lat':self.lat, 'lon':self.lon }
+
+  def __str__(self):
+    return f"{self.name} {self.lat} {self.lon}"
+
+  def getName(self):
+    return self.name
+
+  def getWaypoint(self):
+    return f"{self.name} {self.lat} {self.lon}"
+
+
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
-# YOUR CODE HERE
+class Geocache(Waypoint):
+  diff = ""
+  size = ""
+
+  def __init__(self, name, diff, size, lat, lon):
+    self.diff = diff
+    self.size = size
+
+    super().__init__(name, lat, lon)
+
+  def __str__(self):
+    return f"'{self.name}' Difficulty: {self.diff} Size: {self.size} Coords: {self.lat}, {self.lon}"
+
+  def getDifficulty(self):
+    return self.diff
+
+  def getSize(self):
+    return self.size
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
-# YOUR CODE HERE
+waypoint = Waypoint("Catacombs", 41.70505, -121.51521)
+print(waypoint.getWaypoint())
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
@@ -23,7 +73,7 @@ print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
-# YOUR CODE HERE
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
 print(geocache)
