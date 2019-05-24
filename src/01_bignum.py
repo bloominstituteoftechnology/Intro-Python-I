0f5f43201a88de44617765a83b6ce774dbb26da9
Victor G <victorgordian103@gmail.com>
2019-05-23 21:36:40 -0700

    finish up to 05

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,6 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
-
-# YOUR CODE HERE
+num1 = 2
+num2 = 65536
+answer = num1**num2
+print(answer)
