8e97ff072089e230c79e5765fa3fc298a03dd73e
Michael Trew <mtrew2015@gmail.com>
2019-05-23 21:38:55 -0400

    05 lists done

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,32 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
+x.append(4)
 print(x)
+
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
-print(x)
+# print(x)
+x.extend(y)
+print(x, "x extended")
+# print(x.extend(y))
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
-print(x)
+
+print(x.remove(8))
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
+x.insert(3,99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE 
+print("length", len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+xMultiplied = [x * 100 for x in x]
+print(xMultiplied)
