ed6a649012cc3c2c4dc764cbc84d9b8aa873d598
vy3191 <venkyafhs@gmail.com>
2019-05-25 14:10:49 -0400

    Completed 10_functions.py assignment

--- src/10_functions.py
+++ src/10_functions.py
@@ -9,4 +9,12 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def is_even(n):
+  if n % 2 == 0:
+    print("Even!")
+  else:
+    print("Odd")  
+
+is_even(num)
+
 
