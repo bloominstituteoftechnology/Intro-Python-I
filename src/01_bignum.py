51537c51d53fad6828dc134de6e968e00ca1ca04
shawn <shawnantonucci2018@gmail.com>
2019-05-24 20:08:39 -0700

    hello, bignum, datatypes completed

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,10 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+n = 2
+exponent = 0
+
+while exponent < 16+1:
+    res = n ** exponent
+    exponent = exponent + 1
+    print(res)
