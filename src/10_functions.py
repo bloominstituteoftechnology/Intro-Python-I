0ec66c2886dc16f2abeba5bf89279b8f85c64161
Phinehas <kohler.nai@gmail.com>
2019-05-24 21:18:44 -0600

    functions.py complete

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,6 +2,14 @@
 
 # YOUR CODE HERE
 
+
+def is_even(int):
+    if int % 2 == 0:
+        return True
+    else:
+        return False
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -10,3 +18,7 @@ num = int(num)
 
 # YOUR CODE HERE
 
+if is_even(num):
+    print("Even!")
+else:
+    print("Odd")
