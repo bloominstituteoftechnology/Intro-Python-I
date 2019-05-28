57ae7d244cb2608cbc1a065dfb9743c9c767c0a5
VaderSteve76 <alverson.s@yahoo.com>
2019-05-28 14:58:12 -0400

    Args

--- src/11_args.py
+++ src/11_args.py
@@ -6,12 +6,20 @@
 
 # YOUR CODE HERE
 
+
+def f1(x, y):
+    return x + y
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*num):
+    return sum(num)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -29,6 +37,14 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+
+def f3(*num):
+    if len(num) > 1:
+        return sum(num)
+    else:
+        return sum(num) + 1
+
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -42,6 +58,11 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+
+
+def f4(*arg, **kwargs):
+    for key, value in kwargs.items():
+        print('key:', key, 'value:', value)
 
 # Should print
 # key: a, value: 12
@@ -60,4 +81,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
