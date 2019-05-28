edba493161c468ecdead849cfb5d7fc572d1d91e
James Basile <jbasile145@gmail.com>
2019-05-28 15:20:58 -0400

    first three questions complete

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,15 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+y_num = int(y)
+
+print(x + y_num)
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+
+x_str = str(x)
+
+print(x_str+y)
