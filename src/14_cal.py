ba86b91dbbb300d4224e90cdb382d9c28c423eb1
Jesse Anderson <janders9009@gmail.com>
2019-05-27 23:32:23 -0500

    Done with 14_cal.py

--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,23 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+calendar.setfirstweekday(calendar.SUNDAY)
+
+if len(sys.argv) == 3:
+  m = int(sys.argv[1])
+  y = int(sys.argv[2])
+
+elif len(sys.argv) == 2:
+  m = int(sys.argv[1])
+  y = datetime.now().year
+
+elif len(sys.argv) == 1:
+  m = datetime.now().month
+  y = datetime.now().year
+
+else:
+  print("Please format input as '14_cal.py [month] [year]'.")
+  exit()
+
+print("\n", calendar.month(y, m))
