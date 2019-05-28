c1e7f55490b6ea198f4af2a76771c73962f2fbbe
VaderSteve76 <alverson.s@yahoo.com>
2019-05-28 15:35:40 -0400

    File_IO

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,9 +10,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+myFile = open('foo.txt', 'r')
+print(myFile.read())
+myFile.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+myfile = open('bar.txt', 'w')
+myFile.write('Learning how to write scripts')
+myFile.close()
+new_file = open('bar.txt', 'r')
+print(new_file.read())
