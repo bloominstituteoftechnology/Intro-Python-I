93bb481055bf0c7062d7948fcb9fc5d041f7995a
Victor G <victorgordian103@gmail.com>
2019-05-28 16:09:22 -0700

    15

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -5,14 +5,23 @@ at how to read and write to files here:
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
 
+filename = "foo.txt"
+
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
 
+with open(filename, 'r') as filehandle:
+    for line in filehandle:
+        print(line)
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open("bar.txt", "w") as bar:
+
+    bar.write('This is a test\n')
