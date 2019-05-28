38f41c5383c9970b6db63e6564971b257ee89b0f
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-28 10:30:25 -0500

    :sparkles: Complete exercises in 10_functions.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,17 @@
 
 # YOUR CODE HERE
 
+def is_even(num):
+  if num % 2 == 0:
+    return "Even!"
+
+  return "Odd"
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
 # YOUR CODE HERE
+print(is_even(num))
 
