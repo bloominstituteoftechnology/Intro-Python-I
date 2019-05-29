50f11e7e3346b4fee123ae45d28d9afca4e973e2
Phinehas <kohler.nai@gmail.com>
2019-05-28 20:57:17 -0600

    file_io.py complete

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,9 +10,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('foo.txt') as f:
+    print(f.read())
+    f.closed
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+    f.write('Who enters my domain?')
+    f.write('\nOne who would have your allegiance')
+    f.write('\nThe dead do not suffer the living to pass')
+    f.write('\nYou will suffer me.')
+
+with open('bar.txt') as f:
+    print(f.read())
+    f.closed
