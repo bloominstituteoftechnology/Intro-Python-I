4a39a546ee0d6e6cb2607f2ac202b859b29458eb
blokboy <proxxist@gmail.com>
2019-05-29 10:38:19 -0400

    Completed 13_file_io.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt') as foo:
+    data = foo.read()
+    print(data)
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,12 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar = open('bar.txt', 'w')
+bar.write('This is a test line.\n')
+bar.write('This is the second test line.\n')
+bar.write('This is the final line to test IO using Python')
+bar.close()
+
+with open('bar.txt') as bar:
+    data = bar.read()
+    print(data)
