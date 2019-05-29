d0d5209a3189d402f0394dd004221d5605726383
Carlos Gutierrez <chgutier@gmail.com>
2019-05-29 09:50:19 -0500

    Finished assignment

--- src/14_cal.py
+++ src/14_cal.py
@@ -23,17 +23,39 @@ import sys
 import calendar
 from datetime import datetime
 
-def calendr(*args):
+
+# ____________ MAIN _______________
+#  Accepts user input of the form 
+#  "14_cal.py month [year]"
+
+if __name__ == "__main__":
+
     month = datetime.now().month
     yr = datetime.now().year
-    if len(args) == 0:
-        return (calendar.monthcalendar(yr, month))
-    elif len(args) == 1:
-        return (calendar.monthcalendar(yr, month))
-    elif len(args) == 2:
-        return (calendar.monthcalendar(yr, month))
-    else:
-         return "Provide at least one integer for the month or tow separated by a comma for month and year (MM, YYYY), i.e.: 09, 2018"
 
-print(calendr())
+    args = []
+    for arg in sys.argv[1:]:
+        args.append(int(arg))
+
+    # ___ Assuming 1st value is month and 2nd is year
+
+    if len(args) == 0:  
+        print(calendar.monthcalendar(yr, month))
+
+    elif len(args) == 1:
+        if args[0] not in range(1,13):
+            print('Invalid month given. Please enter a value from 1 to 12.')
+        else:
+            month = args[0]
+            print(calendar.monthcalendar(yr, month))
+
+    elif len(args) == 2:
+      if args[0] not in range(1,13):
+            print('Invalid month given. Please enter a value from 1 to 12.')
+      if args[1] not in range(1500,3000):
+            print('Invalid year given. Please enter a value from 1500 to 3000.')
+      else:
+            month = args[0]
+            yr = args[1]
+            print(calendar.monthcalendar(yr, month))
 
--- src/15_classes.py
+++ src/15_classes.py
@@ -2,21 +2,42 @@
 # constructor
 
 # YOUR CODE HERE
-        
+class LatLong:
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return str(self.lat)+ ":" +str(self.lon)
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+class Waypoint(LatLong):
+    def __init__(self, name, lat, lon):
+        super().__init__(lat, lon)
+        self.name = name
+    def __str__(self):
+        return str(self.lat)+ ":" +str(self.lon)+ "," +str(self.name)
+        
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
-
+class Geocache(Waypoint):
+    def __init__(self, name, difficulty, size, lat, lon):
+        super().__init__(name, lat, lon)
+        self.difficulty = difficulty
+        self.size = size
+    def __str__(self):
+        return str(self.name)+ ", " + str(self.size)+ ", " +str(self.lat)+ ":" +str(self.lon)
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
-
+waypoint = Waypoint("Catacombs", 41.70505, -121.51521)
+print(waypoint)
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
 print(waypoint)
@@ -24,6 +45,6 @@ print(waypoint)
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
-
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 # Print it--also make this print more nicely
 print(geocache)
