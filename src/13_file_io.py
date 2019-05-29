c8dd6e169aadc5979279a010eb3b5a2c3d1a08cc
Jor_Poon <jormpoon@gmail.com>
2019-05-29 12:08:54 -0400

    files 13 -15 completed

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,15 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+f = open('foo.txt', 'r')
+
+for line in f:
+    print(line, end="")
+
+f.close()
+
+
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +25,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+f2 = open("bar.txt", "w")
+
+f2.write("LUKE.......SKYWALKER \n YOU'RE A WIZARD HARRY \n Aragorn..They took the little ones....")
+
+f2.close()
--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,21 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+
+date = datetime.now()
+
+cal = input("Choose a month number from 1-12 then add a space \
+  and ,if you would like, then choose a year --e.g-- 4 1992: ")
+cal = cal.split()
+
+if len(cal) == 0:
+    print(calendar.month(date.year, date.month))
+elif len(cal) == 1 and (int(cal[0]) <= 12):
+    month = int(cal[0])
+    print(calendar.month(date.year, month))
+elif len(cal) == 2 and (int(cal[0]) <= 12) and (len(cal[1]) == 4):
+    month, year = (int(x) for x in cal)
+    print(calendar.month(year, month))
+else:
+    print('Please enter the month in the form of 1 - 12 and, if you like, then the year in the form of YYYY.')
