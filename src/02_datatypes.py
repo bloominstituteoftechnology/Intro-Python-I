fa96a194cf8948436533ca2e43e33e8d21652be5
Alan Perez <alanp9six@gmail.com>
2019-05-29 20:29:06 -0500

    datatypes completed, second exercise you need to cast the int into a string

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -7,15 +7,15 @@ Note that if you try running the following code without making any
 changes, you'll get a TypeError saying you can't perform an operation
 on a string and an integer.
 """
-
-x = 5
 y = "7"
+x = 5
+
 
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+print(x+int("7"))
 
 # Write a print statement that combines x + y into the string value 57
-
+print(str(x)+y)
 # YOUR CODE HERE
