1e2355e13d02167333d48fdba49b9b44f66fedb7
unknown <jerrardsmith215@gmail.com>
2019-05-30 22:56:32 -0400

    file_io completed

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
-
+text = open("foo.txt")
+for line in text:
+    print(line)
+text.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+text2 = open('bar.txt', 'r+')
+text2.write('hello world\n my name is Jerrard\n I am your friend\n')
+
+for line in text2:
+    print(line)
+text2.close()
