4abeb2687a1e690e4e0c4b451881bfb417598e5e
Francis Chen <fncis.chen@gmail.com>
2019-07-08 13:51:14 -0700

    finished data types

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -9,13 +9,13 @@ on a string and an integer.
 """
 
 x = 5
-y = "7"
+y = 7
 
 # Write a print statement that combines x + y into the integer value 12
-
+print(x+y+12);
 # YOUR CODE HERE
 
 
 # Write a print statement that combines x + y into the string value 57
-
+print(x+y+57)
 # YOUR CODE HERE
