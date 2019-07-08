a0f3bc7f0fc7892cc8b7e76efa0017d53a9b2f2f
nomadic4life <orbison.dennis@gmail.com>
2019-07-08 13:59:24 -0700

    args

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,7 +11,8 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-print(sys.argv)
+for arg in sys.argv:
+    print(arg)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
