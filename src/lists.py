7c637627704c306413af8342e71835deb153d2dc
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-07-08 15:50:58 -0500

    printing and lists completed

--- src/lists.py
+++ src/lists.py
@@ -8,23 +8,26 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
-print(x.append(4))
+x.append(4)
+print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
-print(x.append(y))
-
+x.extend(y)
+print(x)
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
-print( del x[4])
+del x[4]
+print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
-print(x.insert(5, 99))
+x.insert(5, 99)
+print(x)
 
 # Print the length of list x
 # YOUR CODE HERE 
-print(x.length)
+print(len(x))
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
 for i in x:
