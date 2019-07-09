d202b7f4cc114eb15245e66619b3845f37dbdead
derek-shing <hpshing@gmail.com>
2019-07-09 00:03:30 -0700

    missing classes.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,11 +1,19 @@
 # Write a function is_even that will return true if the passed-in number is even.
-
+def isEven(number):
+    if number%2 ==0:
+        return True
+    else:
+        return False
 # YOUR CODE HERE
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
+if isEven(num):
+    print ("Even!")
+else:
+    print("Odd")
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+f = open("foo.txt","r")
+data = f.read()
+print(data)
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
+f = open("bar.txt","w")
+f.write("Testing\n")
+f.write("line 2\n")
+f.write("Line 3\n")
+f.close()
 # YOUR CODE HERE
