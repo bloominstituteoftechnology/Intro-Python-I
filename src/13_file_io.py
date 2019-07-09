2696fedc3ebe051de220425364ab9ede8540c431
nomadic4life <orbison.dennis@gmail.com>
2019-07-09 05:49:39 -0700

    classes

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,9 +9,14 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
-f = open('foo.txt', 'r')
-print(f.read())
-f.close()
+# f = open('foo.txt', 'r')
+# print(f.read())
+# f.close()
+
+with open('foo.txt', 'r') as f:
+    read_data = f.read()
+f.close
+print(read_data)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -25,6 +30,10 @@ f.close()
 # # bar.write("1234")
 # bar.writelines(L)
 # bar.close
-test = open("bar.txt", "r")
-print(test.readlines())
-test.close()
+# test = open("bar.txt", "r")
+# print(test.readlines())
+# test.close()
+
+with open('bar.txt', 'w') as t:
+    t.write("asd")
+t.close
