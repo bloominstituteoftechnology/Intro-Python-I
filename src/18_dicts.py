8aaf657ebbf889a61206e4dea61e89c64e9286f2
nomadic4life <orbison.dennis@gmail.com>
2019-07-09 13:28:40 -0700

    added a new toy challenge, dicts

---
+++ src/18_dicts.py
@@ -0,0 +1,10 @@
+
+books = {"man on fire": 5, "the lion king": 5,
+         "batman": 3, "bad man": 4, "into the bad lands": 5}
+print(books)
+print(len(books))
+
+books.pop("batman")
+books["the dark knight"] = 5
+print(books)
+print(len(books))
