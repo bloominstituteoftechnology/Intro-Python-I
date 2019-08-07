6dba598e77c5caab4410deec62a564cac3efe1df
Jesse Theisen <theiseja@gmail.com>
2019-08-06 21:08:30 -0500

    Creates notes file for future reference

---
+++ notes.py
@@ -0,0 +1,27 @@
+## Personal notes while learning python
+
+#console.log in js
+name = "Jesse"
+print("Hello," + name)
+
+
+# creates a variable
+name = "Jesse"
+
+
+# create an empty list
+p = []
+
+# create a list with some numbers
+q = [10, 60, 20, 5]
+
+# add an element to p
+print(p)
+p.append(77)
+print(p)
+
+
+# print all values in each list for loop example
+for element in q:
+    print(element)
+print("for loop done")
