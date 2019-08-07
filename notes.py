b710b60c8d85116c02282ed8b84b68aae88cb729
Jesse Theisen <theiseja@gmail.com>
2019-08-06 21:44:30 -0500

    Updates personal notes file

--- notes.py
+++ notes.py
@@ -28,3 +28,14 @@ print(enumerate(q))
 
 # if statement in one line
 evens = [num for num in numbers if num % 2 == 0]
+
+# dictionaries
+d = {} # create a new dictionary
+
+d = {'apple': 'is a fruit', 'cucumer': 'is a vegetable'} # dictionary example with two key value pairs
+
+print(d['apple']) # access & print an element in the dictionary
+
+# iterate through dictionary
+for key, value in d.items():  # key is what we're referring to in the dictionary, can be called whatever you want.
+    print(f'{key}: {value}')
