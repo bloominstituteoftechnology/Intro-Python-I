34e511c6d7f7b78cb166286e53068f5aef3ea7ce
iradsanchez <ira.d.sanchez@gmail.com>
2019-08-07 16:12:32 -0700

    Add Completed Step 5

--- src/05_lists.py
+++ src/05_lists.py
@@ -7,23 +7,30 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# YOUR CODE HERE 
-print(x)
+# YOUR CODE HERE
+x.append(4)
+print("Change x so that it is [1, 2, 3, 4]:", x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# YOUR CODE HERE 
-print(x)
+# YOUR CODE HERE
+x.extend(y)
+print("using y, change x so that it is [1, 2, 3, 4, 8, 9,10]:", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# YOUR CODE HERE 
-print(x)
+# YOUR CODE HERE
+x.remove(8)
+print("Change x so that it is [1, 2, 3, 4, 9, 10]:", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# YOUR CODE HERE 
-print(x)
+# YOUR CODE HERE
+x.insert(5, 99)
+print("Change x so that it is [1, 2, 3, 4, 9, 99, 10]:", x)
 
 # Print the length of list x
-# YOUR CODE HERE 
+# YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for element in x:
+    print(element*1000)
