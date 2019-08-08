4c8265174518f1fc2fa31e3c7b3887825a9ed015
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-08-08 16:38:30 -0400

    printing modules

--- src/04_printing.py
+++ src/04_printing.py
@@ -7,11 +7,12 @@ method, and by using f-strings.
 x = 10
 y = 2.24552
 z = "I like turtles!"
-
+float = round(y, 2);
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+sentence = f'x is {x}, y is {float}, z is "{z}';
+print(sentence)
 # Use the 'format' string method to print the same thing
 
 # Finally, print the same thing using an f-string
