a037d842e27a5de8b702ff2c42215c8cdff947a4
iradsanchez <ira.d.sanchez@gmail.com>
2019-08-09 21:19:50 -0700

    Add Completed Step 12

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,14 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
-
+filename = r'C:\Users\irads\Desktop\lambdaSchool\Computer Science\Intro to Python\Intro-Python-I\src\foo.txt'
+files = open(filename)
+for line in files:
+    print(line)
+files.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
