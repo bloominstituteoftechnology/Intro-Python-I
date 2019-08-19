a96d3cf3096e8271fbf0f0413544218b391883cb
jkaddu <jkaddu@yahoo.com>
2019-08-19 15:39:37 -0700

    practice

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,2 +1,3 @@
 # Print "Hello, world!" to your terminal
 print("Hello world")
+print("It is time to take learning python seriously and getting prepared to become a software engineer.")
--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,4 +2,10 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
-print(2**65536)
+# print(2**65536)
+print(2**3)
+print(2**4)
+print(2**5)
+print(3**3)
+print(3**4)
+print(3**5)
