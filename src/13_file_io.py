3900c251cb39ae1be844a9d728ec058680922f9e
azrap <azraap@gmail.com>
2019-09-03 15:00:00 -0700

    exercise 13 in the bag

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt') as f:
+    print(f.read())
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', "w+") as f:
+    f.write("this is line 1\n")
+    f.write("this is line 2\n")
+    f.write("this is line 3\n")
+    
+with open('bar.txt', "r") as f:
+    print(f.read())
+    
