5ecfc01a74015f901531e36256e9677e58d0e567
chrispetito <christopherpetito@gmail.com>
2019-09-03 15:41:55 -0700

    complete scopes exercise

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,7 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(num1, num2) : return num1 + num2
 
 print(f1(1, 2))
 
@@ -12,6 +13,12 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+def f2(*args) : 
+    sum = 0
+    for x in range(1, len(args)) : 
+        sum += (args[x])
+        return sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,7 +28,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -29,8 +36,8 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
-print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+# print(f3(1, 2))  # Should print 3
+# print(f3(8))     # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -46,13 +53,13 @@ print(f3(8))     # Should print 9
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-f4(a=12, b=30)
+# f4(a=12, b=30)
 
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+# f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
 d = {
     "monster": "goblin",
@@ -60,4 +67,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+# f4(d)
