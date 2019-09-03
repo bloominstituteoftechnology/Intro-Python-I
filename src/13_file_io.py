a1782a9f8fdabdddc8f95e92c5a93e44ec9051e6
chrispetito <christopherpetito@gmail.com>
2019-09-03 15:56:06 -0700

    complete file io excercise

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open("foo.txt") as f:
+    for line in f:
+        print(line, end="")
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.text', 'w') as f:
+    f.write('Lines are cool 1.\n')
+    f.write('Lines are cool 2.\n')
+    f.write('Lines are cool 3.\n')
+    f.close()
+
+with open('bar.text') as f :
+    for line in f: 
+        print(line, end='')
