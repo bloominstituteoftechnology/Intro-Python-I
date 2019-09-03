a38a3c077e77f20736131bdb501ce67b49957645
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-09-03 18:07:30 -0500

    finished tuples file

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -17,12 +17,14 @@ automatically.
 
 import math
 
+
 def dist(a, b):
     """Compute the distance between two x,y points."""
     x0, y0 = a  # Destructuring assignment
     x1, y1 = b
-    
+
     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+
 
 a = (2, 7)   # <-- x,y coordinates stored in tuples
 b = (-14, 72)
@@ -31,10 +33,15 @@ b = (-14, 72)
 print("Distance is: {:.2f}".format(dist(a, b)))
 
 
-
 # Write a function `print_tuple` that prints all the values in a tuple
 
-# YOUR CODE HERE
+def print_tuple(a):
+
+    if isinstance(a, int):
+        return print(a)
+    for i in a:
+        print(i)
+
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
