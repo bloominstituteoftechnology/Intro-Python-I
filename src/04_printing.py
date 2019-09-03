3bea477289de15532978674a0d35e5f5112c8cc9
chrispetito <christopherpetito@gmail.com>
2019-09-03 11:06:30 -0700

    complete lists exercise

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,6 +11,7 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+f"x is {x}, y is {round(y, 2)}, z is {z}"
 
 # Use the 'format' string method to print the same thing
 
