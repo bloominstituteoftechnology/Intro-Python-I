d608052832041a4c9228b3dfeb36b285cf7d930d
PetejStone <petejstoney@gmail.com>
2019-09-04 14:10:13 -0700

    finished task 10

--- src/10_functions.py
+++ src/10_functions.py
@@ -6,7 +6,29 @@
 num = input("Enter a number: ")
 num = int(num)
 
+
+def is_even(num):
+    if num %2 == 0:
+        return True
+    else:
+        return False
+            
+
+
+
+
+
+print(is_even(num))
+
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+
+def is_odd(num):
+    if num %2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+            
+print(is_odd(num))
 
