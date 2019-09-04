84d343048fe6c083b0f328e1c106284fd62d65ba
Raine Wallace <rainebeaudubs@gmail.com>
2019-09-04 11:24:14 -0400

    08 complete

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -17,7 +17,7 @@ print (y)
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = [x for x in range(1, 6)]
+y = [x **2 for x in range(10)]
 
 print(y)
 
@@ -26,7 +26,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [x.upper() for x in a]
 
 print(y)
 
@@ -36,6 +36,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [int(n) for n in x if int(n)%2==0]
 
 print(y)
