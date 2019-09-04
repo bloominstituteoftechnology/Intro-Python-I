f22c28abceeaee60997c65156f9a9c0bec5c7845
Geraldine Lustgarten <geraldinelustgarten@Geraldines-MacBook.local>
2019-09-03 20:26:58 -0500

    Finished file exercise

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,9 +10,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open("foo.txt") as file:
+    print(file.read())
+file.closed
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open("bar.txt", "w") as file:
+    file.write("Hello, helloooo!")
+file.closed
+
+with open("bar.txt") as file:
+    print(file.read())
+file.closed
