94b7eeee5fc6b0a015e5a759a4b52380ce734b56
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-09-26 23:55:17 -0500

    first three projects

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -13,9 +13,10 @@ y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
 
+print(x+int(y))
 # YOUR CODE HERE
 
 
 # Write a print statement that combines x + y into the string value 57
-
+print(str(x)+y)
 # YOUR CODE HERE
