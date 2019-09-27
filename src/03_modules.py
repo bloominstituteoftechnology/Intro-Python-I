57f0b5b30c4a7a3a7c8380184dce0f7744f04ef4
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-09-27 00:43:24 -0500

    Finished Lists.py

--- src/03_modules.py
+++ src/03_modules.py
@@ -6,19 +6,19 @@ level operating system functionality.
 """
 
 import sys
+import os
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+print(sys.argv)
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print(sys.OS)
 # Print out the version of Python you're using:
 # YOUR CODE HERE
 
 
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
