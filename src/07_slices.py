05c5fafb1c099f178a9993062174dcb88f658916
azrap <azraap@gmail.com>
2019-09-29 13:41:59 -0700

    redid exercises 06 and 07

--- src/07_slices.py
+++ src/07_slices.py
@@ -14,20 +14,32 @@ a = [2, 4, 1, 7, 9, 6]
 # Output the second element: 4:
 print(a[1])
 
+
 # Output the second-to-last element: 9
 print(a[-2])
 
+
+
 # Output the last three elements in the array: [7, 9, 6]
-print(a[3:6])
+print(a[-3:])
+
 
 # Output the two middle elements in the array: [1, 7]
+print(a[int((len(a)/2)-1): int(len(a)/2)+1])
+
 print(a[2:4])
 
+
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print(a[1:len(a)])
+for x in a[1:]:
+    print(x)
+
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print(a[0:len(a)-1])
+
+for x in a[:len(a)]:
+    print (x)
+
 
 # For string s...
 
