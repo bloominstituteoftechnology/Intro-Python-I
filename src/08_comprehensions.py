2a8bcb95251d39d514b4c37f28949373faa17510
azrap <azraap@gmail.com>
2019-09-30 08:14:47 -0700

    up to 9 is done

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,23 +10,29 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = [i for i in range(1,6) ]
+array=[a for a in range(1,6)]
+print(array)
 
-print (y)
+
+
+
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = [i**3 for i in range(0,10)]
+ar=[a**3 for a in range(10)]
+print(ar)
 
-print(y)
+
+
+
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
 
-y = [i.upper() for i in a]
+y = [x.upper()for x in a]
 
 print(y)
 
@@ -35,7 +41,11 @@ print(y)
 
 x = input("Enter comma-separated numbers: ").split(',')
 
+y=[float(a) for a in x if float(a)%2==0]
+
+print (y)
+
 # What do you need between the square brackets to make it work?
-y = [float(i) for i in x if float(i)%2==0]
+y = []
 
 print(y)
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,29 +35,23 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({"lat":44, "lon":-122, "name": "the fourth spot"})
 
-waypoints.append({"lat": 43,
-        "lon": -122,
-        "name": "a 4th place"
-
-})
 print(waypoints)
-
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
-
-waypoints[0]["name"]="not a real place"
-waypoints[0]["lon"]=-130
-
+waypoints[0]={**waypoints[0], 'lon':-130, 'name':"not a real place"}
 print(waypoints)
+
 
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for a in waypoints:
+    for value in a.values():
+        print (value)
 
-for i in waypoints:
-    print(i.values())
 
 
