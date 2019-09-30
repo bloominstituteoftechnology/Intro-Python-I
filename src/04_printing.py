93e2c23e79a6b0faa37b8e6d8d4c828dcd4bbdc1
Nabeelah <noblenab13@gmail.com>
2019-09-30 15:24:41 +0100

    Implement various ways to print in python

--- src/04_printing.py
+++ src/04_printing.py
@@ -1,6 +1,6 @@
 """
 Python provides a number of ways to perform printing. Research
-how to print using the printf operator, the `format` string 
+how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
 
@@ -12,6 +12,18 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print('x is %s' % x)
+print('y is %s' % y)
+print('z is "%s"' % z)
+
 # Use the 'format' string method to print the same thing
 
+print('x is {}'.format(x))
+print('y is {}'.format(y))
+print('z is "{}"'.format(z))
+
 # Finally, print the same thing using an f-string
+
+print(f'x is {x}')
+print(f'y is {y}')
+print(f'z is "{z}"')
