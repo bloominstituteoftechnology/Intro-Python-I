1851073a528d0f32e31a67443df2f6fab7987770
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-09-30 15:38:47 -0400

    0 thru 3

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -11,11 +11,10 @@ on a string and an integer.
 x = 5
 y = "7"
 
-# Write a print statement that combines x + y into the integer value 12
+if __name__=='__main__':
+    # Write a print statement that combines x + y into the integer value 12
+    print(x + int(y))
 
-# YOUR CODE HERE
 
-
-# Write a print statement that combines x + y into the string value 57
-
-# YOUR CODE HERE
+    # Write a print statement that combines x + y into the string value 57
+    print(str(x) + y)
