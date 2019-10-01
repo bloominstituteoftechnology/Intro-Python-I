535149a48ea3e2dc8bdd7de6e3b34fbba4ea0448
alanperez <alanperez9six@gmail.com>
2019-10-01 12:23:13 -0500

    :sparkles: No. 9
    done

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -2,8 +2,8 @@
 Dictionaries are Python's implementation of associative arrays.
 There's not much different with Python's version compared to what
 you'll find in other languages (though you can also initialize and
-populate dictionaries using comprehensions just like you can with 
-lists!). 
+populate dictionaries using comprehensions just like you can with
+lists!).
 
 The docs can be found here:
 https://docs.python.org/3/tutorial/datastructures.html#dictionaries
@@ -20,12 +20,12 @@ waypoints = [
         "lat": 43,
         "lon": -121,
         "name": "a place"
-    }, 
+    },
     {
         "lat": 41,
         "lon": -123,
         "name": "another place"
-    }, 
+    },
     {
         "lat": 43,
         "lon": -122,
@@ -35,10 +35,19 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints[0] = {
+    "lat": 41,
+    "lon": -130,
+    "name": "not a real place"
+}
 
+print(waypoints)
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
+
+for i in waypoints:
+    print(i.values())
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
