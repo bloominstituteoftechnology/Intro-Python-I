8c6a2dbc91d292c889a999de7107db9d02d212d3
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-10-27 14:12:04 -0700

    Finished 02

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -2,7 +2,6 @@
 Python is a strongly-typed language under the hood, which means 
 that the types of values matter, especially when we're trying
 to perform operations on them. 
-
 Note that if you try running the following code without making any
 changes, you'll get a TypeError saying you can't perform an operation
 on a string and an integer.
@@ -14,8 +13,10 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+print(x + int(y))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+
+print(str(x) + y)
