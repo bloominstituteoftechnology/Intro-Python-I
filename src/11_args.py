284832d66f9a80b916d82e7fec344bad81f33ad3
OmarSalah95 <omars.e.995@gmail.com>
2019-10-28 09:39:14 -0700

    Finished args assignment

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(num1, num2):
+    return num1 + num2
 
 print(f1(1, 2))
 
@@ -12,6 +14,11 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*nums):
+    sum = 0
+    for num in nums:
+        sum += num
+    return sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +28,18 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(num, num2 = None):
+    if (num2 is not None):
+        return num + num2
+    else:
+        return num + 1
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +54,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kybArgs):
+    for key, value in kybArgs.items():
+        print(f'key:{key}, value:{value}')
 
 # Should print
 # key: a, value: 12
@@ -60,4 +75,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
