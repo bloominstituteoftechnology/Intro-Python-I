8e8ccffd15ecaa5eeaa9eb128f2b1f4660c49648
York Cook <yorkcook@Johnny-Castle.local>
2019-10-28 18:34:11 -0400

    WIP: up to slices completed

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,14 +8,19 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
+
+x.append(4)
+
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
@@ -25,5 +30,12 @@ print(x)
 # Print the length of list x
 # YOUR CODE HERE 
 
+list_len = x 
+
+print(len(list_len))
+
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+
+x[:] = [i * 1000 for i in x]
+print(x)
