e49daadd440499d7d3ce2da04d65ec34f821c9d2
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-10-28 19:58:28 -0400

    Improves 10_functions by using ternary in print() statement

--- src/10_functions.py
+++ src/10_functions.py
@@ -11,7 +11,4 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-if is_even(num):
-    print("Even!")
-else:
-    print("Odd")
+print("Even!" if is_even(num) else "Odd")
