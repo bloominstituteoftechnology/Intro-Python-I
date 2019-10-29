55d4e3bd917ebd63c40f3d86b74e7e0019a2dd87
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-10-29 10:46:27 -0400

    Changes is_even in 10_functions to lambda fn

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,8 +1,9 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
-def is_even(n):
-    return n % 2 == 0
+# def is_even(n):
+#     return n % 2 == 0
+is_even = lambda n: n % 2 == 0
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
