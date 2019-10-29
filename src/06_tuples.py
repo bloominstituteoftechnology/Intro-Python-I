fef7d9f98d90c66b3fde36d36eaf7a723dbe608a
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-10-29 10:41:51 -0400

    Changes print_tuple in 06_tuples to lambda fn

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -35,8 +35,10 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
-def print_tuple(c):
-    print(*c, sep="\n")
+# def print_tuple(c):
+#     print(*c, sep="\n")
+
+print_tuple = lambda c: print(*c, sep="\n")
         
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
