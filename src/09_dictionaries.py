5907de0d6cb7fb10ed66b026a641aae83ca78fc1
Miguel <diaz4674@gmail.com>
2019-11-12 22:58:54 -0800

    finished lsits

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,10 +35,18 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({"lat": 20, "lon": 30, "name": "My cats place"})
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
+for objects in waypoints:
+    for value in objects:
+        [value["a place"]
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for objects in waypoints:
+    for pair in objects.items():
+        print(f'{pair[1]}')
