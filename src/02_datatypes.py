410feaa0aa767a3161ff383965ff85ae9ce1b559
Tevvin <Tevvin7395@icloud.com>
2019-11-13 22:47:21 -0500

    Switched over to pycharm, will pick up tomorrow

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,7 +14,7 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+print(f"{y} + {x}")
 
 # Write a print statement that combines x + y into the string value 57
 
