42880c7d6cc82b6ab1258ed08bebb5356fde42cb
Reyaad Rafeek <reyaad510@gmail.com>
2019-11-13 19:13:41 -0500

    Completed bignum question

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,7 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+print(2**65536)
+
+# prints out a number way too long for my terminal xD
+# Checked out on a smaller number and works
