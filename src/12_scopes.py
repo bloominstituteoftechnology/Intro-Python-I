0f563fd07856dfc0ea77aef4be22c8f34db95530
Reyaad Rafeek <reyaad510@gmail.com>
2019-11-15 17:31:39 -0500

    Completed scopes question

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -4,12 +4,17 @@
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
+
 def changeX():
+    global x
     x = 99
+
 
 changeX()
 
 # This prints 12. What do we have to modify in changeX() to get it to print 99?
+# In Python, global keyword allows you to modify the variable outside of the current scope. It is used to create a global variable and make changes to the variable in a local context.
+# https://www.programiz.com/python-programming/global-keyword
 print(x)
 
 
@@ -19,12 +24,16 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
 
     inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999? Google "python nested function scope".
+    # Have to use nonlocal keyword
+    # The nonlocal keyword is used to work with variables inside nested functions, where the variable should not belong to the inner function.
     print(y)
+
 
 outer()
