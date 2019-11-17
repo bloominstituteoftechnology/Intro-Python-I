f5b30f8433e1d01a15be6d3d4a1fb3a6cd73517a
Niyazova <zniyazov@NYMIT058770.fios-router.home>
2019-11-17 15:50:23 -0500

    Add classes

--- src/15_classes.py
+++ src/15_classes.py
@@ -1,7 +1,11 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
 
+
 class LatLon:
+    '''
+    class
+    '''
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
@@ -10,31 +14,45 @@ class LatLon:
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
+
 class Waypoint(LatLon):
+    '''
+    class
+    '''
     def __init__(self, name, lat, lon):
         super().__init__(lat, lon)
         self.name = name
+
     def __str__(self):
         return 'Waypoint(name='+self.name+', lat='+str(self.lat)+ ', lon='+str(self.lon) +')'
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
+
 class Geocache(Waypoint):
+    '''
+    class
+    '''
     def __init__(self, name, difficulty, size, lat, lon):
         super().__init__(name, lat, lon)
         self.difficulty = difficulty
         self.size = size
+
     def __str__(self):
-        return 'Geocache(\nname '+self.name+',\n difficulty '+str(self.difficulty)+',\n size '+str(self.size)+',\n lat '+str(self.lat)+',\n lon '+str(self.lon)+')'
+        return 'Geocache(\nname '+self.name+',\n difficulty '+\
+            str(self.difficulty)+',\n size '+str(self.size)+',\n lat '+\
+            str(self.lat)+',\n lon '+str(self.lon)+')'
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
+
+
 waypoint = Waypoint('Catacombs', 41.70505, -121.51521)
-print(Waypoint_1)
+print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
-geocache = Geocache(name = "Newberry Views", difficulty = 1.5, size = 2, lat = 44.052137, lon = -121.41556)
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
 print(geocache)
