9e6e70a5d686b94bbd558cc1643d0dcc2fcd7071
Niyazova <zniyazov@NYMIT058770.fios-router.home>
2019-11-17 14:55:01 -0500

    Add scope

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -4,10 +4,14 @@
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
+
 def changeX():
+    global x
     x = 99
 
+
 changeX()
+
 
 # This prints 12. What do we have to modify in changeX() to get it to print 99?
 print(x)
@@ -19,12 +23,17 @@ def outer():
     y = 120
 
     def inner():
+        global y
         y = 999
 
     inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999? Google "python nested function scope".
-    print(y)
+    # print(y)
+
 
 outer()
+
+
+print(y)
