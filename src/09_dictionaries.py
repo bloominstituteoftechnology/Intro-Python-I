a7fd639560d626f5a6e01707026bc3e7093791a2
Niyazova <zniyazov@NYMIT058770.fios-router.home>
2019-11-17 13:59:01 -0500

    Add functions and dictionaries solutions

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,10 +35,19 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
-
+waypoints.append({
+        "lat": 40.7,
+        "lon": 74,
+        "name": "Pier 11"
+    })
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
-
+waypoints[0]['name'] = "not a real place"
+waypoints[0]['lon'] = -130
+print(waypoints)
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for dictionary in waypoints:
+    for key, value in dictionary.items():
+        print(key, value)
