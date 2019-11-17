7d96db42786d4284011b73e330b526dc8bf4a864
Niyazova <zniyazov@NYMIT058770.fios-router.home>
2019-11-17 14:42:55 -0500

    Add args

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,4 +1,5 @@
-# Write a function is_even that will return true if the passed-in number is even.
+# Write a function is_even that will return true 
+# if the passed-in number is even.
 
 # YOUR CODE HERE
 
@@ -9,6 +10,8 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+
+
 def even_or_odd(n):
     if n % 2 == 0:
         print('Even')
