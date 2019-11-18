3fdcd665d86df3f5abc5180ccec7e4fa0a6df218
justinandrade <j.andrade.john@gmail.com>
2019-11-18 18:36:32 -0500

    scopes.py complete

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -4,8 +4,11 @@
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
+
 def changeX():
+    global x
     x = 99
+
 
 changeX()
 
@@ -19,12 +22,12 @@ def outer():
     y = 120
 
     def inner():
-        y = 999
+        return y * 0 + 999
 
     inner()
-
     # This prints 120. What do we have to change in inner() to get it to print
     # 999? Google "python nested function scope".
-    print(y)
+    print(inner())
+
 
 outer()
