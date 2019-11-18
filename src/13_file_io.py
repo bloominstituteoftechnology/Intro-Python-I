cdb77d605a05134a86b7b8b838515417d66832ce
David <digital0david@gmail.com>
2019-11-17 21:46:31 -0500

    13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,9 +10,27 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+f = open('foo.txt', 'r')
+message = f.read()
+print(message)
+f.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+w = open('bar.txt', 'w')
+w.write('Hello world!' + '\n')
+w.write("It's me David." + '\n')
+w.write('Goodbye.')
+
+
+w.close()
+
+w = open('bar.txt', 'r')
+line = w.read()
+print(line)
+w.close()
