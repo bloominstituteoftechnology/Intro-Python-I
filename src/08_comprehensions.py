e6986e1df3362933816ce9047179264e413f3a6d
jthicks91 <jordanthicks91@gmail.com>
2019-11-18 20:53:58 -0800

    fixed code in comprehensions from function to comprehension structure

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -18,10 +18,7 @@ print (y)
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-cubes = [] 
-for x in range(10):
-   cubes.append(x**3)
-
+cubes = [x**3 for x in range(10)] 
 print(cubes)
 
 # Write a list comprehension to produce the uppercase version of all the
