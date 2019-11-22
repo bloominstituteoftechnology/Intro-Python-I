9349281649662596cb5d7887441a474773b61ce1
fbt328 <katherine.rust.andrews@gmail.com>
2019-11-21 18:26:10 -0800

    first half of Python I through slices

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,35 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# YOUR CODE HERE 
+# YOUR CODE HERE
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE 
-
+print(len(x))
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+# not sure if the question meant to give a result of [1000, 2000, 3000, etc]: 
+
+print([i*1000 for i in x])
+
+# or the sum of the list times 1000 (128000)
+x = [1, 2, 3, 4, 9, 99, 10]
+sum = sum(x)
+multi = (int(sum)*1000)
+print(multi)
