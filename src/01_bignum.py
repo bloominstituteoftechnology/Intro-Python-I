425398565695b2fb2c806625999bfae06f15bda3
Adetunji <Ashennaike@gmail.com>
2019-12-02 18:01:09 -0500

    01 complete

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,10 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+number = 2
+power = 65536
+def toThePowerOf(n, p):
+  return pow(n, p)
+
+print(toThePowerOf(number, power))
