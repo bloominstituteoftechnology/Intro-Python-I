0b451f19de0357da08856806622c7030aa8573b7
Sean Doyle <sdoylelambda@gmail.com>
2019-12-02 18:44:07 -0500

    03-01

--- src/03_modules.py
+++ src/03_modules.py
@@ -5,20 +5,22 @@ methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
 
+import os
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+for i in sys.argv():
+    print(i)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+# print(python)
 
-
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,9 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print('x is %d, y is %f, z is "%s"' % (x, y, z))
+string = 'x is {}, ys is {}, z is "{}"'
+print(string.format(x, y, z))
 # Use the 'format' string method to print the same thing
 
 # Finally, print the same thing using an f-string
