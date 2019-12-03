c31f1b16195087f70e0f258b86d0b15a2dbc0095
Matt.Kirby <mkirby3@angelo.edu>
2019-12-02 20:00:25 -0600

    finish

--- src/15_classes.py
+++ src/15_classes.py
@@ -1,21 +1,49 @@
 # Make a class LatLon that can be passed parameters `lat` and `lon` to the
 # constructor
 
-# YOUR CODE HERE
-        
+class Latlon:
+
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
-# YOUR CODE HERE
+class Waypoint(Latlon):
+
+    def __init__(self, lat, lon, name):
+        self.lat = lat
+        self.lon = lon
+        self.name = name
+
+    def __str__(self):
+        return str(f"Name: {self.name}\n"\
+        f"Latitude: {self.lat}\n"\
+        f"Longitude:{self.lon}\n")
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
-# YOUR CODE HERE
+class Geocache(Waypoint):
+
+    def __init__(self, name, difficulty, size, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+        self.difficulty = difficulty
+        self.size = size
+
+    def __str__(self):
+        return str(f"Name: {self.name}\n"\
+        f"Latitude: {self.lat}\n"\
+        f"Longitude:{self.lon}\n"\
+        f"Difficulty: {self.difficulty}\n"\
+        f"Size: {self.size}")
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
-# YOUR CODE HERE
+waypoint = Waypoint(41.70505, -121.51521, "Catacombs")
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
@@ -23,7 +51,7 @@ print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
-# YOUR CODE HERE
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
 print(geocache)
