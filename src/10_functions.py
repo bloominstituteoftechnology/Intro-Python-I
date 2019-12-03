bda2fa7dcd36dc499e1c6b4684566c52c55e9517
Annique Nsabimana <anniquensabimana@gmail.com>
2019-12-03 17:28:20 -0600

    fixing errors

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,8 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+
+
 def is_even(test):
     if(test % 2 == 0):
         print("Even")
@@ -8,18 +10,20 @@ def is_even(test):
     else:
         print("Odd")
         return False
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
-is_even(num) 
+is_even(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+
+
 def is_even(num):
-
     if num % 2 == 0:
-            return "Even!"
-    else:
-            return "Odd"
-
-print(is_even(num))
+       return "Even!"
+  else:
+       return "Odd"
+       print(is_even(num))
--- src/11_args.py
+++ src/11_args.py
@@ -5,10 +5,10 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
-print(f1(1, 2))
 def f1(arg1, arg2):
     return arg1 + arg2
+print(f1(1, 2))
+
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
@@ -27,7 +27,6 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
 print(f2(sum(a)))    # Should print 22
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
