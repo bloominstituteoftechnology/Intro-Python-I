d1fddbb8dd679d540dfdf1c8564ae15baf5c38e8
Alberto-Funk <laptopreneur@gmail.com>
2019-12-03 13:45:20 -0800

    problemset 6,10,12,13 complete

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+f = open("foo.txt", "r")
+print(f.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +18,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+n = open("bar.txt", "x")
+print("q=finish, enter=new_line")
+while True:
+  cmd = input("Input Text -> ")
+  if cmd == "q":
+    break
+  n.write(cmd)
+  n.write("\n")
