8e796604acc6846d40004b2bfa862e3d237eee93
Connor Holly <connorholly11@gmail.com>
2019-12-06 11:10:26 -0700

    90% done

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+# x = open('foo.txt', 'r')
+# with open('foo.txt') as x:
+#     read_data = x.read()
+#     x.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
+
 # YOUR CODE HERE
+y = open('bar.txt', 'w+')
+y.write('hello')
+y.close()
--- src/15_classes.py
+++ src/15_classes.py
@@ -2,20 +2,46 @@
 # constructor
 
 # YOUR CODE HERE
-        
+
+
+class LatLon():
+    def _init_(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+class Waypoint(LatLon):
+    def _init_(self, lat, lon, name):
+        super()._init_(lat, lon)
+        self.name = name
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
 
+
+class Geocache(Waypoint):
+    def _init_(self, name, difficulty, size, lat, lon):
+        super()._init_(name, lat, lon)
+        self.size = size
+        self.difficulty = difficulty
+
+
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
+
+newWaypoint = Waypoint(41.70505, -121.51521, 'Catacombs')
+print (newWaypoint.lat)
+print (newWaypoint.lon)
+print (newWaypoint.name)
+
+
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
