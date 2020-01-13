1cb6b77e08c2c6b0eda11e1703f12e8f063ba26c
Shadowborn <james.starks023@gmail.com>
2020-01-12 22:35:23 -0700

    modules completed

--- src/03_modules.py
+++ src/03_modules.py
@@ -18,16 +18,20 @@ print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+import platform
+print("Python version", platform.python_version())
+print("Python version", sys.version_info[0])
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print(os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print(os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
