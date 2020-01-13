459894ae66b923d67b90e01e2b1e35dbda99a5fb
Michael Redig <mredig@gmail.com>
2020-01-13 17:41:34 -0600

    finished 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -5,11 +5,16 @@ at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
+from random import seed
+from random import random
+import time
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt') as file:
+    print(file.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +22,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+seed(time.time())
+randomness = random()
+
+outfile = open('bar.txt', 'w')
+outfile.write(str(randomness))
+outfile.close()
