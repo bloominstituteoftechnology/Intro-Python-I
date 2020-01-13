7cd1b5832333ae924666762793792d2b115a6e02
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-01-13 16:45:06 -0500

    Ex 04 syntax works in terminal but not VSCode. Calling it done, nothing to gain from debugging.

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,15 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print('Will it work let us see, %s ' % z)
+print('Okay here is one number, %f ' % y)
+print('And the other: %i ' % x)
+
 # Use the 'format' string method to print the same thing
+print(z.format())
+str_y = str(y)
+print(str_y.format())
+str_x = str(x)
+print(str_x.format())
 
 # Finally, print the same thing using an f-string
