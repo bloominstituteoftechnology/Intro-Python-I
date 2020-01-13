5edf9bcbd971bcb25f2c3cbfe416165f1c2835a0
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-01-13 17:54:04 -0500

    tuples py

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -17,6 +17,7 @@ automatically.
 
 import math
 
+
 def dist(a, b):
     """Compute the distance between two x,y points."""
     x0, y0 = a  # Destructuring assignment
@@ -37,8 +38,9 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # YOUR CODE HERE
 
 t = (1, 2, 5, 7, 99)
-print_tuple(t)  # Prints 1 2 5 7 99, one per line
+for i in t:
+    print(i)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
 u = (1)  # What needs to be added to make this work?
-print_tuple(u)
+print(u)
