4e816b81d423b4d757717866af6411ff69d7c596
ntsum <natsukajiwaram@gmail.com>
2020-01-13 12:31:14 -0500

    testing something

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+foo = open('src/foo.txt', 'r+')
+print(foo.read())
+foo.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar = open('src/bar.txt,' 'r+')
+print(bar.read())
+bar.write('python is cool \n is python cool? \n it is pretty cool')
+bar.close()
