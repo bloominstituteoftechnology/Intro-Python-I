8b229d0b24d8ebdb59b5d01f1b0f79ba0b7d7817
greenhornsr <srollins11279@gmail.com>
2020-01-13 15:43:40 -0800

    05_lists.py complete

--- src/05_lists.py
+++ src/05_lists.py
@@ -4,26 +4,36 @@
 x = [1, 2, 3]
 y = [8, 9, 10]
 
+# my lookup of methods for lists
+print(dir(x))
+
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert(5,99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for num in x:
+    print(num*1000)
