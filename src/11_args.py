5ccda3faf9d694ed07280b96552a1cf81f599f97
Shadowborn <james.starks023@gmail.com>
2020-01-13 18:11:00 -0700

    args finished

--- src/11_args.py
+++ src/11_args.py
@@ -5,13 +5,16 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(a,b):
+    return a+b
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args): 
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -19,16 +22,17 @@ print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
-
+Sum = sum(a)
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(Sum))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3(*args):
+    return sum(args)
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -42,6 +46,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**attributes):
+    for key, value in attributes.items():
+        print("key: %s, value: %s" % (key, value))
 
 # Should print
 # key: a, value: 12
@@ -60,4 +67,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
