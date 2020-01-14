bce577d4bd1639150d038d8e2756cdd83c60298b
Manju KC <kcmanjuthapa@gmail.com>
2020-01-14 15:52:43 -0500

    completed file_io.py

--- src/15_classes.py
+++ src/15_classes.py
@@ -3,6 +3,7 @@
 
 # YOUR CODE HERE
 
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
