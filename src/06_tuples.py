c898c2ece7cfa4ca5b1b7b18203abf7eb42b41ad
greenhornsr <srollins11279@gmail.com>
2020-01-13 16:17:36 -0800

    tuples.py complete

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -35,10 +35,13 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
+def print_tuple(tup):
+    for el in tup:
+        print(el)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?  add a comma so python recognizes a single element as a tuple
 print_tuple(u)
