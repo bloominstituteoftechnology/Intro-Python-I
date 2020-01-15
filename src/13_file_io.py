ade2b447e07a2faf8c4d0f8b3f86aa0cbf3f386a
Marlon <marmoneymar@icloud.com>
2020-01-15 02:49:08 -0500

    13 is done

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+f = open("foo.txt", 'r+')
+print(f.read())
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+snippet = open("bar.txt", 'w')
+snippet.write('''
+* E-flat walks into a bar and the bartender said "Sorry, we dont serve minors." Ba Dum Tss
+* The past, present, and future walk into a bar. It was tense.
+''')
+snippet.write("")
+snippet.close()
