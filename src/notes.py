33b676910956d6c856ba6bc1fd02c9870f841d27
hraya <hraya@lssu.edu>
2020-01-15 20:33:33 -0600

    Completed 0 - 3
    also  added lecture notes

---
+++ src/notes.py
@@ -0,0 +1,118 @@
+''' Intro to Python I - Guided Project
+    This document contains examples for CS Intro to Python, module 1. Examples
+    focus on Python syntax & semantics plus the usage of lists, dicts, sets,
+    and tuples.
+'''
+# How do we print something to the console?
+print("Hello, world!")
+print('"Hello World!"", said Austen!')
+print("Aren't you excited for Python?")
+
+# With f-strings?
+name = "Austen"
+# AVOID -> print("Hello" + name)
+print(f'Hello {name}')
+
+
+# Given an "out" string length 4, such as "<<>>", and a word, return a new 
+# string where the word is in the middle of the out string, e.g. "<<word>>".
+# (from CodingBat)
+def make_out_word(out, word):
+    return out[:2] + word + out[2:]
+  
+  
+print(make_out_word('<<>>', 'Yay')) # → '<<Yay>>'
+print(make_out_word('<<>>', 'WooHoo')) # → '<<WooHoo>>'
+print(make_out_word('[[]]', 'word')) # → '[[word]]'
+
+
+
+# Given an array of ints length 3, return the sum of all the elements.
+# (from CodingBat)
+def sum3(nums):
+    return sum(nums) # nums[0] + nums[1] + nums[2]
+
+
+print(sum3([1, 2, 3])) # → 6
+print(sum3([5, 11, 2])) # → 18
+print(sum3([7, 0, 0])) # → 7
+
+
+# Return the sum of the numbers in the array, except ignore sections of numbers 
+# starting with a 6 and extending to the next 7 (every 6 will be followed by at 
+# least one 7). Return 0 for no numbers.
+# (from CodingBat)
+# def sum67(nums):
+#     sum = 0
+#     for n in nums:
+#         if n == 6:
+#             # ignore until we find a 7, remove?
+#         elif n == 7:
+#             # start adding ints to sum again, remove?
+#         else:
+
+#     return sum
+
+    # && || ! ===  and or not
+
+
+# print(sum67([1, 2, 2])) # → 5
+# print(sum67([1, 2, 2, 6, 99, 99, 7])) # → 5
+# print(sum67([1, 1, 6, 7, 2])) # → 4
+
+
+# Create a new list containing the squares of all values in `numbers`with a 
+# list comprehension
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+squares = [num*num for num in numbers]
+print(squares)
+
+# the equivalent of...
+squares_two = []
+for num in numbers:
+    squares_two.append(num*num)
+
+print(squares_two)
+
+
+# We can also use list comprehensions to filter!
+# Create a new list containing only the even values of `numbers`
+evens = [num for num in numbers if num%2 == 0 ]
+print(evens)
+
+# the equivalent of...
+evens_two = []
+for num in numbers:
+    if num % 2 == 0:
+        evens_two.append(num)
+
+print(evens_two)
+
+# Can you use pieces of both of the above examples to...
+# Create a new list containing only the names that start with `s` so that they 
+# are properly capitalized (regardless of how the name originally appears) 
+names = ["Sarah", "jorge", "sam", "frank", "bob", "sandy", "shawn"]
+print(names)
+# TODO
+s_names = [name.capitalize() for name in names if name[0].lower() == 's']
+print(s_names)
+
+names[1] = "Elissa"
+print(names)
+
+my_string = "hello"
+my_string = "goodbye"
+# NOT ALLOWED -> my_string[0] = "z"
+
+# Dictionary
+person = {"name": "Joe", "age": 35, "city": "Chicago"}
+print(person["name"])
+print(person["age"])
+
+# Set no duplicates allowed
+users = {234234, 13123123, 341343243}
+
+# Tuple
+students = ("student1", "student2")
