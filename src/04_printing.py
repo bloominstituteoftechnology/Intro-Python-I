75daf1a054dbf17e7e40b89ba68952b577ac0d0b
Mariana Huh <mhuh0176@gmail.com>
2020-01-15 22:06:55 -0800

    dry(ed) up code

--- src/04_printing.py
+++ src/04_printing.py
@@ -14,7 +14,7 @@ print('x is %s, y is %s, z is "%s"' % (x, y, z))
 # x is 10, y is 2.25, z is "I like turtles!"
 
 # Use the 'format' string method to print the same thing
-print('x is {0},'.format(x), 'y is {0},'.format(y), 'z is "{0}"'.format(z))
+print('x is {0}, y is {1}, z is "{2}"'.format(x, y, z))
 
 # Finally, print the same thing using an f-string
 print(f'x is {x}, y is {y}, z is "{z}"')
