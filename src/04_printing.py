fb68c22c11c4f278e245e7b71d643b5204d52290
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2020-01-15 23:20:48 -0500

    completed 0-5

--- src/04_printing.py
+++ src/04_printing.py
@@ -9,8 +9,18 @@ y = 2.24552
 z = "I like turtles!"
 
 # Using the printf operator (%), print the following feeding in the values of x,
+
+print(x)
+
+
 # y, and z:
+
+print(f'{y}, and {z}')
 # x is 10, y is 2.25, z is "I like turtles!"
+
+print('x is '+f'{x} '+', y is '+f'{y}'+ ', z is '+z)
+
+
 
 # Use the 'format' string method to print the same thing
 
