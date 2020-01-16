03c3d3b6389a56600d80a3ed2c9e744e96516dc8
Mariana Huh <mhuh0176@gmail.com>
2020-01-15 18:04:03 -0800

    revised printf

--- src/04_printing.py
+++ src/04_printing.py
@@ -10,7 +10,7 @@ z = "I like turtles!"
 
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
-print('x is %s,' % x, 'y is %s,' % y, 'z is "%s"' % z)
+print('x is %s, y is %s, z is "%s"' % (x, y, z))
 # x is 10, y is 2.25, z is "I like turtles!"
 
 # Use the 'format' string method to print the same thing
