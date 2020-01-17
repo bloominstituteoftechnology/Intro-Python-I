34cf70450866b7dc1815b937a92bd5a7306b862c
newnativeabq <vincent.a.brandon@gmail.com>
2020-01-16 19:43:21 -0700

    completed a few more

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt', 'r') as file:
+    contents = file.read()
+print(contents)
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,5 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w+') as file:
+        file.writelines(['Someone today,\n', 'knows someone tomorrow,\n', 'that can help.'])
