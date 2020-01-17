9dc9f20e76c5385573f01ffa8b4039a71c9f6b77
unknown <jjamal95@gmail.com>
2020-01-16 20:42:14 -0600

    initial commit

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,5 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+2 ** 65536 
--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,14 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+x = 5 
+y = 7
+x + y 
 
 # Write a print statement that combines x + y into the string value 57
 
+
 # YOUR CODE HERE
+x = 20
+y = 37
+x + y 
