781fc1440be6a425ffcec774c66f022333043643
Mariana Huh <mhuh0176@gmail.com>
2020-01-17 18:42:41 -0800

    completed

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+f = open('src/foo.txt', 'r')
+print(f.read())
+f.close()
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +20,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.txt', 'w')
+f.write('This is arbitrary content line1\n')
+f.write('This is arbitrary content line2\n')
+f.write('This is arbitrary content line3\n')
+f.close()
