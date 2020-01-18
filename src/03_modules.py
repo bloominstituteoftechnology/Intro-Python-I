4c1981aba8b8f211144c5f310114aab2b2709ec9
unknown <dj.zaragoza@icloud.com>
2020-01-18 10:58:02 -0800

    working on module 3

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,7 +9,14 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+
+import sys
+sys.argv[0] # program_name
+sys.argv[1] # arguments
+count = len(arguments)
+
+
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
