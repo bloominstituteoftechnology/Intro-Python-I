20e0195b3eec6fdf2517e8443e431f2372010ff2
cpscott1 <thacam18gmail.com>
2020-01-22 06:27:53 -0600

    added txt file.

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -2,7 +2,7 @@
 Python makes performing file I/O simple. Take a look
 at how to read and write to files here:
 
-https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+
 """
 
 # Open up the "foo.txt" file (which already exists) for reading
@@ -10,9 +10,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+file = open('foo.txt', "r")
+print(file.read())
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+file = open('bar.txt', 'w')
+
+file.write('Hello')
+file.write('This is Cam')
+file.write('What\s going on')
+
+file.close()
