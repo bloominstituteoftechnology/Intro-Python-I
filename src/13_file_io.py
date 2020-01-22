457670d6d5d2e2d82281158244122aa1a1483b08
unknown <dj.zaragoza@icloud.com>
2020-01-21 19:10:57 -0800

    finished module 13 file.io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+files = open('C:\\user\\Desktop\\Lambda School Projects\\Intro-Python-I\\src\\foo.txt')
+print(files.read())
+files.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+fileTwo = open('C:\\user\\Desktop\\Lambda School Projects\\Intro-Python-I\\src\\bar.txt', 'w+')
+fileTwo.write('Python is awesome.\nI will always love JavaScript my first love\\Both languages will work hand in hand for building scalable software')
+fileTwo.close()
+fileTwo = open('C:\\user\\Desktop\\Lambda School Projects\\Intro-Python-I\\src\\bar.txt')
+print(fileTwo.read())
+fileTwo.close()
