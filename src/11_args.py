2dc447d396c01952fb83ad16ba247ea766d0fac1
unknown <dj.zaragoza@icloud.com>
2020-01-21 18:36:38 -0800

    finished module 11 args file

--- src/11_args.py
+++ src/11_args.py
@@ -14,6 +14,9 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+def f2(*numbers):
+    return sum(numbers)
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -22,13 +25,19 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(22))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+def f3(*numbers)
+    if len(numbers) == 1:
+        return sum(numbers) + 1
+    else:
+        return sum(numbers)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -43,6 +52,10 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+
+def f4(**object):
+    for key, value in object.items():
+        return print("key: {0} value {1}".format(key,value))
 
 # Should print
 # key: a, value: 12
@@ -61,4 +74,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(monster = d['monster'] , hp = d['hp'])
