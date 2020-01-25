b2c7082400fa4eb7c61ae2b6c7b9af52c840ff48
Thierry Joux <tjoux@hotmail.com>
2020-01-24 18:09:43 -0800

    Fix slicing

--- src/07_slices.py
+++ src/07_slices.py
@@ -15,7 +15,7 @@ a = [2, 4, 1, 7, 9, 6]
 print(a[1:2])
 
 # Output the second-to-last element: 9
-print(a[1:5])
+print(a[-2:-1])
 
 # Output the last three elements in the array: [7, 9, 6]
 print(a[3:])
@@ -27,7 +27,7 @@ print(a[2:4])
 print(a[1:])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print(a[0:5])
+print(a[0:len(a)-1])
 
 # For string s...
 
