a54df6512c248cb6d90413f3012633e9171ceb58
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-10 15:05:40 +0100

    complete modules

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -43,5 +43,15 @@ waypoints = [
 
 # YOUR CODE HERE
 
+waypoints[0].update({
+        "lat": 43,
+        "lon": -121,
+        "name": "not a real place"
+    })
+print(waypoints)
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for (v) in waypoints:
+
+    print(v)
