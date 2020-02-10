b4bb56ad0c37549a463a96134b1f7e42982ea33e
damilola.olawumi <damilola_olawumi@yahoo.com>
2020-02-10 16:48:02 +0100

    wip

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -31,10 +31,11 @@ b = (-14, 72)
 print("Distance is: {:.2f}".format(dist(a, b)))
 
 
-
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
+
+
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -12,13 +12,17 @@ for more info regarding list comprehensions.
 
 y = []
 
+for x in range(1,6):
+    y.append(x)
+
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for x in range(10):
+  y.append(x**3)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -27,7 +31,8 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
-
+for x in a:
+    y.append(x.upper())
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
