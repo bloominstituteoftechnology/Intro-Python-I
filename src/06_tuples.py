44c2b29e29875a7dff8b111047993893e2fd769e
BaoPham92 <baopham92@yahoo.com>
2020-02-10 18:14:57 -0500

    Seventh challenge completed

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -27,18 +27,21 @@ def dist(a, b):
 a = (2, 7)   # <-- x,y coordinates stored in tuples
 b = (-14, 72)
 
+# * SOLUTIONS BELOW:
+# * OPEN INTERPRETER && RUN: python3 06_tuples.py
+
 # Prints "Distance is 66.94"
 print("Distance is: {:.2f}".format(dist(a, b)))
 
 
-
 # Write a function `print_tuple` that prints all the values in a tuple
-
-# YOUR CODE HERE
+def print_tuple(param): 
+    if (isinstance(param, tuple)) : 
+        for item in param: print(item)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
