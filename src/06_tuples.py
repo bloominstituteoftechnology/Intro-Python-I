c074fe756716139ae1ee70521fafb0b7d7068865
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-10 14:43:25 +0100

    complete slices

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -35,6 +35,9 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
+def print_tuple(tupleObj):
+  for element in tupleObj:
+    print(element)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
@@ -42,3 +45,7 @@ print_tuple(t)  # Prints 1 2 5 7 99, one per line
 # Declare a tuple of 1 element then print it
 u = (1)  # What needs to be added to make this work?
 print_tuple(u)
+
+# T = (10,20,30,40,50)
+# for var in T:
+# print (T.index(var),var)
