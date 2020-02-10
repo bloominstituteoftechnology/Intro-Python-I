6166d57bb30e415ced3a7aac874d91c0e0943c69
Luis Mendes <cvlopes88@gmail.com>
2020-02-10 16:59:02 -0500

    some of them done

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,11 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+r = x + int(y)
+print(r)
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+t = str(x) + str(y)
+print(t)
