7304cd139e48a9187f0625ca618f4bdd433cea76
Gatrrr <davidgatrmacaulay@gmail.com>
2020-02-10 14:17:04 -0800

    05 finished

--- src/05_lists.py
+++ src/05_lists.py
@@ -24,10 +24,15 @@ print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+y.append(x)
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for i in x:
+    print(i*1000)
