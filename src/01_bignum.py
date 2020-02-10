5c512328cefd31cac0d065aceecf4de7b45ff85d
mogwai <feelirabs@gmail.com>
2020-02-10 17:10:09 +0100

    Add bignum

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,9 +2,4 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
-import math
-num = 2
-exponent = 65536
-power = math.pow(num, exponent)
-
-print("The power of {num} to the power of {exponent} ")
+print(2**65536)
