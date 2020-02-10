c297de62a1338284bd753a49f8efa84cf21f9708
mogwai <feelirabs@gmail.com>
2020-02-10 14:02:04 +0100

    Installed pipenv and solved hello world problem

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,9 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+import math
+num = 2
+exponent = 65536
+power = math.pow(num, exponent)
+
+print("The power of {num} to the power of {exponent} ")
