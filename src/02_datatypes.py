ebb6ad9e5b807090785b5f84b0b2fe465d182d60
Lisa Campbell <lisa@Lisas-MBP.lan>
2020-02-10 12:11:23 -0800

    datatypes

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,14 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+x = 5
+y = 7
+print(5 + 7)
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+x = '5'
+y = '7'
+print(x + y)
