c9edb4f999fc695c8496c14ae1151dcedb6078a8
Luis Mendes <cvlopes88@gmail.com>
2020-02-10 18:27:24 -0500

    some more done

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,26 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+print(x.append(4))
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+
+print(x.extend(y))
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+print(x.pop(4))
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert(-1, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
-
+print(len(x))
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for e in x:
+  print(e * 1000)
