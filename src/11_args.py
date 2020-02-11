ec7abbdddcef333c9624d12f98abd4a607ec2391
nov05 <arwen.liuwenjing@gmail.com>
2020-02-10 21:15:12 -0600

    2020-02-10

--- src/11_args.py
+++ src/11_args.py
@@ -16,7 +16,7 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*argv):
-    pass
+    return sum(argv)
     
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -26,7 +26,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -35,7 +35,10 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 def f3(*argv):
-    pass
+    if len(argv) == 1:
+        return argv[0] + 1
+    else:
+        return argv[0] + argv[1]
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -50,8 +53,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-def f4(*argv):
-    pass
+def f4(**kwargs):
+    for k, v in kwargs.items():
+        print(f'key: {k}, value: {v}')
 
 # Should print
 # key: a, value: 12
@@ -70,4 +74,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
