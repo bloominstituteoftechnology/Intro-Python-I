47c322866df1491b240ada24bc801fcfcfd4e19d
Jake <jlconnerly@gmail.com>
2020-02-11 11:16:56 -0600

    completed 15

--- src/15_classes.py
+++ src/15_classes.py
@@ -12,20 +12,26 @@ class LatLon:
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 class Waypoint(LatLon):
-    name = ""
-    def __init__(self, wp_latitude, wp_longitude,  wp_name):
-        LatLon.__init__(self, wp_latitude, wp_longitude)
-        self.name = wp_name
+    def __init__(self, name, lat, lon):
+        super().__init__(lat, lon)
+        self.name = name
+
     def __str__(self):
-        return 'waypoint' + str(self.name) + ',' + str(self.wp_latitude) + ',' + str(self.wp_longitude) + '.' 
+        return f"Name: {self.name}, latitude: {self.lat}, longitude: {self.lon}"
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
+class Geocache(Waypoint):
+    def __init__(self, name, difficulty, size, lat, lon):
+        super().__init__(name, lat, lon)
+        self.size = size
+        self.difficulty = difficulty
 
+    def __str__(self):
+        return f"Name: {self.name}, difficulty: {self.difficulty}, size: {self.size}, latitude: {self.lat}, longitude: {self.lon}"
 
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
-latLon = LatLon(41.70505, -121.51521)
 waypoint = Waypoint('Catacombs', 41.70505, -121.5152)
 
 # Without changing the following line, how can you make it print into something
@@ -34,7 +40,7 @@ print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
-# YOUR CODE HERE
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
-# print(geocache)
+print(geocache)
