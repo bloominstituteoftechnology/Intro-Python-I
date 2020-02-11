c2645132b84e4343e08d7abda990c50087158a87
Lisa Campbell <lisa@Lisas-MBP.lan>
2020-02-11 14:06:19 -0800

    comprehension amd working on dictionaries.

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,6 +35,9 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append( [{"lat": 57, "lon": -105, "name": "Lambda"}] )
+print(waypoints)
+
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +45,7 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[1].update([{"name": "not a real place"}])
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
