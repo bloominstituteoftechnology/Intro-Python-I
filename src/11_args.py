d0e3f0db6d6d1cf59a6d8d618e091171c11135f1
Maxime Vacher-Materno <37785179+maximematerno@users.noreply.github.com>
2020-02-10 19:24:43 -0800

    Update 11_args.py

--- src/11_args.py
+++ src/11_args.py
@@ -5,14 +5,25 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x,y):
+    '''
+    returns the sum for two numbers
+    '''
+    return x + y
 
 print(f1(1, 2))
+
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    the_sum = 0
+    for x in args:
+        the_sum += x
+    return the_sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -30,6 +41,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(a, b=1):
+    return a + b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +57,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, val in kwargs.items():
+        print(f'key: {key}, value: {val}')
 
 # Should print
 # key: a, value: 12
