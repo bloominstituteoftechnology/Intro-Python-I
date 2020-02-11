8fa6f6cb79d1dce27bd4d76a7b3f8cb493759a50
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-02-11 13:48:29 -0600

    files navigated/wrote

--- src/14_cal.py
+++ src/14_cal.py
@@ -30,3 +30,23 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+#get user input and store it into an array
+user_input = input('Enter a month and year: ').split(' ')
+#use list comprehension to remove any values that aren't numeric and convert them to integers
+user_input = [int(i) for i in user_input if i.isnumeric()]
+
+#get current year and month
+now = datetime.now()
+year = now.year
+month = now.month
+
+#check user_input length; update year and month is needed
+if len(user_input) > 1:
+  year = user_input[0]
+  month = user_input[1]
+elif len(user_input) == 1:
+  year = user_input[0]
+
+#print calendar in terminal
+calendar.TextCalendar().prmonth(year, month)
