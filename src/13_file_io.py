2dd6b11d473ffcf89356704ed9400ff4298424a3
Mildred Awuor <otieno.awuor.m@gmail.com>
2020-02-11 17:10:22 +0300

    worked on file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,8 +8,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
-
 # YOUR CODE HERE
+with open('foo.txt') as f:
+     read_data = f.read()
+     print(read_data)  
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +19,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt',"w+") as g:
+     read_data = g.write("Adhabu ya kaburi, aijua maiti.\n The torture of the grave is known only to the dead.\n Ahadi ni deni.\n A promise is a debt.")
+print(g.closed)
+with open('bar.txt') as h:
+     read_h_data = h.read()
+     print(read_h_data) 
