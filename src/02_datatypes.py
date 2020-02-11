500668ccd04067e7d074183f2d71723882b604cc
Gatrrr <davidgatrmacaulay@gmail.com>
2020-02-10 16:09:10 -0800

    06 07 08 09 10 started/finished

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,12 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+statement = x + int(y)
+print(statement)
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+statement = '5' + '7'
+print(statement)
--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,27 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
+def is_even():
+    try:
+        x = int(raw_input("Enter a number: "))
+        if num % 2 == 0:
+            return x, 'true'
+        else:
+            return x, 'false'
+
+print is_even()
+
 # YOUR CODE HERE
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
+#num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+number = int(input('Number: '))
+if number % 2 == 0:
+    print(number, "is even")
+else:
+    print(number, "is odd")
