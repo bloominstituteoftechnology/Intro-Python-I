d9e8e1e735da5ad47ac8b27fdfb5c0878252838d
Rod <rod.bromberg@yahoo.com>
2020-02-12 19:07:46 -0500

    halfway done

--- src/03_modules.py
+++ src/03_modules.py
@@ -5,27 +5,36 @@ methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
 
+import platform
+import os
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print(str(sys.argv))
+# execute program and pass arguments in
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print(platform.system())
+print(platform.release())
+print(platform.version())
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print(platform.python_version())
 
 
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print(os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print(os.curdir)
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,32 +10,35 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [x for x in range(6)]
 
-print (y)
+
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+cubed = [x**3 for x in range(10)]
 
-print(y)
+print(cubed)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [word.upper() for word in a]
 
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ").split(',')
+###
+# Split was giving errors
+my_list = input("Enter comma-separated numbers: ")
 
 # What do you need between the square brackets to make it work?
-y = []
 
-print(y)
+evensList = [x for x in my_list if x % 2 == 0]
+print(evensList)
