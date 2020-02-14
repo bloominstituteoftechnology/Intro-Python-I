d38876986531408fee38e455ac738ebe64747dad
saganator84 <kingtrav@gmail.com>
2020-02-13 21:47:36 -0700

    mvp sections completed

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,12 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(num):
+    x = num % 2
+    if x == 0:
+        return True
+    return False
+
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -9,4 +15,9 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+if is_even(num) == True:
+    print("Even!")
+else:
+    print("Odd")
+
 
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,12 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+f = open('./src/foo.txt', 'r')
+data = f.read()
+f.close()
+print(data)
+
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +23,6 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+g = open('./src/bar.txt', 'w')
+g.write('We, are a way\nfor the cosmos\nto know itself')
+g.close()
