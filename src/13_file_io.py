4ab7f95af60a549c98fc16cc18d46d7a4d28324b
teej387 <teej387@yahoo.com>
2020-02-13 21:07:02 -0600

    Finished task 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('src\\foo.txt', 'r') as foo:
+  read_foo = foo.read()
+  print(read_foo)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('src\\bar.txt', 'w') as bar:
+  bar.write('Always do your best\nTake pride in your work\nIf at first you do not succeed, try, try again\n')
+  
+with open('src\\bar.txt', 'r') as bar:
+  print(bar.read())
