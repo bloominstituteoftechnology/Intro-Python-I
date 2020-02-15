f76715f2c3bbe28a4598a7711eb3f7961dc11263
Ilgar Ilyasov <iilyasov@awarex.com>
2020-02-14 20:17:23 -0500

    Complete scopes, file_io, cal, classes

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,11 +8,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+with open('foo.txt') as foo:
+    for f in foo:
+        print(f)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
+print("\n")
+with open('bar.txt', 'w') as bar:
+    bar.write("Content 1\n")
+    bar.write("Content 2\n")
+    bar.write("Content 3\n")
 
-# YOUR CODE HERE
+with open('bar.txt') as bar:
+    for b in bar:
+        print(b)
--- src/14_cal.py
+++ src/14_cal.py
@@ -16,9 +16,21 @@ and does the following:
    month and year.
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
-   Then exit the program.
+   Then exit the program..
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+userInput = input("Enter a date in 'month [year]' format: ").split(" ")
+if userInput[0] == "":
+  today = datetime.today()
+  print(today.month)
+elif len(userInput) == 1:
+  m = int(userInput[0])
+  print(calendar.month(2020, m))
+elif len(userInput) == 2:
+  m = int(userInput[0])
+  y = int(userInput[0])
+  print(calendar.month(y, m))
