c7ce6da659ea8928bcec532b9d360b0b68e5d6bc
Rod <rod.bromberg@yahoo.com>
2020-02-16 14:02:27 -0500

    12 down

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,29 @@
-# Write a function is_even that will return true if the passed-in number is even.
+# # Write a function is_even that will return true if the passed-in number is even.
 
-# YOUR CODE HERE
+# # YOUR CODE HERE
 
-# Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
 
-# Print out "Even!" if the number is even. Otherwise print "Odd"
+# # def is_even(num):
+# #     if num % 2 == 0:
+# #         return True
+# #     else:
+# #         return False
 
-# YOUR CODE HERE
 
+# # Read a number from the keyboard
+# num = input("Enter a number: ")
+# num = int(num)
+# print(num % 2 == 0)
+# # Print out "Even!" if the number is even. Otherwise print "Odd"
+
+# # YOUR CODE HERE
+
+
+def double_list(li):
+    for i in range(0, len(li)):
+        li[i] *= 2
+
+
+num_list = [1, 2, 3, 4, 5]
+double_list(num_list)
+print(num_list)
