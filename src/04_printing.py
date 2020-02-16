b84a3a181dcf731161499aedf3fdd9ebda55aa49
sweyome <sweyome1@gmail.com>
2020-02-16 18:26:44 -0500

    Q4 done

--- src/04_printing.py
+++ src/04_printing.py
@@ -7,11 +7,23 @@ method, and by using f-strings.
 x = 10
 y = 2.24552
 z = "I like turtles!"
+print('%d %d %s' % (x, y, z) )
+# attempt two
+data = (10, 2.25, "I like turtles")
+format_string = '%d %d %s'
+print(format_string % data)
 
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
 # Use the 'format' string method to print the same thing
+data = {'x': 10, 'y': 2.25, 'z': 'I like turtles!'}
+print("{x} {y} {z}".format(x=data['x'], y=data['y'], z=data['z']))
 
 # Finally, print the same thing using an f-string
+
+x = 10
+y = round(2.24552,2)
+z = "I like turtles!"
+print(f'{x} {y} {z}')
