0dc9c7a376353f3cdb2cca9ac176d1f6c2333bb3
Victor Tran <victor.n.tran@outlook.com>
2020-02-16 18:34:50 -0500

    made helper functions, cleaned up code, could have used elif's, did not check for valid ranges in month

--- src/14_cal.py
+++ src/14_cal.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+
+# run file by typing `./14_cal.py <month> <year>
+
 """
 The Python standard library's 'calendar' module allows you to
 render a calendar to your terminal.
@@ -30,3 +34,53 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+argument_length = len(sys.argv)
+custom_calendar = calendar.TextCalendar();
+
+def convert_date_str_to_int(values):
+  return [int(value) for value in values]
+
+def create_calendar_options(month=datetime.today().month, year=datetime.today().year, width=5, length=1):
+  return {
+    "themonth": month,
+    "theyear": year,
+    "w": width,
+    "l": length
+  }
+
+def print_calendar(calendar, **options):
+  print(calendar.formatmonth(**options))
+
+def extract_month_year_from_datetime(date=datetime.today()):
+  return str(date).split(' ')[0].split('-')[0:2]
+
+# if year or month is left out, print cal based on today
+if (argument_length == 1):
+  year, month = convert_date_str_to_int(extract_month_year_from_datetime())
+  options =  create_calendar_options()
+
+  print_calendar(custom_calendar, **options)
+  exit(0)
+
+if (argument_length == 2): 
+  # assumes it was a month entered
+  month = convert_date_str_to_int(sys.argv[1:2])[0]
+  options = create_calendar_options(month)
+  print_calendar(custom_calendar, **options)
+  exit(0)
+
+  print_calendar(custom_calendar, **options)
+
+# if there's all arguments necessary
+if (argument_length == 3):
+  month, year = convert_date_str_to_int(sys.argv[1:3])
+  options = create_calendar_options(month=month, year=year)
+  print(options)
+  print_calendar(custom_calendar, **options)
+  exit(0)
+
+print('Usage: ./14_cal.py [month] [year]')
+
+
+
