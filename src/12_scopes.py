626c7a526885c384b3fe7bd576f590c6acbfa264
efrain <efarinva98@gmail.com>
2020-02-21 00:41:29 -0500

    fixed scopes need to do calendar

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -3,8 +3,9 @@
 
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
+print(x)
 def changeX():
-    nonlocal x
+    global x
     x = 99
     print(x)
 changeX()
@@ -17,14 +18,14 @@ print(x)
 
 def outer():
     y = 120
-
+    print(y)
     def inner():
         nonlocal y
         y = 999
     inner()
+    print(y)
 
 #     # This prints 120. What do we have to change in inner() to get it to print
 #     # 999? Google "python nested function scope".
-    print(y)
 
 outer()
