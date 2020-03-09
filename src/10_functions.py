d6c94db69d4dedbed9e7af9da1633d792884f6b2
Samantha Finley <srfinley@uchicago.edu>
2020-03-09 17:02:32 -0400

    exercise 11

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,4 +1,6 @@
-# Write a function is_even that will return true if the passed-in number is even.
+# Write a function is_even that will return true
+# if the passed-in number is even.
+
 
 def is_even(n):
     return n % 2 == 0
