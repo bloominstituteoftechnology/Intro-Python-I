6cb43aeca96857c5feab5a4635dad0f8a3d5b0f0
Samantha Finley <srfinley@uchicago.edu>
2020-03-09 17:20:51 -0400

    exercise 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -7,13 +7,26 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
-# Note: pay close attention to your current directory when trying to open "foo.txt"
+# Note: pay close attention to your current directory when trying to open foo
 
-# YOUR CODE HERE
+with open('foo.txt') as f:
+    print(f.read())
+    f.close
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+arbitrary_content = """first line
+second line
+third line"""
+
+bar = open('bar.txt', 'w')
+
+bar.write(arbitrary_content)
+
+bar.close()
+
+with open('bar.txt') as f:
+    print(f.read())
