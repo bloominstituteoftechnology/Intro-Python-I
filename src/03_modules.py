cdc5eabff98a60a1062b435575d02585caff5116
Nckflannery <Nckflannery@users.noreply.github.com>
2020-03-09 15:02:31 -0700

    Added clarity to print statements

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,23 +9,25 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-print(dir(sys.argv))
+print("The command line arguments in sys.argv are:")
+for i in sys.argv:
+    print(i)
 
 # Print out the OS platform you're using:
-print(sys.platform)
+print(f'The OS platform is: {sys.platform}')
 
 # Print out the version of Python you're using:
-print(sys.version)
+print(f'The system version is: {sys.version}')
 
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-print(os.getpid())
+print(f'The current process ID is: {os.getpid()}')
 
 # Print the current working directory (cwd):
-print(os.getcwd())
+print(f'The CWD is: {os.getcwd()}')
 
 # Print out your machine's login name
-print(os.getlogin())
+print(f'The login name is: {os.getlogin()}')
