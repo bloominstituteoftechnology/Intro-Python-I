8ce836c4644355b748d6c21c7fef04f9c7d3ec9a
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-03-09 15:30:10 -0600

    Update 04_printing.py
    string formatting

--- src/04_printing.py
+++ src/04_printing.py
@@ -4,6 +4,11 @@ how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
 
+import sys
+def printf(format, *args):
+    sys.stdout.write(format % args)
+
+
 x = 10
 y = 2.24552
 z = "I like turtles!"
@@ -11,7 +16,13 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+printf("x is %d, y is %.2f, z is I like turtles!\n",   x , y )
+
 
 # Use the 'format' string method to print the same thing
+txt = "{nodecimal}"
+txt2 = "{decimal: .2f}"
+print("x is" + " " + txt.format(nodecimal = x) + "," + " " + "y is" + txt2.format(decimal = y)+ "," +" " + "z is" + " "+ z)
 
 # Finally, print the same thing using an f-string
+print(f'x is {x}, y is{txt2.format(decimal =y)}, z is {z}')
