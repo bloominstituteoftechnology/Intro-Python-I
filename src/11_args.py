4e8f883017b450141cb3ba7c617caabf1e76d66f
Daniel <danstad2012@gmail.com>
2020-03-10 14:11:58 -0700

    11-14 done

--- src/11_args.py
+++ src/11_args.py
@@ -30,7 +30,7 @@ a = [7, 6, 5, 4]
 # How do you have to modify the f2 call below to make this work?
 print(f2(*a))    # Should print 22
 
-# Write a function f3 that accepts either one or two arguments. If one argument,
+# Write a function f3 that accpyepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments.
 # Note: Google "python default arguments" for a hint.
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,28 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+foo = open('foo.txt', 'r')
+for i in foo:
+  print(i)
+foo.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
+text = '''
+I once knew a man from nantucket,
+who wore as a hat a bucket.
+his head was not strong,
+he did not have long,
+before the pail said fuck it
+'''
 # YOUR CODE HERE
+bar =open('bar.txt', 'w')
+bar.write(text)
+bar.close()
+bar = open('bar.txt','r')
+for i in bar:
+  print(i)
+bar.close()
