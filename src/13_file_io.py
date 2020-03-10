6052a12755fa2698ff3bc6b95dd22cc3d41c134a
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-03-10 00:20:48 -0600

    scopes & r/w
    read and write

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+import os
+
+def read_file(file):
+    try:
+        filee = os.path.dirname(__file__)
+
+        txt = open(f"{filee}/{file}", "r")
+        read_text = txt.read()
+        txt.close()
+        return read_text
+    except FileExistsError:
+        print(f"{FileNotFoundError}")
+print(read_file("foo.txt"))
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +30,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+fp = open("bar.txt", "w")
+
+fp.write("""Line 1
+Line 2
+Line 3""")
+
+# Close the file
+fp.close()
+
