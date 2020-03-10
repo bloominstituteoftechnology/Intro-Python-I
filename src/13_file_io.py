a19309936665f8a9505c409761b4afe2c7005576
TimTree <TimTree@users.noreply.github.com>
2020-03-10 15:14:05 -0700

    Complete assignment

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('foo.txt', 'r') as f:
+    read_data = f.read()
+    print(read_data)
+f.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +21,6 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.txt', 'w')
+f.write('This is a test\nThis is a test 2\nThis is a test 3')
+f.closed
