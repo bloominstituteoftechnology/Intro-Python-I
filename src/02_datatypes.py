c5882b072032a0cc576d738e0c1ead4749ac60cb
Sameera Leola <sameeraleola@gmail.com>
2020-03-09 19:10:49 -0700

    Starting on problem 3 after lots of administrative interruptions

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,17 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+x = 5
+y = 7
+print(5+7)
 
 
 # Write a print statement that combines x + y into the string value 57
 
-# YOUR CODE HERE
+# YOUR CODE 
+x = "5"
+y = "7"
+print(x+y)
+
+z = x + y
+print(z)
