c2681a9477e56bde0bb30e370d775edc6149d092
xander-bennett <xander-bennett@users.noreply.github.com>
2020-03-10 13:23:30 -0700

    uploading commits up to 10_functions.py

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -3,21 +3,22 @@ List comprehensions are one cool and unique feature of Python.
 They essentially act as a terse and concise way of initializing
 and populating a list given some expression that specifies how
 the list should be populated. 
-
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
 """
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+# list comprehension including all numbers in a range of 0-6, excluding 0
+y = [num for num in range(6) if num != 0]
+
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [num **3 for num in range(10)]
 
 print(y)
 
@@ -26,7 +27,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [word.upper() for word in a]
 
 print(y)
 
@@ -36,6 +37,11 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+
+# this was outputting numbers as strings in the list
+# y = [number for number in x if int(number) % 2 == 0]
+
+# casting each number string as an int will work 
+y = [int(number) for number in x if int(number) % 2 == 0]
 
 print(y)
--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,16 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
-# YOUR CODE HERE
+def is_even(x):
+    if x % 2 == 0:
+        return True
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
-# YOUR CODE HERE
+if is_even(num) == True:
+    print('Even!')
+else:
+    print('Odd!')
 
