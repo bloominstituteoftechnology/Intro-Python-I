931b976f97e00ecee02c344e6eb67e3a3d246fc6
Nckflannery <Nckflannery@users.noreply.github.com>
2020-03-10 12:28:06 -0700

    off by 1

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -18,7 +18,7 @@ print (y)
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = [i**3 for i in range(9)]
+y = [i**3 for i in range(10)]
 
 print(y)
 
