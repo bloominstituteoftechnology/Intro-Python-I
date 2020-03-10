ac7e39a9b71330010a110177c80271461f749b1c
hakujouryu <hakujouryu@gmail.com>
2020-03-10 12:26:10 -0400

    almost done

--- src/11_args.py
+++ src/11_args.py
@@ -4,7 +4,7 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+def f1(num1,num2): return num1+num2
 
 print(f1(1, 2))
 
@@ -12,7 +12,10 @@ print(f1(1, 2))
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+def f2(*args):
+    if type(args[0])==list:
+        print(sum(args[0]))
+    else: print(sum(args))
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -29,7 +32,10 @@ print(f2(a))    # Should print 22
 # arguments.
 # Note: Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+def f3(a, b=None):
+    if b != None: return a+b
+    else: return a+1
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
