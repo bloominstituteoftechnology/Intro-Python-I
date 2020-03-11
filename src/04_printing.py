c4498593b58be72627463da121d9b27271c7e95b
Elizabeth Ter Sahakyan <liztersahakyan@gmail.com>
2020-03-10 23:51:03 -0700

    lists, tuples, slices

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,7 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-print('i= %x')
+# print('i= %x')
 
 # Use the 'format' string method to print the same thing
 var = f'x is {x}, y is {round(y,2)}, z is "{z}"'
