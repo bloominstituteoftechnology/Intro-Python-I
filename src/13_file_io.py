9bc1e1d2576f0f842c475790284cde8eabbeda31
JeanFraga <fragajean7@gmail.com>
2020-03-10 19:25:54 -0500

    almost finished assignment, up to 14

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,26 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('src/foo.txt') as f:
+    read_data = f.read()
+print(read_data)
+f.closed
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+a = 'line one. \n'
+b = 'line two. \n'
+c = 'line three.'
+with open('src/bar.txt', 'w') as f:
+    f.write(a)
+    f.write(b)
+    f.write(c)
+f.closed
+print("\n")
+with open('src/bar.txt') as f:
+    read_data = f.read()
+print(read_data)
--- src/14_cal.py
+++ src/14_cal.py
@@ -30,3 +30,6 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+calendar = calendar.Calendar()
+calendar.iterweekdays()
