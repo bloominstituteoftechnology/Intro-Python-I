6e1fae9589d90a311d75710a7029dca44f523216
Heather Nuffer <zzzuzz@gmail.com>
2020-04-02 20:11:29 -0600

    Adds solutions for file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,15 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+'''
+f = open('./foo.txt', 'r')
+for line in f:
+    print(line)
+'''
+
+with open('./foo.txt', 'r') as f:
+    print(f.read())
+print(f.closed)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +26,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('./bar.txt', 'w') as b:
+    b.write('An old silent pond...\n')
+    b.write('A frog jumps into the pond,\n')
+    b.write('splash! Silence again.\n')
+
+with open('./bar.txt', 'r') as b:
+    print(b.read())
