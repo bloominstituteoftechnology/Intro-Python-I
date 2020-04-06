6fe152b1d9ea5228ebc920574dc39ec9f369272d
Karim B <mattiakarim@live.it>
2020-04-06 16:10:17 +0200

    Complete print out of all elements in array exept first one

--- src/07_slices.py
+++ src/07_slices.py
@@ -25,7 +25,7 @@ arr_mid = int(len(a) / 2)
 print(a[arr_mid - 1: arr_mid + 1])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+print(a[1:])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
 print()
