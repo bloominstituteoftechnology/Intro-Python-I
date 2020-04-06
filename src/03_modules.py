329811f6032a0e9b835e30d781a465ec571de999
Karim B <mattiakarim@live.it>
2020-04-06 13:42:39 +0200

    Complete first challenge of sys module file to print out command line arguments

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,6 +10,8 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+for arg in sys.argv:
+    print(arg)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
