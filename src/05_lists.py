19ca39fc6d15c14f376adeae6e755061e03812e8
Miguel <miguelqnicolas@gmail.com>
2020-04-06 17:45:00 -0400

    sixth exercise complete

--- src/05_lists.py
+++ src/05_lists.py
@@ -1,29 +1,36 @@
-# For the exercise, look up the methods and functions that are available for use
-# with Python lists.
+# for the exercise, look up the methods and functions that are available for use with Python lists
 
 x = [1, 2, 3]
 y = [8, 9, 10]
 
-# For the following, DO NOT USE AN ASSIGNMENT (=).
+# for the following, DO NOT USE AN ASSIGNMENT (=)
 
-# Change x so that it is [1, 2, 3, 4]
-# YOUR CODE HERE
+# change x so that it is [1, 2, 3, 4]
+
+x.append(4)
 print(x)
 
-# Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# YOUR CODE HERE
+# using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
+
+for item in y:
+    x.append(item)
 print(x)
 
-# Change x so that it is [1, 2, 3, 4, 9, 10]
-# YOUR CODE HERE
+# change x so that it is [1, 2, 3, 4, 9, 10]
+
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# YOUR CODE HERE
+
+x.insert(5, 99)
 print(x)
 
-# Print the length of list x
-# YOUR CODE HERE
+# print the length of list x
 
-# Print all the values in x multiplied by 1000
-# YOUR CODE HERE
+print(len(x))
+
+# print all the values in x multiplied by 1000
+
+for item in x:
+    print(item * 1000)
