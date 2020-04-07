d08c0355d76d3172e5b75001cf5ce6950f1ba549
Miguel <miguelqnicolas@gmail.com>
2020-04-07 17:37:15 -0400

    tenth exercise completed

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -5,11 +5,9 @@ you'll find in other languages (though you can also initialize and
 populate dictionaries using comprehensions just like you can with
 lists!).
 
-The docs can be found here:
-https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+The docs can be found here: https://docs.python.org/3/tutorial/datastructures.html#dictionaries
 
-For this exercise, you have a list of dictionaries. Each dictionary
-has the following keys:
+For this exercise, you have a list of dictionaries. Each dictionary has the following keys:
  - lat: a signed integer representing a latitude value
  - lon: a signed integer representing a longitude value
  - name: a name string for this location
@@ -17,31 +15,37 @@ has the following keys:
 
 waypoints = [
     {
-        "lat": 43,
-        "lon": -121,
-        "name": "a place"
+        'lat': 43,
+        'lon': -121,
+        'name': 'a place'
     },
     {
-        "lat": 41,
-        "lon": -123,
-        "name": "another place"
+        'lat': 41,
+        'lon': -123,
+        'name': 'another place'
     },
     {
-        "lat": 43,
-        "lon": -122,
-        "name": "a third place"
+        'lat': 43,
+        'lon': -122,
+        'name': 'a third place'
     }
 ]
 
-# Add a new waypoint to the list
-# YOUR CODE HERE
+# add a new waypoint to the list
 
-# Modify the dictionary with name "a place" such that its longitude
-# value is -130 and change its name to "not a real place"
-# Note: It's okay to access the dictionary using bracket notation on the
-# waypoints list.
+waypoint = { 'lat': 69, 'lon': 420, 'name': 'a fourth place' }
+waypoints.append(waypoint)
+print(waypoints)
 
-# YOUR CODE HERE
+# modify the dictionary with name 'a place' such that its longitude value is -130 and change its name to 'not a real place'
+# note: it's okay to access the dictionary using bracket notation on the waypoints list
 
-# Write a loop that prints out all the field values for all the waypoints
-# YOUR CODE HERE
+waypoints[0]['lon'] = -130
+waypoints[0]['name'] = 'not a real place'
+print(waypoints)
+
+# write a loop that prints out all the field values for all the waypoints
+
+for item in waypoints:
+    for key, value in item.items():
+        print(value)
