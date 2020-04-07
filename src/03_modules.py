fd40dff479e04fb49335fdddd938671d0ae4d6c4
sumeyaali <thesumeyaali@gmail.com>
2020-04-06 21:11:52 -0700

    introToPythonDay1

--- src/03_modules.py
+++ src/03_modules.py
@@ -4,12 +4,13 @@ which allows you to access many system specific variables and
 methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
-
+import fileinput
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -12,14 +12,22 @@ for more info regarding list comprehensions.
 
 y = []
 
+for num in range(1,6):
+    y.append(num)
 print (y)
+
+y = [num for num in range(1,6)]
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for num in range(10): 
+    y.append(num ** 3)
 print(y)
+
+
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
@@ -27,7 +35,8 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
-
+for i in a:
+    y.append(i.upper())
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
