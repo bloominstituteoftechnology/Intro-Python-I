85c16cf4f01fffa0265ded8dc26dd9dda8f67596
johnwesleyharding <johnwesleyharding@users.noreply.github.com>
2020-04-07 14:19:53 -0600

    test merge/review process

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -6,8 +6,9 @@ x = 12
 
 def change_x():
     x = 99
+    return x
 
-change_x()
+x = change_x()
 
 # This prints 12. What do we have to modify in change_x() to get it to print 99?
 print(x)
@@ -20,8 +21,9 @@ def outer():
 
     def inner():
         y = 999
+        return y
 
-    inner()
+    y = inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999?
