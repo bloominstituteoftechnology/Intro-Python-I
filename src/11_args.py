11f77758f276591fd2cfe96499946963d6ba8eb6
Karim B <mattiakarim@live.it>
2020-04-07 14:23:37 +0200

    Complete modifying function call to take any number of arguments

--- src/11_args.py
+++ src/11_args.py
@@ -16,10 +16,7 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*nums):
-    if nums.len() > 1:
-        sum(nums)
-    else:
-        return nums[0]
+    return sum(nums)
 
 
 print(f2(1))                    # Should print 1
@@ -30,7 +27,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -39,8 +36,8 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
-print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+# print(f3(1, 2))  # Should print 3
+# print(f3(8))     # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -56,18 +53,18 @@ print(f3(8))     # Should print 9
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-f4(a=12, b=30)
+# f4(a=12, b=30)
 
-# Should print
-# key: city, value: Berkeley
-# key: population, value: 121240
-# key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+# # Should print
+# # key: city, value: Berkeley
+# # key: population, value: 121240
+# # key: founded, value: "March 23, 1868"
+# f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
-    "monster": "goblin",
-    "hp": 3
-}
+# d = {
+#     "monster": "goblin",
+#     "hp": 3
+# }
 
-# How do you have to modify the f4 call below to make this work?
-f4(d)
+# # How do you have to modify the f4 call below to make this work?
+# f4(d)
