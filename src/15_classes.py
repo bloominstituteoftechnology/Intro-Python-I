05bc5c3e42f9c1464e7222a4066e083c06b3415e
Karim B <mattiakarim@live.it>
2020-04-07 19:10:40 +0200

    Complete creation of new class LatLon passing latitude and longitude parameters to constructor of class

--- src/15_classes.py
+++ src/15_classes.py
@@ -2,6 +2,10 @@
 # constructor
 
 # YOUR CODE HERE
+class LatLon:
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
