2391edacba0fd5349d1cae4d1e557b6b94ff6500
Forrest Bingham <bingham_5@live.com>
2020-04-06 18:26:38 -0600

    Finished dictionaries

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -12,12 +12,20 @@ for more info regarding list comprehensions.
 
 y = []
 
-print (y)
+for num in range(6):
+    if num > 0 and num < 6:
+        y.append(num)
+
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+
+for num in range(10):
+    if num < 10:
+        y.append(num**3)
 
 print(y)
 
@@ -27,7 +35,9 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
-
+for x in a:
+    y.append(x.upper())
+print(a)
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
@@ -36,6 +46,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
-
+y = [num for num in x if int(num) % 2 == 0 and num != 0]
+print(x)
 print(y)
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -36,12 +36,29 @@ waypoints = [
 # Add a new waypoint to the list
 # YOUR CODE HERE
 
+new = {
+    "lat": 55,
+    "lon": -100,
+    "name": "a secret place"
+}
+
+waypoints.append(new)
+print(waypoints)
+
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0]={
+    "lan": 43,
+    "lon": -130,
+    "name": "not a real place"
+}
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for key in waypoints:
+    print(key)
