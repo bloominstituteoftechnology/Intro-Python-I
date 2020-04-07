91cc1c24d71cf0ea01ccad50f65b8de7a2b96f2e
Miguel <miguelqnicolas@gmail.com>
2020-04-07 17:07:40 -0400

    ninth exercise complete

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -2,40 +2,34 @@
 List comprehensions are one cool and unique feature of Python.
 They essentially act as a terse and concise way of initializing
 and populating a list given some expression that specifies how
-the list should be populated. 
+the list should be populated.
 
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
 """
 
-# Write a list comprehension to produce the array [1, 2, 3, 4, 5]
+# write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-
-print (y)
-
-# Write a list comprehension to produce the cubes of the numbers 0-9:
-# [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
-
-y = []
-
+y = [item for item in range(1, 6)]
 print(y)
 
-# Write a list comprehension to produce the uppercase version of all the
-# elements in array a. Hint: "foo".upper() is "FOO".
+# write a list comprehension to produce the cubes of the numbers 0-9
+# should output [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-a = ["foo", "bar", "baz"]
-
-y = []
-
+y = [item**3 for item in range(0, 10)]
 print(y)
 
-# Use a list comprehension to create a list containing only the _even_ elements
-# the user entered into list x.
+# write a list comprehension to produce the uppercase version of all the elements in array a
+# hint: 'foo'.upper() is 'FOO'
 
-x = input("Enter comma-separated numbers: ").split(',')
+a = ['foo', 'bar', 'baz']
+y = [item.upper() for item in a]
+print(y)
 
-# What do you need between the square brackets to make it work?
-y = []
+# use a list comprehension to create a list containing only the even elements the user entered into list x
+# what do you need between the square brackets to make it work?
 
+x = input('Enter comma-separated numbers: ').split(',')
+print(x)
+y = [int(item) for item in x if int(item) % 2 == 0]
 print(y)
