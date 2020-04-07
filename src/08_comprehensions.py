43d28fa3548ffe7b5ed14c1d06eebe13055f6bff
Wilma <kima_92@fastmail.com>
2020-04-07 16:17:45 -0400

    08_comprehensions.py Done

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -8,34 +8,35 @@ Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-compr
 for more info regarding list comprehensions.
 """
 
-# Write a list comprehension to produce the array [1, 2, 3, 4, 5]
+print("\nWrite a list comprehension to produce the array [1, 2, 3, 4, 5]")
 
 y = []
+y = [x + 1 for x in range(5)]
 
 print (y)
 
-# Write a list comprehension to produce the cubes of the numbers 0-9:
-# [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
+print("\nWrite a list comprehension to produce the cubes of the numbers 0-9:")
+print("\n[0, 1, 8, 27, 64, 125, 216, 343, 512, 729]")
 
-y = []
+y = [x**3 for x in range(10)]
 
 print(y)
 
-# Write a list comprehension to produce the uppercase version of all the
-# elements in array a. Hint: "foo".upper() is "FOO".
+print("\nWrite a list comprehension to produce the uppercase version of all the")
+print("\nelements in array a. Hint: 'foo'.upper() is 'FOO'.")
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [x.upper() for x in a]
 
 print(y)
 
-# Use a list comprehension to create a list containing only the _even_ elements
-# the user entered into list x.
+print("\nUse a list comprehension to create a list containing only the _even_ elements")
+print("the user entered into list x.\n")
 
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [i for i in x if (int(i) % 2) == 0]
 
 print(y)
