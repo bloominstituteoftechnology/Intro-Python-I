86fddcf2bae5e0c761761da767b53018fa39dbd2
Uzoamaka <uzoamakaanyanwu26@gmail.com>
2020-04-08 09:52:28 +0100

    file 10 to 15 done

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-
+with open('src/foo.txt') as f:
+    print(f.read())
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('src/bar.txt', "w") as f:
+    f.write("Legend tells of a man, tall in stature the man was. \nThe day thus came, whence the ancient ones required a sacrifice to be carried by the chosen one. \nWhom was this chosen one? It was the tall man. ")
