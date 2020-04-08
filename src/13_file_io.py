5b5b183facaea9592d7cf688ddcc8f31198f63e3
Nisha Arya Ahmed <53621406+nishaarya@users.noreply.github.com>
2020-04-08 16:45:14 +0100

    completed

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open("foo.txt", "r") as f:
+    print(f.read())
+    f.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+f = open("bar.txt", "w")
+f.write("Line 1\n")
+f.write("Line 2\n")
+f.write("Line 3\n")
+f.close()
+    
+    
