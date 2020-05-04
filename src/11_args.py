80e95dcc58149dd5caa3b796648ff738db6cb1f4
jwross24 <jwross24@users.noreply.github.com>
2020-05-04 16:20:04 -0400

    update 11_args

--- src/11_args.py
+++ src/11_args.py
@@ -6,13 +6,23 @@
 
 # YOUR CODE HERE
 
+
+def f1(a, b):
+    return a + b
+
+
 print(f1(1, 2))
 
-# Write a function f2 that takes any number of integer arguments and returns the
-# sum.
+# Write a function f2 that takes any number of integer arguments and returns
+# the sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*ints):
+    return sum(ints)
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,14 +32,22 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
-# Write a function f3 that accepts either one or two arguments. If one argument,
-# it returns that value plus 1. If two arguments, it returns the sum of the
-# arguments.
+# Write a function f3 that accepts either one or two arguments. If one
+# argument, it returns that value plus 1. If two arguments, it returns
+# the sum of the arguments.
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+
+def f3(num1, num2=None):
+    if num2 is None:
+        return num1 + 1
+    else:
+        return num1 + num2
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +62,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f'key: {key}, value: {value}')
+
 
 # Should print
 # key: a, value: 12
@@ -62,4 +84,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
