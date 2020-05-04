edd63db0206e748b77afc9547e555c479655e259
Adam <adammonast@Adams-MBP.attlocal.net>
2020-05-04 19:17:04 -0400

    worked on challenges 0-3

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,12 +11,18 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+for arg in sys.argv:
+    print(arg)
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+
+print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
 
+print(sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
