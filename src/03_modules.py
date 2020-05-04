d096e930c0d7502cba4d93858ff35fbcfb53a64e
baugher90 <baugher90@gmail.com>
2020-05-04 19:34:38 -0400

    mostly finished with first day tasks

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,33 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+args = sys.argv
+print(args)
+     
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+platform = sys.platform
+print(platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+version = sys.version
+print(version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+processID = os.getpid()
+print(processID)
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+cwd = os.getcwd()
+print(cwd)
 
 # Print out your machine's login name
 # YOUR CODE HERE
+machineName = os.getlogin()
+print(machineName)
