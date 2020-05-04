b9263df2e75ddb26edff9458c0749ece0fc9629b
ThisIsJorgeLima <ThisIsJorgeLima@users.noreply.github.com>
2020-05-04 17:22:03 -0400

    Assignments 0-5

--- src/03_modules.py
+++ src/03_modules.py
@@ -5,27 +5,40 @@ methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
 
-import sys
+"""
+Import Statements:
+"""
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+import os
+import sys
+print('Command Line Arguments:')
+([(print(i)) for i in sys.argv], '\n')
+print('\n')
 # Print out the OS platform you're using:
+print('+--------------------------+')
 # YOUR CODE HERE
+print('OS Platform:\n', sys.platform, '\n')
 
 # Print out the version of Python you're using:
+print('+--------------------------+')
 # YOUR CODE HERE
+print('Python Version:\n', sys.version, '\n')
 
-
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
+print('+--------------------------+')
 # YOUR CODE HERE
+print('Current Process ID:\n', os.getpid(), '\n')
 
 # Print the current working directory (cwd):
+print('+--------------------------+')
 # YOUR CODE HERE
-
+print('Current Working Directory:\n', os.getcwd(), '\n')
 # Print out your machine's login name
+print('+--------------------------+')
 # YOUR CODE HERE
+print("Machine's Login Name:\n", os.getlogin(), '\n')
--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,32 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+x.append(4)
+print(x, '\n')
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
+print('+---------------------+')
 # YOUR CODE HERE
-print(x)
+x = x + y
+print(x, '\n')
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
+print('+---------------------+')
 # YOUR CODE HERE
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
+print('+---------------------+')
 # YOUR CODE HERE
-print(x)
+x.insert(5, 99)
+print(x, '\n')
 
 # Print the length of list x
+print('+---------------------+')
 # YOUR CODE HERE
-
+print('Length of x:', len(x), '\n')
 # Print all the values in x multiplied by 1000
+print('+---------------------+')
 # YOUR CODE HERE
+print([i*1000 for i in x], '\n')
