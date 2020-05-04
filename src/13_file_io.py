f76612f97d4a16c35f3da0714fef36a48a4bbee9
jwross24 <jwross24@users.noreply.github.com>
2020-05-04 16:40:53 -0400

    update 13_file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -5,11 +5,31 @@ at how to read and write to files here:
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
 
+import os
+
+
+def find_in_subdirectory(filename, subdirectory=''):
+    if subdirectory:
+        path = subdirectory
+    else:
+        path = os.getcwd()
+    for root, dirs, names in os.walk(path):
+        if filename in names:
+            return os.path.join(root, filename)
+    raise 'File not found'
+
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
-# Note: pay close attention to your current directory when trying to open "foo.txt"
+# Note:
+# Pay close attention to your current directory when trying to open "foo.txt"
+
 
 # YOUR CODE HERE
+with open(find_in_subdirectory('foo.txt'), 'r') as foo:
+    for line in foo:
+        print(line, end='')
+
+print()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +37,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('./src/bar.txt', 'w') as bar:
+    bar.write("Happy times ahead.\n")
+    bar.write("Lambda CS is awesome!\n")
+    bar.write("This is not a drill.\n")
