59bdab8c060ee4e2a81f1c74b171de1a855e012d
aaronjan <aaronjan98@gmail.com>
2020-05-04 12:24:32 -0700

    Update 01_bignum.py
    printed 2 to the 65536 power

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,6 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
-# YOUR CODE HERE
+exp = pow(2, 65536)
+print(exp)
+# in the JS console, the output is infinity
