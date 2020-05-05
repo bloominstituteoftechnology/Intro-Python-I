3c23aa722236e60da555102fdafea0309374b59a
Lambda_School_Loaner_222 <lambda_school_loaner_222@Lambda-School-Loaner-222s-MacBook-Pro.local>
2020-05-05 12:45:45 -0700

    12 and 13 done

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -1,19 +1,32 @@
 """
 Python makes performing file I/O simple. Take a look
 at how to read and write to files here:
-
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
-# Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+
+with open('src/foo.txt') as f:
+    read = f.read()
+    print(read)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
+#THIS TOOOK FOREVER LOL
+
 # YOUR CODE HERE
+
+with open('src/bar.txt', 'w') as bar:
+    bar.write('First line\n'
+    'Second line\n'
+    'Third line\n')
+
+with open('src/bar.txt', 'r') as bar:
+    read_bar = bar.read()
+    print(read_bar)
