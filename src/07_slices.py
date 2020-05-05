a3c311d3d78c109c78ce13418ca4ee6d53ebb4ba
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-05-05 16:41:06 -0400

    day 1

--- src/07_slices.py
+++ src/07_slices.py
@@ -1,7 +1,7 @@
 """
-Python exposes a terse and intuitive syntax for performing 
+Python exposes a terse and intuitive syntax for performing
 slicing on lists and strings. This makes it easy to reference
-only a portion of a list or string. 
+only a portion of a list or string.
 
 This Stack Overflow answer provides a brief but thorough
 overview: https://stackoverflow.com/a/509295
@@ -12,26 +12,26 @@ Use Python's slice syntax to achieve the following:
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+print(a[slice(1, 2)])
 
 # Output the second-to-last element: 9
-print()
+print(a[slice(4, 5)])
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+print(a[slice(3, 6)])
 
 # Output the two middle elements in the array: [1, 7]
-print()
+print(a[slice(2, 4)])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+print(a[slice(1, 6)])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+print(a[slice(0, 5)])
 
 # For string s...
 
 s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
-print()
+print(s[slice(7, 12)])
