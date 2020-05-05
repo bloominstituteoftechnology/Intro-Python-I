507b06121c578935897c2cb1b8ffd90afe9c20ce
Jud Taylor <jud.taylor@protonmail.com>
2020-05-05 19:23:46 -0400

    Added primes.py, with comment on improvement.

--- src/primes.py
+++ src/primes.py
@@ -1,20 +1,3 @@
-"""
-
-The Python standard library's 'calendar' module allows you to
-render a calendar to your terminal.
-https://docs.python.org/3.6/library/calendar.html
-
-Write a program that accepts user input of the form
-  `14_cal.py [month] [year]`
-and does the following:
- - If the user doesn't specify any input, your program should
-   print the calendar for the current month. The 'datetime'
-   module may be helpful for this.
- - If the user specifies one argument, assume they passed in a
-   month and render the calendar for that month of the current year.
- - If the user specifies two arguments, assume they passed in
-"""
-
 from sys import argv
 
 args = argv
