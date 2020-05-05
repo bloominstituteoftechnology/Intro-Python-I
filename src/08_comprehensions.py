e0360f9bda7bf5383b21b66dd8e208ceb8b9e764
kika <anamonteiro430@gmail.com>
2020-05-05 21:32:42 +0100

    first seven exercises

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,14 +11,16 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
+for x in range(1,6):
+     y.append(x*1)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for x in range(10):
+     y.append(x**3)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
