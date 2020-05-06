9abab009f320e960014e9c483162984c0699e596
unknown <dj.zaragoza@icloud.com>
2020-05-05 22:07:39 -0700

    finished printing.py

--- src/04_printing.py
+++ src/04_printing.py
@@ -9,18 +9,19 @@ y = 2.24552
 z = "I like turtles!"
 
 
-
-
-
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-print('These are my variables: \nx is: % 2d\ny is: %.2f\nz is: %s' %(x, y, z))
+print('x is %i, y is %4.2f, z is "%s"' %
+      (x, y, z))
 
 # Use the 'format' string method to print the same thing
 
-print('These are my variables: \nx is: {0\ny is: {1}\nz is: {2}'.format(x, y, z))
+print('x is {0}, y is {1}, z is "{2}"'.format(x, round(y, 2), z))
+
 
 # Finally, print the same thing using an f-string
-print(f'These are my variables:\nx is: {x}\ny is: {y}\nz is: {z}') #Using f-strings
+# Using f-strings
+
+print(f'x is {x}, y is {round(y, 2)}, z is "{z}"')
