223f3ad41b0bc6ccc647c18d91c28e5a65e5a21a
unknown <dj.zaragoza@icloud.com>
2020-05-05 22:09:14 -0700

    finished lists.py

--- src/05_lists.py
+++ src/05_lists.py
@@ -18,7 +18,7 @@ print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-x.pop(4)
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
@@ -29,10 +29,10 @@ print(x)
 # Print the length of list x
 # YOUR CODE HERE
 
-print('Length of x:', len(x))
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
 
-for item in x:
-    print(item * 1000)
+for num in x:
+    print(num * 1000)
