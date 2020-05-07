7444b4d24bcfeee289d420707ebf7942722a4a5f
Tevvin <Tevvin7395@icloud.com>
2020-05-06 21:47:20 -0400

    modules.py-pt2

--- src/03_modules.py
+++ src/03_modules.py
@@ -28,9 +28,12 @@ print(sys.version)
 
 # Print the current process ID
 # YOUR CODE HERE
+print(os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print(os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
