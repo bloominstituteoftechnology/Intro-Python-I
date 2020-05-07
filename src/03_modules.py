226cd31540cdac2828e99f4f6931c9d039e28525
Mayankho <yankhot96@gmail.com>
2020-05-06 23:04:58 -0400

    Almost half

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,10 +10,13 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print(sys.argv[0])
+
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print(sys.platform)
 # Print out the version of Python you're using:
 # YOUR CODE HERE
 
@@ -23,6 +26,7 @@ import os
 
 # Print the current process ID
 # YOUR CODE HERE
+
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
--- src/04_printing.py
+++ src/04_printing.py
@@ -11,6 +11,7 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print(f'x is {x}, y is {y}, z is {z}')
 
 # Use the 'format' string method to print the same thing
 
