824daf8466d9560998d8b102f1b9c67ff89329c7
AlbertoGalvan001 <54903554+AlbertoGalvan001@users.noreply.github.com>
2020-05-07 00:10:52 -0400

    file added and completed

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+file = open("foo.txt", "r")
+print(file.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +19,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+#opens file
+f = open("bar.txt", "w")
+# this adds lines
+f.write('''This is a line.
+It is my line...
+Only mine!''')
+f.close
+#this checks for lines
+f = open("bar.txt", "r")
+print(f.read())
