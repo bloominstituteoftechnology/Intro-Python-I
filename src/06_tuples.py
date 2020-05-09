163ad281cf4d79cd25d590c23a5a7ec38c616546
sagdish <forsi1@yahoo.com>
2020-05-08 17:09:58 -0700

    0-9

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -28,17 +28,19 @@ a = (2, 7)   # <-- x,y coordinates stored in tuples
 b = (-14, 72)
 
 # Prints "Distance is 66.94"
-print("Distance is: {:.2f}".format(dist(a, b)))
+print("Distance is: {:.3f}".format(dist(a, b)))
 
 
 
 # Write a function `print_tuple` that prints all the values in a tuple
 
-# YOUR CODE HERE
+def print_tuple(tup):
+    for el in tup:
+        print(el)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
