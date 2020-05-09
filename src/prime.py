e548fbb7e57efb2f92f58fb13d87a7a3dca73773
marilynle <marilynle@users.noreply.github.com>
2020-05-09 16:15:17 -0500

    'Stretch_Goal_updated'

--- src/prime.py
+++ src/prime.py
@@ -1,12 +1,10 @@
 # Write a program to determine if a number, given on the command line, is prime.
 # Stretch Goals
+from math import sqrt
+from itertools import count, islice
 
-if num > 1:
-for i in range(2, num//2):
-if (num % i) == 0:
-print(num, "is not a prime number")
-break
-else:
-print(num, "is a prime number")
-else:
-print(num, "is not a prime number")
+n = int(input("Enter a number: ")) 
+if (n > 1):
+    for number in islice(count(2), int(sqrt(n) - 1)):
+        if n % number == 0:
+            print(n, "is not a prime number")
