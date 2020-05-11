6e4d2e6668bdd229af79d6191f78b134f0495c22
Michael Stoffer <michaels@mdgadvertising.com>
2020-05-11 08:28:07 -0400

    Finished all but 14

--- src/11_args.py
+++ src/11_args.py
@@ -5,8 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-def f1(x, y):
-	return x + y
+def f1(a, b):
+	return a + b
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -15,14 +16,7 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*args):
-	z = 0
-	for nums in args:
-		if type(nums) == list:
-			for num in nums:
-				z += num
-		else:
-			z += nums
-	return z
+	return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -32,7 +26,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -40,18 +34,11 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-def f3(*args):
-	if len(args) == 1:
-		return int(args[0]) + 1
-	else:
-		z = 0
-		for num in args:
-			z += num
-		return z
+def f3(a, b=1):
+	return a + b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
-
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
