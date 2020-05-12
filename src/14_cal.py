edcc41407600ff11d54d6bead51081059e650122
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2020-05-11 21:59:07 -0400

    This commit should record a solution for file 14_cal.py

--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,24 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+today = datetime.today()
+
+month, year = today.month, today.year
+
+cal = calendar.TextCalendar(firstweekday=6)
+
+
+print(sys.argv)
+if len(sys.argv) == 1:
+  calendar.prmonth(today.year, today.month)
+
+elif len(sys.argv) == 2:
+  calendar.prmonth(today.year, int(sys.argv[1]))
+
+elif len(sys.argv) == 3:
+  calendar.prmonth(int(sys.argv[2]), int(sys.argv[1]))
+
+else:
+  print("usage: filename month year")
+  sys.exit(1)
