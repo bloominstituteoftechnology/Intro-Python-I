cfb8464a50c8a8e10f1bd3fd58111b8f3d92d4ae
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2020-05-11 18:26:06 -0700

    Added functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -9,4 +9,10 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+def is_even():
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("false")
+        
+is_even()
