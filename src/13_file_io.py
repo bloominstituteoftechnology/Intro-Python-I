a0a58bf9fda0e85b2ea203ef8892fc27bbad84e6
Mayankho <yankhot96@gmail.com>
2020-05-12 22:05:40 -0400

    Stretch with io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('foo.txt') as f:
+    print(f.read())
+
+    print(f.closed)
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as f2:
+    f2.writelines(["Yankho; \n", ";Wrote \n", "This"])
+
+with open('bar.txt') as f3:
+    print(f3.read())
+
+print(f2.closed)
+print(f3.closed)
