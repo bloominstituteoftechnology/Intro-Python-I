465a62874eb90d5f0efe8fe192ef1a7cd5e51ae2
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-02 00:06:52 +0100

    Printing

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,13 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print("x is %i, y is %.2f, z is \"%s\"" % (x, y, z))
+
 # Use the 'format' string method to print the same thing
 
+text = "x is {}, y is {}, z is \"{}\"".format(x, round(y, 2), z)
+print(text)
+
 # Finally, print the same thing using an f-string
+
+print(f"x is {x}, y is {round(y, 2)}, z is \"{z}\"")
