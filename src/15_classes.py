eca640428630351d96eae895d0f1194724c4daf5
Hector <hectoremil21@hotmail.com>
2020-05-31 23:08:29 -0400

    15_classes exercises

--- src/15_classes.py
+++ src/15_classes.py
@@ -2,28 +2,62 @@
 # constructor
 
 # YOUR CODE HERE
+class LatLon:
+    # Wow I hate this syntax
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+class Waypoint(LatLon):
+    def __init__(self, name, lat, lon):
+        super().__init__(lat, lon)
+        self.name = name
+    def __str__(self):
+        return f"""This is a {self.__class__.__name__} object 
+        \tname:\t{self.name}
+        \tlat:\t{self.lat}
+        \tlon:\t{self.lon}"""
+
+
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
 
+class Geocache(Waypoint):
+    def __init__(self, name, difficulty, size, lat, lon):
+        super().__init__(name, lat, lon)
+        self.difficulty = difficulty
+        self.size = size
+
+    def __str__(self):
+        return f"""This is a {self.__class__.__name__} object 
+        \tname:\t{self.name}
+        \tlat:\t{self.lat}
+        \tlon:\t{self.lon}
+        \tdifficulty:\t{self.difficulty}
+        \tsize:\t{self.size}"""
+
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
 
+waypoint = Waypoint("Catacombs", 41.70505, -121.5121)
+print(waypoint)
+
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-print(waypoint)
+# print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
+geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
 print(geocache)
