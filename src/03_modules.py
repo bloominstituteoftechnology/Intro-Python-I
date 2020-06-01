51271d4b00109b8c409062f7229d81c19cc7145e
Shawn Gee <shawngee@gmail.com>
2020-06-01 16:25:50 -0400

    Files 1-7 Completed

--- src/03_modules.py
+++ src/03_modules.py
@@ -22,10 +22,10 @@ import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+print(os.getpid())
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+print(os.getcwd())
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print(os.getlogin())
