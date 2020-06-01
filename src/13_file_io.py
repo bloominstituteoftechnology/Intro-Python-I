f299d02b631e404b06410db902955a16ba1b4891
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-06-01 17:56:08 +0100

    file-io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,15 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-
+text = open("/Users/alexander/Desktop/Projects/cs/Intro-Python-I/src/foo.txt")
+print(text.read())
+text.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar = open("/Users/alexander/Desktop/Projects/cs/Intro-Python-I/src/bar.txt", 'w')
+bar.write('some stuff \nother stuff \nmore stuff')
+bar.close()
