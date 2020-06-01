bb480029ef036778e6e1ca33412b20948bf27f6c
Pete1701 <pete.pertongonas@pirron.one>
2020-06-02 00:08:41 +0200

    04

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print('x is %(x)d, y is %(y).2f, z is %(z)r' %{'x': 10, 'y': 2.24552, 'z': "I like turtles!"})
 # Use the 'format' string method to print the same thing
-
+print('x is {x}, y is {y:.3}, z is {z!r}'.format(x = 10, y = 2.24552, z = "I like turtles!"))
 # Finally, print the same thing using an f-string
+print(f'x is {x}, y is {y:.3}, z is {z!r}')
