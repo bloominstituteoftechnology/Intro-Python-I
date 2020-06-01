bbce3971453d01c1fa747007a167572d1d17d382
Victoria  Topham <topham.victoria@gmail.com>
2020-06-01 08:33:19 -0700

    03_modules complete

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,21 +11,29 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+#is this right? it's just printing the argument i used to run the file which seems right but then why... one per line
+for argument in sys.argv:
+    print(argument)
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(sys.version_info)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print(os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print(os.getcwdb())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
