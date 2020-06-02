781e063ac7cc63c672a8b7f3af5c27eb87ad13e6
lilchoto3 <lilchoto3@gmail.com>
2020-06-02 15:23:31 -0700

    Tuesday assignment completed

--- src/10_functions.py
+++ src/10_functions.py
@@ -9,4 +9,10 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def is_even(num):
+    if num % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd")
 
+is_even(num)
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('foo.txt') as txt:
+    for line in txt:
+        print(line, end='')
 
+print('\n')
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as txt:
+    txt.write('Line one\n')
+    txt.write('Line two\n')
+    txt.write('Line three\n')
+
+with open('bar.txt') as txt:
+    for line in txt:
+        print(line, end='')
