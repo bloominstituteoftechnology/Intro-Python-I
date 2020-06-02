0509dea056a4b0c78434b9a807e588c01c4ff9f1
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-02 22:24:30 +0100

    Functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,6 +2,12 @@
 
 # YOUR CODE HERE
 
+def is_even(num):
+    if num % 2 == 0:
+        return True
+
+print(is_even(10))
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -10,3 +16,10 @@ num = int(num)
 
 # YOUR CODE HERE
 
+def check_number(num):
+    if is_even(num):
+        print("Even!")
+    else:
+         print("Odd")
+
+check_number(num)
