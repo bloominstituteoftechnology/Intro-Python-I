fcf324da48c5f79ad7676c7701cff6f833729d5b
maiali13 <maryamfawaz@gmail.com>
2020-06-01 17:01:04 -0700

    assignment 1

--- src/03_modules.py
+++ src/03_modules.py
@@ -7,25 +7,35 @@ level operating system functionality.
 
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
-
+"""
+sys.argv is a list in Python, which contains
+the command-line arguments passed to the script.
+"""
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+for argument in sys.argv:
+    print(argument)
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+print('\n Operaing system:')
+print(sys.platform)
+
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
+print('\n Python version:')
+print(sys.version)
 
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+print('\n Current Process ID:')
+print(os.getpid())
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+print('\n Current working directory:')
+print(os.getcwd())
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print('\n Login name:')
+print(os.getlogin())
