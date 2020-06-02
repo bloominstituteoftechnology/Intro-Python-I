9d079545b906497820e47007db0051b9fbed8fd7
Pete1701 <pete.pertongonas@pirron.one>
2020-06-02 10:59:10 +0200

    10

--- src/11_args.py
+++ src/11_args.py
@@ -5,7 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(x, y):
+    return x + y
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -13,6 +14,9 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*num):
+    # print(num)   
+    return sum(num)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,7 +26,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,6 +34,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(x, y=1):    
+    return x + y    
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,11 +50,12 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(x=v1, y=v2):
+    print('key: ' + x + 'value: ' + v1)
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-f4(a=12, b=30)
+# f4(a=12, b=30)
 
 # Should print
 # key: city, value: Berkeley
