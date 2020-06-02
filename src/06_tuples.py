6fdbca46158e1bc6ff07fe0b1c1d51f4214e4b09
tgc <codingviewdmv@gmail.com>
2020-06-02 13:44:09 -0400

    finished

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -3,11 +3,9 @@ Python tuples are sort of like lists, except they're immutable and
 are usually used to hold heterogenous data, as opposed to lists
 which are typically used to hold homogenous data. Tuples use
 parens instead of square brackets.
-
 More specifically, tuples are faster than lists. If you're looking
 to just define a constant set of values and that set of values
 never needs to be mutated, use a tuple instead of a list.
-
 Additionally, your code will be safer if you opt to "write-protect"
 data that does not need to be changed. Tuples enforce immutability
 automatically.
@@ -36,9 +34,16 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # YOUR CODE HERE
 
-t = (1, 2, 5, 7, 99)
+def print_tuple(tuple):
+    print(tuple)
+t = (1, 2, 5, 7, 99)  
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
+# t = (1, 2, 5, 7, 99)
+# for u in t:
+#     print(u)
+
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
+
 print_tuple(u)
