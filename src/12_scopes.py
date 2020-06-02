fba32efda17c50748abb8b8dcd9454120d968868
evrgreen <rcarsten06@gmail.com>
2020-06-02 15:30:50 -0600

    completed scope

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -5,8 +5,9 @@
 x = 12
 
 def change_x():
-    x = 99
-
+      global x 
+      x = 99
+      
 change_x()
 
 # This prints 12. What do we have to modify in change_x() to get it to print 99?
@@ -19,7 +20,9 @@ def outer():
     y = 120
 
     def inner():
-        y = 999
+        
+         nonlocal y
+         y = 999
 
     inner()
 
