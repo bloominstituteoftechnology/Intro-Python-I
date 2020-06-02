43d9c2c2fbbb7cb796138df4e6429a6e49c3353d
Pinefoot <jameslcarpino@gmail.com>
2020-06-02 11:43:39 -0700

    finished 08_comprehensions. Moving on.

--- src/07_slices.py
+++ src/07_slices.py
@@ -12,26 +12,33 @@ Use Python's slice syntax to achieve the following:
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+x = slice(1, 2)
+print(a[x])
 
 # Output the second-to-last element: 9
-print()
+x = slice(4,5)
+print(a[x])
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+x = slice(3,6)
+print(a[x])
 
 # Output the two middle elements in the array: [1, 7]
-print()
+x = slice(2,4)
+print(a[x])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+x = slice(1, 6)
+print(a[x])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+x = slice(0,5)
+print(a[x])
 
 # For string s...
 
 s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
-print()
+x = slice(7,12)
+print(s[x])
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,24 +10,29 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-
+y = [x+1 for x in range(5)]
+# for x in range(5):
+#     y.append(x+1)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-
+y = [x**3 for x in range(10)]
+# for x in range(10):
+#     y.append(x**3)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
-a = ["foo", "bar", "baz"]
+a = [x.upper() for x in ["foo", "bar", "baz"]]
 
 y = []
+y.append(a)
 
+    
+   
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
@@ -35,7 +40,9 @@ print(y)
 
 x = input("Enter comma-separated numbers: ").split(',')
 
+#print(x)
+
 # What do you need between the square brackets to make it work?
-y = []
+y = [num for num in x if int(num) % 2 == 0 ]
 
 print(y)
