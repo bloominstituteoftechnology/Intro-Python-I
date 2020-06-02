c0165c9a06256f89f5e3ac4490c129613699bc8c
Pinefoot <jameslcarpino@gmail.com>
2020-06-02 14:00:34 -0700

    finished 09 and 10 moving on to 11

--- src/05_lists.py
+++ src/05_lists.py
@@ -40,3 +40,5 @@ print(multipliedArray)
 
 
 
+
+
--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,20 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(x):
+    if x % 2 == 0:
+        print('Even!')
+        return True
+    else:
+        print('Odd!')
+        return False
+        
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
+print(is_even(num))
 # YOUR CODE HERE
 
