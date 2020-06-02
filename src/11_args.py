f0ee29594a1ddbd657e65cd1518ae4167513305f
evrgreen <rcarsten06@gmail.com>
2020-06-02 15:23:43 -0600

    completed args

--- src/11_args.py
+++ src/11_args.py
@@ -5,7 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(num1,num2):
+  return num1+num2
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -13,16 +14,24 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-
+def f2(*args): 
+  result = 0
+  for num in args:
+    if type(num)==list:
+       num = sum(item for item in num)     
+    result+=num
+  return result 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
+b = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
 print(f2(a))    # Should print 22
+print(f2(a,b,a,3,5,100))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,7 +39,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3(num1,num2=1):
+  return num1+num2
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,6 +54,11 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+  for key,item in kwargs.items():
+    print(f'key: {key}, value: {item}')
+    
+  
 
 # Should print
 # key: a, value: 12
@@ -62,4 +77,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
