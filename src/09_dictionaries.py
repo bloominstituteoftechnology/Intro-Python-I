237ea7eeaf224587d8aeda53c01c5ba41b7adad7
jose-marquez89 <jlmarquez89@gmail.com>
2020-06-02 18:40:23 -0500

    completes assignment

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,13 +35,24 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({"lat": 45,
+                  "lon": -100,
+                  "name": "a place that wasn't there before"})
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
+waypoints[0]["lon"] = -130
+waypoints[0]["name"] = "not a real place"
 
 # YOUR CODE HERE
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for waypoint in waypoints:
+    print("Waypoint Name: %s" % waypoint["name"])
+    contents = list(waypoint.items())
+    for key, value in contents[:-1]:
+        print(f"\t{key}: {value}")
+    print("~~~~~~~~~~~~~")
--- src/14_cal.py
+++ src/14_cal.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 """
 The Python standard library's 'calendar' module allows you to
 render a calendar to your terminal.
@@ -18,15 +19,46 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
-prompted input. Also, the brackets around year are to denote that the argument is
+Note: the user should provide argument input (in the initial call
+to run the file) and not prompted input. Also, the brackets
+around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call
+`python3 14_cal.py 4 2015` to print out a calendar for April
+in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+
+def print_cal(y, m):
+    c = calendar.Calendar(firstweekday=6)
+    for week in c.monthdayscalendar(y, m):
+        week = [str(x) if x != 0 else ' ' for x in week]
+        for day in week:
+            if week.index(day) < 6:
+                print(day.rjust(2), end=' ')
+            else:
+                print(day.rjust(2))
+    print()
+
+
+def prtext_cal(y, m):
+    t = calendar.TextCalendar(firstweekday=6)
+    return t.prmonth(y, m)
+
+
+if __name__ == "__main__":
+    today = datetime.today()
+    if len(sys.argv) == 1:
+        prtext_cal(today.year, today.month)
+    elif len(sys.argv) == 2:
+        prtext_cal(today.year, int(sys.argv[1]))
+    elif len(sys.argv) == 3:
+        prtext_cal(int(sys.argv[2]), int(sys.argv[1]))
+    else:
+        print("cal.py - usage: 14_cal.py [month] [year]")
