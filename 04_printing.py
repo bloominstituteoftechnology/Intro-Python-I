8d84022cdca8b122a75e91ecd415ecfb3874dbdf
ChrisHartig44 <59840488+ChrisHartig44@users.noreply.github.com>
2020-06-03 11:02:18 -0500

    completed assignment

---
+++ 04_printing.py
@@ -0,0 +1,21 @@
+"""
+Python provides a number of ways to perform printing. Research
+how to print using the printf operator, the `format` string 
+method, and by using f-strings.
+"""
+
+x = 10
+y = 2.24552
+z = "I like turtles!"
+
+# Using the printf operator (%), print the following feeding in the values of x,
+# y, and z:
+# x is 10, y is 2.25, z is "I like turtles!"
+
+print ('x is %s, y is %s, "z is %s", ' % (x, y, z))
+
+# Use the 'format' string method to print the same thing
+print('x is 10, y is 2.25, z is "I like turtles!"')
+
+# Finally, print the same thing using an f-string
+print(f'x is 10, y is 2.25, z is "I like turtles!"')
---
+++ 08_comprehensions.py
@@ -0,0 +1,45 @@
+"""
+List comprehensions are one cool and unique feature of Python.
+They essentially act as a terse and concise way of initializing
+and populating a list given some expression that specifies how
+the list should be populated. 
+
+Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+for more info regarding list comprehensions.
+"""
+import math
+
+# Write a list comprehension to produce the array [1, 2, 3, 4, 5]
+
+y = [num for num in range(1, 6)]
+
+print(y)
+
+# Write a list comprehension to produce the cubes of the numbers 0-9:
+# [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
+
+y = [int(math.pow(num, 3)) for num in range(10)]
+
+print(y)
+
+# Write a list comprehension to produce the uppercase version of all the
+# elements in array a. Hint: "foo".upper() is "FOO".
+
+a = ["foo", "bar", "baz"]
+y = [str.lower() for str in a]
+
+print(y)
+
+# Use a list comprehension to create a list containing only the _even_ elements
+# the user entered into list x.
+
+x = [10, 21, 4, 45, 66, 93]
+
+even_nos = [num for num in x if num % 2 == 0] 
+
+
+
+# What do you need between the square brackets to make it work?
+y = [even_nos]
+
+print(y)
