b5b036c5323eb7c73e7de8224a57f15e9ba4f046
Sherexmykes <sherexmykes@DESKTOP-6JB0NBQ.localdomain>
2020-06-04 16:58:56 -0400

    completed first 4 pieces of the py

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -12,10 +12,10 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
-
+print(x + int(y))
 # YOUR CODE HERE
 
 
 # Write a print statement that combines x + y into the string value 57
-
+print(str(x)+(y))
 # YOUR CODE HERE
