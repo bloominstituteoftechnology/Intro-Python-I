34622070382f7aedf3060ed229c6b1570c2c7d58
Sherexmykes <sherexmykes@gmail.com>
2020-06-04 17:17:36 -0400

    10 pieces of the py

--- src/05_lists.py
+++ src/05_lists.py
@@ -5,25 +5,32 @@ x = [1, 2, 3]
 y = [8, 9, 10]
 
 # For the following, DO NOT USE AN ASSIGNMENT (=).
-
+x.append(4)
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
+
 # YOUR CODE HERE
+for i in x:
+    print(i * 1000)
