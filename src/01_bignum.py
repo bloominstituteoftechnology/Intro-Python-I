d202e7bb9b7e8db596561cd7b370eac741fb2eb2
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2020-06-03 21:12:54 -0400

    Completed 00 - 04, working on 05

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,7 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+print(2 ** 65536)
+# console.log(2 ** 65536)
+# returns => name 'console' is not defined => under problems tab returns => Undefined variable 'console' with a pylint function
--- src/05_lists.py
+++ src/05_lists.py
@@ -8,7 +8,8 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+print(x.append(4))
+
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
