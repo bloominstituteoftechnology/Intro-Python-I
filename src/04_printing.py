b1dff1554856d46a0c0bbea2f01232c80d729c10
Trevor Thompson <Thompson_Trevor@bah.com>
2020-06-06 16:54:49 -0400

    Successfully completed all of the code for the 04 file

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,9 +12,10 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-print("x is %i y is %f z is %s", % (x, y, z))
+print('x is %d y is %.2f z is %s' % (x, y, z))
 
 # Use the 'format' string method to print the same thing
-print(format)
+print('x is {0} y is {1} z is {2}' .format(x, y, z))
 
 # Finally, print the same thing using an f-string
+print(F"x is {x} y is {y} z is {z}")
