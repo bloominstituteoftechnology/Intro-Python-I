ac425322a96ac43b38ab4978757b8b7455fe4bc6
Logan Negley <logannegley@gmail.com>
2020-06-06 15:44:33 -0500

    file.io done and bar.txt added with content

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('foo.txt', 'r') as f:
+    data = f.read()
+    print(data)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+    f.write('This is my first line.\n')
+    f.write('This is my second line. \n')
+    f.write('This is my third line. \n')
