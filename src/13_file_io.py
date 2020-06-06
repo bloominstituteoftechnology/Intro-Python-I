2f06c425c714b37b078d4cf4c78cf8918825b13b
robertelias <robert.elias@live.com>
2020-06-05 17:53:59 -0700

    file io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open("foo.txt", "r") as f:
+    print(f.read())
+    f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+    f.write('Hello my name is Robert Elias, \nI really enjoy learning about, \nPython Programming')
+    f.close()
+
+
+with open('bar.txt', 'r') as f:
+    print(f.read())
+    f.close()
