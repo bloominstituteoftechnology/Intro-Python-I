a9ac774c598d9a9ee68b64fcbfe2af4b740a7dc6
PercivalN <percival.ngan@gmail.com>
2020-06-12 23:43:16 -0500

    Added some functions

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -7,3 +7,18 @@
 
 a = pow(2, 65536)
 print(a)
+
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+# Read a number from the keyboard
+num = input("Enter a number: ")
+num = int(num)
+
+# Print out "Even!" if the number is even. Otherwise print "Odd"
+
+if is_even(num) == True:
+    print("Even")
+else:
+    print("Odd")
