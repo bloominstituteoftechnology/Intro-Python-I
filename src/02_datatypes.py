e8e6dbf0bbfc5e3707ab6e74d47dd67c75cab68e
VodeniZeko <salexy.es@gmail.com>
2020-06-25 13:41:27 -0700

    types

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,12 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+integer = x + int(y)
 
+print(integer, 'is', type(integer))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+string = str(x) + y
+print(string, "is", type(string))
