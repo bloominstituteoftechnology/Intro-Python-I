1d34dc0057c2da8980bd50693fc1be652272dc4b
Nckflannery <Nckflannery@users.noreply.github.com>
2020-07-06 15:50:29 -0700

    Better

--- src/is_prime.py
+++ src/is_prime.py
@@ -1,6 +1,6 @@
 def is_prime(num: int) -> str:
     if num > 1:
-        for i in range(2, num):
+        for i in range(2, num//2):
             if num % i == 0:
                 return f'{num} is not prime!'
         return f'{num} is prime!'  
@@ -10,10 +10,13 @@ def is_prime(num: int) -> str:
 while True:
     print("Check if a given number is prime (q) to quit")
     inp = input("Type a number: ")
+    
+    while not inp.isnumeric() and inp != 'q':
+        inp = input("Must be a positive number, try again: ")
+    
     if inp == 'q':
         break
-    while not inp.isnumeric():
-        inp = input("Must be a number, try again: ")
+
     given = int(inp)
     
     print(is_prime(given))
