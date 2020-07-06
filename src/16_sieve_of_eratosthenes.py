4679e7af49d767a5183142669653980cea527213
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-07-06 15:56:34 -0600

    Complete Sieve of Eratosthenes stretch goal

---
+++ src/16_sieve_of_eratosthenes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+
+def sieve_of_eratosthenes(limit):
+    candidates = set([*range(2, limit+1)])
+    for x in range(2, (limit+1)):
+        if (x not in candidates):
+            continue
+        composite = x + x
+        while composite < limit + 1:
+            candidates.discard(composite)
+            composite += x
+    return candidates
+
+args = sys.argv[1:]
+
+if len(args) != 1:
+    print("Expected one, and only one, argument.", file=sys.stderr)
+    print("Usage:", file=sys.stderr)
+    print(f"  {sys.argv[0]} <integer>", file=sys.stderr)
+    exit(1)
+
+number = int(args[0])
+
+if number <= 0:
+    print("Number must be greater than 0 !", file=sys.stderr)
+    exit(2)
+
+primes_up_to_number = sieve_of_eratosthenes(number)
+
+print(str(number in primes_up_to_number))
