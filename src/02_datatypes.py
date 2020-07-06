357d709500a0a642755bbb2944a3d8b03eb94194
Timothy Gilliam <timgill987@gmail.com>
2020-07-06 16:12:21 -0600

    halfway done

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -11,11 +11,11 @@ on a string and an integer.
 x = 5
 y = "7"
 
+print(x + int(y))
+
+
+print(str(x) + y)
+# YOUR CODE HERE
 # Write a print statement that combines x + y into the integer value 12
 
-# YOUR CODE HERE
-
-
-# Write a print statement that combines x + y into the string value 57
-
-# YOUR CODE HERE
+# YOUR CODE HERE# Write a print statement that combines x + y into the string value 57
