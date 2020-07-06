88d7164da0c13a9837830a127cc2f67cbb8aab6f
amindazad <amindazad@icloud.com>
2020-07-06 16:36:46 -0700

    Done with first 8 notebooks

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,21 +11,34 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+# total arguments 
+n = len(sys.argv) 
+print("Total arguments passed:", n) 
+  
+# Arguments passed 
+print("\nName of Python script:", sys.argv[0]) 
+  
+print("\nArguments passed:", end = " ") 
+for i in range(1, n): 
+    print(sys.argv[i], end = " ") 
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+import os
+print(os.name)
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(sys.version_info)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print(os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print(os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+print(os.getlogin())
--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,29 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(number_list):
+    for i in number_list:
+        if i % 2 == 0:
+            return "Even"
+        else: 
+            return "Odd"
+x = [1]
+y = [2]
+print(is_even(x))
+print(is_even(y))
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
+num = input(12)
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def is_even(number_list):
+    for i in number_list:
+        if i % 2 == 0:
+            return "Even"
+        else: 
+            return "Odd"
 
+print(is_even(num))
