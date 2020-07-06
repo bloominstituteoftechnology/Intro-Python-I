89156bcd8b771478ebfc3082e4960fe59e22f3ac
brian <bfvilchez95@gmail.com>
2020-07-06 14:11:34 -0600

    added Numerical Values to to the bignum file

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,18 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+print(2 ** 65536)
+
+sum1 = 33
+sum2 = 44
+total = sum1 + sum2
+print(total)
+
+longInt = 311
+smallInt = 11 
+print(longInt * smallInt)
+print(type(longInt))
+print(type(smallInt))
+
+famousLine = "Alright " 
+print(famousLine * 3)
