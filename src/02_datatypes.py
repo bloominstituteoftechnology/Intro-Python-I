1d9a5774a0dc0dfed5da12245bf249c3815b0f54
james-coulter <95coulter@gmail.com>
2020-07-06 11:41:31 -0600

    Finished 0-9

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -15,7 +15,12 @@ y = "7"
 
 # YOUR CODE HERE
 
+print(x + int(y))
+print(type(x + int(y)))
+
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print(str(x) + y)
+print(type(str(x) + y))
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,6 +35,13 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+new_waypoint = {"lat": 41,
+                "lon": -123,
+                "name": "a fourth place"}
+waypoints.append(new_waypoint)
+
+print(waypoints)
+
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +49,13 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0]['lon'] = -130
+waypoints[0]['name'] = 'not a real place'
+print(waypoints)
+
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+for num in range(len(waypoints)):
+    print(waypoints[num])
