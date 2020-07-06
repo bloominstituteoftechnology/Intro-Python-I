dd459befd84853b4e15dba24b0cdad8304c9816f
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-07-06 15:00:40 -0600

    Complete task 11_args

--- src/11_args.py
+++ src/11_args.py
@@ -4,7 +4,8 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+def f1(x, y):
+    return x + y
 
 print(f1(1, 2))
 
@@ -12,12 +13,20 @@ print(f1(1, 2))
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+def f2(*args):
+    sum = 0
+    for value in args:
+        if isinstance(value, list):
+            for listvalue in value:
+                sum += listvalue
+        else:
+            sum += value
+    return sum
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
-print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
+print("f2(1) = ", f2(1))                    # Should print 1
+print("f2(1, 3) = ", f2(1, 3))                 # Should print 4
+print("f2(1, 4, -12) = ", f2(1, 4, -12))            # Should print -7
+print("f2(7, 9, 1, 3, 4, 9, 0) = ", f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
@@ -29,7 +38,8 @@ print(f2(a))    # Should print 22
 # arguments.
 # Note: Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+def f3(a, b=1):
+    return a + b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -43,7 +53,11 @@ print(f3(8))     # Should print 9
 #
 # Note: Google "python keyword arguments".
 
-# YOUR CODE HERE
+def f4(arg=None, **kwargs):
+    if (arg != None):
+        kwargs = arg
+    for key in kwargs:
+        print(f"key: {key}, value: {kwargs[key]}")
 
 # Should print
 # key: a, value: 12
