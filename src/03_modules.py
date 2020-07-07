e0ebfe6a8b68d67d8ae2e8ad3698b417ec506331
john <jgreenberg@runbox.com>
2020-07-07 17:16:24 -0400

    added to it

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,12 +11,20 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+print (sys.argv.__len__)
+print (sys.argv[0])
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+
+import platform
+
+print ('Operating System: ', platform.system(), platform.release())
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
 
+print ('Python Version: ', sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
@@ -24,8 +32,16 @@ import os
 # Print the current process ID
 # YOUR CODE HERE
 
+print (os.getpid())
+
 # Print the current working directory (cwd):
 # YOUR CODE HERE
 
+print ('Current Working Directory: ', os.getcwd())
+
 # Print out your machine's login name
 # YOUR CODE HERE
+
+import getpass
+
+print ('Machine Login Name: ', getpass.getuser())
