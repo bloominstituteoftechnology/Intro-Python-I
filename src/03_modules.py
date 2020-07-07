18ac8c924d71cfb00bdafdadb2db5c711c459b45
Ryan Mecking <rsmecking@gmail.com>
2020-07-07 19:23:54 -0400

    completed comprehensions

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,7 +10,9 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE
-print(sys.argv)
+for x in sys.argv:
+    print(x)
+# print(sys.argv)
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 print(sys.platform)
