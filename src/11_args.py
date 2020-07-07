56c844c478b679abc14ea6f20fdb3726bfaec54f
Nick <ptnguyen1901@gmail.com>
2020-07-07 10:24:32 -0400

    arguments

--- src/11_args.py
+++ src/11_args.py
@@ -4,25 +4,34 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+def f1(first,second):
+    return first + second
 
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
-
-# YOUR CODE HERE
+def f2(*args):
+    result = 0
+    for x in args:
+        result += x
+    return result
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
-a = [7, 6, 5, 4]
-
-# How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+# a = [7, 6, 5, 4]
+#
+# # How do you have to modify the f2 call below to make this work?
+# # def f2(list):
+# #     result = 0
+# #     for x in list:
+# #         result += x
+# #     return result
+# print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,7 +39,9 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3(first = 1,second = 1):
+    result = first + second
+    return result
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -45,6 +56,10 @@ print(f3(8))     # Should print 9
 
 # YOUR CODE HERE
 
+def f4(**kwargs):
+    for i,k in kwargs.items():
+        print(("key: %s,"%i),"value:",k)
+
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -54,12 +69,18 @@ f4(a=12, b=30)
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
-
+f4(city="Berkeley",
+   population=121240,
+   founded="March 23, 1868")
+#
 d = {
     "monster": "goblin",
     "hp": 3
 }
+#
+# # How do you have to modify the f4 call below to make this work?
+def f4(dictionary):
+    for key in dictionary:
+        print(("key: %s,"%key),("value: %s,"%dictionary[key]))
 
-# How do you have to modify the f4 call below to make this work?
 f4(d)
