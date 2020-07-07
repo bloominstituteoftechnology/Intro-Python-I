25093f102801d35d013430370702ebe22bd91bed
David Shestopal <dshestopal@yahoo.com>
2020-07-07 13:02:43 -0700

    did a couple more

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x, y):
+    return x + y
+
 
 print(f1(1, 2))
 
@@ -14,15 +17,20 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
+
+def f2(*integers):
+    return sum([x for x in integers])
+
+
+print(f2(1))  # Should print 1
+print(f2(1, 3))  # Should print 4
+print(f2(1, 4, -12))  # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))  # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -31,8 +39,16 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+
+def f3(x, y=None):
+    if x and y:
+        return x + y
+    else:
+        return x + 1
+
+
 print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+print(f3(8))  # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -45,6 +61,12 @@ print(f3(8))     # Should print 9
 
 # YOUR CODE HERE
 
+
+def f4(**kwargs):
+    for x in kwargs:
+        print(f"key: {x}, value: {kwargs[x]}")
+
+
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -56,10 +78,7 @@ f4(a=12, b=30)
 # key: founded, value: "March 23, 1868"
 f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
-    "monster": "goblin",
-    "hp": 3
-}
+d = {"monster": "goblin", "hp": 3}
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
