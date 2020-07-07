f49362f24e4dd763d48952f72077d74741cceb0b
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-07-07 13:10:13 -0700

    finished scopes.py

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -4,10 +4,10 @@
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
-def change_x():
+def change_x(x):
     x = 99
 
-change_x()
+change_x(x)
 
 # This prints 12. What do we have to modify in change_x() to get it to print 99?
 print(x)
@@ -19,6 +19,7 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
 
     inner()
