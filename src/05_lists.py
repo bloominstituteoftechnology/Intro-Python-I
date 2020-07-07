778875977dac349f083032fa3a9937dad1e6bd10
Ezra Black <dev.ezrablack@gmail.com>
2020-07-06 23:42:30 -0500

    Day 1 MVP/Stretch
    implemented pip8 syntax

--- src/05_lists.py
+++ src/05_lists.py
@@ -1,6 +1,3 @@
-# For the exercise, look up the methods and functions that are available for use
-# with Python lists.
-
 x = [1, 2, 3]
 y = [8, 9, 10]
 
@@ -9,21 +6,34 @@ y = [8, 9, 10]
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
 print(x)
-
+x.append(4)
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
 print(x)
-
+for num in y:
+    x.append(num)
+print(x)
+# or concont with y + x but making sure each value
+# feels safe.
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
 print(x)
-
+del x[4]
+print(x)
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
 print(x)
-
+x.insert(5, 99)
+print(x)
 # Print the length of list x
 # YOUR CODE HERE
+length = len(x)
+print(f"amount of object in list: {length}")
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+y = []
+for v in x.copy():
+    i = v * 1000
+    y.append(i)
+print(y)
