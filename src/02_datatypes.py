f0af2f28b8e86a2d808eaa69bfdf58f0be8ce0e6
brian <bfvilchez95@gmail.com>
2020-07-07 13:47:08 -0600

    finished DataTypes

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -11,11 +11,19 @@ on a string and an integer.
 x = 5
 y = "7"
 
+text = "Hello there!"
+print(text)
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+print(x + int(y))
+print(y.capitalize())
+print('Hello bb')
 
-
+randomWord = 'pizza'  
+print(randomWord.capitalize())
+newword = randomWord.replace( 'pizza', 'chesseburgers')
+print(newword.capitalize())
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
