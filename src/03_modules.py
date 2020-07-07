2ae48f3d3fe16a2e61deb1e8b50b169f1d00a98f
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-07-07 15:05:13 -0700

    Worked on the sys.argv

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,20 +10,23 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+for index, line in enumerate(sys.argv):
+    print(f"sys.argv[{index}] = {line}")
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print(f"sys.platform = {sys.platform}")
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(f"sys.version_info = {sys.version_info[3]}")
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print(f"os.getpgid(0) = {os.getpgid(0)}")
 # Print the current working directory (cwd):
 # YOUR CODE HERE
 
