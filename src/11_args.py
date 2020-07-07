947e3912be140e29311d30ea0a2832b0b572e705
Ramses Gasque <cedro.gasque@wsu.edu>
2020-07-07 14:46:05 -0700

    finished

--- src/11_args.py
+++ src/11_args.py
@@ -5,7 +5,7 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+f1 = lambda x, y : x + y
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -13,7 +13,7 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-
+f2 = lambda *args : sum(args)
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -22,7 +22,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,7 +30,7 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+f3 = lambda a, b=1 : a + b
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,7 +44,10 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+f4 = lambda **kwargs : print(
+                                *map(lambda t : "key: %s, value: %s" % t,
+                                     kwargs.items()),
+                                sep='\n')
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -62,4 +65,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,22 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
-import calendar
+from calendar import prmonth
 from datetime import datetime
+
+args = list(map(int, sys.argv[1:]))
+args.extend([-1] * (2 - len(args)))
+now = datetime.now()
+month = now.month
+year = now.year
+prmonth(args[1] if args[1] > 0 else year, args[0] if args[0] > 0 else month)
