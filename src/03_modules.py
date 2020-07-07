032ba77c30f799fd29a16fe18e348291962b1b8d
Tate Keller <tatek1993@gmail.com>
2020-07-07 18:19:10 -0500

    completed tasks 10 and 11. Having issues compiling

--- src/03_modules.py
+++ src/03_modules.py
@@ -5,12 +5,14 @@ methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
 
+import os
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+for thing in sys.argv:
+    print(thing)
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 
@@ -18,7 +20,6 @@ import sys
 # YOUR CODE HERE
 
 
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
