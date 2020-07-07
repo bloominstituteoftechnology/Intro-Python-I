ea51855203ff839387a386c4017554653a91babd
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-07 19:38:43 -0400

    Complete 14_cal.py

--- src/14_cal.py
+++ src/14_cal.py
@@ -30,3 +30,42 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+print("-----" * 2 +"BEGIN"+ "-----" *2)
+n = len(sys.argv)
+print("Total arguments provided:", n)
+
+print("\nArguments passed:") 
+for i in range(1, n): 
+    print(sys.argv[i])
+
+today = datetime.today()
+current_year = today.year
+current_month = today.month
+
+# If the user doesn't specify any input, your program should
+# print the calendar for the current month. The 'datetime'
+# module may be helpful for this.
+if n == 1:
+  print(calendar.month(current_year, current_month))
+# If the user specifies one argument, assume they passed in a
+# month and render the calendar for that month of the current year.
+if n == 2:
+  month = int(sys.argv[1])
+  print(calendar.month(current_year, month))
+# If the user specifies two arguments, assume they passed in
+# both the month and the year. Render the calendar for that
+# month and year.
+elif n == 3:
+  month = int(sys.argv[1])
+  year = int(sys.argv[2])
+  print(calendar.month(year, month))
+# Otherwise, print a usage statement to the terminal indicating
+# the format that your program expects arguments to be given.
+# Then exit the program
+else:
+  print("""USAGE ERROR:
+  Please provide date and year in following format: 
+    14_cal.py month year
+  Example: 14_cal.py 8 2010""")
+
+print("-----" * 2 +"DONE"+ "-----" *2)
