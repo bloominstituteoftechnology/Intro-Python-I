278eb821bd139f8e8eaf4cb25a4bb219cdf9443c
CSLSDS <56779420+CSLSDS@users.noreply.github.com>
2020-07-08 19:44:46 -0400

    Add files via upload

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -6,18 +6,18 @@ the list should be populated.
 
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
-""" 
+"""
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = [n for n in range(1, 6)]
+y = []
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = [n ** 3 for n in range(10)]
+y = []
 
 print(y)
 
@@ -26,16 +26,16 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = [n.upper() for n in a]
+y = []
 
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ").split(', ')
+x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = [int(n) for ix, n in enumerate(x) if ix % 2 == 1]
+y = []
 
 print(y)
