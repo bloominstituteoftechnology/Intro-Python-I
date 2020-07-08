6c22c13820712a3fd35b35e143d573a27ba8fab2
sametweb <sametmutevellioglu@gmail.com>
2020-07-08 01:35:57 -0700

    calendar and classes

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,12 +18,12 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
@@ -44,23 +44,28 @@ def print_calendar():
     month = today.month
     year = today.year
 
-    if len(args) == 2:
-        if type(args[1]) != int or type(int(args[1])) != int:
-            print(
-                f"{Fore.RED}To view specific month's calendar, run the file with [month] and [year] parameters.{Style.RESET_ALL}")
-            return
-    elif len(args) > 2:
-        if type(args[1]) != int or type(int(args[1])) != int or type(args[2]) != int or type(int(args[2])) != int:
-            print(
-                f"{Fore.RED}To view specific month's calendar, run the file with [month] and [year] parameters.{Style.RESET_ALL}")
-            return
+    try:
+        if len(args) == 1:
+            month = today.month
+            year = today.year
 
-    if len(args) == 2:
-        month = int(args[1])
-        year = today.year
-    elif len(args) == 3:
-        month = int(args[1])
-        year = int(args[2])
+        elif len(args) == 2:
+            month = int(args[1])
+            year = today.year
+
+            if month < 1 or month > 12:
+                print(
+                    f"{Fore.RED}Month value must be between [1 - 12].{Style.RESET_ALL}")
+                return
+
+        elif len(args) > 2:
+            month = int(args[1])
+            year = int(args[2])
+
+    except ValueError:
+        print(
+            f"{Fore.RED}To view specific month's calendar, run the file with [month] and [year] parameters.{Style.RESET_ALL}")
+        return
 
     print(my_calendar.formatmonth(year, month))
 
