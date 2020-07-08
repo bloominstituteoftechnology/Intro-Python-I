cd2ab0ba8142b313a8481f208734a8596639bf55
chrishuskey <chrishuskey@users.noreply.github.com>
2020-07-08 01:17:50 -0400

    Add first couple of exercises to open a draft PR.

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,6 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
-# YOUR CODE HERE
+# 2 to the 65536th power:
+answer = 2**65536
+print(f"\n2 to the 65536th power is: \n{answer}")
