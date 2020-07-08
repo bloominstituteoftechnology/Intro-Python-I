fcb7500e6dfe55c78801124d350081cb03c39db2
chrishuskey <chrishuskey@users.noreply.github.com>
2020-07-08 15:16:15 -0400

    Add modules 03 and 04.

--- src/03_modules.py
+++ src/03_modules.py
@@ -5,27 +5,41 @@ methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
 
+# Import modules we will use:
+import getpass
+import os
 import sys
+
+
+# --------------------------------------------------------------------------------
+# PART 1: SYS MODULE
+
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+print("\nArguments entered:")
+for num, arg in enumerate(sys.argv):
+    print(f"Argument #{num}: {arg}")
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+current_platform = sys.platform
+print(f"\nUsing platform: {current_platform}" + (" (macOS)" if current_platform == "darwin" else ""))
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
+print(f"Using Python version: {sys.version}\n")
 
 
-import os
+# --------------------------------------------------------------------------------
+# PART 2: OS MODULE
+
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+print(f"Current process ID: {os.getpid()}")
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+print(f"Current working directory: {os.getcwd()}")
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print(f"Login name: {os.getlogin()}")
+print(f"Portable version (not only local): Current login name: {getpass.getuser()}\n")
--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,12 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print("Method 1.a, C-style printf: x is %d, y is %.2f, z is %s" % (x, y, z))
+print("Method 1.b, C-style printf: x is %(x)d, y is %(y).2f, z is %(z)s" % {"x": x, "y": y, "z": z})
 
 # Use the 'format' string method to print the same thing
+print("Method 2, format(): x is {x}, y is {y:.2f}, z is {z}".format(x = x, y = y, z = z))
 
-# Finally, print the same thing using an f-string
+
+# Finally, print the same thing using an f-string:
+print(f"Method 3, f-string: x is {x}, y os {y:.2f}, z is {z}")
