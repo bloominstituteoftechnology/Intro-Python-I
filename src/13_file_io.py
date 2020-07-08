11658727029ddd96e004871dd61fec204d6736eb
Avery Quinn <avery.quinn1493@gmail.com>
2020-07-08 10:00:20 -0700

    file

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('foo.txt') as f:
+    read_data = f.read()
+print(read_data)
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +21,6 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+script = "This is the first line of the file.\nSecond line of the file\nNot second line of the file -Quinn\n"
+with open('boo.txt', "w") as f:
+    f.write(script)
