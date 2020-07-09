ad9b59d2e624d7403c032025e7191d56ca6a12ed
Sal B Amer <iosdevsalman@gmail.com>
2020-07-08 19:49:16 -0700

    finished task 1-3

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,7 +9,8 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+print(sys.argv[0])
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
