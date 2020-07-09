9683afa0f03ae6277f502e3b9697a288cda32ace
Serina Grill <gril0044@umn.edu>
2020-07-09 01:01:14 -0400

    module 1 assignment 00-08 files

--- src/05_lists.py
+++ src/05_lists.py
@@ -7,23 +7,32 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# YOUR CODE HERE
+
+x = [1, 2, 3, 4]
+
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# YOUR CODE HERE
+
+y = [1, 2, 3, 4, 8, 9, 10]
+
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# YOUR CODE HERE
+
+x = [1, 2, 3, 4, 9, 10]
+
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# YOUR CODE HERE
+
+x = [1, 2, 3, 4, 9, 99, 10]
+
 print(x)
 
 # Print the length of list x
-# YOUR CODE HERE
+print("Length of list x:", len(x))
 
 # Print all the values in x multiplied by 1000
-# YOUR CODE HERE
+
+print("All values in x multiplied by 1000", [i*1000 for i in x])
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -2,7 +2,7 @@
 List comprehensions are one cool and unique feature of Python.
 They essentially act as a terse and concise way of initializing
 and populating a list given some expression that specifies how
-the list should be populated. 
+the list should be populated.
 
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
@@ -10,25 +10,29 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+arr = [1, 2, 3, 4, 5]
+
+y = [i for i in arr]
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+to_be_cubed = [0,1,2,3,4,5,6,7,8,9]
 
-print(y)
+y = [i**3 for i in to_be_cubed]
+
+print("Cubed:", y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [i.upper() for i in a]
 
-print(y)
+print("Upper:", y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
@@ -36,6 +40,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [x[1::2]]
 
-print(y)
+print("List containing only even elements user entered into list:", y)
