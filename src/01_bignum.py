ea090cd0fcda341b18ab85b7e5dcaf573296c683
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-07-08 23:33:04 -0400

    some answers so far

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,11 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+
+from mpmath import *
+mp.dps = 50
+print(power(2, 65536))
+
+
+
--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -10,12 +10,15 @@ on a string and an integer.
 
 x = 5
 y = "7"
-
+Y = int(y)
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+print(x + Y)
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+x_string = str(x)
+str=x_string+y
+print(str)
