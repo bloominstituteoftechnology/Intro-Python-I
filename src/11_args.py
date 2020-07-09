9e1d7dc39b1148b426244d29c2bc8909f8fa7e2f
Rob LeCheminant <roblecheminant@gmail.com>
2020-07-08 19:28:20 -0600

    uploading assignment .py files

--- src/11_args.py
+++ src/11_args.py
@@ -4,7 +4,8 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+def f1(x, y):
+    return x + y
 
 print(f1(1, 2))
 
@@ -12,7 +13,8 @@ print(f1(1, 2))
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+def f2(*args):
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
