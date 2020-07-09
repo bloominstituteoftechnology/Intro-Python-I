ace3715d9d3355cff6a428eca30e7008a25bbf53
Jessica Kimbril <57114403+jessicakimbril@users.noreply.github.com>
2020-07-08 22:55:22 -0500

    assignment

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,4 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
-# YOUR CODE HERE
+print(f'So 2 to the power of 65536 is {2**65536}')
--- src/03_modules.py
+++ src/03_modules.py
@@ -9,7 +9,6 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
