a9c93ccf63ec7ff23b3bf4052a5a683e1a5dbb45
patrick devincentis <patrickdevincentis@MacBook-Pro.local>
2020-07-09 18:54:52 -0400

    file 13 finished

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,12 +8,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
-
+with open("foo.txt", "r") as foo:
+    print(foo.read())
 # YOUR CODE HERE
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
-
+with open("bar.txt", "w") as bar:
+    bar.write("Python is great.\n")
+    bar.write("JS is great.\n")
+    bar.write("React is great.\n")
 # YOUR CODE HERE
