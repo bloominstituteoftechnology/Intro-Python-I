1e78cb9f934d43e55ed89d809109eff6e94c2a89
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-07-09 17:29:56 -0400

    Completed exercise 8

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,32 +10,34 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [i for i in range(1,6)]
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+cubes = [i**3 for i in numbers]
+print(cubes)
 
-print(y)
+#
+## Write a list comprehension to produce the uppercase version of all the
+## elements in array a. Hint: "foo".upper() is "FOO".
+#
+words = ["foo", "bar", "baz"]
 
-# Write a list comprehension to produce the uppercase version of all the
-# elements in array a. Hint: "foo".upper() is "FOO".
+uppercase = [w.upper() for w in words]
 
-a = ["foo", "bar", "baz"]
+print(uppercase)
 
-y = []
-
-print(y)
-
-# Use a list comprehension to create a list containing only the _even_ elements
-# the user entered into list x.
-
+## Use a list comprehension to create a list containing only the _even_ elements
+## the user entered into list x.
+#
 x = input("Enter comma-separated numbers: ").split(',')
+for i in range(0, len(x)):
+	x[i] = int(x[i])
 
+evens = [i for i in x if i % 2 == 0]
+print(evens)
 # What do you need between the square brackets to make it work?
-y = []
-
-print(y)
