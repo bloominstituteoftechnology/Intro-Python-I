86433dee03d5ccd0685cab13d604b7ca12d91ec1
Toniddarden1 <Toniddarden@gmail.com>
2020-07-08 23:50:23 -0400

    finished up to tuples ex

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,7 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+fst = 2
+scd = 65536
+print(fst ** scd)
--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,32 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+  
+print 'Number of arguments:', len(sys.argv), 'argument(s).'
+# one found
+print 'Argument List:', str(sys.argv)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+import platform
+print(platform.system())
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-
+pid = os.getpid() 
+print(pid)
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+cd = os.getcwd()
+print(cd)
 # Print out your machine's login name
 # YOUR CODE HERE
+import getpass
+print(getpass.getuser())
