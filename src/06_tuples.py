4a986e9e65e2a0ea430468da1ddff3727d96363d
tyler broadway <tbroadway96@gmail.com>
2020-07-08 21:17:28 -0500

    Completed tuples

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -35,8 +35,14 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
+from collections.abc import Iterable
+
 def print_tuple(tuple):
-    print('tuple')
+    if not isinstance(tuple, Iterable):
+        print(tuple)
+    else:
+        for elem in tuple:
+            print(elem)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
