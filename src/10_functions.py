1dfd207e36dc25b60b800c40ba1f69eceaebcff9
chrishuskey <chrishuskey@users.noreply.github.com>
2020-07-08 23:24:51 -0400

    Add modules 09 and 10.

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,29 @@
+"""
+Program that asks for a number from the user, and returns whether 
+that number is even or odd.
+"""
+
 # Write a function is_even that will return true if the passed-in number is even.
+def is_even(number:int):
+    """
+    Determines if the input integer is an even number or not.
+    """
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 
-# YOUR CODE HERE
+# Get a number from the keyboard as user input:
+while True:
+    num = input("Enter a number: ")
+    try:
+        num = int(num)
+        break
+    except ValueError as value_error:
+        print(f"Cannot accept this input. Please enter an integer.\n")
 
-# Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
-
-# Print out "Even!" if the number is even. Otherwise print "Odd"
-
-# YOUR CODE HERE
-
+# Print out "Even!" if the number is even. Otherwise print "Odd":
+if is_even(number=num):
+    print("Even\n")
+else:
+    print("Odd\n")
