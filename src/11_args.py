bd53e44035b9a58e7443713c02742cceb72e5c1a
mpHarm88 <mpHarm88@users.noreply.github.com>
2020-07-09 23:37:07 -0700

    add 6-11 .py files

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(a,b):
+    return a+b
 
 print(f1(1, 2))
 
@@ -12,7 +14,8 @@ print(f1(1, 2))
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+def f2(*args):
+    return [sum(args) if (type(args)==tuple) & (type(args[0]) != list) else sum(args[0])][0]
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -31,6 +34,13 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+def f3(*args):
+
+    if len(args) == 1:
+        return args[0]+1
+    else:
+        return sum(args)
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,6 +54,19 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(*args,**kwargs):
+
+    if len(kwargs) == 0:
+        pass
+    else:
+        for x in kwargs:
+            print(f"key:{x}, value:{kwargs[x]}")
+    if len(args) == 0:
+        pass
+    else:
+        for x in args[0]:
+            print(f"key:{x}, value:{args[0][x]}")
+
 
 # Should print
 # key: a, value: 12
