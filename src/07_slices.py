7a076b3a9949629d5a67e339690515ff38d08272
Jessie <jessie.griffin@icloud.com>
2020-07-10 22:32:14 -0400

    changes made during 1:1

--- src/07_slices.py
+++ src/07_slices.py
@@ -12,8 +12,9 @@ Use Python's slice syntax to achieve the following:
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print(f'{a[1:2]}')
-print(a[slice(1,2)])
+#print(f'{a[1:2]}')
+#print(a[slice(1,2)])
+print(a[1])
 
 # Output the second-to-last element: 9
 print(a[-2])
