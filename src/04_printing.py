45084da769d00983539ecbb8b39c16c7b5b5ddcb
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-07-12 18:58:59 -0400

    Added f-string print statement

--- src/04_printing.py
+++ src/04_printing.py
@@ -3,7 +3,6 @@ Python provides a number of ways to perform printing. Research
 how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
-
 x = 10
 y = 2.24552
 z = "I like turtles!"
@@ -15,5 +14,8 @@ z = "I like turtles!"
 print("X is:", x, "y is:", y, "z is:", z)
 
 # Use the 'format' string method to print the same thing
+text = "X is: {}, y is: {}, z is: {}".format(x, y, z)
+print(text)
 
 # Finally, print the same thing using an f-string
+print(f"X is: {x}, y is: {y}, z is: {z}")
