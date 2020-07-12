b19228f13949e54717f3a38bf031ee54cbd5b0db
Sunil Karki <karkisunil1200@gmail.com>
2020-07-12 12:41:41 -0500

    Done with list compherension

--- src/05_lists.py
+++ src/05_lists.py
@@ -13,9 +13,11 @@ print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-for num in y: 
-    x.append(num)
-print(x)
+# for num in y: 
+#     x.append(num)
+# print(x)
+
+x.extend(y)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -36,6 +36,9 @@ waypoints = [
 # Add a new waypoint to the list
 # YOUR CODE HERE
 
+
+print(waypoints)
+
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
