0153286f0541bb5b40309d4b2deffa9e07fd134c
Serina Grill <gril0044@umn.edu>
2020-07-12 13:26:37 -0400

    more elegant solution to 03_modules.py #1

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,8 +9,8 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-for x in sys.argv:
-    print("CL argument:", x)
+for i in range(len(sys.argv)):
+    print(f"Argument #{i} is: {sys.argv[i]}")
 
 # Print out the OS platform you're using:
 print("OS platform:", sys.platform)
