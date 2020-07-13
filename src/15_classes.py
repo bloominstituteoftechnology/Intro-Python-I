6b3d1d7fa2f475e61af0f31fe503e217a2cc4a42
chrishuskey <chrishuskey@users.noreply.github.com>
2020-07-13 17:40:14 -0400

    Add modules 14 and 15.

--- src/15_classes.py
+++ src/15_classes.py
@@ -5,6 +5,12 @@ class LatLon():
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
+    
+    def __repr__(self):
+        return f"LatLon(lat={self.lat}, lon={self.lon}"
+    
+    def __str__(self):
+        return f"LatLon(lat={self.lat}, lon={self.lon}"
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
@@ -15,27 +21,40 @@ class Waypoint(LatLon):
         self.name = name
     
     def __repr__(self):
-        return "To add 01!"
+        return f"Waypoint(name={self.name}, lat={self.lat}, lon={self.lon}"
     
     def __str__(self):
-        return "To add 02!"
+        return f"Waypoint(name={self.name}, lat={self.lat}, lon={self.lon}"
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
-# YOUR CODE HERE
+class Geocache(Waypoint):
+    def __init__(self, name, lat, lon, difficulty, size):
+        super().__init__(name, lat, lon)
+        self.difficulty = difficulty
+        self.size = size
+    
+    def __repr__(self):
+        return f"Geocache(name={self.name}, lat={self.lat}, lon={self.lon}, difficulty={self.difficulty}, size={self.size}"
+    
+    def __str__(self):
+        return f"Geocache(name={self.name}, lat={self.lat}, lon={self.lon}, difficulty={self.difficulty}, size={self.size}"
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
-waypoint = Waypoint("Catacombs", 41.70505, -121.51521)
+catacombs = Waypoint(name="Catacombs", lat=41.70505, lon=-121.51521)
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-print(waypoint.__str__())
-print(waypoint.__repr__())
+print(catacombs)  # This is the same as writing: print(catacombs.__str__())
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
-# YOUR CODE HERE
+newberry_views = Geocache(name="Newberry Views", 
+                          lat=44.052137, 
+                          lon=-121.41556, 
+                          difficulty=1.5, 
+                          size=2)
 
 # Print it--also make this print more nicely
-print(geocache)
+print(newberry_views)
