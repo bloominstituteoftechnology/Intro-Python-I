a026536977f3209f4e327fa92062d0c712512f60
mpHarm88 <mpHarm88@users.noreply.github.com>
2020-07-12 18:23:30 -0700

    add 13-14 .py files

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open("foo.txt") as f:
+    read_data = f.read()
+    print(read_data)
+
+print(f"\nDid the File close successfully?: {f.closed}\n")
+
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +24,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open("bar.txt", "w+") as g:
+    for x in range (1, 4):
+        g.write(f"This is line {x} and the next line would be {x+1}\n")
+
+with open("bar.txt", "r") as h:
+    data = h.read()
+    print(data)
