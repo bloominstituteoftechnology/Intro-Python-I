40e0dfabc81563451fcef0d0d8021eeb62032ccc
Pyrom E <e.pyrom@gmail.com>
2020-07-14 19:51:36 -0400

    Pyrom E Intro-to-Python I Assignment 1
    Finished 2nd half of assignment.

--- src/14_cal.py
+++ src/14_cal.py
@@ -2,7 +2,6 @@
 The Python standard library's 'calendar' module allows you to
 render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
-
 Write a program that accepts user input of the form
   `14_cal.py [month] [year]`
 and does the following:
@@ -17,11 +16,9 @@ and does the following:
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
-
 Note: the user should provide argument input (in the initial call to run the file) and not 
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
-
 This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
 print out a calendar for April in 2015, but if you omit either the year or both values, 
 it should use today’s date to get the month and year.
@@ -30,3 +27,15 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+def create_calendar(month=datetime.today().month, year=datetime.today().year):
+    print(calendar.month(year, month))
+
+# User inputs 1 argument, return corresponding month of current year
+# User inputs 2 arguments, return corresponding month and year
+if len(sys.argv) == 3:    
+    create_calendar(int(sys.argv[1]), int(sys.argv[2]))
+
+# User gives no input, return current month
+else:
+    create_calendar()
