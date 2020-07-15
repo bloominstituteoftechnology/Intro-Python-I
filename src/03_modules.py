a9a9f93fd395a4b56098973f776122babfdd934a
Sunil Karki <karkisunil1200@gmail.com>
2020-07-14 20:15:03 -0500

    Added everything

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,6 +10,7 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print(sys.argv)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
--- src/10_functions.py
+++ src/10_functions.py
@@ -2,16 +2,22 @@
 
 # YOUR CODE HERE
 def is_even(num): 
-    if(num % 2 == 0):
-        return "even"
+    if num % 2 == 0:
+        return True
     else:
-    return "odd"
+        return False
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-print(is_even(34))
+print(is_even(num))
+
+if is_even(num) == True:
+    print("Even")
+else:
+    print('ODD')
+
 # YOUR CODE HERE
 
