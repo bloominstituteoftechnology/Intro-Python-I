4e8b187d9d64a70d01ae31148dfdef7dded1e4c0
Alex Shillingford <alex.shillingford@icloud.com>
2020-07-14 19:50:19 -0600

    Reduce code needed to print out file data read to console

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,8 +11,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 with open('foo.txt') as f:
-    read_data = f.read()
-    print(read_data)
+    print(f.read())
     f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
