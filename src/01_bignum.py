873be722033f189c7f1e96fb2fbed6aaa50c1bba
unknown <tommyconner96@gmail.com>
2020-07-31 12:54:21 -0400

    first three done

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,7 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+print(2**65536)
+
+# checked against https://sites.google.com/site/largenumbers/home/appendix/a/numbers/265536
--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -9,13 +9,17 @@ on a string and an integer.
 """
 
 x = 5
-y = "7"
+y = 7
 
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
 
+print(5 + 7)
+
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+string = str(x + y)
+print(string)
