8b643d70674383fa83d1c870223b021f68983413
ScottLightfoot <gslightfoot@gmail.com>
2020-08-03 12:13:35 -0500

    05_

--- src/05_lists.py
+++ src/05_lists.py
@@ -1,5 +1,5 @@
-# For the exercise, look up the methods and functions that are available for use
-# with Python lists.
+# For the exercise, look up the methods and functions that are available
+# for use with Python lists.
 
 x = [1, 2, 3]
 y = [8, 9, 10]
@@ -7,23 +7,24 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# YOUR CODE HERE
+for i in y:
+    x.append(i)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# YOUR CODE HERE
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# YOUR CODE HERE
+x.insert(-1, 99)
 print(x)
 
 # Print the length of list x
-# YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
-# YOUR CODE HERE
+print([i*1000 for i in x])
