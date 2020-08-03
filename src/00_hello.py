585b43497aa32919b24eec52fe3c541e8c34321b
karina-rodriguez <karminer60@gmail.com>
2020-08-03 13:06:18 -0700

    code from guided project

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,30 @@
 # Print "Hello, world!" to your terminal
+print("Hello world")
+
+#How does this differ from how you would do it in your core track language?
+#JS: console.log("Hello world")
+
+#How does how we ran this program differ from how we'd do it in your core track language?
+#iOS: compile the swift file with Xcode, and then run the binary 
+#JS: 1. run it in browser
+#    2. node [filename.js]
+#Java: 1. compile using the java compiler into a jar file
+#       2. execute the jar file
+
+name = "Bob"
+#no variable keyword
+
+print("Hello")
+
+#String interpolation using '+'
+print("Hello" + name)
+
+#String interpolation using f-strings
+print(f"Hello {name}")
+
+# if statements
+# in Python, whitesace is important
+if name!= "Sean":
+    print("Hey, you aren't Sean")
+else:
+    print(f"Hello {name}")
--- src/05_lists.py
+++ src/05_lists.py
@@ -1,3 +1,42 @@
+#In Python, arrays are called "lists"
+
+#create an empty list
+#empty = []
+
+#create a list with some numbers
+nums = [10, 60, 20, 5]
+
+#print out our list of numbers
+#print(nums)
+
+#add a number to our `nums` list
+#nums.append(77)
+
+#print(nums)
+
+#let's print out every value in the `nums` list
+#one at a time
+#for elem in nums:
+    #print(elem)
+
+#what if we want to iterate x number of times
+#for n in range(10):
+    #print(n)
+
+#what if we want to iterate along the length of a list?
+#for i in range(len(nums)):
+    #print(i)
+
+
+for i in range(len(nums)):
+    print(i, nums[i])
+#another way to print out elements from a list with their 
+#assocciated index
+for i, v in enumerate(nums):
+    print(i,v)
+
+
+
 # For the exercise, look up the methods and functions that are available for use
 # with Python lists.
 
