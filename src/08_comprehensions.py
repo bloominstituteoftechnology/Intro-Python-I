5f640c8370ef8efc8c6af66f21e93a3ccf5abbab
Naomi Ruemmely Torres <59476335+NaomiPriest97@users.noreply.github.com>
2020-08-03 19:39:24 -0400

    saving

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -6,15 +6,21 @@ the list should be populated.
 
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
+
+[<map expression] for <name> in <sequence expression> if <filter expression>]
 """
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-for num in range(6):
-    y.append(num ** 1)
-print (y)
+# y = []
+# for num in range(6):
+#     y.append(num ** 1)
+# print (y)
 
+y = []
+for item in range(1, 6):
+    y.append(item)
+print(y)   
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
@@ -44,4 +50,24 @@ x = input("Enter comma-separated numbers: ").split(',')
 # What do you need between the square brackets to make it work?
 y = [num for num in x if int(num) % 2 == 0]
 
+print(y)
+
+
+
+
+## Intro to Python 3 Notes
+
+# Write a list comprehension to produce the array [1, 2, 3, 4, 5]
+y = [item for item in  range(1, 6)]
+print(y)
+
+# Write a list comprehension to produce the cubes of the numbers 0-9:
+# [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
+def cube_something(n):
+    return n ** 3
+y = [cube_something(item) for item in range(10)]  
+
+# y = [item **3 for item in range(10)]
+# for item in range(10):
+#     y.append(item ** 3)
 print(y)
