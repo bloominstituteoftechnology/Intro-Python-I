761c332cb98020451b2a3f02e798e57a1328bac1
Dahna <dbuenrostro09@gmail.com>
2020-08-03 18:16:20 -0400

    progress on files 1-6

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,8 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+# Tried two different approaches. I think I like the ** operator best.
+# print(pow(2, 65536))
+
+print (2 ** 65536)
--- src/03_modules.py
+++ src/03_modules.py
@@ -11,21 +11,31 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+for argument in sys.argv:
+    print(argument)
+
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+import platform
+import os
+print(platform.system())
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
+print(sys.version)
 
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+pid = os.getpid()
+print(pid)
+
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+path = os.getcwd()
+print(path)
 
 # Print out your machine's login name
-# YOUR CODE HERE
+import getpass
+user = getpass.getuser()
+print(user)
