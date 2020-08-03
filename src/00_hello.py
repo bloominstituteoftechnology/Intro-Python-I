b322f3e2c15f5b81c84aad3021254848ee4562cd
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-08-03 09:42:46 -0700

    Printing, String Interpolation

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,23 @@
 # Print "Hello, world!" to your terminal
+
+print("Hello, world!")
+
+
+name = "Bob"
+
+# String Interpolation using +
+
+# print("Hello " + name)
+
+# String interpolation using f-strings
+
+# print(f"Hello {name}")
+
+
+# if statements
+# in Python, whitespace is important
+
+if name != "Sean":
+    print("Hey, you aren't Sean")
+else:
+    print(f"Hello {name}")
