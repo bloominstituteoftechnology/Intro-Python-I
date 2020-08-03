ac9011a64bce467e17e1baeeba5942fc13de860c
NateTheDev1 <nthnlrichards@gmail.com>
2020-08-03 13:28:12 -0400

    8, 9, 10 DONE

--- src/10_functions.py
+++ src/10_functions.py
@@ -7,6 +7,12 @@ num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
 # YOUR CODE HERE
 
+def checker(n): 
+    if(n % 2 == 1): 
+        return 'Odd'
+    else: 
+        return 'Even'
+
+print(checker(num))
