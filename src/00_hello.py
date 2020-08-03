a840f43352b1b288c56f56c5b94625567fbd4ba2
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-08-03 13:04:24 -0700

    Day 1 Intro to Python

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,23 +1,4 @@
 # Print "Hello, world!" to your terminal
 
-print("Hello, world!")
+print('Hello, world!')
 
-
-name = "Bob"
-
-# String Interpolation using +
-
-# print("Hello " + name)
-
-# String interpolation using f-strings
-
-# print(f"Hello {name}")
-
-
-# if statements
-# in Python, whitespace is important
-
-if name != "Sean":
-    print("Hey, you aren't Sean")
-else:
-    print(f"Hello {name}")
