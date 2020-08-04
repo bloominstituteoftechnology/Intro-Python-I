d47d213621bbb199115d1dcedc5a2ac2e0b5e0ea
nick-ussery <nick.ussery@gmail.com>
2020-08-04 11:57:37 -0500

    mvp

--- src/15_classes.py
+++ src/15_classes.py
@@ -2,28 +2,47 @@
 # constructor
 
 # YOUR CODE HERE
+class LatLon:
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+    
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+class Waypoint(LatLon):
+    def __init__(self, lat, lon, name):
+        LatLon.__init__(self, lat, lon)
+        self.name = name
+    def __str__(self):
+        return {"name= {}, lat={}, lon={}".format(self.name, self.lat, self.lon)}
+
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
-
+class Geocache(Waypoint):
+    def __init__(self, lat, lon, name, difficulty, size):
+        Waypoint.__init__(self, lat, lon, name)
+        self.difficulty = difficulty
+        self.size = size
+    def __str__(self):
+        return {"name= {}, lat={}, lon={}, difficulty={}, size={}".format(self.name, self.lat, self.lon, self.difficulty, self.size)}
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
+mywp = Waypoint(41.70505, -121.51521, "Catacombs")
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-print(waypoint)
+print(mywp.__str__())
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
-
+mygeo = Geocache(44.052137, -121.41556, "Newberry Views", 1.5, 2)
 # Print it--also make this print more nicely
-print(geocache)
+print(mygeo.__str__())
