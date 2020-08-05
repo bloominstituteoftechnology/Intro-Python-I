f2c3f4e4ba85e92df59afce05fb5354d95ae10aa
unknown <tommyconner96@gmail.com>
2020-08-05 18:54:12 -0400

    first 3 done

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -9,17 +9,14 @@ on a string and an integer.
 """
 
 x = 5
-y = 7
+y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
 
-# YOUR CODE HERE
-
-print(5 + 7)
+print(x + int(y))
 
 
 # Write a print statement that combines x + y into the string value 57
 
-# YOUR CODE HERE
-string = str(x + y)
-print(string)
+string = str(x)
+print(string + y)
