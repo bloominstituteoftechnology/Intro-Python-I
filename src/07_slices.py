27d336492fc186c2d911255a1293e9c921f5ccbe
Harmony Radley <harmonyradley99@gmail.com>
2020-08-05 12:14:27 -0400

    Re-did some of the slices

--- src/07_slices.py
+++ src/07_slices.py
@@ -12,19 +12,19 @@ Use Python's slice syntax to achieve the following:
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print(a[1:2])
+print(a[1])
 
 # Output the second-to-last element: 9
-print(a[4:5])
+print(a[-2])
 
 # Output the last three elements in the array: [7, 9, 6]
-print(a[3:6])
+print(a[3:]) # start from 3 and give me everything to the right of the list.
 
 # Output the two middle elements in the array: [1, 7]
 print(a[2:4])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print(a[1:6])
+print(a[1:]) # start at index 1, go all the way to the end 
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
 print(a[:5])
