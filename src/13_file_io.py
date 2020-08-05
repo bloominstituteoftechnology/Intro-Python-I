23117185c65bc107488eee3cdc6a04bc2e689e3f
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-08-04 19:37:13 -0700

    Finished file_io, cal, and classes. MVP

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open("foo.txt") as f:
+    print(f.read())
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open("bar.txt", 'w') as b:
+    b.write("Why do you go to the bar?\n")
+    b.write("I go to the bar to drink.\n")
+    b.write("What is your favorite drink?")
+
+with open("bar.txt") as b:
+    print(b.read())
