cbd0ab796a23a21d49373601ff95c216db55c6b4
karina-rodriguez <karminer60@gmail.com>
2020-08-04 18:25:34 -0700

    working on 06

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -37,6 +37,10 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # YOUR CODE HERE
 
 t = (1, 2, 5, 7, 99)
+
+def print_tuple(x):
+    return print(x)
+
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
