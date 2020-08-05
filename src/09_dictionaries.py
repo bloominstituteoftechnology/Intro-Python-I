28a048ea1c7b66caf090fbbd7a367dc225cf2005
Joe V <joeveverka89@gmail.com>
2020-08-05 09:58:45 -0400

    MVP.

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -34,14 +34,22 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
+waypoints.append({
+    "lat": 24,
+    "lon": 89,
+    "name": "over there"
+})
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
-# YOUR CODE HERE
+waypoints[0]["name"] = "not a real place"
+
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
-# YOUR CODE HERE
+for waypoint in waypoints:
+    for value in waypoint.values():
+        print(value)
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+import os
+print(os.getcwd())
+
+def print_file(fileName):
+    with open(fileName, 'r') as file:
+        data = file.read()
+        
+print_file('foo.txt')
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open('bar.txt', 'w') as newFile:
+    newFile.write('Hello, hello, Hello\n')
+    newFile.write('Dumdee Dum Dum\n')
+    newFile.write('Testing Testing')
