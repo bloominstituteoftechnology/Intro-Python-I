575cd49b7a4cdcbf5a4ebfdf1c45f6a24f23c888
unknown <tommyconner96@gmail.com>
2020-08-06 15:34:32 -0400

    7 done

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -34,11 +34,14 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # Write a function `print_tuple` that prints all the values in a tuple
 
-# YOUR CODE HERE
+def print_tuple(something):
+    for numbers in something:
+        print(numbers)
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
+          # answer: a comma.
 print_tuple(u)
--- src/07_slices.py
+++ src/07_slices.py
@@ -12,26 +12,30 @@ Use Python's slice syntax to achieve the following:
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+# note: this seems weird because I would have expected it to be
+# print(a[0:1])
+print(a[1:2])
 
-# Output the second-to-last element: 9
-print()
+# # Output the second-to-last element: 9
+print(a[4:5])
 
-# Output the last three elements in the array: [7, 9, 6]
-print()
+# # Output the last three elements in the array: [7, 9, 6]
+print(a[3:6])
 
-# Output the two middle elements in the array: [1, 7]
-print()
+# # Output the two middle elements in the array: [1, 7]
+print(a[2:4])
 
-# Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+# # Output every element except the first one: [4, 1, 7, 9, 6]
+# note: I think I get it now. above, it's starting with element 1 (2nd number)
+# and then stopping before 2
+print(a[1:6])
 
-# Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+# # Output every element except the last one: [2, 4, 1, 7, 9]
+# print()
 
-# For string s...
+# # For string s...
 
-s = "Hello, world!"
+# s = "Hello, world!"
 
-# Output just the 8th-12th characters: "world"
-print()
+# # Output just the 8th-12th characters: "world"
+# print()
