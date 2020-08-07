70eb410abf97b0f6c3b93a3bd0dee542ff39c08c
chiragthesia <harithesia@gmail.com>
2020-08-07 18:06:37 -0500

    Second part done as well

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,25 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('foo.txt') as f:
+    print(f.read())
+    f.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+f = open('bar.txt', 'w')
+strings = ["My name is Chirag.\n I love python!\n Lets get it! "]
+
+for s in strings:
+    f.write(s)
+
+with open('bar.txt') as f:
+    read_data = f.read()
+    print(read_data)
+    f.close()
+
+print(f"File has been closed:{f.closed}")
--- src/15_classes.py
+++ src/15_classes.py
@@ -2,11 +2,16 @@
 # constructor
 
 # YOUR CODE HERE
+class LatLon:
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
 
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
@@ -16,7 +21,6 @@
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
-
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
 print(waypoint)
