17e1a49d250a023992047f7c723ee91f007211d3
Marcus Bass <mbass@Marcuss-MacBook-Pro.local>
2020-08-15 14:11:33 -0700

    cal.py complete

--- src/14_cal.py
+++ src/14_cal.py
@@ -6,12 +6,12 @@ https://docs.python.org/3.6/library/calendar.html
 Write a program that accepts user input of the form
   `14_cal.py [month] [year]`
 and does the following:
- - If the user doesn't specify any input, your program should
+ DONE - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
    module may be helpful for this.
  - If the user specifies one argument, assume they passed in a
-   month and render the calendar for that month of the current year.
- - If the user specifies two arguments, assume they passed in
+   month and render the calendar for that month of the current year. WHAT IF USER SUBMITS FOUR DIGITS?
+ DONE - If the user specifies two arguments, assume they passed in
    both the month and the year. Render the calendar for that
    month and year.
  - Otherwise, print a usage statement to the terminal indicating
@@ -30,3 +30,25 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+    
+
+month = input('Enter month ')
+if(len(month) < 2):
+  print('please enter a valid two digit month. Ex: 01 ')
+  sys.exit()
+#Makes sure month is always entered
+year = input('Enter year ')
+if(len(year) < 4):
+  print('please enter a valid four-digit year. Ex: 2020 ')
+  #Prints current month calendar if no user input
+if month == '' and year == '':
+  currYear = datetime.now().year
+  currMonth = datetime.now().month
+  print(calendar.month(currYear, currMonth, 1, 1))
+
+if month and year:
+  currYear = int(year)
+  currMonth = int(month)
+  print(calendar.month(currYear, currMonth, 1, 1))
+
+
