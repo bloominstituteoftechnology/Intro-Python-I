6cf3ad2fe6a82af56636a4d34ae71558d91bd0ba
Marcus Bass <mbass@Marcuss-MacBook-Pro.local>
2020-08-15 12:23:22 -0700

    args.py & scopes.py complete

--- src/11_args.py
+++ src/11_args.py
@@ -6,6 +6,9 @@
 
 # YOUR CODE HERE
 
+def f1(a,b):
+    return a + b
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -13,6 +16,9 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+def f2(*args):
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,7 +28,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(sum(a)))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,6 +36,13 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+import sys
+
+def f3(a, b=0):
+    if(b > 0):
+        return a + b
+    elif(a):
+        return a + 1
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -45,6 +58,9 @@ print(f3(8))     # Should print 9
 
 # YOUR CODE HERE
 
+def f4(a,b):
+    print(a,b)
+
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -54,7 +70,8 @@ f4(a=12, b=30)
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+
+f4(city="Berkeley", population=121240, founded="March 23, 1868", monster="goblin", hp="3")
 
 d = {
     "monster": "goblin",
