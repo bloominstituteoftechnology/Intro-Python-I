6f013672347f7bbb7d6250162f5da9703b1171f0
Ash W <amsheppard2016@gmail.com>
2020-08-25 23:32:04 -0400

    classes done

--- src/14_cal.py
+++ src/14_cal.py
@@ -30,3 +30,10 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+def cal(m,y):
+    c = calendar.TextCalendar(calendar.SUNDAY)
+    str = c.formatmonth(y,m)
+    print(str)
+
+cal(2,2002)
