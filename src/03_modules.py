e520438eab95ccb54d827126fa17ef58c9fa056d
Stephanie <user.name>
2020-08-31 13:07:49 -0700

    full notes

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,7 +11,9 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
-# Print out the OS platform you're using:
+
+
+# Prinw out the OS platform you're using:
 # YOUR CODE HERE
 
 # Print out the version of Python you're using:
