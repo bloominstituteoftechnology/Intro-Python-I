7eb37468e27722f5d574f3e51a2b1cbcc2f44b70
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-08-31 15:51:56 -0700

    0-3 done

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,9 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
+# (JS console returns infinity!)
+
+
 # YOUR CODE HERE
+print(2 ** 65536)
+
--- src/04_printing.py
+++ src/04_printing.py
@@ -4,6 +4,8 @@ how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
 
+# NOT FINISHED!
+
 x = 10
 y = 2.24552
 z = "I like turtles!"
@@ -11,6 +13,7 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print(x : %  
 
 # Use the 'format' string method to print the same thing
 
