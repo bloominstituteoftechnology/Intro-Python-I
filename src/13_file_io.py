399a36e84930751dd8617016417100e3e24bfa4f
Lauren Emick <laurenemick6@gmail.com>
2020-09-01 13:26:06 -0700

    scopes and fileio done

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('/Users/adrienneemick/Desktop/Lambda/web32cs/Intro-Python-I/src/foo.txt') as f:
+    read_data = print(f.read())
+
+print(f.closed)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +21,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('/Users/adrienneemick/Desktop/Lambda/web32cs/Intro-Python-I/src/bar.txt') as b:
+    read_data = print(b.read())
+
+print(b.closed)
