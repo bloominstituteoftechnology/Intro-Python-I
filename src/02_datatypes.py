db44e2e68917f19f5890fd28fa6860fca2aee2ab
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-08-31 22:41:22 -0700

    python1

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,13 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+ print (x + int(y))
+ print (x + int('7'))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print (str(x) + y)
+
+# Python is generally strongly typed so it it will often complain if you try to mix and match types. 
+# You can coerce a type with the int(), float(), str(), and bool() functions.
