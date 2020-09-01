a16551633d35e660f40ca6d23bdbab989f5bad30
Anthony N <foto.arn@gmail.com>
2020-08-31 22:46:32 -0700

    + Stretch

---
+++ src/stretch.py
@@ -0,0 +1,19 @@
+#sieve of Eratosthenes
+def soe(n):
+
+    is_prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
+        if(is_prime[p] == True):
+            for i in range(p * 2, n + 1, p):
+                is_prime[i] = False
+        p += 1
+    is_prime[0] = False
+    is_prime[1] = False
+
+    for p in range(n + 1):
+        if is_prime[p]:
+            print(p)
+
+print(soe(29))
+
