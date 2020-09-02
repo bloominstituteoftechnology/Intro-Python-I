d17ca4c658c73760c1993a2d9a46079965b72202
Edudeiko <evgeniy.dudeyko@gmail.com>
2020-09-02 00:29:35 -0400

    done

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,7 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
-# YOUR CODE HERE
+num = 2
+while num <= 65536:
+    print(num)
+    num *= 2
