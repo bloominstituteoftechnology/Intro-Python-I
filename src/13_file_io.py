4d3e7dbb3caff7a3ae3165a094f0169c7c6bacfa
ANA TULEA <anatulea14@gmail.com>
2020-09-02 16:22:51 -0700

    completed file

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-
+# myfile = open('src/foo.txt')
+# myfile.read()
+with open('src/foo.txt') as foo:
+    for bar in foo:
+        print(bar, end='')
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('src/bar.txt', 'w') as bar:
+    bar.write('\nLambda\nSchool\nPython\n')
+    bar.close()
+
+# with open('src/bar.txt') as foo:
+#     for bar in foo:
+#         print(bar, end='')
