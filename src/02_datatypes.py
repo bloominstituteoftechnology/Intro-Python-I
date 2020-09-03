0e4a0d61998f0df8012c5507342e49c383f9d50c
Christopher Johnston <cmjohnston@gmail.com>
2020-09-02 23:07:27 -0500

    00 through 05 complete

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -9,13 +9,18 @@ on a string and an integer.
 """
 
 x = 5
-y = "7"
+y = 7
 
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+print(x + y)
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+x = "5"
+y = "7"
+z = x + y
+print(z)
