8d0d99c2eafe8ae62d805b5fdbe66a64ff5684b0
tauan-longaretti <tauanlongaretti@gmail.com>
2020-09-02 22:29:02 -0400

    sixth set of problems complete

--- src/05_lists.py
+++ src/05_lists.py
@@ -1,3 +1,6 @@
+import os
+os.system("cls")
+
 # For the exercise, look up the methods and functions that are available for use
 # with Python lists.
 
@@ -8,22 +11,29 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for values in x:
+    print(values * 1000)
