42c1521fe770b570828e95da82c5637099305868
Christian Lorenzo <clbmiami2004@gmail.com>
2020-09-02 22:33:43 -0700

    Ready with Printing out the OS and the Python version

--- src/03_modules.py
+++ src/03_modules.py
@@ -6,17 +6,44 @@ level operating system functionality.
 """
 
 import sys
+import argparse
+import platform
+import os
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+# Total Arguments:
+n = len(sys.argv)
+print("Total arguments passed:", n)
+
+# Arguments passed:
+print("\nName of Python script:", sys.argv[0])
+
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+    print(sys.argv[i], end = " ")
+
+# Addition of numbers:
+Sum = 0
+# Using argparse module
+for i in range(1, n):
+    Sum += int(sys.argv[i])
+    print("\n\nResult:", Sum)
+
+# for arg in sys.argv[1:]:
+#     print(arg)
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print("My OS is:", os.name)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
 
+print("My Python Version is:", sys.version)
+print("My Python Version Info is:", sys.version_info)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
