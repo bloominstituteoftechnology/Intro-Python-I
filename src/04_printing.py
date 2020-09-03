d61edac4bd397b0bc58eb454b3b4c9edaf183eee
Stephen Plainte <sjplaint@gmail.com>
2020-09-02 23:49:23 -0400

    completed 13 of 15 tasks

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,12 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print("%(x)s is 10, %(y)s is 2.25, '%(z)s' is 'I like turtles!'" \
+    %{'x': x, 'y': y, 'z': z})
+
 # Use the 'format' string method to print the same thing
+print("{} is 10, {} is 2.25, '{}' is 'I like turtles!'".format(x, y, z))
 
 # Finally, print the same thing using an f-string
+
+print(f"{x} is 10, {y} is 2.25, '{z}' is 'I like turtles!'")
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -34,14 +34,18 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
+waypoints.insert(0, {"lat": 94, "lon": -85, "name": "a galaxy far, far away"})
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
-# YOUR CODE HERE
+waypoints[0]["lon"] = -130
+waypoints[0]["name"] = "not a real place"
 
 # Write a loop that prints out all the field values for all the waypoints
-# YOUR CODE HERE
+
+for waypoint in waypoints:
+    for _, value in waypoint.items():
+        print(value)
