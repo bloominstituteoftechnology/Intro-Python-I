5bc499d53bff62014feb72811d1c60de889e7721
Shya Biswas <shyabiswas@gmail.com>
2020-09-02 20:29:07 -0400

    making changes

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,9 +10,12 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+sys.argv[]
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+sys.platform
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
