7633de4a763bf36f21c6ad92ebb764d9a9de5446
Hamza HArmouche <58759442+harmouche@users.noreply.github.com>
2020-09-02 23:21:29 -0400

    6 files completed

--- src/04_printing.py
+++ src/04_printing.py
@@ -6,12 +6,18 @@ method, and by using f-strings.
 
 x = 10
 y = 2.24552
-z = "I like turtles!"
+z = '"I like turtles!"'
 
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
 # Use the 'format' string method to print the same thing
+# print("x is", x,", y is", y, ", z is", z)
+# txt = "x is {param1}', y is  {param2}, z is {param3}"
+# print(txt.format(param1 = x, param2 = y, param3 = z))
+
+print("x is {}, y is {}, z is {}".format(x, y, z))
 
 # Finally, print the same thing using an f-string
+print('x is %s, y is %s, z is %s' % (x,y,z))
