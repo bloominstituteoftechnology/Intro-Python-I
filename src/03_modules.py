5178d21b37104c31223a379981df619d1d683c1a
Ash827 <ashley.eddings89@gmail.com>
2020-09-02 22:09:53 -0400

    first half complete!

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,23 +9,28 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+
+print(sys.argv[0])
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+import platform
+print(platform.system())
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
-
+print(sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+pid = os.getpid()
+print(pid)
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+path = os.getcwd()
+
+print(path)
 
 # Print out your machine's login name
-# YOUR CODE HERE
+import getpass
+print(getpass.getuser())
--- src/04_printing.py
+++ src/04_printing.py
@@ -13,5 +13,7 @@ z = "I like turtles!"
 # x is 10, y is 2.25, z is "I like turtles!"
 
 # Use the 'format' string method to print the same thing
+print("x is {}, y is {}, z is {}". format(x, y, z))
 
 # Finally, print the same thing using an f-string
+print(f"x is {x}, y is {y}, z is {z}")
