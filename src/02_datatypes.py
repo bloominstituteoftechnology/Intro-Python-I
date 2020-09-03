382d910ebe9cce605ff0098479dde8f047e7ce0e
Ben Whitman <benjamin.whitman90@gmail.com>
2020-09-02 23:51:56 -0400

    finished first 7 exercises

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -13,9 +13,9 @@ y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
 
-# YOUR CODE HERE
+print("ints:", x + int(y))
 
 
 # Write a print statement that combines x + y into the string value 57
 
-# YOUR CODE HERE
+print("strings:", str(x) + y)
