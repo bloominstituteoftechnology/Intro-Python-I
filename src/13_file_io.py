e9fd0b12d875bd36c85b40957d07fe672df47fb2
Dominic Bridgette <54685761+Afrodo1@users.noreply.github.com>
2020-09-02 20:41:48 -0700

    got to cal.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,15 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-
+f = open('src/foo.txt','r+')
+print(f.read())
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+g = open('bar.txt','w')
+g.write("These are words \nthese are more words \nand these are the last words")
+g = open('bar.txt', 'r+')
+print(g.read())
