b68b6a5eb415654993b68d1a1b867763e0e79186
Claudia Maciel <clc_80@yahoo.com>
2020-09-03 19:15:15 -0700

    Fixed f strings

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,7 +35,8 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
-
+waypoints.append({"lat": 44, "lon": -124, "name": "new place"})
+print(waypoints)
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
