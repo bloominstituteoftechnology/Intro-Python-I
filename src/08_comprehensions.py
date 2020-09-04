019ac01742aa4c10c7b91eb911043a9ec3f847cb
Jarren <jarrenmcampos@gmail.com>
2020-09-03 18:50:27 -0700

    Finished 09_Dictionaries

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,14 +11,17 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
+for num in range(6):
+    y.append(num)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for num in range(10):
+    num = num ** 3
+    y.append(num)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -27,7 +30,8 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
-
+for element in a:
+    y.append(element.upper())
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
