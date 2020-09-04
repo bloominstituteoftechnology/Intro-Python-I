47828bf1a133f3d4a11328157c9c9eb479a2a851
Ben Whitman <benjamin.whitman90@gmail.com>
2020-09-03 23:43:46 -0400

    branch commit

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -44,3 +44,5 @@ print_tuple(t)  # Prints 1 2 5 7 99, one per line
 # Declare a tuple of 1 element then print it
 u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
+
+# Initial commit
