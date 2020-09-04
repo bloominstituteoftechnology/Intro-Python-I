0dafd74a8990416ae8c4c04ab1e92688ad06ee9c
Khislatz <47505600+Khislatz@users.noreply.github.com>
2020-09-03 23:21:29 -0400

    Completed

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+my_file = open("foo.txt", "r")  # Open the file
+print(my_file.read()) # Read the file 
+my_file.close() # Close the file
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+new_file = open("bar.txt", "w")
+new_file.write("""Python is an interpreted, high-level and general-purpose programming language. 
+               It was created by Guido van Rossum and first released in 1991.
+               Python's design philosophy emphasizes code readability with its notable use of significant whitespace.""")
+new_file.close()
