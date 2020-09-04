6eb497c54f850446ad2ea9dfab3c2c97847f317a
Jarren <jarrenmcampos@gmail.com>
2020-09-03 19:14:46 -0700

    Finished 04_Printing

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,10 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print('x is %d, y is %.2f, z is "%s"' % (x, y, z))
 
 # Use the 'format' string method to print the same thing
+print('x is {}, y is {:.2f}, z is "{}"'.format(x, y, z))
 
 # Finally, print the same thing using an f-string
+print(f'x is {x}, y is {y:.2f}, z is "{z}"')
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,18 +10,18 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-for num in range(6):
-    y.append(num)
+y = [num for num in range(6)]
+# for num in range(6):
+#     y.append(num)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-for num in range(10):
-    num = num ** 3
-    y.append(num)
+y = [num ** 3 for num in range(10)]
+# for num in range(10):
+#     num = num ** 3
+#     y.append(num)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -29,9 +29,9 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
-for element in a:
-    y.append(element.upper())
+y = [element.upper() for element in a]
+# for element in a:
+#     y.append(element.upper())
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
