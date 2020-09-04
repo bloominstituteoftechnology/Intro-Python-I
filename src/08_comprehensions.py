8c50afe34c28ddc966ff41a42b556c5cb707d7ae
Christopher Johnston <cmjohnston@gmail.com>
2020-09-03 19:58:19 -0500

    I'm not comprehending comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,9 +10,8 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-
-print (y)
+y = [1, 2, 3, 4, 5]
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
