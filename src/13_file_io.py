88c6f7a5a66e8f21769e9051bec481ec4ab27e58
Veto Ramirez <vetorobert3@gmail.com>
2020-09-05 11:46:01 -0700

    Created new file in file_io.

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -13,6 +13,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 with open("src/foo.txt") as f:
   print(f.read())
 
+foo_file = open('src/foo.txt', 'r')
+print(foo_file.read())
+foo_file.close() 
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -20,6 +24,7 @@ with open("src/foo.txt") as f:
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
-with open('src/bar.txt', 'w') as b:
-  for hole in reversed(dive):
-    writer.write(hole) 
+bar_file = open('src/bar.txt', 'w')
+
+bar_file.write("Lorem ipsum dolor. \nLorem ipsum dolor. \nLorem ipsum dolor.")
+bar_file.close()
