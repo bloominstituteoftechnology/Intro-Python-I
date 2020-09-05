65b1fe4543984394c09058999d009448e53eaddf
Veto Ramirez <vetorobert3@gmail.com>
2020-09-05 11:22:21 -0700

    Fixed args

--- src/11_args.py
+++ src/11_args.py
@@ -15,9 +15,9 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-def f2(*a):
+def f2(*args):
   result = 0
-  for x in a:
+  for x in args:
     result += x
   return result
 
@@ -29,7 +29,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-#print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -58,8 +58,7 @@ print(f3(8))     # Should print 9
 # YOUR CODE HERE
 def f4(**kwargs):
   for key, value in kwargs.items():
-    print(key)
-    print(value)
+    print(key, value)
 
 # Should print
 # key: a, value: 12
