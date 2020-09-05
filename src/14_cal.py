8963297db0e6c3aa6c8ec79104d130e9c2ecf084
Stephen Plainte <sjplaint@gmail.com>
2020-09-04 23:00:09 -0400

    finished assignment

--- src/14_cal.py
+++ src/14_cal.py
@@ -30,3 +30,27 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+month = datetime.today().month
+year = datetime.today().year
+
+print(sys.argv)
+
+if len(sys.argv) == 3:
+  month = int(sys.argv[1])
+  year = int(sys.argv[2])
+elif len(sys.argv) == 2:
+  month = int(sys.argv[1])
+elif len(sys.argv) > 3:
+  print("Too many arguments detected. To use this script you must pass a month and/or a year in integer format: 14_cal.py [4] [2020] \
+    \
+    Printing the calendar for the current month and year.")
+else:
+  print("No arguments detected. To use this script you must pass a month and/or a year in integer format: 14_cal.py [4] [2020] \
+    \
+    Printing the calendar for the current month and year.")
+
+print(f'month: {month} | year: {year}')
+
+cal = calendar.TextCalendar().formatmonth(year, month)
+print(cal)
