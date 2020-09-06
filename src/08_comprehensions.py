38c6305e528dd87f5ae4a83dde0e1cbf81f7fcfb
ameralhomdy <ameralhomdy97@gmail.com>
2020-09-06 05:03:45 -0700

    dictionaries

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -41,5 +41,8 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 # What do you need between the square brackets to make it work?
 y = []
+for i in x:
+    if i % 2 == 0:
+        y.append(i)
 
 print(y)
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -34,14 +34,24 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
+waypoints.append({
+    "lat": 22,
+    "lon": -166,
+    "name": "a different place"
+})
+
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
-# YOUR CODE HERE
+waypoints[0]['lon'] = -130
+waypoints[0]['name'] = "not a real place"
+
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
-# YOUR CODE HERE
+for i in waypoints:
+    print(i)
