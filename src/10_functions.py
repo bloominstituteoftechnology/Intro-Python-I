7601f8350084cab2bec09771ed5c68f71a69f4b6
Nikita6599 <43905004+Nikita6599@users.noreply.github.com>
2020-10-01 11:03:14 +0530

    Update 10_functions.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,6 +2,12 @@
 
 # YOUR CODE HERE
 
+def is_even(n):
+  if n%2==0:
+    return True
+  else:
+    return False
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -9,4 +15,8 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+if is_even(num):
+  print("Even!")
+else:
+  print("Odd")
 
