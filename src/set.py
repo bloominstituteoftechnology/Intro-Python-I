214e1c25ea74080df543bcab9cc058ecb12ba185
Rohan Zanjal <rohanzanjal1122@gmail.com>
2020-10-02 23:12:35 +0530

    Adding one new data-structure called set

---
+++ src/set.py
@@ -0,0 +1,18 @@
+# data-structure called "SET"
+"""
+SET is an unordered and unindened collection of elements enclosed with {}
+
+Duplicates are not allowed:
+"""
+
+# Example for SET
+
+# Set can be written in curly braces{}
+s1 = { 1, 'a', True, 2, "b", False}
+
+s1.add("Hello")
+
+s1
+
+# This is the basic example to update set element
+
