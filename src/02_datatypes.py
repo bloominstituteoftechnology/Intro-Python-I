1848f244d393c86132956adf475aa05a543b4983
AHardik <72928008+AHardik@users.noreply.github.com>
2020-10-15 19:28:21 +0530

    Update 02_datatypes.py
    Add int and value and str value using typecasting

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,6 +14,8 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+print(x + int(y))
+# is it right or wrong 
 
 
 # Write a print statement that combines x + y into the string value 57
