595ee553b6e5c680c65e20b2a9c25029af0affff
shaneH <106199007+homeGrownCheese@users.noreply.github.com>
2022-08-18 18:14:18 -0700

    Update 11_args.py

--- src/11_args.py
+++ src/11_args.py
@@ -4,7 +4,8 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+def f1(a,b):
+    return a + b
 
 print(f1(1, 2))
 
